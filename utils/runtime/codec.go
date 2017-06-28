d59f52eb4674833e738d2bfd2edd3b44a53aa99c
Ravi Gadde <ravi.gadde@gmail.com>
2017-06-28 15:04:16 -0700

    Added protobuf codec (#92)
    * Added protobuf codec
    
    * Addressed review comments
    
    * Removed unused struct

--- utils/runtime/codec.go
+++ utils/runtime/codec.go
@@ -2,6 +2,12 @@ package runtime
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/pensando/sw/api"
 )
 
 // JSONCodec implements a JSON codec for an object.
@@ -51,4 +57,55 @@ func (j *JSONCodec) findKind(data []byte) (string, error) {
 		return kind.Kind, nil
 	}
 	return kind.ObjectKind, nil
+}
+
+// findKind finds the kind from a serialized version of an API object.
+func (j *ProtoCodec) findKind(data []byte) (string, error) {
+	var meta api.TypeMeta
+	if err := proto.Unmarshal(data, &meta); err != nil {
+		return "", err
+	}
+
+	return meta.Kind, nil
+}
+
+// ProtoCodec implements a protobuf codec for an object.
+type ProtoCodec struct {
+	creater ObjectCreater
+}
+
+// NewProtoCodec creates a new JsonCodec.
+func NewProtoCodec(creater ObjectCreater) *ProtoCodec {
+	return &ProtoCodec{creater: creater}
+}
+
+// Encode implements the json serialization of an object.
+func (p *ProtoCodec) Encode(obj Object) ([]byte, error) {
+	protoIntf, ok := reflect.ValueOf(obj).Interface().(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("Object is not a proto")
+	}
+	return proto.Marshal(protoIntf)
+}
+
+// Decode implements the json deserialization in to an object.
+func (j *ProtoCodec) Decode(data []byte, into Object) (Object, error) {
+	if into == nil {
+		kind, err := j.findKind(data)
+		if err != nil {
+			return nil, err
+		}
+		into, err = j.creater.New(kind)
+		if err != nil {
+			return nil, err
+		}
+	}
+	protoIntf, ok := reflect.ValueOf(into).Interface().(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("Object is not a proto")
+	}
+	if err := proto.Unmarshal(data, protoIntf); err != nil {
+		return nil, err
+	}
+	return into, nil
 }
--- utils/runtime/codec_test.go
+++ utils/runtime/codec_test.go
@@ -1,9 +1,13 @@
 package runtime
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
+
 	"github.com/pensando/sw/api"
+	"github.com/pensando/sw/api/generated/cmd"
 )
 
 type TestObj struct {
@@ -12,17 +16,31 @@ type TestObj struct {
 	Bar string
 }
 
-func newCodec() *JSONCodec {
-	s := NewScheme()
-	s.AddKnownTypes(&TestObj{})
-	return NewJSONCodec(s)
+type NonProtoObj struct {
+	Bar string
 }
 
-func TestEncode(t *testing.T) {
+func (n *NonProtoObj) GetObjectKind() string       { return "NonProtoObj" }
+func (n *NonProtoObj) GetObjectAPIVersion() string { return "v1" }
+
+func newCodec(codec string) Codec {
+	s := NewScheme()
+	switch codec {
+	case "json":
+		s.AddKnownTypes(&TestObj{})
+		return NewJSONCodec(s)
+	case "proto":
+		s.AddKnownTypes(&cmd.Cluster{})
+		return NewProtoCodec(s)
+	}
+	return nil
+}
+
+func TestJSONEncode(t *testing.T) {
 	test := TestObj{foo: "abc", Bar: "def"}
 	expectedOut := "{\"Bar\":\"def\"}"
 
-	codec := newCodec()
+	codec := newCodec("json")
 
 	out, err := codec.Encode(&test)
 	if err != nil || string(out) != expectedOut {
@@ -30,14 +48,53 @@ func TestEncode(t *testing.T) {
 	}
 }
 
-func TestDecode(t *testing.T) {
+func TestJSONDecode(t *testing.T) {
 	expectedObj := TestObj{Bar: "def"}
 	var obj TestObj
 	input := "{\"Bar\":\"def\"}"
 
-	codec := newCodec()
+	codec := newCodec("json")
 
 	if _, err := codec.Decode([]byte(input), &obj); err != nil || obj != expectedObj {
+		t.Fatalf("Decode test failed, err %v, obj %v", err, obj)
+	}
+}
+
+func TestInvalidProtoEncode(t *testing.T) {
+	if _, err := newCodec("proto").Encode(&NonProtoObj{Bar: "def"}); err == nil {
+		t.Fatalf("Proto encode passed with non proto object")
+	}
+}
+
+func TestProtoEncode(t *testing.T) {
+	test := cmd.Cluster{ObjectMeta: api.ObjectMeta{Name: "testCluster"}}
+	expectedOut, err := proto.Marshal(&test)
+	if err != nil {
+		t.Fatalf("Failed to marshal proto, error %v", err)
+	}
+
+	codec := newCodec("proto")
+
+	out, err := codec.Encode(&test)
+	if err != nil || string(out) != string(expectedOut) {
+		t.Fatalf("Encode test failed, err %v, out %v", err, string(out))
+	}
+}
+
+func TestProtoDecode(t *testing.T) {
+	expectedObj := cmd.Cluster{
+		TypeMeta:   api.TypeMeta{Kind: "Cluster"},
+		ObjectMeta: api.ObjectMeta{Name: "testCluster"},
+	}
+	var obj cmd.Cluster
+	input, err := proto.Marshal(&expectedObj)
+	if err != nil {
+		t.Fatalf("Failed to marshal proto, error %v", err)
+	}
+
+	codec := newCodec("proto")
+
+	if _, err := codec.Decode(input, &obj); err != nil || !reflect.DeepEqual(obj, expectedObj) {
 		t.Fatalf("Decode test failed, err %v, obj %v", err, obj)
 	}
 }
