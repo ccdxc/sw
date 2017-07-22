4178cacd482b79dadbd33d823deb8759bbb41e84
enricoschiattarella <enricoschiattarella@users.noreply.github.com>
2017-07-22 09:42:59 -0700

    Add ECDSA support and validation routine to cert utils (#119)
    * Add ECDSA support and validation routine to cert utils
    
    * Added OpenSSL EC key test to improve coverage
    
    * Incorporated review comments

--- utils/certs/certs.go
+++ utils/certs/certs.go
@@ -1,54 +1,127 @@
 package certs
 
 import (
+	"crypto"
+	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha1"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/asn1"
 	"encoding/pem"
+	"io"
 	"io/ioutil"
 	"math/big"
 	"net"
 	"os"
+	"reflect"
 	"time"
 
 	netutils "github.com/pensando/sw/utils/net"
 	"github.com/pkg/errors"
 )
 
+const (
+	certificateRequestPemBlockType = "CERTIFICATE REQUEST"
+	ecParametersPemBlockType       = "EC PARAMETERS"
+	ecPrivateKeyPemBlockType       = "EC PRIVATE KEY"
+	rsaPrivateKeyPemBlockType      = "RSA PRIVATE KEY"
+)
+
+func saveRsaPrivateKey(pemfile io.Writer, privatekey *rsa.PrivateKey) error {
+	pemkey := &pem.Block{
+		Type:  rsaPrivateKeyPemBlockType,
+		Bytes: x509.MarshalPKCS1PrivateKey(privatekey)}
+	return pem.Encode(pemfile, pemkey)
+}
+
+func saveEcPrivateKey(pemfile io.Writer, privatekey *ecdsa.PrivateKey) error {
+	outputKey, err := x509.MarshalECPrivateKey(privatekey)
+	if err != nil {
+		return errors.Wrap(err, "Invalid ECDSA private key during SavePrivateKey")
+	}
+
+	pemkey := &pem.Block{
+		Type:  ecPrivateKeyPemBlockType,
+		Bytes: outputKey}
+	return pem.Encode(pemfile, pemkey)
+}
+
 // SavePrivateKey saves private key to a file
-func SavePrivateKey(outFile string, privatekey *rsa.PrivateKey) error {
+func SavePrivateKey(outFile string, privatekey crypto.PrivateKey) error {
 	pemfile, err := os.Create(outFile)
 	if err != nil {
 		return errors.Wrap(err, "Unable to create file during SavePrivateKey")
 	}
 	defer pemfile.Close()
 
-	pemkey := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privatekey)}
-	return pem.Encode(pemfile, pemkey)
+	switch keyType := privatekey.(type) {
+	case *rsa.PrivateKey:
+		return saveRsaPrivateKey(pemfile, privatekey.(*rsa.PrivateKey))
+	case *ecdsa.PrivateKey:
+		return saveEcPrivateKey(pemfile, privatekey.(*ecdsa.PrivateKey))
+	default:
+		return errors.Wrapf(err, "Unknown key type: %T. Only RSA and ECDSA keys are supported", keyType)
+	}
+}
+
+func readRsaPrivateKey(data []byte) (crypto.PrivateKey, error) {
+	privatekey, err := x509.ParsePKCS1PrivateKey(data)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to Parse RSA Private Key from PEM block.")
+	}
+	return privatekey, err
+}
+
+func readEcPrivateKey(key, params []byte) (crypto.PrivateKey, error) {
+	// Golang crypto library assumes that key parameters are always specified in the key block itself.
+	// If we find a separate "EC PARAMETERS" block, we check the syntax but otherwise ignore it.
+	if params != nil {
+		namedCurveOID := new(asn1.ObjectIdentifier)
+		_, err := asn1.Unmarshal(params, namedCurveOID)
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to parse EC curve parameters from PEM block.")
+		}
+	}
+
+	privateKey, err := x509.ParseECPrivateKey(key)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to Parse EC Private Key from PEM block.")
+	}
+
+	return privateKey, err
 }
 
 // ReadPrivateKey from a file
-func ReadPrivateKey(caKeyFile string) (*rsa.PrivateKey, error) {
+func ReadPrivateKey(caKeyFile string) (crypto.PrivateKey, error) {
 	privKeyBytes, err := ioutil.ReadFile(caKeyFile)
 	if err != nil {
 		return nil, errors.Wrap(err, "Unable to read private key")
 	}
-	block, _ := pem.Decode(privKeyBytes)
+
+	block, rest := pem.Decode(privKeyBytes)
 	if block == nil {
 		return nil, errors.New("PEM block is nil while decoding privateKey")
 	}
-	if block.Type != "RSA PRIVATE KEY" {
-		return nil, errors.New("RSA PRIVATE KEY not found in PEM block")
+
+	switch block.Type {
+	case rsaPrivateKeyPemBlockType:
+		return readRsaPrivateKey(block.Bytes)
+
+	case ecPrivateKeyPemBlockType:
+		return readEcPrivateKey(block.Bytes, nil)
+
+	case ecParametersPemBlockType:
+		keyBlock, _ := pem.Decode(rest)
+		if keyBlock == nil || keyBlock.Type != ecPrivateKeyPemBlockType {
+			return nil, errors.New("No EC PRIVATE KEY block found")
+		}
+		return readEcPrivateKey(keyBlock.Bytes, block.Bytes)
+
+	default:
+		return nil, errors.New("No keys found in PEM block")
 	}
-	privatekey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to Parse Private Key from PEM block.")
-	}
-	return privatekey, err
 }
 
 // SaveCertificate to a file
@@ -90,7 +163,7 @@ func SaveCSR(certFile string, cert *x509.CertificateRequest) error {
 	pemfile, _ := os.Create(certFile)
 	defer pemfile.Close()
 	pemkey := &pem.Block{
-		Type:  "CERTIFICATE REQUEST",
+		Type:  certificateRequestPemBlockType,
 		Bytes: cert.Raw}
 	return pem.Encode(pemfile, pemkey)
 }
@@ -105,7 +178,7 @@ func ReadCSR(csrFile string) (*x509.CertificateRequest, error) {
 	if block == nil {
 		return nil, errors.New("PEM block is nil while decoding CSR")
 	}
-	if block.Type != "CERTIFICATE REQUEST" {
+	if block.Type != certificateRequestPemBlockType {
 		return nil, errors.New("CERTIFICATE  REQUEST not found in PEM block")
 	}
 	csr, err := x509.ParseCertificateRequest(block.Bytes)
@@ -117,7 +190,7 @@ func ReadCSR(csrFile string) (*x509.CertificateRequest, error) {
 }
 
 // SelfSign CA privatekey for specified days
-func SelfSign(days int, privatekey *rsa.PrivateKey) (*x509.Certificate, error) {
+func SelfSign(days int, privatekey crypto.PrivateKey) (*x509.Certificate, error) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		hostname = "*"
@@ -142,7 +215,15 @@ func SelfSign(days int, privatekey *rsa.PrivateKey) (*x509.Certificate, error) {
 		return nil, errors.Wrap(err, "Failed to Generate Random number during Self-signing certificate.")
 	}
 
-	publickey := &privatekey.PublicKey
+	var publickey crypto.PublicKey
+	switch keyType := privatekey.(type) {
+	case *rsa.PrivateKey:
+		publickey = privatekey.(*rsa.PrivateKey).Public()
+	case *ecdsa.PrivateKey:
+		publickey = privatekey.(*ecdsa.PrivateKey).Public()
+	default:
+		return nil, errors.Wrapf(err, "Unknown private key type: %T", keyType)
+	}
 
 	// create a self-signed certificate. template = parent
 	parent := template
@@ -159,7 +240,7 @@ func SelfSign(days int, privatekey *rsa.PrivateKey) (*x509.Certificate, error) {
 }
 
 // CreateCSR for my host and IP
-func CreateCSR(privatekey *rsa.PrivateKey, dnsnames []string, ipaddrs []net.IP) (*x509.CertificateRequest, error) {
+func CreateCSR(privatekey crypto.PrivateKey, dnsnames []string, ipaddrs []net.IP) (*x509.CertificateRequest, error) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		return nil, errors.Wrap(err, "Unable to determine hostname")
@@ -196,7 +277,7 @@ func CreateCSR(privatekey *rsa.PrivateKey, dnsnames []string, ipaddrs []net.IP) 
 
 // SignCSRwithCA signs the CSR for specified number of days, by the CA privateKey and CA certificate
 // Any security checks (whether CSR should be signed or not) are done by the caller
-func SignCSRwithCA(days int, csr *x509.CertificateRequest, caCert *x509.Certificate, privatekey *rsa.PrivateKey) (*x509.Certificate, error) {
+func SignCSRwithCA(days int, csr *x509.CertificateRequest, caCert *x509.Certificate, privatekey crypto.PrivateKey) (*x509.Certificate, error) {
 	publickey := csr.PublicKey
 
 	template := &x509.Certificate{
@@ -236,4 +317,23 @@ func SignCSRwithCA(days int, csr *x509.CertificateRequest, caCert *x509.Certific
 	}
 	return cert[0], err
 
+}
+
+// IsSelfSigned returns whether the certificate is self-signed or not
+func IsSelfSigned(cert *x509.Certificate) bool {
+	return reflect.DeepEqual(cert.Issuer, cert.Subject)
+}
+
+// ValidateKeyCertificatePair checks whether a private key matches the public key in a certificate
+func ValidateKeyCertificatePair(privateKey crypto.PrivateKey, cert *x509.Certificate) (bool, error) {
+	switch keyType := privateKey.(type) {
+	case *rsa.PrivateKey:
+		expectedPubKey := privateKey.(*rsa.PrivateKey).PublicKey
+		return reflect.DeepEqual(cert.PublicKey, &expectedPubKey), nil
+	case *ecdsa.PrivateKey:
+		expectedPubKey := privateKey.(*ecdsa.PrivateKey).PublicKey
+		return reflect.DeepEqual(cert.PublicKey, &expectedPubKey), nil
+	default:
+		return false, errors.Errorf("Unknown key type: %T. Only RSA and ECDSA keys are supported", keyType)
+	}
 }
