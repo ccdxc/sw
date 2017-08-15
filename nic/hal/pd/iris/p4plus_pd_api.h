4e86e6581f6c863d6450a3d1d2c71c127801cf0d
Saurabh Jain <saurabh@pensando.io>
2017-08-15 14:11:40 -0700

    Remove swizzle code before hbm write. Removed obj file for sample_client_tcp

--- nic/hal/pd/iris/p4plus_pd_api.h
+++ nic/hal/pd/iris/p4plus_pd_api.h
@@ -14,58 +14,16 @@
 typedef int p4pd_error_t;
 #endif
 
-#define P4PLUS_MAX_DATA_LEN_IN_BYTES    512
-
-static inline void
-p4plus_swizzle_bytes(uint8_t* data, uint32_t size_in_bytes)
-{
-    size_in_bytes += (size_in_bytes & 1) ? 1 : 0;
-
-    for(uint32_t i = 0; i < (size_in_bytes >> 1); i++) {
-        uint8_t temp = data[i];
-        data[i] = data[size_in_bytes - i - 1];
-        data[size_in_bytes -i -1] = temp;
-    }
-}
-
 static inline bool
 p4plus_hbm_write(uint64_t addr, uint8_t* data, uint32_t size_in_bytes)
 {
-#if 0
-    if(size_in_bytes > P4PLUS_MAX_DATA_LEN_IN_BYTES) {
-        printf("Cannot write more than 64 byte data\n");
-        return false;    
-    } 
-
-    uint8_t buffer[P4PLUS_MAX_DATA_LEN_IN_BYTES] = {0}; 
-    memcpy(buffer, data, size_in_bytes);
-    
-    // Swizzle entry 
-    p4plus_swizzle_bytes(buffer, size_in_bytes);
-    size_in_bytes += (size_in_bytes & 1) ? 1 : 0;
-#endif
-    return write_mem(addr, data, size_in_bytes);            
+    return write_mem(addr, data, size_in_bytes);
 }
 
 static inline bool
 p4plus_hbm_read(uint64_t addr, uint8_t* data, uint32_t size_in_bytes)
 {
-    if(size_in_bytes > P4PLUS_MAX_DATA_LEN_IN_BYTES) {
-        printf("Cannot read more than 64 byte data\n");
-        return false;    
-    } 
-
-    uint8_t buffer[P4PLUS_MAX_DATA_LEN_IN_BYTES] = {0}; 
-    int readlen_in_bytes = (size_in_bytes & 1) ? size_in_bytes + 1 : size_in_bytes;
-
-    if(!read_mem(addr, buffer, readlen_in_bytes)) {
-        return false;    
-    }
-
-    p4plus_swizzle_bytes(buffer, readlen_in_bytes);
-
-    memcpy(data, buffer, size_in_bytes);
-    return true;
+    return read_mem(addr, data, size_in_bytes);
 }
 
 #endif
