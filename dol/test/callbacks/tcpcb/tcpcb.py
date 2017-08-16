aea83241296e2b5480a6cd28666b310841eddace
Alok Rathore <arathore@pensando.io>
2017-08-15 17:10:01 -0700

    fixing testspec

--- dol/test/callbacks/tcpcb/tcpcb.py
+++ dol/test/callbacks/tcpcb/tcpcb.py
@@ -6,6 +6,27 @@ import infra.api.api as infra_api
 from infra.common.logging       import logger
 import infra.common.defs as defs
 
+def GetSMac (tc, pkt):
+    return "00:22:22:22:22:22"
+
+def GetDMac (tc, pkt):
+    return "00:33:33:33:33:33"
+
+def GetVlanId (tc, pkt):
+    return 2
+
+def GetSip (tc, pkt):
+    return "64.2.0.4"
+
+def GetDip (tc, pkt):
+    return "64.2.0.5"
+
+def GetDPort (tc, pkt):
+    return 80
+
+def GetSPort (tc, pkt):
+    return 47273
+
 def GetSeqNum (tc, pkt):
     return 0xbabababa
 
