2f4f508fee839c5e54407e7fb6b6a9619186431c
Sameer Kittur <skittur@pensando.io>
2017-08-15 18:10:45 -0700

    Integration of xNMDR, xNMPR and SERQ

--- dol/config/hal/api.py
+++ dol/config/hal/api.py
@@ -41,6 +41,9 @@ HAL_MAX_BATCH_SIZE = 16
 import tcp_proxy_cb_pb2_grpc       as tcpcb_pb2_grpc
 import tls_proxy_cb_pb2_grpc       as tls_pb2_grpc
 import descriptor_aol_pb2_grpc as descriptor_aol_pb2_grpc
+import wring_pb2_grpc       as wring_pb2_grpc
+
+HAL_MAX_BATCH_SIZE = 16
 
 HalChannel = None
 class HalInterfaceSegmentAssociation:
