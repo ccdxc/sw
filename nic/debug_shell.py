186b3e6cef2917d42b055a0a13d092b1fdce4708
Ajeer Pudiyapura <ajeer@pensando.io>
2017-08-16 18:01:04 -0700

    Adding Error messages.

--- nic/debug_shell.py
+++ nic/debug_shell.py
@@ -7,9 +7,6 @@ from cmd2 import Cmd
 import os
 import sys
 
-cur_path = os.path.abspath(__file__)
-cur_path = os.path.split(cur_path)[0]
-
 class debugCmd(Cmd):
 
     prompt = "debug> "
@@ -30,6 +27,21 @@ class debugCmd(Cmd):
         self.exclude_from_help.append('do_shortcuts')
         self.exclude_from_help.append('do__relative_load')
         self.exclude_from_help.append('do_cmdenvironment')
+        self.nic_dir = os.path.abspath(__file__)
+        self.nic_dir = os.path.split(self.nic_dir)[0]
+        self.iris_inited = False
+        self.do_init_modules("")
+
+    def __del__(self):
+        if self.iris_inited:
+            try:
+                from p4pd_cli import cleanup as iris_cleanup
+                iris_cleanup()
+                self.iris_inited = False
+                debugCmd.do_iris = 0
+
+            except ImportError:
+                print("Could not cleanup Iris.")
 
     def do_quit(self, args):
         """Exits to the previous level"""
@@ -39,20 +51,35 @@ class debugCmd(Cmd):
 
     do_exit = do_quit
 
-    iris_path = cur_path + '/gen/iris/cli'
-    if os.path.isfile(iris_path + '/_p4pd.so'):
-        sys.path.append(iris_path)
-        from p4pd_cli import init as iris_init
-        iris_init()
-        def do_iris(self, args):
-            """Interact with Iris Tables"""
-            from p4pd_cli import rootCmd as iris_root
-            cmd = iris_root()
-            cmd.cmdloop()
+    def iris(self, a, b):
+        """Interact with Iris Tables"""
+        from p4pd_cli import rootCmd as iris_root
+        cmd = iris_root()
+        cmd.cmdloop()
 
+    def do_init_modules(self, args):
+        """Scan for new CLI modules"""
+        if self.iris_inited == False:
+            iris_path = self.nic_dir + '/gen/iris/cli'
+            if os.path.isfile(iris_path + '/p4pd_cli.py'):
+                print("Module Iris found!")
+                sys.path.append(iris_path)
+                try:
+                    from p4pd_cli import init as iris_init
+                    iris_init()
+                    self.iris_inited = True
+                    debugCmd.do_iris = classmethod(self.iris)
+
+                except ImportError:
+                    print("Could not initialize Iris. Do 'make' in sw/nic/gen/iris/cli/ directory, and run 'init_modules' command here.")
+            else:
+                print("Module Iris not found. Do 'make' in sw/nic directory, and run 'init_modules' command here to load missing modules.")
+        else:
+            print("All modules initialized.")
 
 if __name__ == '__main__':
 
     cmd = debugCmd()
     cmd.cmdloop()
+    del cmd
 
