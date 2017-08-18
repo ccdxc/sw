3b46e4342f6be833df133d02482f3ef58a867e56
Alok Rathore <arathore@pensando.io>
2017-08-17 17:54:29 -0700

    EF to F0

--- dol/test/tcp-tls-proxy/tcp_proxy.py
+++ dol/test/tcp-tls-proxy/tcp_proxy.py
@@ -27,7 +27,7 @@ def TestCaseSetup(tc):
     # 1. Configure TCB in HBM before packet injection
     tcb = tc.infra_data.ConfigStore.objects.db["TcpCb0000"]
     tcb.rcv_nxt = 0xBABABABA
-    tcb.snd_nxt = 0xEFEFEFEF
+    tcb.snd_nxt = 0xEFEFEFF0
     tcb.snd_una = 0xEFEFEFEF
     tcb.rcv_tsval = 0xFAFAFAFA
     tcb.ts_recent = 0xFAFAFAF0
