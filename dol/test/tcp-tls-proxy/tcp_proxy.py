f133596c977949ce0ba906e3e1aef632d9b9dd3f
Alok Rathore <arathore@pensando.io>
2017-08-17 19:17:17 -0700

    fixing bafa to bb16

--- dol/test/tcp-tls-proxy/tcp_proxy.py
+++ dol/test/tcp-tls-proxy/tcp_proxy.py
@@ -50,7 +50,7 @@ def TestCaseVerify(tc):
     print("rcv_nxt value pre-sync from HBM 0x%x" % tcb.rcv_nxt)
     tcb.GetObjValPd()
     print("rcv_nxt value post-sync from HBM 0x%x" % tcb.rcv_nxt)
-    if tcb.rcv_nxt is not 0xbababfa:
+    if tcb.rcv_nxt is not 0xbababb16:
         print("rcv_nxt not as expected")
         return False
     print("rcv_nxt as expected")
