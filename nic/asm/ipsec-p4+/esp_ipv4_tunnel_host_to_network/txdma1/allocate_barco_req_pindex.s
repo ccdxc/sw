2f2ab785fa5084a431c5e1be12ebc162de7772e8
Ramesh Ponnapalli <rameshp@srv13.pensando.io>
2017-08-22 13:40:59 -0700

    Ipsec code reorg

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/txdma1/allocate_barco_req_pindex.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/txdma1/allocate_barco_req_pindex.s
@@ -16,6 +16,6 @@ ipsec_encap_txdma_load_head_desc_int_header:
     phvwri p.common_te0_phv_table_raw_table_size, 3
     add r1, r0, d.barco_pindex
     sll r1, r1, BRQ_RING_ENTRY_SIZE_SHIFT 
-    sll r1, r1, BRQ_REQ_RING_BASE_ADDR
-    phvwr p.common_te0_phv_table_addr, d.barco_pindex
+    addi r1, r1, BRQ_REQ_RING_BASE_ADDR
+    phvwr p.common_te0_phv_table_addr, r1 
     
