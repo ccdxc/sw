19fe50fd3ace4c2864d85c54a31ddea894d29149
vijasrin <vijasrin@users.noreply.github.com>
2017-09-05 11:25:29 -0700

    TCP Normalization DOL (#622)
    - Adding callbacks for TCP packet fields

--- dol/test/callbacks/normalization/callbacks.py
+++ dol/test/callbacks/normalization/callbacks.py
@@ -99,6 +99,226 @@ def GetExpectedIcmpType(testcase, packet):
         return 0x5
     return 0
 
+def GetInputTcpReserved(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_RSVD_FLAGS_ACTION' in profile_name:
+        return 0x1
+    return 0x0
+
+def GetInputTcpFlags(testcase, packet):
+    #TBD: TCP_URG_PAYLOAD_MISSING_ACTION_ALLOW
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_UNEXPECTED_MSS_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_MSS_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_MSS_ACTION_EDIT' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_EDIT' in profile_name:
+        return None
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_EDIT' in profile_name:
+        return None
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_ALLOW' in profile_name:
+        return 'urg'
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_DROP' in profile_name:
+        return 'urg'
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_EDIT' in profile_name:
+        return 'urg'
+    elif 'TCP_RST_WITH_DATA_ACTION_ALLOW' in profile_name:
+        return 'rst'
+    elif 'TCP_RST_WITH_DATA_ACTION_DROP' in profile_name:
+        return 'rst'
+    elif 'TCP_RST_WITH_DATA_ACTION_EDIT' in profile_name:
+        return 'rst'
+    # Need more invalid flags combinations
+    elif 'TCP_INVALID_FLAGS_DROP_ENABLE' in profile_name:
+        return 'syn,rst'
+    elif 'TCP_INVALID_FLAGS_DROP_DISABLE' in profile_name:
+        return 'syn,rst'
+    elif 'TCP_FLAGS_NONSYN_NOACK_DROP_ENABLE' in profile_name:
+        return None
+    elif 'TCP_FLAGS_NONSYN_NOACK_DROP_DISABLE' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_EDIT' in profile_name:
+        return None
+    return None
+
+def GetInputTcpOptionsKind(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_UNEXPECTED_MSS_ACTION_ALLOW' in profile_name:
+        return 'MSS'
+    elif 'TCP_UNEXPECTED_MSS_ACTION_DROP' in profile_name:
+        return 'MSS'
+    elif 'TCP_UNEXPECTED_MSS_ACTION_EDIT' in profile_name:
+        return 'MSS'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_ALLOW' in profile_name:
+        return 'WScale'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_DROP' in profile_name:
+        return 'WScale'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_EDIT' in profile_name:
+        return 'WScale'
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_ALLOW' in profile_name:
+        return 'Timestamp'
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_DROP' in profile_name:
+        return 'Timestamp'
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_EDIT' in profile_name:
+        return 'Timestamp'
+    return None
+
+def GetInputTcpOptionsData(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_UNEXPECTED_MSS_ACTION_ALLOW' in profile_name:
+        return 0x400
+    elif 'TCP_UNEXPECTED_MSS_ACTION_DROP' in profile_name:
+        return 0x400
+    elif 'TCP_UNEXPECTED_MSS_ACTION_EDIT' in profile_name:
+        return 0x400
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_ALLOW' in profile_name:
+        return 0x4
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_DROP' in profile_name:
+        return 0x4
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_EDIT' in profile_name:
+        return 0x4
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_ALLOW' in profile_name:
+        return 0x12
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_DROP' in profile_name:
+        return 0x12
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_EDIT' in profile_name:
+        return 0x12
+    return None
+
+def GetInputTcpUrgPtr(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_URG_FLAG_NOT_SET_ACTION_ALLOW' in profile_name:
+        return 0x1
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_DROP' in profile_name:
+        return 0x1
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_EDIT' in profile_name:
+        return 0x1
+    return 0x0
+
+def GetExpectedTcpReserved(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_RSVD_FLAGS_ACTION_EDIT' in profile_name:
+        return 0x0
+    return 0x0
+
+def GetExpectedTcpFlags(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_UNEXPECTED_MSS_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_MSS_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_MSS_ACTION_EDIT' in profile_name:
+        return 'syn'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_EDIT' in profile_name:
+        return 'syn'
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_EDIT' in profile_name:
+        return None
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_ALLOW' in profile_name:
+        return 'urg'
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_DROP' in profile_name:
+        return 'urg'
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_EDIT' in profile_name:
+        return None
+    elif 'TCP_RST_WITH_DATA_ACTION_ALLOW' in profile_name:
+        return 'rst'
+    elif 'TCP_RST_WITH_DATA_ACTION_DROP' in profile_name:
+        return 'rst'
+    elif 'TCP_RST_WITH_DATA_ACTION_EDIT' in profile_name:
+        return None
+    # Need more invalid flags combinations
+    elif 'TCP_INVALID_FLAGS_DROP_ENABLE' in profile_name:
+        return None
+    elif 'TCP_INVALID_FLAGS_DROP_DISABLE' in profile_name:
+        return 'syn,rst'
+    elif 'TCP_FLAGS_NONSYN_NOACK_DROP_ENABLE' in profile_name:
+        return None
+    elif 'TCP_FLAGS_NONSYN_NOACK_DROP_DISABLE' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_ALLOW' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_DROP' in profile_name:
+        return None
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_EDIT' in profile_name:
+        return None
+    return None
+
+def GetExpectedTcpOptionsKind(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_UNEXPECTED_MSS_ACTION_ALLOW' in profile_name:
+        return 'MSS'
+    elif 'TCP_UNEXPECTED_MSS_ACTION_DROP' in profile_name:
+        return 'MSS'
+    elif 'TCP_UNEXPECTED_MSS_ACTION_EDIT' in profile_name:
+        return 'MSS'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_ALLOW' in profile_name:
+        return 'WScale'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_DROP' in profile_name:
+        return 'WScale'
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_EDIT' in profile_name:
+        return 'WScale'
+    return None
+
+def GetExpectedTcpOptionsData(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_UNEXPECTED_MSS_ACTION_ALLOW' in profile_name:
+        return 0x400
+    elif 'TCP_UNEXPECTED_MSS_ACTION_DROP' in profile_name:
+        return 0x400
+    elif 'TCP_UNEXPECTED_MSS_ACTION_EDIT' in profile_name:
+        return 0x400
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_ALLOW' in profile_name:
+        return 0x4
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_DROP' in profile_name:
+        return 0x4
+    elif 'TCP_UNEXPECTED_WIN_SCALE_ACTION_EDIT' in profile_name:
+        return 0x4
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_ALLOW' in profile_name:
+        return 0x12
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_DROP' in profile_name:
+        return 0x12
+    elif 'TCP_UNEXPECTED_ECHO_TS_ACTION_EDIT' in profile_name:
+        return 0
+    return None
+
+def GetExpectedTcpUrgPtr(testcase, packet):
+    profile_name = testcase.module.iterator.Get()
+    if 'TCP_URG_FLAG_NOT_SET_ACTION_ALLOW' in profile_name:
+        return 0x1
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_DROP' in profile_name:
+        return 0x1
+    elif 'TCP_URG_FLAG_NOT_SET_ACTION_EDIT' in profile_name:
+        return 0x0
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_ALLOW' in profile_name:
+        return 0x0
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_DROP' in profile_name:
+        return 0x0
+    elif 'TCP_URG_PTR_NOT_SET_ACTION_EDIT' in profile_name:
+        return 0x0
+    return 0x0
+
+
 def GetInputPayloadSize(testcase, packet):
     return 150
 
