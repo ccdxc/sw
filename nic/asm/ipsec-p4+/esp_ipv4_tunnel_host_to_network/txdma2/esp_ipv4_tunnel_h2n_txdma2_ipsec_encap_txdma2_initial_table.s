fa2fab48bdd26a343a9c28d3adcac86b6639dba9
Ramesh Ponnapalli <rameshp@pensando.io>
2017-09-07 16:46:20 -0700

    tm_oport set for txdma2

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/txdma2/esp_ipv4_tunnel_h2n_txdma2_ipsec_encap_txdma2_initial_table.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/txdma2/esp_ipv4_tunnel_h2n_txdma2_ipsec_encap_txdma2_initial_table.s
@@ -14,7 +14,9 @@ esp_ipv4_tunnel_h2n_txdma2_ipsec_encap_txdma2_initial_table:
     phvwr p.txdma2_global_ipsec_cb_index, d.ipsec_cb_index
     phvwr p.txdma2_global_iv_size, d.iv_size
     phvwr p.txdma2_global_icv_size, d.icv_size
-
+    phvwri p.p4_intr_global_tm_oport, TM_OPORT_P4INGRESS 
+    phvwri p.p4_intr_global_tm_iport, TM_OPORT_DMA 
+    phvwri p.p4_intr_global_lif, LIF_IPSEC_ESP 
     phvwri p.app_header_table0_valid, 1
     phvwri p.common_te0_phv_table_lock_en, 1
     phvwri p.common_te0_phv_table_raw_table_size, 3
