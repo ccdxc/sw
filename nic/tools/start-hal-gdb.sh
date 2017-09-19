c6ec804d9d0ca9972b1b97a306e180039eef7a3c
lseshan <8272455+lseshan@users.noreply.github.com>
2017-09-18 18:35:42 -0700

    Moving scripts at the top-level sw/nic to sw/nic/tools (#875)
    execute the scripts from sw/nic/tools directory
    or
    set NIC_DIR env to /sw/nic path

--- nic/tools/start-hal-gdb.sh
+++ nic/tools/start-hal-gdb.sh
@@ -2,5 +2,9 @@
 if [ -z "$NIC_DIR" ]; then
     export NIC_DIR=`dirname $PWD`
 fi
+if [ `basename $NIC_DIR` != "nic" ]; then
+    echo "Run $0 from tools dir or set env NIC_DIR"
+    exit 1
+fi
 export HAL_CONFIG_PATH=$NIC_DIR/conf/
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NIC_DIR/obj gdb --args $NIC_DIR/obj/hal -c hal.json 2>&1 | tee $NIC_DIR/hal-gdb.log
--- nic/tools/start-model-gdb.sh
+++ nic/tools/start-model-gdb.sh
@@ -2,5 +2,9 @@ cd `dirname $0`
 if [ -z "$NIC_DIR" ]; then
     export NIC_DIR=`dirname $PWD`
 fi
+if [ `basename $NIC_DIR` != "nic" ]; then
+    echo "Run $0 from tools dir or set env NIC_DIR"
+    exit 1
+fi
 export LD_LIBRARY_PATH=$NIC_DIR/model_sim/:$NIC_DIR/model_sim/libs:/home/asic/tools/src/0.25/x86_64/lib64:$LD_LIBRARY_PATH        
 gdb --args $NIC_DIR/model_sim/build/cap_model +plog=info +model_debug=$NIC_DIR/gen/iris/dbg_out/model_debug.json | tee $NIC_DIR/model_gdb.log
