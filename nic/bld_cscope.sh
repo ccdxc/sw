49fcea3183c0f545f65e9ae621da6e24b3e6a821
Harinadh Nagulapalli <harinadhn@gmail.com>
2017-09-19 19:09:10 -0700

    resubmitting for sanity Added .asm files to bld_cscope script

--- nic/bld_cscope.sh
+++ nic/bld_cscope.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo -e "\033[36m Building Cscope ...\033[0m"
-find -type f -and -regex '.*/.*\.\(c\|cpp\|cc\|h\|hpp\|p4\|s\)$' > cscope.files
+find -type f -and -regex '.*/.*\.\(c\|cpp\|cc\|h\|hpp\|p4\|s\|asm\)$' > cscope.files
 cscope -b -q
 echo -e "\033[36m Done !! \033[0m"
