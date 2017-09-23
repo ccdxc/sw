c7cbb3eaa5c5007b869e6995d8752a446cc41654
Erik Hollensbe <github@hollensbe.org>
2017-09-22 21:45:54 -0700

    nic: tooling to support asset-tools compile
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- nic/box.rb
+++ nic/box.rb
@@ -1,13 +1,20 @@
 from "srv1.pensando.io:5000/pensando/nic:1.1"
 
+env GOPATH: "/usr"
+
 inside "/etc" do
-    run "rm localtime"
-    run "ln -s /usr/share/zoneinfo/US/Pacific localtime"
+  run "rm localtime"
+  run "ln -s /usr/share/zoneinfo/US/Pacific localtime"
 end 
 
 # in the CI this block is triggered; in the Makefiles it is not.
 if getenv("NO_COPY") == ""
   copy ".", ".", ignore_list: %w[.git]
 end
+
+copy "entrypoint.sh", "/entrypoint.sh"
+run "chmod +x /entrypoint.sh"
+
+entrypoint "/entrypoint.sh"
 
 tag "pensando/nic"
---
+++ nic/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+dir=/usr/src/github.com/pensando/sw
+
+echo 1>&2 syncing and building asset tools...
+
+mkdir -p $dir
+rsync -a /sw/build /sw/vendor ${dir}
+
+cd $dir && go install ./build/...
+
+exec $*
