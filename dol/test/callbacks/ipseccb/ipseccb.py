f7d01f5f73a17fb3fa59f98c6988b337674d61d6
Ramesh Ponnapalli <rameshp@pensando.io>
2017-10-03 17:17:17 -0700

    dol changes for removing tunnel hard-coding - wip

---
+++ dol/test/callbacks/ipseccb/ipseccb.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+import pdb
+from infra.api.objects import PacketHeader
+import infra.api.api as infra_api
+from infra.common.logging       import logger
+import infra.common.defs as defs
+
+def GetESPPktSeqNum(tc, pkt):
+    return "0" 
+
+def GetESPPktSPI(tc, pkt):
+    return "0" 
+
+def GetIPSecTunnelDstIp (tc, pkt):
+    return "10.1.0.2"
+def GetIPSecTunnelSrcIp (tc, pkt):
+    return "10.1.0.1"
