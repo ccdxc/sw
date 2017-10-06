f61030c8deecd9aa8b36eef76cf6eb0ba4d3d945
ksmurty <ksmurty@gmail.com>
2017-10-05 18:11:56 -0700

    Enabling Send Only Immediate and Send FL Immediate tests (#1212)

---
+++ dol/test/rdma/resp_rx_send_only_imm_2sges.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python3
+
+from test.rdma.utils import *
+
+def Setup(infra, module):
+    return
+
+def Teardown(infra, module):
+    return
+
+def TestCaseSetup(tc):
+    tc.info("RDMA TestCaseSetup() Implementation.")
+    rs = tc.config.rdmasession
+
+    # Read RQ pre state
+    rs.lqp.rq.qstate.Read()
+    tc.pvtdata.rq_pre_qstate = rs.lqp.rq.qstate.data
+    tc.pvtdata.send_only_psn = tc.pvtdata.rq_pre_qstate.e_psn
+    tc.pvtdata.imm_data = 0x0F0E0D0C;
+
+    # Read CQ pre state
+    rs.lqp.rq_cq.qstate.Read()
+    tc.pvtdata.rq_cq_pre_qstate = rs.lqp.rq_cq.qstate.data
+    return
+
+def TestCaseTrigger(tc):
+    tc.info("RDMA TestCaseTrigger() Implementation.")
+    return
+
+def TestCaseVerify(tc):
+    tc.info("RDMA TestCaseVerify() Implementation.")
+    rs = tc.config.rdmasession
+    rs.lqp.rq.qstate.Read()
+    ring0_mask = (rs.lqp.num_rq_wqes - 1)
+    tc.pvtdata.rq_post_qstate = rs.lqp.rq.qstate.data
+
+    ############     RQ VALIDATIONS #################
+    # verify that e_psn is incremented by 1
+    if not VerifyFieldModify(tc, tc.pvtdata.rq_pre_qstate, tc.pvtdata.rq_post_qstate, 'e_psn', 1):
+        return False
+
+    # verify that proxy_cindex is incremented by 1
+    if not VerifyFieldMaskModify(tc, tc.pvtdata.rq_pre_qstate, tc.pvtdata.rq_post_qstate, 'proxy_cindex', ring0_mask, 1):
+        return False
+
+    # verify that token_id is incremented by 1
+    if not VerifyFieldModify(tc, tc.pvtdata.rq_pre_qstate, tc.pvtdata.rq_post_qstate, 'token_id', 1):
+        return False
+
+    # verify that nxt_to_go_token_id is incremented by 1
+    if not VerifyFieldModify(tc, tc.pvtdata.rq_pre_qstate, tc.pvtdata.rq_post_qstate, 'nxt_to_go_token_id', 1):
+        return False
+
+    # verify that busy is 0
+    if not VerifyFieldAbsolute(tc, tc.pvtdata.rq_post_qstate, 'busy', 0):
+        return False
+
+    ############     CQ VALIDATIONS #################
+    if not ValidateRespRxCQChecks(tc):
+        return False
+
+    return True
+
+def TestCaseTeardown(tc):
+    tc.info("RDMA TestCaseTeardown() Implementation.")
+    return
