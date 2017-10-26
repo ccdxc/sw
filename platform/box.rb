c06a41c8f0c210cdc48d9849e05054fd4fa0e830
Erik Hollensbe <github@hollensbe.org>
2017-10-26 14:55:03 -0700

    platform/box.rb: set up an array for package installations
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- platform/box.rb
+++ platform/box.rb
@@ -1,7 +1,12 @@
 from "registry.test.pensando.io:5000/pensando/nic:1.1"
 
+PACKAGES = %w[
+  patch
+]
+
 env GOPATH: "/usr"
 workdir "/sw"
+run "yum install -y #{PACKAGES.join(" ")}"
 
 # in the CI this block is triggered; in the Makefiles it is not.
 if getenv("NO_COPY") == ""
