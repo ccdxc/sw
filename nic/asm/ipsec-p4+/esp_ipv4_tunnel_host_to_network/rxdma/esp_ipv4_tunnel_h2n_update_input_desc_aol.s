dddab28ed7820b11f2cdc1c56722297734192222
Ramesh Ponnapalli <rameshp@pensando.io>
2017-11-03 15:14:35 -0700

    cleanup and asm warnings

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/rxdma/esp_ipv4_tunnel_h2n_update_input_desc_aol.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/rxdma/esp_ipv4_tunnel_h2n_update_input_desc_aol.s
@@ -53,7 +53,7 @@ dma_cmd_to_move_input_pkt_to_mem:
     
     add r1, r1, k.t0_s2s_in_page_addr 
     phvwr p.dma_cmd_pkt2mem_dma_cmd_addr, r1 
-    phvwr p.dma_cmd_pkt2mem_dma_cmd_size, k.ipsec_to_stage3_packet_len
+    phvwr p.dma_cmd_pkt2mem_dma_cmd_size, k.ipsec_to_stage3_packet_len[13:0]
 
 dma_cmd_to_write_pad_bytes:
     add r2, r1, k.ipsec_to_stage3_packet_len
@@ -62,7 +62,7 @@ dma_cmd_to_write_pad_bytes:
     phvwri p.dma_cmd_pad_byte_src_dma_cmd_type, CAPRI_DMA_COMMAND_MEM_TO_MEM
     phvwri p.dma_cmd_pad_byte_src_dma_cmd_mem2mem_type, DMA_CMD_TYPE_MEM2MEM_TYPE_SRC
     phvwri p.dma_cmd_pad_byte_src_dma_cmd_host_addr, 0
-    phvwr  p.dma_cmd_pad_byte_src_dma_cmd_addr, k.ipsec_to_stage3_pad_addr 
+    phvwr  p.dma_cmd_pad_byte_src_dma_cmd_addr, k.ipsec_to_stage3_pad_addr[51:0] 
     phvwr p.dma_cmd_pad_byte_src_dma_cmd_size, k.ipsec_to_stage3_pad_size
     //destination - to the end of the page where pkt is DMA'ed.
     phvwri p.dma_cmd_pad_byte_dst_dma_cmd_type, CAPRI_DMA_COMMAND_MEM_TO_MEM 
@@ -80,7 +80,7 @@ dma_cmd_to_write_pad_size_l4_proto:
 
 dma_cmd_write_salt_to_in_desc:
     phvwri p.dma_cmd_iv_salt_dma_cmd_type, CAPRI_DMA_COMMAND_PHV_TO_MEM
-    phvwr p.dma_cmd_iv_salt_dma_cmd_addr, k.t0_s2s_in_page_addr
+    phvwr p.dma_cmd_iv_salt_dma_cmd_addr, k.t0_s2s_in_page_addr[51:0]
     phvwri p.dma_cmd_iv_salt_dma_cmd_phv_start_addr, IPSEC_IN_DESC_IV_SALT_START
     phvwri p.dma_cmd_iv_salt_dma_cmd_phv_end_addr, IPSEC_IN_DESC_IV_SALT_END 
 
