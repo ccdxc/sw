ad4d123e720a0002fd40a415747ca1a1e1210aba
pmathison <pmathison@pensando.io>
2017-11-20 19:14:39 -0800

    snort version update

--- nic/third-party/snort3/export/include/snort_api.h
+++ nic/third-party/snort3/export/include/snort_api.h
@@ -6,11 +6,12 @@
 #define SO_PUBLIC  __attribute__ ((visibility("default")))
 
 #define NUM_APP_IDS 4
-struct App
+struct SnortFlowInfo
 {
-	int appids[NUM_APP_IDS];
-	int app_detection_status;
-	int error;
+    int appids[NUM_APP_IDS];
+    int app_detection_status;
+    int error;
+    void* flow_handle;
 };
 
 // Does basic initializations of snort main thread
@@ -30,13 +31,25 @@ SO_PUBLIC int init_pkt_thread(int thread_num);
 // A non-zero return status indicates that packet thread is exiting and
 // cleanup_pkt_thread needs to be called
 // thread_num is a zero-based number
-SO_PUBLIC struct App run_pkt_thread_loop(int thread_num, void* pkt, uint32_t pkt_len, void *priv_ptr=NULL);
+// Returns flow_handle to be used for lookup or cleanup
+SO_PUBLIC int run_pkt_thread_loop(int thread_num, uint8_t* pkt, uint32_t pkt_len,
+        void **flow_handle, void *priv_ptr=NULL);
+// Returns info on the last flow processed by the thread
+SO_PUBLIC int get_pkt_thread_flow_info(int thread_num, struct SnortFlowInfo* flow_info);
+// Returns info on a specific flow
+SO_PUBLIC int get_flow_info_by_handle(void* flow_handle, struct SnortFlowInfo* flow_info);
+SO_PUBLIC int get_flow_info_by_key(uint8_t ip_proto, bool is_v6,
+        const uint8_t* sip, const uint8_t* dip, uint16_t sport, uint16_t dport,
+        uint16_t vlan_id, uint16_t tenant_id, struct SnortFlowInfo* flow_info);
 // Cleans up the thread.
 // thread_num is a zero-based number
 SO_PUBLIC int cleanup_pkt_thread(int thread_num);
 
-// Placeholder for flow lifecycle mgmt
-SO_PUBLIC int cleanup_flow(int flow_id);
+// Cleans up the flow
+SO_PUBLIC int cleanup_flow(void* flow_handle);
+SO_PUBLIC int cleanup_flow_by_key(uint8_t ip_proto, bool is_v6,
+        const uint8_t* sip, const uint8_t* dip, uint16_t sport, uint16_t dport,
+        uint16_t vlan_id, uint16_t tenant_id);
 
 
 #endif
