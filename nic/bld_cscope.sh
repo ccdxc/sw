17b1729c63db4b5932245e4f39406f6f140d7e54
Vipin Jain <jainvipin@gmail.com>
2017-11-22 01:36:29 -0800

    allow cscope to build on darwin/mac systems (#1905)

--- nic/bld_cscope.sh
+++ nic/bld_cscope.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 echo -e "\033[36m Building Cscope ...\033[0m"
-find -type f -and -regex '.*/.*\.\(c\|cpp\|cc\|h\|hpp\|p4\|s\|asm\|py\|proto\|spec\)$' > cscope.files
-# find . -name '*.c' 2> /dev/null > cscope.files
-# find . -name '*.cpp' 2> /dev/null >> cscope.files
-# find . -name '*.cc' 2> /dev/null >> cscope.files
-# find . -name '*.hpp' 2> /dev/null >> cscope.files
-# find . -name '*.p4' 2> /dev/null >> cscope.files
-# find . -name '*.s' 2> /dev/null >> cscope.files
-# find . -name '*.asm' 2> /dev/null >> cscope.files
-# find . -name '*.py' 2> /dev/null >> cscope.files
-# find . -name '*.proto' 2> /dev/null >> cscope.files
-# find . -name '*.spec' 2> /dev/null >> cscope.files
+uname=`uname -s`
+if [ x$uname == xLinux ]
+then
+  find -type f -and -regex '.*/.*\.\(c\|cpp\|cc\|h\|hpp\|p4\|s\|asm\|py\|proto\|spec\)$' > cscope.files
+elif [ x$uname == xDarwin ]
+then
+  find . -name '*.c' 2> /dev/null > cscope.files
+  find . -name '*.cpp' 2> /dev/null >> cscope.files
+  find . -name '*.cc' 2> /dev/null >> cscope.files
+  find . -name '*.hpp' 2> /dev/null >> cscope.files
+  find . -name '*.p4' 2> /dev/null >> cscope.files
+  find . -name '*.s' 2> /dev/null >> cscope.files
+  find . -name '*.asm' 2> /dev/null >> cscope.files
+  find . -name '*.py' 2> /dev/null >> cscope.files
+  find . -name '*.proto' 2> /dev/null >> cscope.files
+  find . -name '*.spec' 2> /dev/null >> cscope.files
+fi
 
 cscope -b -q
 echo -e "\033[36m Done !! \033[0m"
