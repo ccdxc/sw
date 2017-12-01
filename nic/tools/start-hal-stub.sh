520502189d75bd2f86b6177f27710288eb01b4c1
Paul Mathison <pmathison@pensando.io>
2017-11-30 16:27:28 -0800

    fix venice testcase failing due to libsnort.so

--- nic/tools/start-hal-stub.sh
+++ nic/tools/start-hal-stub.sh
@@ -4,4 +4,10 @@ ABS_TOOLS_DIR=`readlink -f $TOOLS_DIR`
 NIC_DIR=`dirname $ABS_TOOLS_DIR`
 
 export HAL_CONFIG_PATH=$NIC_DIR/conf/
-CAPRI_MOCK_MODE=1 $NIC_DIR/../bazel-bin/nic/hal/hal -c hal.json 2>&1 | tee $NIC_DIR/hal.log
+#export HAL_PLUGIN_PATH=$NIC_DIR/../bazel-bin/nic/hal/plugins/
+export SNORT_LUA_PATH=$NIC_DIR/third-party/snort3/export/lua/
+export LUA_PATH="$NIC_DIR/third-party/snort3/export/lua/?.lua;;"
+export SNORT_DAQ_PATH=$NIC_DIR/third-party/snort3/export/daqs/
+export SNORT_LD_PATH=$NIC_DIR/third-party/snort3/export/bin/libsnort.so
+
+LD_PRELOAD=$SNORT_LD_PATH CAPRI_MOCK_MODE=1 $NIC_DIR/../bazel-bin/nic/hal/hal -c hal.json 2>&1 | tee $NIC_DIR/hal.log
