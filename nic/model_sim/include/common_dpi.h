b9d7675b982bc6830054d7ab33635136b3ff2f27
Vikas B Dharwadkar <vikasbd@users.noreply.github.com>
2018-01-14 18:19:56 -0800

    Update asic ptr 0354ca6 (#2571)

--- nic/model_sim/include/common_dpi.h
+++ nic/model_sim/include/common_dpi.h
@@ -14,7 +14,7 @@ void dpi_c_init_all();
 void dpi_sv_puvm_mem_write(uint64_t addr, const unsigned char* data, int len);
 void dpi_sv_puvm_mem_read(uint64_t addr, unsigned char* data, int len);
 void dpi_sv_get_sim_time_ns(char* ts);
-
+void dpi_force_finish();
 
 }
 
