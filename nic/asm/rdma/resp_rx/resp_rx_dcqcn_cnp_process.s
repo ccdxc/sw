d00a8212c6685f7ee79e3e24597d6a9e3065b975
vdanivas <32657496+vdanivas@users.noreply.github.com>
2018-01-18 11:54:57 -0800

    Model Error fix for CNP test. Also enabling model error check for rdma in job.yml (#2655)

--- nic/asm/rdma/resp_rx/resp_rx_dcqcn_cnp_process.s
+++ nic/asm/rdma/resp_rx/resp_rx_dcqcn_cnp_process.s
@@ -14,8 +14,10 @@ struct resp_rx_ecn_process_k_t k;
 resp_rx_dcqcn_cnp_process:
 
     tblmincri   d.num_cnp_rcvd, 8, 1 //num_cnp_recvd is 8-bit field.
-    // Ring doorbell to cut-rate and reset dcqcn params.
+    // Ring doorbell to cut-rate and reset dcqcn params. Also drop PHV since further 
+    // processing will be done in TxDMA rate-compute-ring.
     DOORBELL_INC_PINDEX(k.global.lif,  k.global.qtype, k.global.qid, DCQCN_RATE_COMPUTE_RING_ID, r5, r6)
+    phvwr       p.common.p4_intr_global_drop, 1
     CAPRI_SET_TABLE_2_VALID(0)
     nop.e
     nop
