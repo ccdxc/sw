1b739d7e2383b3fe9597ffaa276c244f48bd6871
Yuva Shankar <yuva29@users.noreply.github.com>
2018-01-22 17:27:23 -0800

    Events proto change (#2727)
    * Rename `events_manager` to `eventsmanager`
    
    - underscores are not recommended in package names
    
    Signed-off-by: Yuva Shankar <yuva29@users.noreply.github.com>
    
    * Change events proto to accommodate sending multiple events at once
    
    Signed-off-by: Yuva Shankar <yuva29@users.noreply.github.com>

--- venice/ctrler/eventsmanager/rpcserver/eventsproto/events.pb.go
+++ venice/ctrler/eventsmanager/rpcserver/eventsproto/events.pb.go
@@ -9,6 +9,7 @@
 
 	It has these top-level messages:
 		Empty
+		EventList
 */
 package eventsproto
 
@@ -46,8 +47,25 @@ func (m *Empty) String() string            { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()               {}
 func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptorEvents, []int{0} }
 
+type EventList struct {
+	Events []events.Event `protobuf:"bytes,1,rep,name=Events" json:"Events"`
+}
+
+func (m *EventList) Reset()                    { *m = EventList{} }
+func (m *EventList) String() string            { return proto.CompactTextString(m) }
+func (*EventList) ProtoMessage()               {}
+func (*EventList) Descriptor() ([]byte, []int) { return fileDescriptorEvents, []int{1} }
+
+func (m *EventList) GetEvents() []events.Event {
+	if m != nil {
+		return m.Events
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Empty)(nil), "eventsproto.Empty")
+	proto.RegisterType((*EventList)(nil), "eventsproto.EventList")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -58,39 +76,39 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// Client API for Events service
+// Client API for EventsAPI service
 
-type EventsClient interface {
+type EventsAPIClient interface {
 	// any component can use this method to generate an event
-	SendEvent(ctx context.Context, in *events.Event, opts ...grpc.CallOption) (*Empty, error)
+	SendEvents(ctx context.Context, in *EventList, opts ...grpc.CallOption) (*Empty, error)
 	// any component can establish a watch to get a stream of events.
 	// e.g. alerting system could watch for events to trigger an alert.
-	WatchEvents(ctx context.Context, in *Empty, opts ...grpc.CallOption) (Events_WatchEventsClient, error)
+	WatchEvents(ctx context.Context, in *Empty, opts ...grpc.CallOption) (EventsAPI_WatchEventsClient, error)
 }
 
-type eventsClient struct {
+type eventsAPIClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewEventsClient(cc *grpc.ClientConn) EventsClient {
-	return &eventsClient{cc}
+func NewEventsAPIClient(cc *grpc.ClientConn) EventsAPIClient {
+	return &eventsAPIClient{cc}
 }
 
-func (c *eventsClient) SendEvent(ctx context.Context, in *events.Event, opts ...grpc.CallOption) (*Empty, error) {
+func (c *eventsAPIClient) SendEvents(ctx context.Context, in *EventList, opts ...grpc.CallOption) (*Empty, error) {
 	out := new(Empty)
-	err := grpc.Invoke(ctx, "/eventsproto.Events/SendEvent", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/eventsproto.EventsAPI/SendEvents", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *eventsClient) WatchEvents(ctx context.Context, in *Empty, opts ...grpc.CallOption) (Events_WatchEventsClient, error) {
-	stream, err := grpc.NewClientStream(ctx, &_Events_serviceDesc.Streams[0], c.cc, "/eventsproto.Events/WatchEvents", opts...)
+func (c *eventsAPIClient) WatchEvents(ctx context.Context, in *Empty, opts ...grpc.CallOption) (EventsAPI_WatchEventsClient, error) {
+	stream, err := grpc.NewClientStream(ctx, &_EventsAPI_serviceDesc.Streams[0], c.cc, "/eventsproto.EventsAPI/WatchEvents", opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &eventsWatchEventsClient{stream}
+	x := &eventsAPIWatchEventsClient{stream}
 	if err := x.ClientStream.SendMsg(in); err != nil {
 		return nil, err
 	}
@@ -100,89 +118,89 @@ func (c *eventsClient) WatchEvents(ctx context.Context, in *Empty, opts ...grpc.
 	return x, nil
 }
 
-type Events_WatchEventsClient interface {
-	Recv() (*events.Event, error)
+type EventsAPI_WatchEventsClient interface {
+	Recv() (*EventList, error)
 	grpc.ClientStream
 }
 
-type eventsWatchEventsClient struct {
+type eventsAPIWatchEventsClient struct {
 	grpc.ClientStream
 }
 
-func (x *eventsWatchEventsClient) Recv() (*events.Event, error) {
-	m := new(events.Event)
+func (x *eventsAPIWatchEventsClient) Recv() (*EventList, error) {
+	m := new(EventList)
 	if err := x.ClientStream.RecvMsg(m); err != nil {
 		return nil, err
 	}
 	return m, nil
 }
 
-// Server API for Events service
+// Server API for EventsAPI service
 
-type EventsServer interface {
+type EventsAPIServer interface {
 	// any component can use this method to generate an event
-	SendEvent(context.Context, *events.Event) (*Empty, error)
+	SendEvents(context.Context, *EventList) (*Empty, error)
 	// any component can establish a watch to get a stream of events.
 	// e.g. alerting system could watch for events to trigger an alert.
-	WatchEvents(*Empty, Events_WatchEventsServer) error
+	WatchEvents(*Empty, EventsAPI_WatchEventsServer) error
 }
 
-func RegisterEventsServer(s *grpc.Server, srv EventsServer) {
-	s.RegisterService(&_Events_serviceDesc, srv)
+func RegisterEventsAPIServer(s *grpc.Server, srv EventsAPIServer) {
+	s.RegisterService(&_EventsAPI_serviceDesc, srv)
 }
 
-func _Events_SendEvent_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(events.Event)
+func _EventsAPI_SendEvents_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(EventList)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(EventsServer).SendEvent(ctx, in)
+		return srv.(EventsAPIServer).SendEvents(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/eventsproto.Events/SendEvent",
+		FullMethod: "/eventsproto.EventsAPI/SendEvents",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(EventsServer).SendEvent(ctx, req.(*events.Event))
+		return srv.(EventsAPIServer).SendEvents(ctx, req.(*EventList))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Events_WatchEvents_Handler(srv interface{}, stream grpc.ServerStream) error {
+func _EventsAPI_WatchEvents_Handler(srv interface{}, stream grpc.ServerStream) error {
 	m := new(Empty)
 	if err := stream.RecvMsg(m); err != nil {
 		return err
 	}
-	return srv.(EventsServer).WatchEvents(m, &eventsWatchEventsServer{stream})
+	return srv.(EventsAPIServer).WatchEvents(m, &eventsAPIWatchEventsServer{stream})
 }
 
-type Events_WatchEventsServer interface {
-	Send(*events.Event) error
+type EventsAPI_WatchEventsServer interface {
+	Send(*EventList) error
 	grpc.ServerStream
 }
 
-type eventsWatchEventsServer struct {
+type eventsAPIWatchEventsServer struct {
 	grpc.ServerStream
 }
 
-func (x *eventsWatchEventsServer) Send(m *events.Event) error {
+func (x *eventsAPIWatchEventsServer) Send(m *EventList) error {
 	return x.ServerStream.SendMsg(m)
 }
 
-var _Events_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "eventsproto.Events",
-	HandlerType: (*EventsServer)(nil),
+var _EventsAPI_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "eventsproto.EventsAPI",
+	HandlerType: (*EventsAPIServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "SendEvent",
-			Handler:    _Events_SendEvent_Handler,
+			MethodName: "SendEvents",
+			Handler:    _EventsAPI_SendEvents_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "WatchEvents",
-			Handler:       _Events_WatchEvents_Handler,
+			Handler:       _EventsAPI_WatchEvents_Handler,
 			ServerStreams: true,
 		},
 	},
@@ -207,6 +225,36 @@ func (m *Empty) MarshalTo(dAtA []byte) (int, error) {
 	return i, nil
 }
 
+func (m *EventList) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *EventList) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if len(m.Events) > 0 {
+		for _, msg := range m.Events {
+			dAtA[i] = 0xa
+			i++
+			i = encodeVarintEvents(dAtA, i, uint64(msg.Size()))
+			n, err := msg.MarshalTo(dAtA[i:])
+			if err != nil {
+				return 0, err
+			}
+			i += n
+		}
+	}
+	return i, nil
+}
+
 func encodeVarintEvents(dAtA []byte, offset int, v uint64) int {
 	for v >= 1<<7 {
 		dAtA[offset] = uint8(v&0x7f | 0x80)
@@ -219,6 +267,18 @@ func encodeVarintEvents(dAtA []byte, offset int, v uint64) int {
 func (m *Empty) Size() (n int) {
 	var l int
 	_ = l
+	return n
+}
+
+func (m *EventList) Size() (n int) {
+	var l int
+	_ = l
+	if len(m.Events) > 0 {
+		for _, e := range m.Events {
+			l = e.Size()
+			n += 1 + l + sovEvents(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -264,6 +324,87 @@ func (m *Empty) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Empty: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipEvents(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthEvents
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *EventList) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowEvents
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: EventList: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: EventList: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Events", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEvents
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthEvents
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Events = append(m.Events, events.Event{})
+			if err := m.Events[len(m.Events)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipEvents(dAtA[iNdEx:])
@@ -393,20 +534,23 @@ var (
 func init() { proto.RegisterFile("events.proto", fileDescriptorEvents) }
 
 var fileDescriptorEvents = []byte{
-	// 239 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0xc1, 0x4a, 0xc4, 0x30,
-	0x10, 0x86, 0x9b, 0x83, 0x2b, 0x66, 0x15, 0x24, 0xc7, 0x22, 0x3d, 0xf4, 0x6e, 0x66, 0x51, 0x9f,
-	0x40, 0xe8, 0x5d, 0xf0, 0xe0, 0x39, 0x6d, 0xc7, 0x6c, 0xa0, 0x3b, 0x13, 0x4c, 0xba, 0xe2, 0x9b,
-	0xf8, 0x48, 0x1e, 0x7d, 0x04, 0xa9, 0x2f, 0x22, 0x3b, 0x56, 0xa8, 0xa0, 0xb7, 0x7c, 0xc9, 0xf7,
-	0xff, 0x49, 0x46, 0x9f, 0xe2, 0x1e, 0x29, 0x27, 0x1b, 0x9f, 0x38, 0xb3, 0x59, 0x7f, 0x93, 0x40,
-	0x79, 0xe1, 0x99, 0xfd, 0x80, 0xe0, 0x62, 0x00, 0x47, 0xc4, 0xd9, 0xe5, 0xc0, 0x34, 0xab, 0x65,
-	0xe3, 0x43, 0xde, 0x8e, 0xad, 0xed, 0x78, 0x07, 0x11, 0x29, 0x39, 0xea, 0x19, 0xd2, 0x33, 0xec,
-	0x91, 0x42, 0x87, 0x30, 0xe6, 0x30, 0xa4, 0x43, 0xd4, 0x23, 0x2d, 0xd3, 0x10, 0xa8, 0x1b, 0xc6,
-	0x1e, 0x7f, 0x6a, 0x2e, 0x17, 0x35, 0x9e, 0x3d, 0x83, 0x6c, 0xb7, 0xe3, 0xa3, 0x90, 0x80, 0xac,
-	0x66, 0xdd, 0xfe, 0x73, 0xeb, 0xe1, 0x8d, 0x62, 0x24, 0x58, 0x7e, 0xa8, 0x3e, 0xd6, 0x47, 0xcd,
-	0x2e, 0xe6, 0x97, 0xab, 0xa8, 0x57, 0x8d, 0x1c, 0x98, 0x8d, 0x3e, 0xb9, 0x47, 0xea, 0x85, 0xcc,
-	0x99, 0x9d, 0x75, 0xc1, 0xd2, 0xd8, 0xc5, 0x00, 0xac, 0x24, 0xeb, 0xc2, 0xdc, 0xe8, 0xf5, 0x83,
-	0xcb, 0xdd, 0x76, 0x2e, 0xf8, 0x43, 0x2a, 0x7f, 0xf7, 0xd4, 0xc5, 0x46, 0xdd, 0x9e, 0xbf, 0x4d,
-	0x95, 0x7a, 0x9f, 0x2a, 0xf5, 0x31, 0x55, 0xea, 0xf5, 0xb3, 0x2a, 0xee, 0x54, 0xbb, 0x92, 0xcc,
-	0xf5, 0x57, 0x00, 0x00, 0x00, 0xff, 0xff, 0x77, 0xa2, 0x4d, 0xfe, 0x76, 0x01, 0x00, 0x00,
+	// 285 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x49, 0x2d, 0x4b, 0xcd,
+	0x2b, 0x29, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x86, 0xf0, 0xc0, 0x1c, 0x29, 0x99,
+	0xf4, 0xfc, 0xfc, 0xf4, 0x9c, 0x54, 0xfd, 0xc4, 0x82, 0x4c, 0xfd, 0xc4, 0xbc, 0xbc, 0xfc, 0x92,
+	0xc4, 0x92, 0xcc, 0xfc, 0x3c, 0xa8, 0x52, 0x29, 0xd7, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd,
+	0xe4, 0xfc, 0x5c, 0xfd, 0x82, 0xd4, 0xbc, 0xe2, 0xc4, 0xbc, 0x94, 0x7c, 0xfd, 0xe2, 0x72, 0xfd,
+	0xb2, 0xd4, 0xbc, 0xcc, 0xe4, 0x54, 0xfd, 0xd2, 0x92, 0xcc, 0x9c, 0x62, 0x90, 0xd6, 0xf4, 0xd4,
+	0x3c, 0x64, 0xdd, 0xfa, 0x99, 0x79, 0xc9, 0x39, 0xa5, 0x29, 0xa9, 0x30, 0x63, 0x74, 0x91, 0x8c,
+	0x49, 0xcf, 0x4f, 0xcf, 0xd7, 0x07, 0x0b, 0x27, 0x95, 0xa6, 0x81, 0x79, 0x60, 0x0e, 0x98, 0x05,
+	0x55, 0xae, 0x87, 0xc3, 0x56, 0x90, 0x1b, 0xc1, 0x2a, 0x8a, 0xf5, 0x91, 0x3d, 0xa4, 0xc4, 0xce,
+	0xc5, 0xea, 0x9a, 0x5b, 0x50, 0x52, 0xa9, 0x64, 0xc1, 0xc5, 0xe9, 0x0a, 0x92, 0xf0, 0xc9, 0x2c,
+	0x2e, 0x11, 0xd2, 0xe6, 0x62, 0x03, 0x73, 0x8a, 0x25, 0x18, 0x15, 0x98, 0x35, 0xb8, 0x8d, 0x78,
+	0xf5, 0xa0, 0x9a, 0xc0, 0xa2, 0x4e, 0x2c, 0x27, 0xee, 0xc9, 0x33, 0x04, 0x41, 0x95, 0x18, 0xb5,
+	0x31, 0x42, 0xb5, 0x16, 0x3b, 0x06, 0x78, 0x0a, 0x59, 0x71, 0x71, 0x05, 0xa7, 0xe6, 0xa5, 0x40,
+	0x04, 0x84, 0xc4, 0xf4, 0x90, 0x02, 0x4c, 0x0f, 0x6e, 0x81, 0x94, 0x10, 0xaa, 0x38, 0xd8, 0x05,
+	0x0c, 0x42, 0xb6, 0x5c, 0xdc, 0xe1, 0x89, 0x25, 0xc9, 0x19, 0x50, 0xcd, 0x58, 0x14, 0x49, 0xe1,
+	0x30, 0x50, 0x89, 0xc1, 0x80, 0xd1, 0x49, 0xe0, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18,
+	0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf1, 0x58, 0x8e, 0x21, 0x80, 0x31, 0x89, 0x0d, 0xac, 0xd0, 0x18,
+	0x10, 0x00, 0x00, 0xff, 0xff, 0xda, 0x0f, 0xcc, 0x71, 0xc7, 0x01, 0x00, 0x00,
 }
