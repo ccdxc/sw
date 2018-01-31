0f82f12cd82ea85d9307d79a794c9f8db012486b
rameshp <rameshpvn77@users.noreply.github.com>
2018-01-31 15:38:24 -0800

    enable couple of new test cases for bad signature, enable extra-pad dummy case (#2896)

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_network_to_host/txdma2/esp_ipv4_tunnel_n2h_txdma2_load_ipsec_int.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_network_to_host/txdma2/esp_ipv4_tunnel_n2h_txdma2_load_ipsec_int.s
@@ -10,6 +10,8 @@ struct phv_ p;
         .align
 esp_ipv4_tunnel_n2h_txdma2_load_ipsec_int:
     phvwri p.app_header_table2_valid, 0
+    seq c1, d.status_addr, 0
+    bcf [!c1], esp_ipv4_tunnel_n2h_txdma2_load_ipsec_int_phv_drop
     phvwr p.txdma2_global_pad_size, d.pad_size
     phvwr p.txdma2_global_l4_protocol, d.l4_protocol
     phvwr p.txdma2_global_payload_size, d.payload_size
@@ -19,4 +21,14 @@ esp_ipv4_tunnel_n2h_txdma2_load_ipsec_int:
     phvwr p.ipsec_to_stage4_headroom, d.headroom
     nop.e
     nop
- 
+
+esp_ipv4_tunnel_n2h_txdma2_load_ipsec_int_phv_drop:
+    phvwri p.p4_intr_global_drop, 1
+    phvwri p.app_header_table0_valid, 0
+    phvwri p.app_header_table1_valid, 0
+    phvwri p.app_header_table2_valid, 0
+    phvwri p.app_header_table3_valid, 0
+    nop.e
+    nop
+     
+    
