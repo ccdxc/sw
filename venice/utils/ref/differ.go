296168ee8b850bd33d7d73c3307770779a63d955
Sanjay Thyamagundalu <tsanjay@gmail.com>
2018-02-06 13:02:44 -0800

    Object Differ utility (#2754)
    * Object Differ utility
    
    * Address review comments

---
+++ venice/utils/ref/differ.go
@@ -0,0 +1,353 @@
+// Deep differ using reflection
+// Inspired by DeepEqual from reflect package
+
+package ref
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+	"unsafe"
+)
+
+const (
+	// subscriptsRegex is used for regular expression for finding subsscripts
+	subscriptsRegex = "([a-zA-Z0-9_\\-]*)*\\[([a-zA-Z0-9_\\-]*)*\\]"
+	// defaultMaxDepth is used as max depth when no max depth is specified by user.
+	defaultMaxDepth = 32
+)
+
+// visit keeps track of checks that are
+// in progress. The comparison algorithm assumes that all
+// checks in progress are true when it reencounters them.
+// Visited comparisons are stored in a map indexed by visit.
+type visit struct {
+	a1  unsafe.Pointer
+	a2  unsafe.Pointer
+	typ reflect.Type
+}
+
+// options for the ObjDiffer
+type options struct {
+	// maxDepth specifies how deep to check. default Depth is 32. If depth is specified
+	//  the ObjDiff only finds diffs to the depth specified.
+	maxDepth int
+}
+
+// Elem is each node of the diff.
+type Elem struct {
+	Field  string
+	Parent string
+	Elems  map[string]Elem
+}
+
+// makeFieldName generates the field name given the parent and the field
+func makeFieldName(parent, delimiter, field string) string {
+	str := ""
+	if parent == "<root>" || parent == "." {
+		str = fmt.Sprintf("%s", field)
+	} else if field == "<root>" {
+		str = fmt.Sprintf(".")
+	} else {
+		str = fmt.Sprintf("%s%s%s", parent, delimiter, field)
+	}
+	return str
+}
+
+func (e *Elem) listDiff(l []string) []string {
+	str := makeFieldName(e.Parent, ".", e.Field)
+	if len(e.Field) != 0 {
+		// When using a subscripted field like a slice or a map, do not delimit with a "."
+		//  Map[Key] rather than Map.[Key]
+		if string(e.Field[0]) == "[" {
+			str = makeFieldName(e.Parent, "", e.Field)
+		}
+	}
+	l = append(l, str)
+
+	for _, v := range e.Elems {
+		l = v.listDiff(l)
+	}
+	return l
+}
+
+// Option is used to pass options to ObjDiff
+type Option func(*options)
+
+// WithMaxDepth option restricts the differ to a max depth. The differ bails
+//  after the first change is found. Results only have diffs upto that depth.
+func WithMaxDepth(depth int) Option {
+	return func(i *options) {
+		i.maxDepth = depth
+	}
+}
+
+// Diffs is the result of the Differ
+type Diffs struct {
+	Root Elem
+	re   *regexp.Regexp
+}
+
+// List lists all paths where a diff was found as a slice of strings
+func (d *Diffs) List() []string {
+	var diffs []string
+	return d.Root.listDiff(diffs)
+}
+
+// Lookup examines if there is a diff at the path given or below. Returns true if there is a diff
+func (d *Diffs) Lookup(path string) bool {
+	c := strings.Split(path, ".")
+	e := d.Root
+	ok := true
+	for i := range c {
+		if params := d.re.FindStringSubmatch(c[i]); params != nil {
+			if len(params) != 3 {
+				return false
+			}
+			params[2] = "[" + params[2] + "]"
+			for _, v := range params[1:] {
+				if e, ok = e.Elems[v]; !ok {
+					return false
+				}
+			}
+		} else {
+			if e, ok = e.Elems[c[i]]; !ok {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// deepDiff recursively walks the objects v1 and v2 to find diffs in the objects. The diffs are accumulated in cur
+//  return true if the objects are the same.
+func deepDiff(v1, v2 reflect.Value, visited map[visit]bool, opts *options, depth int, cur Elem, name, parent string) bool {
+	equal := true
+	ok := true
+	if !v1.IsValid() || !v2.IsValid() {
+		// If one of them is valid and not the other, no need for further checking.
+		if v1.IsValid() != v2.IsValid() {
+			cur.Elems[name] = Elem{Elems: make(map[string]Elem), Field: name, Parent: parent}
+			return false
+		}
+		return true
+	}
+	if v1.Type() != v2.Type() {
+		cur.Elems[name] = Elem{Elems: make(map[string]Elem), Field: name, Parent: parent}
+		return false
+	}
+
+	// We want to avoid putting more in the visited map than we need to.
+	// For any possible reference cycle that might be encountered,
+	// hard(t) needs to return true for at least one of the types in the cycle.
+	hard := func(k reflect.Kind) bool {
+		switch k {
+		case reflect.Map, reflect.Slice, reflect.Ptr, reflect.Interface:
+			return true
+		}
+		return false
+	}
+
+	if v1.CanAddr() && v2.CanAddr() && hard(v1.Kind()) {
+		addr1 := unsafe.Pointer(v1.UnsafeAddr())
+		addr2 := unsafe.Pointer(v2.UnsafeAddr())
+		if uintptr(addr1) > uintptr(addr2) {
+			// Canonicalize order to reduce number of entries in visited.
+			// Assumes non-moving garbage collector.
+			addr1, addr2 = addr2, addr1
+		}
+
+		// Short circuit if references are already seen.
+		typ := v1.Type()
+		v := visit{addr1, addr2, typ}
+		if visited[v] {
+			return true
+		}
+
+		// Remember for later.
+		visited[v] = true
+	}
+	delimiter := "."
+	if strings.HasPrefix(name, "[") {
+		delimiter = ""
+	}
+	elem := Elem{Elems: make(map[string]Elem), Field: name, Parent: parent}
+	switch v1.Kind() {
+	case reflect.Array:
+		same := true
+		p := makeFieldName(parent, ".", name)
+		for i := 0; i < v1.Len(); i++ {
+			ok = deepDiff(v1.Index(i), v2.Index(i), visited, opts, depth+1, elem, fmt.Sprintf("[%d]", i), p)
+			if !ok {
+				if depth > opts.maxDepth {
+					return false
+				}
+				same = false
+			}
+		}
+		equal = same
+		if !equal {
+			cur.Elems[name] = elem
+			return false
+		}
+		return true
+	case reflect.Slice:
+		if v1.IsNil() != v2.IsNil() {
+			if depth <= opts.maxDepth {
+				cur.Elems[name] = elem
+			}
+			return false
+		}
+		if v1.Len() != v2.Len() {
+			if depth <= opts.maxDepth {
+				cur.Elems[name] = elem
+			}
+			return false
+		}
+		if v1.Pointer() == v2.Pointer() {
+			return true
+		}
+		same := true
+		p := makeFieldName(parent, delimiter, name)
+		for i := 0; i < v1.Len(); i++ {
+			ok = deepDiff(v1.Index(i), v2.Index(i), visited, opts, depth+1, elem, fmt.Sprintf("[%d]", i), p)
+			if !ok {
+				if depth > opts.maxDepth {
+					return false
+				}
+				same = false
+			}
+		}
+		equal = same
+		if !equal {
+			cur.Elems[name] = elem
+			return false
+		}
+		return true
+	case reflect.Interface:
+		if v1.IsNil() || v2.IsNil() {
+			if v1.IsNil() == v2.IsNil() {
+				return true
+			}
+			if depth <= opts.maxDepth {
+				cur.Elems[name] = elem
+			}
+			return false
+		}
+		if ok := deepDiff(v1.Elem(), v2.Elem(), visited, opts, depth+1, cur, name, parent); !ok {
+			return false
+		}
+		return true
+	case reflect.Ptr:
+		if v1.Pointer() == v2.Pointer() {
+			return true
+		}
+		if ok = deepDiff(v1.Elem(), v2.Elem(), visited, opts, depth, cur, name, parent); !ok {
+			return false
+		}
+		return true
+	case reflect.Struct:
+		same := true
+		p := makeFieldName(parent, delimiter, name)
+		for i, n := 0, v1.NumField(); i < n; i++ {
+			if ok = deepDiff(v1.Field(i), v2.Field(i), visited, opts, depth+1, elem, fmt.Sprintf("%s", v1.Type().Field(i).Name), p); !ok {
+				if depth > opts.maxDepth {
+					return false
+				}
+				same = false
+			}
+		}
+		if !same {
+			cur.Elems[name] = elem
+			return false
+		}
+		return true
+	case reflect.Map:
+		if v1.IsNil() != v2.IsNil() {
+			if depth <= opts.maxDepth {
+				cur.Elems[name] = elem
+			}
+			return false
+		}
+		if v1.Len() != v2.Len() {
+			if depth <= opts.maxDepth {
+				cur.Elems[name] = elem
+			}
+			return false
+		}
+		if v1.Pointer() == v2.Pointer() {
+			return true
+		}
+		same := true
+		p := makeFieldName(parent, delimiter, name)
+		for _, k := range v1.MapKeys() {
+			val1 := v1.MapIndex(k)
+			val2 := v2.MapIndex(k)
+			if !val1.IsValid() || !val2.IsValid() {
+				if depth <= opts.maxDepth {
+					elem.Elems[fmt.Sprintf("%v", k)] = Elem{Elems: make(map[string]Elem), Field: fmt.Sprintf("[%v]", k), Parent: p}
+					same = false
+				} else {
+					return false
+				}
+			}
+			if ok = deepDiff(v1.MapIndex(k), v2.MapIndex(k), visited, opts, depth+1, elem, fmt.Sprintf("[%v]", k), p); !ok {
+				if depth > opts.maxDepth {
+					return false
+				}
+				same = false
+			}
+		}
+		if !same {
+			cur.Elems[name] = elem
+			return false
+		}
+		return true
+	case reflect.Func:
+		if v1.IsNil() && v2.IsNil() {
+			return true
+		}
+		if depth <= opts.maxDepth {
+			cur.Elems[name] = elem
+		}
+		// Can't do better than this:
+		return false
+	default:
+		// Normal equality suffices
+		if v1.CanInterface() && v2.CanInterface() {
+			if v1.Interface() == v2.Interface() {
+				return true
+			}
+			if depth <= opts.maxDepth {
+				cur.Elems[name] = elem
+			}
+			return false
+		}
+		return true
+	}
+}
+
+// ObjDiff returns a Diffs object with diffs and a bool that is set to true if there is a diff.
+//  The returned Diff is typically used through accessor functions Lookup() and List()
+func ObjDiff(x, y interface{}, opts ...Option) (Diffs, bool) {
+	if x == nil || y == nil {
+		return Diffs{}, !(x == nil && y == nil)
+	}
+	o := options{
+		maxDepth: defaultMaxDepth,
+	}
+	for _, fn := range opts {
+		fn(&o)
+	}
+	v1 := reflect.ValueOf(x)
+	v2 := reflect.ValueOf(y)
+	if v1.Type() != v2.Type() {
+		return Diffs{}, true
+	}
+	root := Elem{Elems: make(map[string]Elem), Field: "<root>", Parent: ""}
+	if deepDiff(v1, v2, make(map[visit]bool), &o, 0, root, "<root>", "") {
+		return Diffs{}, false
+	}
+	return Diffs{Root: root.Elems["<root>"], re: regexp.MustCompile(subscriptsRegex)}, true
+}
---
+++ venice/utils/ref/differ_test.go
@@ -0,0 +1,517 @@
+package ref
+
+import (
+	"testing"
+
+	mapset "github.com/deckarep/golang-set"
+)
+
+type nestedStruct struct {
+	String string
+}
+
+type simplestruct struct {
+	Int    int
+	String string
+	Struct nestedStruct
+}
+
+type simpleInterface interface {
+	DummyFunc1()
+}
+
+type intImpl1 struct{}
+
+func (i *intImpl1) DummyFunc1() {
+	return
+}
+
+type intImpl2 struct{}
+
+func (i *intImpl2) DummyFunc1() {
+	return
+}
+
+type strEnum string
+
+const (
+	One   strEnum = "one"
+	Two   strEnum = "two"
+	Three strEnum = "three"
+)
+
+type intEnum int
+
+const (
+	OneInt   intEnum = iota
+	TwoInt   intEnum = iota
+	ThreeInt intEnum = iota
+)
+
+type strct struct {
+	Int       int
+	intg      int
+	String    string
+	IntP      *int
+	StringP   *string
+	Struct    simplestruct
+	StructP   *simplestruct
+	Map       map[int]string
+	MapS      map[string]simplestruct
+	SliceI    []int
+	SliceS    []string
+	SliceSt   []simplestruct
+	Array     [10]simplestruct
+	Interface simpleInterface
+	StrEnum   strEnum
+	IntEnum   intEnum
+}
+
+func (s *strct) Init() *strct {
+	s.Int = 111
+	s.intg = 222
+	s.String = "TestVal1"
+	t := 333
+	s.IntP = &t
+	str := "TestString"
+	s.StringP = &str
+	s.Struct = simplestruct{Int: 321, String: "StructEmbedded"}
+	s.StructP = &simplestruct{Int: 322, String: "StructPointer"}
+	s.Map = make(map[int]string)
+	s.MapS = make(map[string]simplestruct)
+	s.Map[1] = "Key1Value"
+	s.Map[2] = "Key2Value"
+	sstrc1 := simplestruct{Int: 444, String: "Strv1"}
+	sstrc2 := simplestruct{Int: 444, String: "Strv1"}
+	s.MapS["skey1"] = sstrc1
+	s.MapS["skey2"] = sstrc2
+	s.SliceI = nil
+	s.SliceI = append(s.SliceI, 10, 11, 12)
+	s.SliceS = nil
+	s.SliceS = append(s.SliceS, "StringExa1", "StringExa3", "StringExa2")
+	s.Array = [10]simplestruct{
+		{Int: 1, String: "One", Struct: nestedStruct{String: "nested"}},
+		{Int: 2, String: "two", Struct: nestedStruct{String: "nested"}},
+		{Int: 3, String: "three", Struct: nestedStruct{String: "nested"}},
+		{Int: 4, String: "four", Struct: nestedStruct{String: "nested"}},
+		{Int: 5, String: "five", Struct: nestedStruct{String: "nested"}},
+		{Int: 6, String: "six", Struct: nestedStruct{String: "nested"}},
+		{Int: 7, String: "seven", Struct: nestedStruct{String: "nested"}},
+		{Int: 8, String: "eight", Struct: nestedStruct{String: "nested"}},
+		{Int: 9, String: "nine", Struct: nestedStruct{String: "nested"}},
+		{Int: 10, String: "ten", Struct: nestedStruct{String: "nested"}},
+	}
+	s.Interface = &intImpl1{}
+	s.StrEnum = One
+	s.IntEnum = OneInt
+	return s
+}
+
+func TestDiffer2(t *testing.T) {
+	var t1, t2 strct
+	cases := []struct {
+		name    string
+		fn      func(s *strct)
+		changed bool
+		exp     []string
+		lookup  []string
+		depth   int
+	}{
+		{
+			"No Changes",
+			func(s *strct) {},
+			false,
+			nil,
+			nil,
+			0,
+		},
+		{
+			"No Changes",
+			func(s *strct) {},
+			false,
+			nil,
+			nil,
+			4,
+		},
+		{
+			"Integer change",
+			func(s *strct) { s.Int = 12222 },
+			true,
+			[]string{".", "Int"},
+			[]string{"Int"},
+			0,
+		},
+		{
+			"String change",
+			func(s *strct) { s.String = "changed" },
+			true,
+			[]string{".", "String"},
+			[]string{"String"},
+			0,
+		},
+		{
+			"String change",
+			func(s *strct) { s.String = "changed" },
+			true,
+			[]string{".", "String"},
+			[]string{"String"},
+			1,
+		},
+		{
+			"unexported member",
+			func(s *strct) { s.intg = 2222222 },
+			false,
+			nil,
+			nil,
+			0,
+		},
+		{
+			"Integer Pointer same value",
+			func(s *strct) {
+				newint := 333
+				s.IntP = &newint
+			},
+			false,
+			nil,
+			nil,
+			0,
+		},
+		{
+			"Integer Pointer changed value",
+			func(s *strct) {
+				newint := 1313131
+				s.IntP = &newint
+			},
+			true,
+			[]string{".", "IntP"},
+			[]string{"IntP"},
+			0,
+		},
+		{
+			"String Pointer same value",
+			func(s *strct) {
+				newstr := "TestString"
+				s.StringP = &newstr
+			},
+			false,
+			nil,
+			nil,
+			0,
+		},
+		{
+			"String Pointer changed value",
+			func(s *strct) {
+				newstr := "Changed String"
+				s.StringP = &newstr
+			},
+			true,
+			[]string{".", "StringP"},
+			[]string{"StringP"},
+			0,
+		},
+		{
+			"Struct changed",
+			func(s *strct) {
+				s.Struct.Int = 3321
+				s.Struct.Struct.String = "nestednow"
+			},
+			true,
+			[]string{".", "Struct", "Struct.Int", "Struct.Struct", "Struct.Struct.String"},
+			[]string{"Struct", "Struct.Int", "Struct.Struct", "Struct.Struct.String"},
+			0,
+		},
+		{
+			"Struct Pointer changed",
+			func(s *strct) { s.StructP.Int = 3321 },
+			true,
+			[]string{".", "StructP", "StructP.Int"},
+			[]string{"StructP", "StructP.Int"},
+			0,
+		},
+		{
+			"Map changed value",
+			func(s *strct) { s.Map[2] = "ChangedValue" },
+			true,
+			[]string{".", "Map", "Map[2]"},
+			[]string{"Map", "Map[2]"},
+			0,
+		},
+		{
+			"Map changed value",
+			func(s *strct) { s.Map[2] = "ChangedValue" },
+			true,
+			[]string{".", "Map"},
+			[]string{"Map"},
+			1,
+		},
+		{
+			"Map New Key",
+			func(s *strct) { s.Map[3] = "NewValue" },
+			true,
+			[]string{".", "Map"},
+			[]string{"Map"},
+			0,
+		},
+		{
+			"Map[]Structure Changed Value",
+			func(s *strct) { s.MapS["skey1"] = simplestruct{Int: 555, String: "Strv1"} },
+			true,
+			[]string{".", "MapS[skey1].Int", "MapS", "MapS[skey1]"},
+			[]string{"MapS[skey1].Int", "MapS", "MapS[skey1]"},
+			0,
+		},
+		{
+			"Map[]Structure Changed Value(depth 3)",
+			func(s *strct) { s.MapS["skey1"] = simplestruct{Int: 555, String: "Strv1"} },
+			true,
+			[]string{".", "MapS[skey1].Int", "MapS", "MapS[skey1]"},
+			[]string{"MapS[skey1].Int", "MapS", "MapS[skey1]"},
+			3,
+		},
+		{
+			"Map[]Structure Changed Value(depth 2)",
+			func(s *strct) { s.MapS["skey1"] = simplestruct{Int: 555, String: "Strv1"} },
+			true,
+			[]string{".", "MapS", "MapS[skey1]"},
+			[]string{"MapS", "MapS[skey1]"},
+			2,
+		},
+		{
+			"Map[]Structure new Key",
+			func(s *strct) { s.MapS["skey3"] = simplestruct{Int: 555, String: "Strv1"} },
+			true,
+			[]string{".", "MapS"},
+			[]string{"MapS"},
+			0,
+		},
+		{
+			"Integer Slice changed value",
+			func(s *strct) { s.SliceI[0] = 13 },
+			true,
+			[]string{".", "SliceI", "SliceI[0]"},
+			[]string{"SliceI", "SliceI[0]"},
+			0,
+		},
+		{
+			"Integer Slice changed size",
+			func(s *strct) { s.SliceI = append(s.SliceI, 13) },
+			true,
+			[]string{".", "SliceI"},
+			[]string{"SliceI"},
+			0,
+		},
+		{
+			"Integer Slice nil value",
+			func(s *strct) { s.SliceI = nil },
+			true,
+			[]string{".", "SliceI"},
+			[]string{"SliceI"},
+			0,
+		},
+		{
+			"String Slice changed value",
+			func(s *strct) { s.SliceS[1] = "changed value" },
+			true,
+			[]string{".", "SliceS", "SliceS[1]"},
+			[]string{"SliceS", "SliceS[1]"},
+			0,
+		},
+		{
+			"String Slice changed size",
+			func(s *strct) { s.SliceS = append(s.SliceS, "new value") },
+			true,
+			[]string{".", "SliceS"},
+			[]string{"SliceS"},
+			0,
+		},
+		{
+			"Array changed value",
+			func(s *strct) {
+				s.Array[8] = simplestruct{Int: 18, String: "eighteen", Struct: nestedStruct{String: "nested"}}
+			},
+			true,
+			[]string{".", "Array[8].Int", "Array[8].String", "Array[8]", "Array"},
+			[]string{"Array[8].Int", "Array[8].String", "Array[8]", "Array"},
+			0,
+		},
+		{
+			"Array changed value(depth 4)",
+			func(s *strct) {
+				s.Array[8] = simplestruct{Int: 18, String: "eighteen", Struct: nestedStruct{String: "nested"}}
+			},
+			true,
+			[]string{".", "Array[8].Int", "Array[8].String", "Array[8]", "Array"},
+			[]string{"Array[8].Int", "Array[8].String", "Array[8]", "Array"},
+			4,
+		},
+		{
+			"Array changed value(depth 2)",
+			func(s *strct) {
+				s.Array[8] = simplestruct{Int: 18, String: "eighteen", Struct: nestedStruct{String: "nested"}}
+			},
+			true,
+			[]string{".", "Array[8]", "Array"},
+			[]string{"Array[8]", "Array"},
+			2,
+		},
+		{
+			"Changed interface",
+			func(s *strct) { s.Interface = &intImpl2{} },
+			true,
+			[]string{".", "Interface"},
+			[]string{"Interface"},
+			0,
+		},
+		{
+			"Nil interface",
+			func(s *strct) { s.Interface = nil },
+			true,
+			[]string{".", "Interface"},
+			[]string{"Interface"},
+			0,
+		},
+		{
+			"String Enum",
+			func(s *strct) { s.StrEnum = Two },
+			true,
+			[]string{".", "StrEnum"},
+			[]string{"StrEnum"},
+			0,
+		},
+		{
+			"Int Enum",
+			func(s *strct) { s.IntEnum = TwoInt },
+			true,
+			[]string{".", "IntEnum"},
+			[]string{"IntEnum"},
+			0,
+		},
+		{
+			"Multiple changes",
+			func(s *strct) {
+				s.Array[8] = simplestruct{18, "eighteen", nestedStruct{"nested"}}
+				s.SliceS[1] = "changed value"
+				s.SliceS = append(s.SliceS, "new value")
+				s.Map[2] = "ChangedValue"
+			},
+			true,
+			[]string{".", "Array[8].Int", "Array[8].String", "Array[8]", "Array", "SliceS", "Map", "Map[2]"},
+			[]string{"Array[8].Int", "Array[8].String", "Array[8]", "Array", "SliceS", "Map", "Map[2]"},
+			0,
+		},
+		{
+			"Multiple changes(depth 2)",
+			func(s *strct) {
+				s.Array[8] = simplestruct{18, "eighteen", nestedStruct{"nested"}}
+				s.SliceS[1] = "changed value"
+				s.SliceS = append(s.SliceS, "new value")
+				s.Map[2] = "ChangedValue"
+			},
+			true,
+			[]string{".", "Array", "Array[8]", "SliceS", "Map", "Map[2]"},
+			[]string{"Array", "SliceS", "Map"},
+			2,
+		},
+	}
+	for _, c := range cases {
+		t1.Init()
+		t2.Init()
+		c.fn(&t1)
+		var opts []Option
+		if c.depth != 0 {
+			opts = append(opts, WithMaxDepth(c.depth))
+		}
+		diff, ok := ObjDiff(t1, t2, opts...)
+		list := diff.List()
+		if c.exp != nil && !ok {
+			t.Fatalf("--%s  : expecting no diff buf found [%v]", c.name, list)
+		}
+		if ok != c.changed {
+			t.Fatalf("--%s  : changed - expecting [%v] got [%v]", c.name, c.changed, ok)
+		}
+		if ok && len(list) != len(c.exp) {
+			t.Fatalf("-%s : expecting [%v] diff got [%v]", c.name, c.exp, list)
+		}
+		if ok && len(list) > 0 {
+			var e, a []interface{}
+			for i := range c.exp {
+				e = append(e, c.exp[i])
+			}
+			for i := range list {
+				a = append(a, list[i])
+			}
+			if !mapset.NewSetFromSlice(e).Equal(mapset.NewSetFromSlice(a)) {
+				t.Fatalf("-%s : expecting [%v] diff got [%v]", c.name, c.exp, list)
+			}
+			for i := range c.lookup {
+				if ok := diff.Lookup(c.lookup[i]); !ok {
+					t.Errorf("-%s : Expecting lookup to pass for [%s]", c.name, c.lookup[i])
+				}
+			}
+		}
+	}
+}
+
+func TestDifferentTypes(t *testing.T) {
+	var t1 strct
+	t1.Init()
+	t2 := struct {
+		dummy string
+	}{}
+	diff, ok := ObjDiff(t1, t2)
+	if !ok {
+		t.Fatalf("did not find diff for different types")
+	}
+	got := diff.List()
+	if len(got) != 1 {
+		t.Fatalf("expecting {.}, got %v", got)
+	}
+	if got[0] != "." {
+		t.Fatalf("expecting {.}, got %v", got)
+	}
+}
+
+func TestUninitialized(t *testing.T) {
+	var t1, t2 strct
+	t1.Init()
+	_, ok := ObjDiff(t1, t2)
+	if !ok {
+		t.Fatalf("did not find diff")
+	}
+	var t3 strct
+	_, ok = ObjDiff(t2, t3)
+	if ok {
+		t.Fatalf("found change in uninitialized")
+	}
+}
+
+func BenchmarkDiffer(b *testing.B) {
+	var t1, t2 strct
+	t1.Init()
+	t2.Init()
+	for i := 0; i < b.N; i++ {
+		ObjDiff(t1, t2)
+	}
+}
+
+func BenchmarkDifferWithChange(b *testing.B) {
+	var t1, t2 strct
+	t1.Init()
+	t2.Init()
+	t1.Struct.Int = 3321
+	t1.Struct.Struct.String = "nestednow"
+	for i := 0; i < b.N; i++ {
+		ObjDiff(t1, t2)
+	}
+}
+
+func BenchmarkDifferWithMaxDepth(b *testing.B) {
+	var t1, t2 strct
+	t1.Init()
+	t2.Init()
+	t1.Struct.Int = 3321
+	t1.Struct.Struct.String = "nestednow"
+	for i := 0; i < b.N; i++ {
+		ObjDiff(t1, t2, WithMaxDepth(1))
+	}
+}
