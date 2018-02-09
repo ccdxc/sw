26862a547b553dcc337c68241f7008c3128fae83
Sumit Gupta <22799207+sumit-g@users.noreply.github.com>
2018-02-09 15:33:41 -0800

    Disabled compression tests for model upgrade. (#3062)

--- dol/test/storage/main.cc
+++ dol/test/storage/main.cc
@@ -175,10 +175,10 @@ int main(int argc, char**argv) {
       run_nvme_tests = true;
       run_nvme_be_tests = true;
       run_local_e2e_tests = true;
-      run_comp_tests = true;
+      run_comp_tests = false;
       run_xts_tests = true;
       run_rdma_tests = true;
-      run_comp_perf_tests = true;
+      run_comp_perf_tests = false;
       run_pdma_tests = true;
   } else if (FLAGS_test_group == "unit") {
       run_unit_tests = true;
