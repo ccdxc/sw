1aef3f36564aeefd6420509d5eaf47bc8c198080
Vikas B Dharwadkar <vikasbd@users.noreply.github.com>
2018-02-12 22:48:36 -0800

    Update asic ptr ee9fe26 (#3093)

--- nic/asm/gft/rx_vport_stats.asm
+++ nic/asm/gft/rx_vport_stats.asm
@@ -13,6 +13,7 @@ rx_vport_stats:
                            capri_intrinsic_lif_sbit3_ebit10}, EXCEPTION_VPORT
     phvwr.c1        p.capri_txdma_intrinsic_valid, TRUE
     phvwr.c1        p.p4plus_to_p4_valid, TRUE
+    phvwr.c1        p.capri_intrinsic_tm_oq, 0
     phvwrpair.c1    p.capri_intrinsic_tm_oport, TM_PORT_EGRESS, \
                         p.capri_intrinsic_drop, FALSE
 
