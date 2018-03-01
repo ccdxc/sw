0bd6293dea56621d5c5a3d4c3cedf5bb7443d6e9
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2018-02-28 17:09:11 -0800

    fix for arm build issues (#3333)

--- nic/make/build.mk
+++ nic/make/build.mk
@@ -2,7 +2,7 @@
 WARN = -Wall -Werror -Wno-strict-aliasing -Wno-error=strict-aliasing -Wno-write-strings -Wno-deprecated
 
 CPPFLAGS := $(WARN) \
-          -g -D__USE_GNU -D_GNU_SOURCE -std=c++11 -fPIC
+          -g -D__USE_GNU -D_GNU_SOURCE -std=c++11
 
 CFLAGS := $(WARN) \
           -g -D__USE_GNU -D_GNU_SOURCE
