730b840d7ea593e314603b3649bf862a76536499
Ramesh Ponnapalli <rameshp@pensando.io>
2018-03-14 19:16:34 -0400

    minor change

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/rxdma/esp_ipv4_tunnel_h2n_ipsec_encap_rxdma_initial_table2.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/rxdma/esp_ipv4_tunnel_h2n_ipsec_encap_rxdma_initial_table2.s
@@ -15,19 +15,19 @@ struct phv_ p;
 esp_ipv4_tunnel_h2n_ipsec_encap_rxdma_initial_table2:
     phvwri p.{app_header_table0_valid...app_header_table3_valid}, 15
     phvwri p.common_te0_phv_table_pc, esp_ipv4_tunnel_h2n_allocate_input_desc_semaphore[33:6] 
-    phvwri p.{common_te0_phv_table_lock_en...common_te0_phv_table_raw_table_size}, 11 
+    phvwri p.{common_te0_phv_table_lock_en...common_te0_phv_table_raw_table_size}, 3 
     phvwri p.common_te0_phv_table_addr, INDESC_SEMAPHORE_ADDR
 
     phvwri p.common_te1_phv_table_pc, esp_ipv4_tunnel_h2n_allocate_output_desc_semaphore[33:6] 
-    phvwri p.{common_te1_phv_table_lock_en...common_te1_phv_table_raw_table_size}, 11 
+    phvwri p.{common_te1_phv_table_lock_en...common_te1_phv_table_raw_table_size}, 3 
     phvwri p.common_te1_phv_table_addr, OUTDESC_SEMAPHORE_ADDR
   
     phvwri p.common_te2_phv_table_pc, esp_ipv4_tunnel_h2n_allocate_input_page_semaphore[33:6] 
-    phvwri p.{common_te2_phv_table_lock_en...common_te2_phv_table_raw_table_size}, 11 
+    phvwri p.{common_te2_phv_table_lock_en...common_te2_phv_table_raw_table_size}, 3 
     phvwri p.common_te2_phv_table_addr, INPAGE_SEMAPHORE_ADDR
 
     phvwri p.common_te3_phv_table_pc, esp_ipv4_tunnel_h2n_allocate_output_page_semaphore[33:6] 
-    phvwri p.{common_te3_phv_table_lock_en...common_te3_phv_table_raw_table_size}, 11 
+    phvwri p.{common_te3_phv_table_lock_en...common_te3_phv_table_raw_table_size}, 3 
     phvwri.f p.common_te3_phv_table_addr, OUTPAGE_SEMAPHORE_ADDR
     nop.e
     nop
