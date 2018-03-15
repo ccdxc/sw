c60ab13832461aef1da6a7455f877ce32183d695
Vikas B Dharwadkar <vikasbd@users.noreply.github.com>
2018-03-14 19:10:05 -0700

    skip asset pull if version is same. (#3618)

--- scripts/pull-assets.sh
+++ scripts/pull-assets.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if cmp minio/VERSIONS minio/.VERSIONS.orig
+then
+    echo "minio/VERSIONS same as current. Skipping pull-assets."
+    exit 0
+fi
+
 echo "pulling assets"
 
 for name in $(find minio -name '*.txt' | xargs basename -s .txt)
@@ -9,3 +15,4 @@ do
   version=$(grep "${name}" minio/VERSIONS | awk '{ print $2 }')
   asset-pull ${name} ${version} /dev/stdout | tar xvz
 done
+cp minio/VERSIONS minio/.VERSIONS.orig
