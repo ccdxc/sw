66a987212fc366c8fff202b1d08d804f08641f07
Ramesh Ponnapalli <rameshp@pensando.io>
2018-03-16 01:24:01 -0400

    n2h opt3

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_network_to_host/txdma1/esp_ipv4_tunnel_n2h_txdma_initial_table.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_network_to_host/txdma1/esp_ipv4_tunnel_n2h_txdma_initial_table.s
@@ -17,20 +17,18 @@ esp_ipv4_tunnel_n2h_txdma1_initial_table:
     b.c1 esp_ipv4_tunnel_n2h_txdma1_initial_table_do_nothing
     phvwri.c1 p.p4_intr_global_drop, 1
 
-    phvwr p.p4_intr_global_lif, k.{p4_intr_global_lif_sbit0_ebit2...p4_intr_global_lif_sbit3_ebit10}
-    phvwr p.p4_intr_global_tm_iq, k.p4_intr_global_tm_iq
-    phvwr p.p4_txdma_intr_qtype, k.p4_txdma_intr_qtype
-    phvwr p.p4_txdma_intr_qid, k.p4_txdma_intr_qid
-    //phvwr p.p4_txdma_intr_qstate_addr, k.{p4_txdma_intr_qstate_addr_sbit0_ebit1...p4_txdma_intr_qstate_addr_sbit2_ebit33}
+    //phvwr p.p4_intr_global_lif, k.{p4_intr_global_lif_sbit0_ebit2...p4_intr_global_lif_sbit3_ebit10}
+    //phvwr p.p4_intr_global_tm_iq, k.p4_intr_global_tm_iq
+    //phvwr p.p4_txdma_intr_qtype, k.p4_txdma_intr_qtype
+    //phvwr p.p4_txdma_intr_qid, k.p4_txdma_intr_qid
     phvwr p.txdma1_global_ipsec_cb_addr, k.{p4_txdma_intr_qstate_addr_sbit0_ebit1...p4_txdma_intr_qstate_addr_sbit2_ebit33} 
     phvwr p.barco_req_command, d.barco_enc_cmd
     phvwr p.t0_s2s_iv_size, d.iv_size
     phvwr p.t0_s2s_icv_size, d.icv_size
 
-    phvwri p.app_header_table0_valid, 1 
-    phvwri p.common_te0_phv_table_lock_en, 1 
+    phvwri p.{app_header_table0_valid...app_header_table2_valid}, 7 
     phvwri p.common_te0_phv_table_pc, esp_v4_tunnel_n2h_get_in_desc_from_cb_cindex[33:6] 
-    phvwri p.common_te0_phv_table_raw_table_size, 3
+    phvwri p.{common_te0_phv_table_lock_en...common_te0_phv_table_raw_table_size}, 11 
     sll r2, d.{rxdma_ring_cindex}.hx, 3
     add r2, r2, d.cb_ring_base_addr
     tbladd d.{rxdma_ring_cindex}.hx, 1
@@ -41,16 +39,12 @@ esp_ipv4_tunnel_n2h_txdma1_initial_table:
     memwr.dx  r4, r3
     tbladd d.cb_cindex, 1
  
-    phvwri p.app_header_table1_valid, 1 
-    phvwri p.common_te1_phv_table_lock_en, 1 
     phvwri p.common_te1_phv_table_pc, esp_v4_tunnel_n2h_txdma1_allocate_barco_req_pindex[33:6] 
-    phvwri p.common_te1_phv_table_raw_table_size, 2
+    phvwri p.{common_te1_phv_table_lock_en...common_te1_phv_table_raw_table_size}, 10 
     phvwri p.common_te1_phv_table_addr, CAPRI_BARCO_MD_HENS_REG_GCM0_PRODUCER_IDX 
 
-    phvwri p.app_header_table2_valid, 1 
-    phvwri p.common_te2_phv_table_lock_en, 1 
     phvwri p.common_te2_phv_table_pc, esp_v4_tunnel_n2h_load_part2[33:6] 
-    phvwri p.common_te2_phv_table_raw_table_size, 3
+    phvwri p.{common_te2_phv_table_lock_en...common_te2_phv_table_raw_table_size}, 11 
     add r4, k.{p4_txdma_intr_qstate_addr_sbit0_ebit1...p4_txdma_intr_qstate_addr_sbit2_ebit33}, 64
     phvwr p.common_te2_phv_table_addr, r4 
 
