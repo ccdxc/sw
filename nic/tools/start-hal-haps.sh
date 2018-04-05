d7ddacab142a7c5531aa66f19d7982a86a04297f
akoradha <32655671+akoradha@users.noreply.github.com>
2018-04-04 20:57:06 -0700

    haps changes (#3914)

--- nic/tools/start-hal-haps.sh
+++ nic/tools/start-hal-haps.sh
@@ -1,4 +1,5 @@
 export HAL_CONFIG_PATH=/nic/conf/
 export LD_LIBRARY_PATH=/nic/lib:/usr/local/lib:/usr/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
+export HAL_PBC_INIT_CONFIG="2x100_hbm"
 ulimit -c unlimited
 $GDB /nic/bin/hal -c hal_haps.json > hal.log 2>&1
