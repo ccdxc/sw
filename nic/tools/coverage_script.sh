fd79e0e53ae1fab12f2d60811aa2b7c23b88621c
sudhiaithal <sudhiaithal@gmail.com>
2018-04-11 10:31:01 -0700

    Ignore errors in coverage run (#4013)

--- nic/tools/coverage_script.sh
+++ nic/tools/coverage_script.sh
@@ -4,7 +4,8 @@ mount -o bind /asictools $ASIC_TOOLS_DIR
 
 make pull-assets
 #Skipping asm instruction stats as GFT will not generate instruction data.
-tools/run-coverage --run-time 120 -s gft/gtest -s gft -s gft_rdma -c hal_coverage.json --skip-asm-ins-stats
+#Ignore job run errors for now.
+tools/run-coverage --run-time 120 -s gft/gtest -s gft -s gft_rdma -c hal_coverage.json --skip-asm-ins-stats -i
 run_status=$?
 if [ $run_status -ne 0 ];then
     echo "Coverage run failed...."
