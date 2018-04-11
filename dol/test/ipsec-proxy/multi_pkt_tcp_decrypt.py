084ddbac4800cae2765255b191c6b08e6d550021
Ramesh Ponnapalli <rameshp@pensando.io>
2018-04-11 11:05:11 -0400

    decrypt multipacket

--- dol/test/ipsec-proxy/multi_pkt_tcp_decrypt.py
+++ dol/test/ipsec-proxy/multi_pkt_tcp_decrypt.py
@@ -62,6 +62,7 @@ def TestCaseSetup(tc):
     ipseccb.sip6.v6_addr              = sip6 
     ipseccb.dip6.ip_af                = 2
     ipseccb.dip6.v6_addr              = dip6 
+    ipseccb.vrf_vlan                  = 0x0006
     ipseccb.SetObjValPd()
     # 2. Clone objects that are needed for verification
     rnmdr = copy.deepcopy(tc.infra_data.ConfigStore.objects.db["RNMDR"])
@@ -98,110 +99,6 @@ def TestCaseSetup(tc):
 def TestCaseVerify(tc):
     global ipseccbq
     global ipseccb
-
-    # 1. Verify pi/ci got update got updated
-    ipseccb_cur = tc.infra_data.ConfigStore.objects.db["IPSECCB0000"]
-    print("pre-sync: ipseccb_cur.pi %d ipseccb_cur.ci %d" % (ipseccb_cur.pi, ipseccb_cur.ci))
-    ipseccb_cur.GetObjValPd()
-    print("post-sync: ipseccb_cur.pi %d ipseccb_cur.ci %d" % (ipseccb_cur.pi, ipseccb_cur.ci))
-    if (ipseccb_cur.pi != ipseccb.pi or ipseccb_cur.ci != ipseccb.ci):
-        print("serq pi/ci not as expected")
-        return False
-    # 3. Fetch current values from Platform
-    ipseccbqq_cur = tc.infra_data.ConfigStore.objects.db["IPSECCB0000_IPSECCBQ"]
-    ipseccbqq_cur.Configure()
-  
-    rnmdr = tc.pvtdata.db["RNMDR"]
-    rnmpr = tc.pvtdata.db["RNMPR"]
-    tnmdr = tc.pvtdata.db["TNMDR"]
-    tnmpr = tc.pvtdata.db["TNMPR"]
-
-    rnmdr_cur = tc.infra_data.ConfigStore.objects.db["RNMDR"]
-    rnmdr_cur.GetMeta()
-    rnmdr_cur.GetRingEntries([rnmdr_cur.pi, rnmdr_cur.pi + 1])
-    rnmdr_cur.GetRingEntryAOL([rnmdr_cur.pi, rnmdr_cur.pi + 1])
-
-    rnmpr_cur = tc.infra_data.ConfigStore.objects.db["RNMPR"]
-    rnmpr_cur.GetMeta()
-    rnmpr_cur.GetRingEntries([rnmpr.pi,rnmpr_cur.pi])
-
-    tnmdr_cur = tc.infra_data.ConfigStore.objects.db["TNMDR"]
-    tnmdr_cur.GetMeta()
-    tnmdr_cur.GetRingEntries([tnmdr.pi,tnmdr_cur.pi])
-
-    tnmpr_cur = tc.infra_data.ConfigStore.objects.db["TNMPR"]
-    tnmpr_cur.GetMeta()
-    tnmpr_cur.GetRingEntries([tnmpr.pi,tnmpr_cur.pi])
-
-    # 4. Verify PI for RNMDR got incremented by 1
-    if ((rnmdr_cur.pi != rnmdr.pi+2) and (rnmdr_cur.pi != rnmdr.pi+1)):
-        print("RNMDR pi check failed old %d new %d" % (rnmdr.pi, rnmdr_cur.pi))
-        return False
-
-    # 6. Verify pi/ci got update got updated for BRQ
-    brq = tc.pvtdata.db["BRQ_ENCRYPT"]
-    brq_cur = tc.infra_data.ConfigStore.objects.db["BRQ_ENCRYPT"]
-    print("pre-sync: brq_cur.pi %d brq_cur.ci %d" % (brq_cur.pi, brq_cur.ci))
-    brq_cur.GetMeta()
-    brq_cur.GetRingEntries([brq.pi, brq_cur.pi])
-    print("post-sync: brq_cur.pi %d brq_cur.ci %d" % (brq_cur.pi, brq_cur.ci))
-    if (brq_cur.pi != (brq.pi+1) or brq_cur.ci != (brq.ci+1)):
-        print("brq pi/ci not as expected")
-        #needs fix in HAL and support in model/p4+ for this check to work/pass
-        #return False
-
-    print("BRQ:")
-    print("ilist_addr 0x%x" % brq_cur.ring_entries[0].ilist_addr)
-    print("olist_addr 0x%x" % brq_cur.ring_entries[0].olist_addr)
-    print("command 0x%x" % brq_cur.ring_entries[0].command)
-    print("key_desc_index 0x%x" % brq_cur.ring_entries[0].key_desc_index)
-    print("iv_addr 0x%x" % brq_cur.ring_entries[0].iv_addr)
-    print("status_addr 0x%x" % brq_cur.ring_entries[0].status_addr)
-    # There is an offset of 64 to go past scratch when queuing to barco. Pls modify
-    # this when this offset is removed.
-    #maxflows check should be reverted when we remove the hardcoding for idx 0 with pi/ci for BRQ
-    # 7. Verify brq input desc and rnmdr
-    if (rnmdr.ringentries[rnmdr.pi].handle != (brq_cur.ring_entries[brq.pi].ilist_addr - 0x40)):
-        print("Descriptor handle not as expected in ringentries 0x%x 0x%x" % (rnmdr.ringentries[rnmdr.pi].handle, brq_cur.ring_entries[0].ilist_addr))
-        return False
-    print("RNMDR Entry: 0x%x, BRQ ILIST: 0x%x" % (rnmdr.ringentries[rnmdr.pi].handle, brq_cur.ring_entries[0].ilist_addr))
-
-    # 5. Verify descriptor
-    if rnmdr.ringentries[rnmdr.pi].handle != ipseccbqq_cur.ringentries[ipseccb_cur.pi-1].handle:
-        print("Descriptor handle not as expected in ringentries 0x%x 0x%x" % (rnmdr.ringentries[rnmdr.pi].handle, ipseccbqq_cur.ringentries[ipseccb_cur.pi-1].handle))
-        return False
-
-    print("Descriptor handle expected in ringentries 0x%x 0x%x" % (rnmdr.ringentries[rnmdr.pi].handle, ipseccbqq_cur.ringentries[ipseccb_cur.pi-1].handle))
-    if rnmdr.swdre_list[rnmdr.pi].DescAddr != ipseccbqq_cur.swdre_list[ipseccb_cur.pi-1].DescAddr:
-        print("Descriptor handle not as expected in swdre_list 0x%x 0x%x" % (rnmdr.swdre_list[rnmdr.pi].DescAddr, ipseccbqq_cur.swdre_list[ipseccb_cur.pi-1].DescAddr))
-        return False
-    print("Descriptor handle expected in swdre_list 0x%x 0x%x" % (rnmdr.swdre_list[rnmdr.pi].DescAddr, ipseccbqq_cur.swdre_list[ipseccb_cur.pi-1].DescAddr))
-
-    # 8. Verify PI for TNMDR got incremented by 1
-    if (tnmdr_cur.pi != tnmdr.pi+1):
-        print("TNMDR pi check failed old %d new %d" % (tnmdr.pi, tnmdr_cur.pi))
-        return False
-    print("Old TNMDR PI: %d, New TNMDR PI: %d" % (tnmdr.pi, tnmdr_cur.pi))
-
-    # 9. Verify PI for TNMPR got incremented by 1
-    if (tnmpr_cur.pi != tnmpr.pi+1):
-        print("TNMPR pi check failed old %d new %d" % (tnmpr.pi, tnmpr_cur.pi))
-        return False
-    print("Old TNMPR PI: %d, New TNMPR PI: %d" % (tnmpr.pi, tnmpr_cur.pi))
-    # 10. Verify SeqNo increment
-    if (ipseccb_cur.esn_lo != seq+1):
-        print ("seq_no 0x%x 0x%x" % (ipseccb_cur.esn_lo, ipseccb.esn_lo))
-        return False
-
-    if (ipseccb_cur.iv != iv+1):
-        print ("iv : 0x%x 0x%x" % (ipseccb_cur.iv, ipseccb.iv))
-        return False
-    print("Input Page : 0x%x IV Addr: 0x%x" % (rnmpr.ringentries[rnmpr.pi].handle, brq_cur.ring_entries[brq.pi].iv_addr))
-    if (rnmpr.ringentries[rnmpr.pi].handle != brq_cur.ring_entries[brq.pi].iv_addr):
-        print("Input Page : 0x%x IV Addr: 0x%x" % (rnmpr.ringentries[rnmpr.pi].handle, brq_cur.ring_entries[brq.pi].iv_addr))
-        return False
-    if (rnmdr.ringentries[rnmdr.pi].handle != (brq_cur.ring_entries[brq.pi].status_addr - 56)):
-        return False
 
     return True
 
