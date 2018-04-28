341a86fe2aa96866513717c1bbc376f12c7665ab
Mohan Kumar <mohan@pensando.io>
2018-04-28 03:47:41 +0000

    tools: Fix 'struct should normally be const' warning in checkpatch.pl
    This commit includes the patch (as posted in the following link) to suppress
    warning on every struct struct that doesn't have 'const' specifier.
    
    	https://lkml.org/lkml/2017/1/10/434
    
    Signed-off-by: Mohan Kumar <mohan@pensando.io>

--- storage/offload/tools/checkpatch.pl
+++ storage/offload/tools/checkpatch.pl
@@ -6190,7 +6190,8 @@ sub process {
 
 # check for various structs that are normally const (ops, kgdb, device_tree)
 # and avoid what seem like struct definitions 'struct foo {'
-		if ($line !~ /\bconst\b/ &&
+		if ($const_structs ne "" &&
+		    $line !~ /\bconst\b/ &&
 		    $line =~ /\bstruct\s+($const_structs)\b(?!\s*\{)/) {
 			WARN("CONST_STRUCT",
 			     "struct $1 should normally be const\n" . $herecurr);
