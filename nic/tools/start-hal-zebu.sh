758d2adfa4343a99374385bcb4166decccf47adc
akoradha <32655671+akoradha@users.noreply.github.com>
2018-05-01 12:21:50 -0700

    fix zebu LD path (#4306)

--- nic/tools/start-hal-zebu.sh
+++ nic/tools/start-hal-zebu.sh
@@ -2,7 +2,7 @@ TOOLS_DIR=`dirname $0`
 ABS_TOOLS_DIR=`readlink -f $TOOLS_DIR`
 NIC_DIR=`dirname $ABS_TOOLS_DIR`
 export HAL_CONFIG_PATH=$NIC_DIR/conf/
-export LD_LIBRARY_PATH=$NIC_DIR/lib:/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NIC_DIR/conf/sdk/external:$NIC_DIR/conf/sdk:$NIC_DIR/lib:/usr/local/lib:$LD_LIBRARY_PATH
 export MODEL_ZMQ_TYPE_TCP=1
 ulimit -c unlimited
 ldd $NIC_DIR/bin/hal
