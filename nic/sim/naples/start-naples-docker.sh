bf7d2b2c12af21adcab60d81f1dbbc9100dac000
Abhijeet Mahagaonkar <31998629+abhijeet-mahagaonkar@users.noreply.github.com>
2018-05-16 19:29:11 -0700

    Fix salesforce naples validate docker sanity. (#4558)
    Changes:
    	1. Fixes the name mismatch of built container vs run container.
    	2. Re-enabled naples-docker sanity.

--- nic/sim/naples/start-naples-docker.sh
+++ nic/sim/naples/start-naples-docker.sh
@@ -4,6 +4,7 @@ VER=v1
 NAPLES_DIR=$HOME/naples/data
 AGENT_PORT=9007
 HAL_PORT=50054
+CONTAINER="pensando/naples:$VER"
 
 # create a dir so we can mount logs etc. from NAPLES into this
 mkdir -p $NAPLES_DIR
@@ -18,6 +19,6 @@ echo "Loading docker image into registry ..."
 docker load --input naples-docker-$VER.tgz
 
 echo "Running the NAPLES container ..."
-docker run --rm -d --name naples-$VER --privileged -ti -p $AGENT_PORT:$AGENT_PORT  -p $HAL_PORT:$HAL_PORT --mount type=bind,source=$NAPLES_DIR,target=/naples/data naples:v1
+docker run --rm -d --name naples-$VER --privileged -ti -p $AGENT_PORT:$AGENT_PORT  -p $HAL_PORT:$HAL_PORT --mount type=bind,source=$NAPLES_DIR,target=/naples/data "$CONTAINER"
 
 echo "NAPLES bring up completed"
