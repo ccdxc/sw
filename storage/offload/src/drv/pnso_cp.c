6e52fb56719e5a0a3f1e0e877438e9a274d9f80f
Mohan Kumar <mohan@pensando.io>
2018-05-31 14:21:10 +0000

    pnso_cp: Bring in CP/CPDC for cleanup
    This commit includes the code that was used while prototyping from
    mohan/pnso_drv branch.  This should undergo soem surgery to make it
    work under new environment.
    
    Signed-off-by: Mohan Kumar <mohan@pensando.io>
    
    Conflicts:
    	storage/offload/src/drv/pnso_cp.c

--- storage/offload/src/drv/pnso_cp.c
+++ storage/offload/src/drv/pnso_cp.c
@@ -10,47 +10,460 @@
 #include "pnso_cpdc.h"
 #include "pnso_cpdc_cmn.h"
 
+#define MAX_CP_THRESHOLD_LEN	(1 << 16)
+
+static bool
+__is_cp_algo_type_valid(uint16_t algo_type)
+{
+	switch (algo_type) {
+	case PNSO_COMPRESSION_TYPE_LZRW1A:
+		return true;
+	default:
+		PNSO_ASSERT(0);	/* unreachable code */
+		return false;
+	}
+	return false;
+}
+
+static bool
+__is_cp_threshold_len_valid(uint16_t threshold_len)
+{
+	/* TODO-cp: check for i/p buffer len and max threshold len?? */
+	if (threshold_len > MAX_CP_THRESHOLD_LEN)
+		return false;
+
+	return true;
+}
+
+static bool
+__is_cp_flags_valid(uint16_t flags)
+{
+	/* no special checks needed, as cp flags are independent ones today */
+	return true;
+}
+
+static bool __attribute__((unused))
+__is_dflag_zero_pad_enabled(uint16_t flags)
+{
+	if (flags & PNSO_CP_DFLAG_ZERO_PAD)
+		return true;
+
+	return false;
+}
+
+static bool __attribute__((unused))
+__is_dflag_insert_header_enabled(uint16_t flags)
+{
+	if (flags & PNSO_CP_DFLAG_INSERT_HEADER)
+		return true;
+
+	return false;
+}
+
+static bool __attribute__((unused))
+__is_dflag_bypass_onfail_enabled(uint16_t flags)
+{
+	if (flags & PNSO_CP_DFLAG_BYPASS_ONFAIL)
+		return true;
+
+	return false;
+}
+
+static inline void
+__clear_insert_header(struct cpdc_desc *desc)
+{
+	desc->u.cd_bits.cc_insert_header = 0;
+}
+
+static inline void
+__clear_header_present(struct cpdc_desc *desc)
+{
+	/* reset to invalidate the verification against header */
+	desc->u.cd_bits.cc_header_present = 0;
+	desc->u.cd_bits.cc_chksum_verify_enabled = 0;
+}
+
+static bool
+__is_compression_desc_valid(struct pnso_compression_desc *desc)
+{
+	pnso_error_t err = EINVAL;
+
+	if (!__is_cp_algo_type_valid(desc->algo_type)) {
+		PNSO_LOG_ERROR(err, "invalid cp algo type specified! algo_type: %hu",
+				desc->algo_type);
+		return false;
+	}
+
+	if (!__is_cp_threshold_len_valid(desc->threshold_len)) {
+		PNSO_LOG_ERROR(err, "invalid cp threshold len specified! threshold_len: %hu",
+				desc->threshold_len);
+		return false;
+	}
+
+	if (!__is_cp_flags_valid(desc->flags)) {
+		PNSO_LOG_ERROR(err, "invalid cp flags specified! flags: %hu",
+				desc->flags);
+		return false;
+	}
+
+	PNSO_LOG_INFO(PNSO_OK, "compression desc is valid algo_type: %hu threshold_len: %hu flags: %hu",
+				desc->algo_type, desc->threshold_len,
+				desc->flags);
+
+	return true;
+}
+
+static void __attribute__((unused))
+__clear_list_bits_in_desc(struct cpdc_desc *desc)
+{
+	desc->u.cd_bits.cc_src_is_list = 0;
+	desc->u.cd_bits.cc_dst_is_list = 0;
+}
+
+static void
+__fill_cp_desc(struct cpdc_desc *desc, void *src_buf, void *dst_buf,
+		void *status_buf, uint32_t sbuf_len)
+{
+	memset(desc, 0, sizeof(*desc));
+
+	desc->cd_src = (uint64_t) pnso_virt_to_phys(src_buf);
+	desc->cd_dst = (uint64_t) pnso_virt_to_phys(dst_buf);
+
+	desc->u.cd_bits.cc_enabled = 1;
+	desc->u.cd_bits.cc_insert_header = 1;
+
+	desc->u.cd_bits.cc_src_is_list = 1;
+	desc->u.cd_bits.cc_dst_is_list = 1;
+
+	desc->cd_datain_len = (sbuf_len == MAX_CP_THRESHOLD_LEN) ? 0 : sbuf_len;
+	desc->cd_threshold_len = sbuf_len - sizeof(struct pnso_compression_header);
+	desc->cd_status_addr = (uint64_t) pnso_virt_to_phys(status_buf);
+	desc->cd_status_data = 1234;
+
+	cpdc_pprint_desc(desc);
+}
+
 static pnso_error_t
 compress_setup(struct service_info *svc_info,
 		const struct service_params *svc_params)
 {
-	return EOPNOTSUPP;
+	pnso_error_t err;
+	struct pnso_compression_desc *pnso_cp_desc;
+	struct cpdc_desc *cp_desc;
+	struct cpdc_status_sha512 *status_sha512 = NULL;
+	size_t sbuf_len, dbuf_len;
+	bool setup_seq;
+
+	if (!svc_info || !desc || !src_buf || !dst_buf) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "invalid input specified! svc_info: %p desc %p src_buf: %p dst_buf: %p",
+				svc_info, desc, src_buf, dst_buf);
+		goto out;
+	}
+
+	sbuf_len = pbuf_get_buffer_list_len(src_buf);
+	if (sbuf_len == 0) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "invalid src buf len specified! sbuf_len: %d",
+				sbuf_len);
+		goto out;
+	}
+
+	dbuf_len = pbuf_get_buffer_list_len(dst_buf);
+	if (dbuf_len == 0) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "input buf len mismatch! sbuf_len: %d dbuf_len: %d",
+				sbuf_len, dbuf_len);
+		goto out;
+	}
+
+	pnso_cp_desc = (struct pnso_compression_desc *) desc;
+	if (!__is_compression_desc_valid(pnso_cp_desc)) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "invalid cp desc specified!");
+		goto out;
+	}
+
+	cp_desc = (struct cpdc_desc *) dpool_get_desc(cpdc_dpool);
+	if (!cp_desc) {
+		err = ENOMEM;
+		PNSO_LOG_ERROR(err, "cannot obtain cp desc from pool!");
+		goto out;
+	}
+
+#if USE_DPOOL
+	status_sha512 = (struct cpdc_status_sha512 *)
+		dpool_get_desc(cpdc_status_sha512_dpool);
+#else
+	err = pnso_memalign(PNSO_MEM_ALIGN_BUF,
+			sizeof(struct cpdc_status_sha512),
+			(void **) &status_sha512);
+#endif
+	if (!status_sha512) {
+		err = ENOMEM;
+		PNSO_LOG_ERROR(err, "cannot obtain cp status desc from pool!");
+		goto out_cp_desc;
+	}
+
+	err = cpdc_convert_buffer_list_to_sgl(svc_info, src_buf, dst_buf);
+	if (err) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "cannot obtain cp src/dst sgl from pool!");
+		goto out_status_desc;
+	}
+
+	svc_info->si_type = PNSO_SVC_TYPE_COMPRESS;
+	svc_info->si_desc = cp_desc;
+	svc_info->si_status_buf = status_sha512;
+
+	svc_info->si_sbuf = NULL;
+	svc_info->si_dbuf = NULL;
+
+	__fill_cp_desc(cp_desc, svc_info->si_src_sgl,
+			svc_info->si_dst_sgl, status_sha512, sbuf_len);
+
+	/* TODO: fix this initialization */
+	memset(&svc_info->si_seq_info, 0, sizeof(struct sequencer_info));
+
+	setup_seq = (svc_info->si_flags & CHAIN_SFLAG_LONE_SERVICE) ||
+		(svc_info->si_flags & CHAIN_SFLAG_FIRST_SERVICE);
+	if (setup_seq)
+		seq_submit_desc(cp_desc_pa, &seq_info->queue,
+				seq_info->submit_type, seq_info->ring,
+				&seq_info->index);
+
+	err = PNSO_OK;
+	PNSO_LOG_INFO(err, "service initialized!");
+
+	return err;
+
+out_status_desc:
+	cpdc_release_sgl(svc_info->si_dst_sgl);
+	cpdc_release_sgl(svc_info->si_src_sgl);
+
+#if USE_DPOOL
+	err = dpool_put_desc(cpdc_status_sha512_dpool, status_sha512);
+	if (err)
+		PNSO_LOG_ERROR(err, "failed to return status desc to pool!");
+#else
+	pnso_free(status_sha512);
+#endif
+out_cp_desc:
+	err = dpool_put_desc(cpdc_dpool, cp_desc);
+	if (err)
+		PNSO_LOG_ERROR(err, "failed to return cp desc to pool!");
+out:
+	return err;
 }
 
 static pnso_error_t
 compress_chain(struct chain_entry *centry)
 {
-	return EOPNOTSUPP;
+	pnso_error_t err = PNSO_OK;
+
+	PNSO_LOG_INFO(err, "enter ...");
+
+	assert(centry);
+
+	err = cpdc_common_chain(centry);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to chain");
+		goto out;
+	}
+
+	/* TODO: anything more? */
+
+out:
+	PNSO_LOG_INFO(err, "exit!");
+	return err;
 }
 
 static pnso_error_t
 compress_schedule(const struct service_info *svc_info)
 {
-	return EOPNOTSUPP;
+	struct sequencer_info *seq_info;
+	bool ring_db;
+
+	PNSO_LOG_INFO(PNSO_OK, "enter ... ");
+
+	assert(svc_info);
+	seq_info = &svc_info->si_seq_info;
+
+	ring_db = (svc_info->si_flags & CHAIN_SFLAG_LONE_SERVICE) ||
+		(svc_info->si_flags & CHAIN_SFLAG_FIRST_SERVICE);
+	if (ring_db) {
+		PNSO_LOG_INFO(PNSO_OK, "ring door bell <===");
+		seq_ring_db(&seq_info->queue, seq_info->index);
+	}
+
+	PNSO_LOG_INFO(PNSO_OK, "exit!");
+	return PNSO_OK;
 }
 
 static pnso_error_t
 compress_poll(const struct service_info *svc_info)
 {
-	return EOPNOTSUPP;
+	uint32_t i;
+	struct cpdc_status_sha512 *status_sha512;
+
+	PNSO_LOG_INFO(PNSO_OK, "enter ...");
+
+	assert(svc_info);
+
+	status_sha512 = (struct cpdc_status_sha512 *) svc_info->si_status_buf;
+	assert(status_sha512);
+
+	for (i = 0; i < PNSO_UT_NUM_POLL; i++) {
+		PNSO_LOG_INFO(PNSO_OK, "status updated (%d) status_sha512: 0x%jx", i + 1, status_sha512);
+
+		if (status_sha512->css512_valid) {
+			PNSO_LOG_INFO(PNSO_OK, "status updated (%d)", i + 1);
+			break;
+		}
+		usleep(PNSO_UT_POLL_DELAY);
+	}
+
+	PNSO_LOG_INFO(PNSO_OK, "exit!");
+	return PNSO_OK;
 }
 
 static pnso_error_t
 compress_read_status(const struct service_info *svc_info)
 {
-	return EOPNOTSUPP;
+	pnso_error_t err;
+	struct cpdc_desc *cp_desc;
+	struct cpdc_status_sha512 *status_sha512;
+
+	PNSO_LOG_INFO(PNSO_OK, "enter ...");
+
+	assert(svc_info);
+
+	cp_desc = svc_info->si_desc;
+	assert(cp_desc);
+
+	status_sha512 = (struct cpdc_status_sha512 *) svc_info->si_status_buf;
+	assert(status_sha512);
+
+	cpdc_pprint_status_sha512(status_sha512);
+
+	if (!status_sha512->css512_valid) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "valid bit not set!");
+		goto out;
+	}
+
+	if (status_sha512->css512_err) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "error reported! error: %d",
+				status_sha512->css512_err);
+		goto out;
+	}
+
+	if (status_sha512->css512_partial_data != cp_desc->cd_status_data) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "partial data mismatch, expected 0x%x received: 0x%x",
+				cp_desc->cd_status_data,
+				status_sha512->css512_partial_data);
+		goto out;
+	}
+
+	if (cp_desc->u.cd_bits.cc_enabled &&
+			cp_desc->u.cd_bits.cc_insert_header) {
+		/*
+		 * TODO: check
+		 *	- header version
+		 *	- zero checksum
+		 *	- len
+		 */
+	}
+
+	if (cp_desc->u.cd_bits.cc_hash_enabled) {
+		/* TODO: check SHA */
+	}
+
+	if (status_sha512->css512_integrity_data == 0) {
+		err = EINVAL;
+		PNSO_LOG_ERROR(err, "integrity data is zero");
+		goto out;
+	}
+
+	err = PNSO_OK;
+	PNSO_LOG_ERROR(err, "status verification success!");
+
+out:
+	PNSO_LOG_INFO(PNSO_OK, "exit");
+	return err;
 }
 
 static pnso_error_t
 compress_write_result(struct service_info *svc_info)
 {
-	return EOPNOTSUPP;
+	pnso_error_t err;
+	struct pnso_service_status *svc_status;
+	struct cpdc_status_sha512 *status_sha512;
+
+	PNSO_LOG_INFO(PNSO_OK, "enter ...");
+
+	assert(svc_info);
+
+	svc_status = svc_info->si_status;
+	assert(svc_status->svc_type == svc_info->si_type);
+
+	status_sha512 = (struct cpdc_status_sha512 *) svc_info->si_status_buf;
+	assert(status_sha512);
+
+	/* TODO: handle this better */
+	svc_status->err = EINVAL;
+	if (status_sha512->css512_valid) {
+		cpdc_convert_desc_error(status_sha512->css512_err);
+		svc_status->err = status_sha512->css512_err;
+	}
+
+	svc_status->u.dst.data_len = status_sha512->css512_output_data_len;
+
+	cpdc_release_sgl(svc_info->si_src_sgl);
+	cpdc_release_sgl(svc_info->si_dst_sgl);
+
+	err = PNSO_OK;
+	PNSO_LOG_ERROR(err, "status/result update success!");
+
+	PNSO_LOG_INFO(PNSO_OK, "exit");
+	return err;
 }
 
 static void
 compress_teardown(const struct service_info *svc_info)
 {
-	/* EOPNOTSUPP */
+	pnso_error_t err;
+	struct cpdc_desc *cp_desc;
+	struct cpdc_status_sha512 *status_sha512;
+
+	PNSO_LOG_INFO(PNSO_OK, "enter ...");
+
+	assert(svc_info);
+
+	cp_desc = svc_info->si_desc;
+	assert(cp_desc);
+
+	status_sha512 = (struct cpdc_status_sha512 *) svc_info->si_status_buf;
+	assert(status_sha512);
+
+#if USE_DPOOL
+	err = dpool_put_desc(cpdc_status_sha512_dpool, status_sha512);
+	if (err) {
+		PNSO_LOG_ERROR(EINVAL, "failed to return status desc to pool! status_sha512: %p", status_sha512);
+		assert(0);
+	}
+#else
+	pnso_free(status_sha512);
+#endif
+	err = dpool_put_desc(cpdc_dpool, cp_desc);
+	if (err) {
+		PNSO_LOG_ERROR(EINVAL, "failed to return cp desc to pool! cp_desc: %p", cp_desc);
+		assert(0);
+	}
+
+	PNSO_LOG_INFO(PNSO_OK, "exit!");
 }
 
 struct service_ops cp_ops = {
--- storage/offload/src/drv/pnso_cpdc.c
+++ storage/offload/src/drv/pnso_cpdc.c
@@ -9,6 +9,455 @@
 #include "pnso_chain.h"
 #include "pnso_cpdc.h"
 
+#if 0
+struct desc_pool *cpdc_dpool;
+struct desc_pool *cpdc_sgl_dpool;
+struct desc_pool *cpdc_status_nosha_dpool;
+struct desc_pool *cpdc_status_sha256_dpool;
+struct desc_pool *cpdc_status_sha512_dpool;
+
+static void
+__pprint_cp_cmd(struct cpdc_cmd *cmd)
+{
+	if (!cmd)
+		return;
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_enabled", cmd->cc_enabled);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_header_present",
+			cmd->cc_header_present);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_insert_header",
+			cmd->cc_insert_header);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_db_on", cmd->cc_db_on);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_otag_on", cmd->cc_otag_on);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_src_is_list",
+			cmd->cc_src_is_list);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_dst_is_list",
+			cmd->cc_dst_is_list);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_chksum_verify_enabled",
+			cmd->cc_chksum_verify_enabled);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_chksum_adler",
+			cmd->cc_chksum_adler);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_hash_enabled",
+			cmd->cc_hash_enabled);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_hash_type",
+			cmd->cc_hash_type);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_integrity_src",
+			cmd->cc_integrity_src);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cc_integrity_type",
+			cmd->cc_integrity_type);
+}
+
+void
+cpdc_pprint_desc(struct cpdc_desc *desc)
+{
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "cpdc_desc", desc);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "cd_src", desc->cd_src);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "cd_dst", desc->cd_dst);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "=== cpdc_cmd", &desc->u.cd_bits);
+	__pprint_cp_cmd(&desc->u.cd_bits);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cd_datain_len",
+			desc->cd_datain_len);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cd_extended_len",
+			desc->cd_extended_len);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cd_threshold_len",
+			desc->cd_threshold_len);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "cd_status_addr",
+			desc->cd_status_addr);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "cd_db_addr",
+			desc->cd_db_addr);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cd_db_data",
+			desc->cd_db_data);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "cd_otag_addr",
+			desc->cd_otag_addr);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cd_otag_data",
+			desc->cd_otag_data);
+
+	/* TODO: group near cd_status_addr */
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "cd_status_data",
+			desc->cd_status_data);
+}
+
+void
+cpdc_pprint_status_nosha(struct cpdc_status_nosha *status_nosha)
+{
+	if (!status_nosha)
+		return;
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "=== status_nosha", status_nosha);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "csn_err",
+			status_nosha->csn_err);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "csn_valid",
+			status_nosha->csn_valid);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "csn_output_data_len",
+			status_nosha->csn_output_data_len);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "csn_partial_data",
+			status_nosha->csn_partial_data);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "csn_integrity_data",
+			status_nosha->csn_integrity_data);
+}
+
+void
+cpdc_pprint_status_sha256(struct cpdc_status_sha256 *status_sha256)
+{
+	if (!status_sha256)
+		return;
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "=== status_sha256", status_sha256);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css256_err",
+			status_sha256->css256_err);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css256_valid",
+			status_sha256->css256_valid);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css256_output_data_len",
+			status_sha256->css256_output_data_len);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css256_partial_data",
+			status_sha256->css256_partial_data);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css256_integrity_data",
+			status_sha256->css256_integrity_data);
+
+	/* TODO: print SHA */
+}
+
+void
+cpdc_pprint_status_sha512(struct cpdc_status_sha512 *status_sha512)
+{
+	if (!status_sha512)
+		return;
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %p", "=== status_sha512", status_sha512);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css512_err",
+			status_sha512->css512_err);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css512_valid",
+			status_sha512->css512_valid);
+
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css512_output_data_len",
+			status_sha512->css512_output_data_len);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css512_partial_data",
+			status_sha512->css512_partial_data);
+	PNSO_LOG_INFO(PNSO_OK, "%30s: %d", "css512_integrity_data",
+			status_sha512->css512_integrity_data);
+
+	/* TODO: print SHA */
+}
+
+void
+cpdc_release_sgl(struct cpdc_sgl *sgl)
+{
+	pnso_error_t err;
+	struct cpdc_sgl *sgl_next;
+
+	if (!sgl)
+		return;
+
+	while (sgl) {
+		sgl_next = (struct cpdc_sgl *) sgl->cs_next;
+		sgl->cs_next = 0;
+
+		err = dpool_put_desc(cpdc_sgl_dpool, sgl);
+		if (err) {
+			PNSO_LOG_ERROR(err, "failed to return cp sgl desc to pool!");
+			assert(0);
+		}
+
+		sgl = sgl_next;
+	}
+}
+
+static struct cpdc_sgl *
+__populate_sgl(struct pnso_buffer_list *buf_list)
+{
+	pnso_error_t err;
+	struct cpdc_sgl *sgl_head = NULL;
+	struct cpdc_sgl *sgl_prev = NULL;
+	struct cpdc_sgl *sgl = NULL;
+	uint32_t count;
+	void *ptr;
+	uint32_t i;
+
+	count = buf_list->count;
+	while (count) {
+		sgl = (struct cpdc_sgl *) dpool_get_desc(cpdc_sgl_dpool);
+		if (!sgl) {
+			err = ENOMEM;
+			PNSO_LOG_ERROR(err, "cannot obtain cp sgl desc from pool!");
+			goto out;
+		}
+		memset(sgl, 0, sizeof(struct cpdc_sgl));
+
+		if (!sgl_head)
+			sgl_head = sgl;
+		else
+			sgl_prev->cs_next = (uint64_t) sgl;
+
+		i = 0;
+		ptr = (void *) buf_list->buffers[i].buf;
+		sgl->cs_addr_0 = (uint64_t) pnso_virt_to_phys(ptr);
+		sgl->cs_len_0 = buf_list->buffers[i].len;
+		i++;
+		count--;
+
+		if (count == 0) {
+			sgl->cs_next = 0;
+			break;
+		}
+
+		if (count) {
+			ptr = (void *) buf_list->buffers[i].buf;
+			sgl->cs_addr_1 = (uint64_t) pnso_virt_to_phys(ptr);
+			sgl->cs_len_1 = buf_list->buffers[i].len;
+			i++;
+			count--;
+		} else {
+			sgl->cs_next = 0;
+			break;
+		}
+
+		if (count) {
+			ptr = (void *) buf_list->buffers[i].buf;
+			sgl->cs_addr_2 = (uint64_t) pnso_virt_to_phys(ptr);
+			sgl->cs_len_2 = buf_list->buffers[i].len;
+			i++;
+			count--;
+		} else {
+			sgl->cs_next = 0;
+			break;
+		}
+
+		sgl_prev = sgl;
+	}
+
+	return sgl_head;
+
+out:
+	cpdc_release_sgl(sgl_head);
+	return NULL;
+}
+
+pnso_error_t
+cpdc_convert_buffer_list_to_sgl(struct service_info *svc_info,
+		struct pnso_buffer_list *src_buf,
+		struct pnso_buffer_list *dst_buf)
+{
+	pnso_error_t err;
+	bool is_sgl;
+
+	is_sgl = pbuf_is_buffer_list_sgl(src_buf);
+	if (!is_sgl) {
+		err = EINVAL;
+		// goto out;	/* TODO */
+	}
+	svc_info->si_src_sgl = __populate_sgl(src_buf);
+
+	is_sgl = pbuf_is_buffer_list_sgl(dst_buf);
+	if (!is_sgl) {
+		err = EINVAL;
+		// goto out;	/* TODO */
+	}
+	svc_info->si_dst_sgl = __populate_sgl(dst_buf);
+
+	err = PNSO_OK;
+// out:
+	return err;
+}
+
+void
+cpdc_populate_buffer_list(struct cpdc_sgl *sgl,
+		struct pnso_buffer_list *buf_list)
+{
+	struct cpdc_sgl *sgl_head = NULL;
+	uint32_t i;
+
+	if (!sgl_head || !buf_list)
+		return;
+
+	i = 0;
+	sgl_head = sgl;
+	while (sgl) {
+		if (sgl->cs_len_0 > 0) {
+			buf_list->buffers[i].buf = sgl->cs_addr_0;
+			buf_list->buffers[i].len = sgl->cs_len_0;
+			i++;
+		}
+
+		if (sgl->cs_len_1 > 0) {
+			buf_list->buffers[i].buf = sgl->cs_addr_1;
+			buf_list->buffers[i].len = sgl->cs_len_1;
+			i++;
+		}
+
+		if (sgl->cs_len_2 > 0) {
+			buf_list->buffers[i].buf = sgl->cs_addr_2;
+			buf_list->buffers[i].len = sgl->cs_len_2;
+			i++;
+		}
+
+		sgl = (struct cpdc_sgl *) sgl->cs_next;
+	}
+	buf_list->count = i;
+
+	// TODO:  will be taken care in teardown
+	// cpdc_release_sgl(sgl_head);
+}
+
+
+pnso_error_t
+cpdc_common_chain(struct chain_entry *centry)
+{
+	pnso_error_t err;
+	struct chain_entry *centry_next;
+	struct service_info *svc_info;
+
+	/* NULL denotes terminal centry or valid error */
+	if (!centry)
+		return PNSO_OK;
+
+	err = PNSO_OK;
+	PNSO_LOG_INFO(PNSO_OK, "get and set sequencer info");
+
+	centry_next = centry->ce_next;
+	if (!centry_next) {
+		err = PNSO_OK;
+		PNSO_LOG_INFO(err, "nothing more to chain");
+		goto out;
+	}
+
+	/* get next in list to chain */
+	svc_info = &centry_next->ce_sinfo;
+	err = svc_info->si_ops.chain(centry_next);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to chain");
+		goto out;
+	}
+
+out:
+	return err;
+}
+
+void
+cpdc_common_teardown(void *desc)
+{
+	pnso_error_t err;
+	struct cpdc_desc *cp_desc;
+
+	if (!desc)
+		return;
+
+	cp_desc = (struct cpdc_desc *) desc;
+	err = dpool_put_desc(cpdc_dpool, cp_desc);
+	if (err)
+		assert(0);
+
+	/* TODO: do other clean-up stuff */
+}
+
+pnso_error_t
+cpdc_start_accelerator(const struct cpdc_init_params *init_params)
+{
+	pnso_error_t err;
+
+	PNSO_LOG_INFO(PNSO_OK, "enter ...");
+
+	/* TODO: do the resource discovery */
+
+	err = dpool_create(DPOOL_TYPE_CPDC, PNSO_UT_MAX_DBUFS, &cpdc_dpool);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to create CPDC descriptor pool");
+		goto out;
+	}
+
+	err = dpool_create(DPOOL_TYPE_CPDC_SGL, PNSO_UT_MAX_DBUFS,
+			&cpdc_sgl_dpool);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to create CPDC sgl descriptor pool");
+		goto out_free_cpdc;
+	}
+
+	err = dpool_create(DPOOL_TYPE_CPDC_STATUS_NOSHA, PNSO_UT_MAX_DBUFS,
+			&cpdc_status_nosha_dpool);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to create CPDC no sha status pool");
+		goto out_free_cpdc_sgl;
+	}
+
+	err = dpool_create(DPOOL_TYPE_CPDC_STATUS_SHA256, PNSO_UT_MAX_DBUFS,
+			&cpdc_status_sha256_dpool);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to create CPDC sha256 status pool");
+		goto out_free_nosha;
+	}
+
+	err = dpool_create(DPOOL_TYPE_CPDC_STATUS_SHA512, PNSO_UT_MAX_DBUFS,
+			&cpdc_status_sha512_dpool);
+	if (err) {
+		PNSO_LOG_INFO(err, "failed to create CPDC sha512 status pool");
+		goto out_free_sha256;
+	}
+
+	dpool_pprint(cpdc_dpool);
+	dpool_pprint(cpdc_sgl_dpool);
+	dpool_pprint(cpdc_status_nosha_dpool);
+	dpool_pprint(cpdc_status_sha256_dpool);
+	dpool_pprint(cpdc_status_sha512_dpool);
+
+	return err;
+
+out_free_sha256:
+	dpool_destroy(&cpdc_status_sha256_dpool);
+out_free_nosha:
+	dpool_destroy(&cpdc_status_nosha_dpool);
+out_free_cpdc_sgl:
+	dpool_destroy(&cpdc_sgl_dpool);
+out_free_cpdc:
+	dpool_destroy(&cpdc_dpool);
+out:
+	PNSO_LOG_INFO(PNSO_OK, "exit!");
+	return err;
+}
+
+pnso_error_t
+cpdc_stop_accelerator(void)
+{
+	/* TODO: add quiesce queue API */
+
+	dpool_pprint(cpdc_dpool);
+	dpool_pprint(cpdc_sgl_dpool);
+	dpool_pprint(cpdc_status_nosha_dpool);
+	dpool_pprint(cpdc_status_sha256_dpool);
+	dpool_pprint(cpdc_status_sha512_dpool);
+
+	dpool_destroy(&cpdc_status_nosha_dpool);
+	dpool_destroy(&cpdc_status_sha256_dpool);
+	dpool_destroy(&cpdc_status_sha512_dpool);
+	dpool_destroy(&cpdc_sgl_dpool);
+	dpool_destroy(&cpdc_dpool);
+
+	return PNSO_OK;
+}
+
+pnso_error_t
+cpdc_convert_desc_error(int error)
+{
+	/* TODO: ... */
+	return PNSO_OK;
+}
+#endif
+
 pnso_error_t
 cpdc_start_accelerator(const struct cpdc_init_params *init_params)
 {
