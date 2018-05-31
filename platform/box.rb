29d6595d035d226f299c12332b76b362f20a2b45
Neel Patel <neel@pensando.io>
2018-05-31 08:45:07 -0700

    - Set toolchain path in aarch64 Makefile - Added gitignore paths for binary assets - Added job for testing platform cross compilation for Aarch64

--- platform/box.rb
+++ platform/box.rb
@@ -1,9 +1,16 @@
 from "registry.test.pensando.io:5000/pensando/nic:1.22"
 
 PACKAGES = %w[
-  patch readline-devel libedit-devel
+  patch readline-devel libedit2 libedit-devel
 ]
 
 env GOPATH: "/usr"
+
 workdir "/sw"
+
 run "yum install -y #{PACKAGES.join(" ")}"
+
+copy "platform/entrypoint.sh", "/entrypoint.sh"
+run "chmod +x /entrypoint.sh"
+
+entrypoint "/entrypoint.sh"
