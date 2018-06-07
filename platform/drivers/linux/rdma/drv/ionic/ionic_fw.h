0f832bb44a7c5eb21353d1b0fbb7f28b9cba829d
Allen Hubbe <allenbh@pensando.io>
2018-06-07 10:24:38 -0700

    ionic_rdma: fw abi v1 for rdma adminq

--- platform/drivers/linux/rdma/drv/ionic/ionic_fw.h
+++ platform/drivers/linux/rdma/drv/ionic/ionic_fw.h
@@ -3,6 +3,454 @@
 
 #include <linux/kernel.h>
 
+/* completion queue v1 cqe */
+struct ionic_v1_cqe {
+	union {
+		struct {
+			u8		rsvd[24];
+			__le16		old_sq_cindex;
+			__le16		old_rq_cq_cindex;
+		} admin;
+		struct {
+			u16		wqe_id;
+			u8		op;
+			u8		flags;
+			__be32		src_qpn;
+			u8		src_mac[6];
+			__be16		pkey_index;
+			u8		rsvd[4];
+			__be32		imm_data_rkey;
+		} recv;
+	};
+	union {
+		__le32			status_length_cindex;
+		__be32			msn;
+	};
+	__le32				qid_type_flags;
+};
+
+/* bits for cqe qid_type_flags */
+enum ionic_v1_cqe_qtf_bits {
+	IONIC_V1_CQE_COLOR		= BIT(31),
+	IONIC_V1_CQE_ERROR		= BIT(30),
+	IONIC_V1_CQE_TYPE_SHIFT		= 24,
+	IONIC_V1_CQE_TYPE_MASK		= 0x3f,
+	IONIC_V1_CQE_QID_MASK		= 0xffffff,
+
+	IONIC_V1_CQE_TYPE_ADMIN		= 0,
+	IONIC_V1_CQE_TYPE_RECV		= 1,
+	IONIC_V1_CQE_TYPE_SEND_MSN	= 2,
+	IONIC_V1_CQE_TYPE_SEND_NPG	= 3,
+};
+
+static inline bool ionic_v1_cqe_color(struct ionic_v1_cqe *cqe)
+{
+	return !!(cqe->qid_type_flags & cpu_to_le32(IONIC_V1_CQE_COLOR));
+}
+
+static inline u32 ionic_v1_cqe_qtf(struct ionic_v1_cqe *cqe)
+{
+	return le32_to_cpu(cqe->qid_type_flags);
+}
+
+static inline bool ionic_v1_cqe_qtf_error(u32 qtf)
+{
+	return !!(qtf & IONIC_V1_CQE_ERROR);
+}
+
+static inline bool ionic_v1_cqe_qtf_type(u32 qtf)
+{
+	return (qtf >> IONIC_V1_CQE_TYPE_SHIFT) & IONIC_V1_CQE_TYPE_MASK;
+}
+
+static inline u32 ionic_v1_cqe_qtf_qid(u32 qtf)
+{
+	return qtf & IONIC_V1_CQE_QID_MASK;
+}
+
+/* queue pair v1 sge */
+struct ionic_v1_sge {
+	__le64				va;
+	__le32				length;
+	__le32				lkey;
+};
+
+/* queue pair v1 recv wqe */
+struct ionic_v1_recv_wqe {
+	u8				op_rsvd;
+	u8				num_sge;
+	u16				wqe_id;
+	__le32				length;
+	u8				rsvd[8];
+	struct ionic_v1_sge		sgl[3];
+};
+
+/* queue pair v1 send wqe */
+struct ionic_v1_send_wqe {
+	u8				op;
+	u8				num_sge_key;
+	u16				flags;
+	__le32				length_key;
+	union {
+		struct {
+			u8		rsvd[8];
+			__le32		ah_id;
+			__be32		dest_qpn;
+			__be32		dest_qkey;
+			__be32		imm_data_rkey;
+			union {
+				struct ionic_v1_sge sgl[2];
+				u8	data[32];
+			};
+		} send;
+		struct {
+			u8		rsvd[8];
+			__be64		remote_va;
+			__be32		remote_rkey;
+			__be32		imm_data;
+			union {
+				struct ionic_v1_sge sgl[2];
+				u8	data[32];
+			};
+		} rdma;
+		struct {
+			u8		rsvd[8];
+			__be64		remote_va;
+			__be32		remote_rkey;
+			__be32		swap_add_high;
+			__be32		swap_add_low;
+			__be32		compare_high;
+			__be32		compare_low;
+			u8		rsvd2[4];
+			struct ionic_v1_sge sgl[1];
+		} atomic;
+		struct {
+			__le64		va;
+			__le64		length;
+			__le64		offset;
+			__le64		dma_addr;
+			u8		dir_size_log2;
+			u8		page_size_log2;
+			u8		rsvd[22];
+		} reg_mr;
+		struct {
+			__le64		va;
+			__le64		length;
+			__le32		lkey;
+			u8		rsvd[36];
+		} bind_mw;
+	};
+};
+
+/* queue pair v1 send opcodes */
+enum ionic_v1_op {
+	IONIC_V1_OP_SEND,
+	IONIC_V1_OP_SEND_IMM,
+	IONIC_V1_OP_SEND_INV,
+	IONIC_V1_OP_READ,
+	IONIC_V1_OP_WRITE,
+	IONIC_V1_OP_WRITE_IMM,
+	IONIC_V1_OP_ATOMIC_CS,
+	IONIC_V1_OP_ATOMIC_FA,
+	IONIC_V1_OP_FAST_MR,
+	IONIC_V1_OP_BIND_MW,
+	IONIC_V1_OP_INVAL,
+};
+
+/* queue pair v1 cqe status */
+enum ionic_v1_status {
+	IONIC_V1_STS_OK,
+	IONIC_V1_STS_LOCAL_LEN_ERR,
+	IONIC_V1_STS_LOCAL_QP_OPER_ERR,
+	IONIC_V1_STS_LOCAL_PROT_ERR,
+	IONIC_V1_STS_WQE_FLUSHED_ERR,
+	IONIC_V1_STS_MEM_MGMT_OPER_ERR,
+	IONIC_V1_STS_BAD_RESP_ERR,
+	IONIC_V1_STS_LOCAL_ACC_ERR,
+	IONIC_V1_STS_REMOTE_INV_REQ_ERR,
+	IONIC_V1_STS_REMOTE_ACC_ERR,
+	IONIC_V1_STS_REMOTE_OPER_ERR,
+	IONIC_V1_STS_RETRY_EXCEEDED,
+	IONIC_V1_STS_RNR_RETRY_EXCEEDED,
+	IONIC_V1_STS_XRC_VIO_ERR,
+};
+
+static inline size_t ionic_v1_send_wqe_min_size(int min_sge, int min_data)
+{
+	return max(sizeof(struct ionic_v1_send_wqe),
+		   max(offsetof(struct ionic_v1_send_wqe, send.sgl[min_sge]),
+		       offsetof(struct ionic_v1_send_wqe, send.data[min_data])));
+}
+
+static inline int ionic_v1_send_wqe_max_sge(u8 stride_log2)
+{
+	struct ionic_v1_send_wqe *wqe = (void *)0;
+	struct ionic_v1_sge *sge = (void *)BIT_ULL(stride_log2);
+
+	return sge - wqe->send.sgl;
+}
+
+static inline int ionic_v1_send_wqe_max_data(u8 stride_log2)
+{
+	struct ionic_v1_send_wqe *wqe = (void *)0;
+	u8 *data = (void *)BIT_ULL(stride_log2);
+
+	return data - wqe->send.data;
+}
+
+static inline size_t ionic_v1_recv_wqe_min_size(int min_sge)
+{
+	return max(sizeof(struct ionic_v1_recv_wqe),
+		   offsetof(struct ionic_v1_recv_wqe, sgl[min_sge]));
+}
+
+static inline int ionic_v1_recv_wqe_max_sge(u8 stride_log2)
+{
+	struct ionic_v1_recv_wqe *wqe = (void *)0;
+	struct ionic_v1_sge *sge = (void *)BIT_ULL(stride_log2);
+
+	return sge - wqe->sgl;
+}
+
+/* admin queue v1 wqe */
+struct ionic_v1_admin_wqe {
+	u8				op;
+	u8				type_state;
+	__le16				dbid;
+	__le32				id_ver;
+	union {
+		struct {
+			__le64		dma_addr;
+			__le32		length;
+			u8		rsvd[44];
+		} stats;
+		struct {
+			__le64		dma_addr;
+			u8		rsvd[48];
+		} ah;
+		struct {
+			__le32		lkey;
+			__le32		rkey;
+			__le64		va;
+			__le64		length;
+			__le64		offset;
+			__le32		odp_id;
+			__le16		access_flags;
+			u8		dir_size_log2;
+			u8		page_size_log2;
+			__le32		tbl_index;
+			__le32		map_count;
+			__le64		dma_addr;
+		} mr;
+		struct {
+			__le32		eq_id;
+			u8		depth_log2;
+			u8		stride_log2;
+			u8		dir_size_log2_rsvd;
+			u8		page_size_log2;
+			u8		rsvd[32];
+			__le32		tbl_index;
+			__le32		map_count;
+			__le64		dma_addr;
+		} cq;
+		struct {
+			__le32		pd_id;
+			__le16		access_perms_flags;
+			__le16		access_perms_rsvd;
+			__le32		sq_cq_id;
+			u8		sq_depth_log2;
+			u8		sq_stride_log2;
+			u8		sq_dir_size_log2_rsvd;
+			u8		sq_page_size_log2;
+			__le32		sq_tbl_index_xrcd_id;
+			__le32		sq_map_count;
+			__le64		sq_dma_addr;
+			__le32		rq_cq_id;
+			u8		rq_depth_log2;
+			u8		rq_stride_log2;
+			u8		rq_dir_size_log2_rsvd;
+			u8		rq_page_size_log2;
+			__le32		rq_tbl_index_srq_id;
+			__le32		rq_map_count;
+			__le64		rq_dma_addr;
+		} qp;
+		struct {
+			u8		pmtu;
+			u8		retry;
+			u8		rnr_timer;
+			u8		retry_timeout;
+			__le16		access_perms_flags;
+			__le16		access_perms_mask;
+			__le32		rq_psn;
+			__le32		sq_psn;
+			__le32		qkey_dest_qpn;
+			__le32		rate_limit_kbps;
+			u8		rsq_depth;
+			u8		rrq_depth;
+			__le16		pkey_id;
+			__le32		ah_id_len;
+			u8		rsvd[16];
+			__le64		dma_addr;
+		} mod_qp;
+	};
+};
+
+/* side data for modify qp if resize */
+struct ionic_v1_admin_resize_qp {
+	u8				sq_depth_log2;
+	u8				sq_stride_log2;
+	u8				sq_dir_size_log2_rsvd;
+	u8				sq_page_size_log2;
+	u8				rq_depth_log2;
+	u8				rq_stride_log2;
+	u8				rq_dir_size_log2_rsvd;
+	u8				rq_page_size_log2;
+	__le32				sq_table_index;
+	__le32				sq_map_count;
+	__le64				sq_dma_addr;
+	__le32				rq_table_index;
+	__le32				rq_map_count;
+	__le64				rq_dma_addr;
+};
+
+/* side data for query qp */
+struct ionic_v1_admin_query_qp {
+	u8				state_pmtu;
+	u8				retry_rnrtry;
+	u8				rnr_timer;
+	u8				retry_timeout;
+	__le16				access_perms_flags;
+	__le16				access_perms_rsvd;
+	__le32				rq_psn;
+	__le32				sq_psn;
+	__le32				qkey_dest_qpn;
+	__le32				rate_limit_kbps;
+	u8				rsq_depth;
+	u8				rrq_depth;
+	__le16				pkey_id;
+	__le32				ah_id_len;
+};
+
+/* admin queue v1 opcodes */
+enum ionic_v1_admin_op {
+	IONIC_V1_ADMIN_NOOP,
+	IONIC_V1_ADMIN_STATS_HDRS,
+	IONIC_V1_ADMIN_STATS_VALS,
+	IONIC_V1_ADMIN_REG_MR,
+	IONIC_V1_ADMIN_DEREG_MR,
+	IONIC_V1_ADMIN_CREATE_CQ,
+	IONIC_V1_ADMIN_RESIZE_CQ,
+	IONIC_V1_ADMIN_DESTROY_CQ,
+	IONIC_V1_ADMIN_CREATE_QP,
+	IONIC_V1_ADMIN_MODIFY_QP,
+	IONIC_V1_ADMIN_QUERY_QP,
+	IONIC_V1_ADMIN_DESTROY_QP,
+};
+
+/* admin queue v1 cqe status */
+enum ionic_v1_admin_status {
+	IONIC_V1_ASTS_OK,
+	IONIC_V1_ASTS_BAD_INDEX,
+	IONIC_V1_ASTS_BAD_STATE,
+	IONIC_V1_ASTS_BAD_TYPE,
+	IONIC_V1_ASTS_BAD_ATTR,
+};
+
+/* admin queue v1 mr type */
+enum ionic_v1_mr_type {
+	IONIC_V1_MRT_REG_MR,
+	IONIC_V1_MRT_PHYS_MR,
+	IONIC_V1_MRT_ODP_MR,
+	IONIC_V1_MRT_MW_1,
+	IONIC_V1_MRT_MW_2A,
+	IONIC_V1_MRT_MW_2B,
+};
+
+/* admin queue v1 qp type */
+enum ionic_v1_qp_type {
+	IONIC_V1_QPT_RC,
+	IONIC_V1_QPT_UC,
+	IONIC_V1_QPT_UD,
+	IONIC_V1_QPT_RD,
+	IONIC_V1_QPT_SRQ,
+	IONIC_V1_QPT_XRC_INI,
+	IONIC_V1_QPT_XRC_TGT,
+	IONIC_V1_QPT_XRC_SRQ,
+};
+
+/* admin queue v1 qp state */
+enum ionic_v1_qp_state {
+	IONIC_V1_QPS_RESET,
+	IONIC_V1_QPS_INIT,
+	IONIC_V1_QPS_RTR,
+	IONIC_V1_QPS_RTS,
+	IONIC_V1_QPS_SQD,
+	IONIC_V1_QPS_SQE,
+	IONIC_V1_QPS_ERR,
+};
+
+/* event queue v1 eqe */
+struct ionic_v1_eqe {
+	__le32				evt;
+};
+
+/* bits for cqe queue_type_flags */
+enum ionic_v1_eqe_evt_bits {
+	IONIC_V1_EQE_COLOR		= BIT(31),
+	IONIC_V1_EQE_TYPE_SHIFT		= 28,
+	IONIC_V1_EQE_TYPE_MASK		= 0x7,
+	IONIC_V1_EQE_CODE_SHIFT		= 24,
+	IONIC_V1_EQE_CODE_MASK		= 0xf,
+	IONIC_V1_EQE_QID_MASK		= 0xffffff,
+
+	/* cq events */
+	IONIC_V1_EQE_TYPE_CQ		= 0,
+	/* cq normal events */
+	IONIC_V1_EQE_CQ_NOTIFY		= 0,
+	/* cq error events */
+	IONIC_V1_EQE_CQ_ERR		= 8,
+
+	/* qp and srq events */
+	IONIC_V1_EQE_TYPE_QP		= 1,
+	/* qp normal events */
+	IONIC_V1_EQE_SRQ_LEVEL		= 0,
+	IONIC_V1_EQE_SQ_DRAIN		= 1,
+	IONIC_V1_EQE_QP_COMM_EST	= 2,
+	IONIC_V1_EQE_QP_LAST_WQE	= 3,
+	/* qp error events */
+	IONIC_V1_EQE_QP_ERR		= 8,
+	IONIC_V1_EQE_QP_ERR_NOCQE	= 9,
+	IONIC_V1_EQE_QP_ERR_REQUEST	= 10,
+	IONIC_V1_EQE_QP_ERR_ACCESS	= 11,
+};
+
+static inline bool ionic_v1_eqe_color(struct ionic_v1_eqe *eqe)
+{
+	return !!(eqe->evt & cpu_to_le32(IONIC_V1_EQE_COLOR));
+}
+
+static inline u32 ionic_v1_eqe_evt(struct ionic_v1_eqe *eqe)
+{
+	return le32_to_cpu(eqe->evt);
+}
+
+static inline u32 ionic_v1_eqe_evt_type(u32 evt)
+{
+	return (evt >> IONIC_V1_EQE_TYPE_SHIFT) & IONIC_V1_EQE_TYPE_MASK;
+}
+
+static inline u32 ionic_v1_eqe_evt_code(u32 evt)
+{
+	return (evt >> IONIC_V1_EQE_CODE_SHIFT) & IONIC_V1_EQE_CODE_MASK;
+}
+
+static inline u32 ionic_v1_eqe_evt_qid(u32 evt)
+{
+	return evt & IONIC_V1_EQE_QID_MASK;
+}
+
+/* --- below: from makeshift driver --- */
+
 #define IONIC_CQE_SIZE		32
 #define IONIC_SQ_WQE_SIZE	32
 #define IONIC_SQ_WQE_MINSIZE	64
