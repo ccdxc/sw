8ed270903404248db0c30395fe996f8522d396c6
Pirabhu Raman <pirabhur@gmail.com>
2018-06-27 18:57:17 +0000

    1. Commenting warmd test for now as it doesnt seem to work 2. Addressing some review comments

--- storage/offload/src/osal/linux/kernel/osal_atomic.c
+++ storage/offload/src/osal/linux/kernel/osal_atomic.c
@@ -18,11 +18,15 @@ void osal_atomic_init(osal_atomic_int_t* addr, int val)
 
 int osal_atomic_fetch_add(osal_atomic_int_t* addr, int val) 
 {
+	// Userspace and kernel space apis behave differently. In kernel, the returned value is after addition 
+	// while in userspace it is before addition. Sticking with c11 behavior for now.
 	return atomic_add_return(val, addr) - val;
 }
 
 int osal_atomic_fetch_sub(osal_atomic_int_t* addr, int val) 
 {
+	// Userspace and kernel space apis behave differently. In kernel, the returned value is after subtraction
+	// while in userspace it is before subtraction. Sticking with c11 behavior for now.
 	return atomic_sub_return(val, addr) + val;
 }
 
--- storage/offload/src/osal/linux/kernel/sim/osal_mem.c
+++ storage/offload/src/osal/linux/kernel/sim/osal_mem.c
@@ -1,6 +1,15 @@
 #include <linux/slab.h>
 #include "osal_mem.h"
 
+/*
+ * TODO:
+ * 1. Using GFP_NOIO flag for now to get sim going - this will need to change when we have real driver
+ * 2. Kernel doesnt provide an elegant way to allocate aligned memory. This may need to be moved to upper layer 
+ *    or serviced via memory manager within osal . Sim is not using any aligned access right now
+ * 3. kmalloc has a limitation that allocated size should be less than KMALLOC_MAXSIZE. This again may need to 
+ *    be moved to upper layer or serviced via memory manager within osal . 
+ */
+
 void* osal_alloc(size_t size) 
 {
 	return kmalloc(size, GFP_KERNEL|GFP_NOFS|GFP_NOIO);
