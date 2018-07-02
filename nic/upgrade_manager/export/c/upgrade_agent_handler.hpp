25343141ebaac24e9dabad3d7250b3f5aafe1c70
rathorea <32275334+rathorea@users.noreply.github.com>
2018-07-02 13:54:30 -0700

    adding comments for APIs (#5234)

--- nic/upgrade_manager/export/c/upgrade_agent_handler.hpp
+++ nic/upgrade_manager/export/c/upgrade_agent_handler.hpp
@@ -14,6 +14,11 @@ class UpgAgentHandler {
 public:
     UpgAgentHandler() {}
 
+    /*
+     * All the following APIs are invoked for agents.
+     * The APIs are invoked on a per-application basis after the application has
+     *     returned back to upgrade-manager with a success/fail
+     */
     virtual void UpgStateCompatCheckCompletionHandler(HdlrResp &resp, string svcName);
     virtual void UpgStateProcessQuiesceCompletionHandler(HdlrResp &resp, string svcName);
     virtual void UpgStatePostBinRestartCompletionHandler(HdlrResp &resp, string svcName);
@@ -24,6 +29,10 @@ public:
     virtual void UpgStateCleanupCompletionHandler(HdlrResp &resp, string svcName);
     virtual void UpgStateAbortedCompletionHandler(HdlrResp &resp, string svcName);
 
+    /*
+     * All the following APIs are invoked for agents.
+     * These are APIs called at the end of a successful/failed/aborted upgrade processing
+     */
     virtual void UpgSuccessful(void);
     virtual void UpgFailed(vector<string> &errStrList);
     virtual void UpgAborted(vector<string> &errStrList);
--- nic/upgrade_manager/export/c/upgrade_handler.hpp
+++ nic/upgrade_manager/export/c/upgrade_handler.hpp
@@ -12,16 +12,61 @@ using namespace std;
 class UpgHandler {
 public:
     UpgHandler() {}
+    /*
+     * Applications are expected to perform compat check 
+     *     and let Upgrade manager know if Upgrade is allowed or not
+     */
     virtual HdlrResp HandleUpgStateCompatCheck(UpgCtx& upgCtx);
-    virtual HdlrResp HandleUpgStatePostBinRestart(UpgCtx& upgCtx);
+    /*
+     * Applications are expected to perform any quiescing needed in control plane.
+     * Here, NMD-like agents are expected to stop accepting 
+     *     any configurations from their north-bound agents
+     */
     virtual HdlrResp HandleUpgStateProcessQuiesce(UpgCtx& upgCtx);
+    /*
+     * Applications are expected to do any post-binary restart handling here.
+     */
+    virtual HdlrResp HandleUpgStatePostBinRestart(UpgCtx& upgCtx);
+    /*
+     * At this stage, following is expected:
+     * 1. FTE is supposed to disable flow-miss
+     * 2. Dataplane is quiesced
+     */
     virtual HdlrResp HandleUpgStateDataplaneDowntimePhase1(UpgCtx& upgCtx);
+    /*
+     * At this stage, following is expected:
+     * 1. AdminQ comes up after transforming its relevant data to new format
+     */
     virtual HdlrResp HandleUpgStateDataplaneDowntimePhase2(UpgCtx& upgCtx);
+    /*
+     * At this stage, following is expected:
+     * 1. All table data transformed to new format
+     * 2. All table base addresses fixed up
+     * 3. Programs in pipeline are ready to go
+     */
     virtual HdlrResp HandleUpgStateDataplaneDowntimePhase3(UpgCtx& upgCtx);
+    /*
+     * At this stage, following is expected:
+     * 1. Dataplane is re-enabled
+     * 2. FTE flow-miss re-enabled
+     */
     virtual HdlrResp HandleUpgStateDataplaneDowntimePhase4(UpgCtx& upgCtx);
+    /*
+     * At this stage, following is expected:
+     * 1. Cleanup any state needed
+     */
     virtual HdlrResp HandleUpgStateCleanup(UpgCtx& upgCtx);
+    /*
+     * Handle upgrade success
+     */
     virtual void HandleUpgStateSuccess(UpgCtx& upgCtx);
+    /*
+     * Handle upgrade failed
+     */
     virtual void HandleUpgStateFailed(UpgCtx& upgCtx);
+    /*
+     * Handle upgrade aborted
+     */
     virtual void HandleUpgStateAbort(UpgCtx& upgCtx);
 };
 typedef std::shared_ptr<UpgHandler> UpgHandlerPtr;
