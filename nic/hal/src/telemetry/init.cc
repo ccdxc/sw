89049dbff6d5475d81c7119cd52dc4b136ef95f0
rsrikanth86 <33850156+rsrikanth86@users.noreply.github.com>
2018-07-03 11:27:14 -0700

    Make telemetry a hal module (#5243)

---
+++ nic/hal/src/telemetry/init.cc
@@ -0,0 +1,47 @@
+// {C} Copyright 2017 Pensando Systems Inc. All rights reserved
+
+#include "nic/gen/hal/svc/telemetry_svc_gen.hpp"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+namespace hal {
+namespace telemetry {
+
+TelemetryServiceImpl    g_telemetry_svc;
+
+void
+svc_reg (ServerBuilder *server_builder, hal::hal_feature_set_t feature_set)
+{
+    if (!server_builder) {
+        return;
+    }
+
+    // register all "telemetry" services
+    HAL_TRACE_DEBUG("Registering gRPC telemetry services ...");
+    if (feature_set == hal::HAL_FEATURE_SET_IRIS) {
+        server_builder->RegisterService(&g_telemetry_svc);
+    } else if (feature_set == hal::HAL_FEATURE_SET_GFT) {
+    }
+    HAL_TRACE_DEBUG("gRPC telemetry services registered ...");
+    return;
+}
+
+// initialization routine for telemetry module
+extern "C" hal_ret_t
+telemetry_init (hal_cfg_t *hal_cfg)
+{
+    svc_reg(hal_cfg->server_builder, hal_cfg->features);
+    return HAL_RET_OK;
+}
+
+// cleanup routine for mcast module
+extern "C" void
+telemetry_exit (void)
+{
+}
+
+}    // namespace telemetry
+}    // namespace hal
