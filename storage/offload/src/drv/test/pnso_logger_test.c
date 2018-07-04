8191662a3c6ed962321761a9b33440ddec815fc3
Mohan Kumar <mohan@pensando.io>
2018-07-04 13:34:00 +0000

    pnso_logger: Exercise logger APIs
    This commit is not adding a gtest, but adds a stub binary that exercises
    the log macros.
    
    Signed-off-by: Mohan Kumar <mohan@pensando.io>

---
+++ storage/offload/src/drv/test/pnso_logger_test.c
@@ -0,0 +1,54 @@
+/*
+ * {C} Copyright 2018 Pensando Systems Inc.
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "osal.h"
+#include "pnso_logger.h"
+
+static void
+exercise_logger_to_file(void)
+{
+	pnso_log_init(false, 7, "test");
+
+	PNSO_LOG_EMERG(PNSO_OK, "Emergency");
+	PNSO_LOG_ALERT(PNSO_OK, "Alert");
+	PNSO_LOG_CRITICAL(PNSO_OK, "Critical");
+	PNSO_LOG_ERROR(PNSO_OK, "Error");
+	PNSO_LOG_WARN(PNSO_OK, "Warning");
+	PNSO_LOG_NOTICE(PNSO_OK, "Notice");
+	PNSO_LOG_INFO(PNSO_OK, "Info");
+	PNSO_LOG_DEBUG(PNSO_OK, "Debug");
+
+	pnso_log_deinit();
+}
+
+static void
+exercise_logger_to_console(void)
+{
+	pnso_log_init(true, 7, "");
+
+	PNSO_LOG_EMERG(PNSO_OK, "Emergency");
+	PNSO_LOG_ALERT(PNSO_OK, "Alert");
+	PNSO_LOG_CRITICAL(PNSO_OK, "Critical");
+	PNSO_LOG_ERROR(PNSO_OK, "Error");
+	PNSO_LOG_WARN(PNSO_OK, "Warning");
+	PNSO_LOG_NOTICE(PNSO_OK, "Notice");
+	PNSO_LOG_INFO(PNSO_OK, "Info");
+	PNSO_LOG_DEBUG(PNSO_OK, "Debug");
+
+	pnso_log_deinit();
+}
+
+int
+main(void)
+{
+	exercise_logger_to_file();
+
+	exercise_logger_to_console();
+
+	return 0;
+}
