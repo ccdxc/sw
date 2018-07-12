13032db10dcf7c02f90cb8a5485e170f62ee00b0
Ramesh Ponnapalli <rameshp@pensando.io>
2018-07-12 13:02:11 -0700

    minor change for rtl

--- nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/rxdma/esp_ipv4_tunnel_h2n_update_output_desc_aol.s
+++ nic/asm/ipsec-p4+/esp_ipv4_tunnel_host_to_network/rxdma/esp_ipv4_tunnel_h2n_update_output_desc_aol.s
@@ -16,7 +16,9 @@ esp_ipv4_tunnel_h2n_update_output_desc_aol:
     add r6, k.t1_s2s_payload_size, k.ipsec_to_stage3_pad_size
     addi r6, r6, ESP_FIXED_HDR_SIZE+2
     phvwr p.barco_desc_out_L0, r6.wx 
-    phvwri p.{app_header_table0_valid...app_header_table3_valid}, 10 
+    phvwri p.app_header_table0_valid, 1 
+    phvwri p.app_header_table2_valid, 1 
+    phvwri p.app_header_table1_valid, 0
     phvwri p.common_te0_phv_table_pc, esp_ipv4_tunnel_h2n_ipsec_cb_tail_enqueue_input_desc[33:6]
     phvwri p.{common_te0_phv_table_lock_en...common_te0_phv_table_raw_table_size}, ((1 << 3) | 6 ) 
     phvwr p.common_te0_phv_table_addr, k.{ipsec_global_ipsec_cb_addr_sbit0_ebit31...ipsec_global_ipsec_cb_addr_sbit32_ebit33}
