9fd69e1d68a23e37ab3621448a29d10a0333ef23
akoradha <32655671+akoradha@users.noreply.github.com>
2018-07-20 07:21:21 -0700

    Add avago aapl (#5427)

--- nic/linkmgr/linkmgr_src.cc
+++ nic/linkmgr/linkmgr_src.cc
@@ -1282,3 +1282,22 @@ serdes_info_get(uint32_t sbus_addr,
                                                cable_type);
 }
 
+extern "C" {
+
+uint32_t jtag_wr (unsigned char chip,
+                  unsigned long long int reg_addr,
+                  uint32_t *sbus_data,
+                  unsigned long long int flag)
+{
+    return 0;
+}
+
+uint32_t jtag_rd (unsigned char chip,
+                  unsigned long long int reg_addr,
+                  uint32_t sbus_data,
+                  unsigned long long int flag)
+{
+    return 0;
+}
+}
+
--- nic/sdk/linkmgr/port_serdes.cc
+++ nic/sdk/linkmgr/port_serdes.cc
@@ -8,6 +8,7 @@
 #include "port.hpp"
 #include "linkmgr_rw.hpp"
 #include "linkmgr.hpp"
+#include "aapl.h"
 
 namespace sdk {
 namespace linkmgr {
@@ -108,6 +109,83 @@ serdes_reset_default (uint32_t sbus_addr, bool reset)
     return SDK_RET_OK;
 }
 
+//---------------------------------------------------------------------------
+// HW methods
+//---------------------------------------------------------------------------
+
+bool
+serdes_signal_detect_hw (uint32_t sbus_addr)
+{
+    return true;
+}
+
+bool
+serdes_rdy_hw (uint32_t sbus_addr)
+{
+    return true;
+}
+
+int
+serdes_output_enable_hw (uint32_t sbus_addr, bool enable)
+{
+    return SDK_RET_OK;
+}
+
+uint32_t sbus_access (Aapl_t *aapl, uint32_t addr, unsigned char reg_addr,
+                      unsigned char command, uint *sbus_data)
+{
+    return 0;
+}
+
+uint32_t spico_int (Aapl_t *aapl, uint32_t addr, int int_code, int int_data)
+{
+    return 0;
+}
+
+int
+serdes_cfg_hw (uint32_t sbus_addr, serdes_info_t *serdes_info)
+{
+    Aapl_comm_method_t comm_method = AVAGO_OFFLINE;
+
+    uint32_t   divider      = serdes_info->sbus_divider;
+    uint32_t   debug        = 1;   /* default debug level */
+    Aapl_t     *aapl         = aapl_construct();
+
+    aapl->communication_method = comm_method;
+    aapl->debug = debug;
+
+    aapl_register_sbus_fn(aapl, sbus_access, NULL, NULL);
+
+    aapl_register_spico_int_fn(aapl, spico_int);
+
+    // Make a connection to the device
+    aapl_connect(aapl, NULL, 0); if( aapl->return_code < 0 ) AAPL_EXIT(1);
+
+    /* Gather information about the device and place into AAPL struct */
+    aapl_get_ip_info(aapl,1);                 if( aapl->return_code < 0 ) AAPL_EXIT(1);
+
+    /* Initialize serdes (requires that firmware has already been loaded) */
+    avago_serdes_init_quick(aapl, sbus_addr, divider);
+
+    if( aapl->return_code ) SDK_TRACE_ERR("ERROR: Failed to initialize SerDes\n");
+
+    aapl_destruct(aapl);
+
+    return SDK_RET_OK;
+}
+
+int
+serdes_tx_rx_enable_hw (uint32_t sbus_addr, bool enable)
+{
+    return SDK_RET_OK;
+}
+
+int
+serdes_reset_hw (uint32_t sbus_addr, bool reset)
+{
+    return SDK_RET_OK;
+}
+
 sdk_ret_t
 port::port_serdes_fn_init(linkmgr_cfg_t *cfg)
 {
@@ -129,6 +207,16 @@ port::port_serdes_fn_init(linkmgr_cfg_t *cfg)
         serdes_fn->serdes_output_enable = &serdes_output_enable_haps;
         serdes_fn->serdes_tx_rx_enable = &serdes_tx_rx_enable_haps;
         serdes_fn->serdes_reset = &serdes_reset_haps;
+        break;
+
+    case platform_type_t::PLATFORM_TYPE_HW:
+    case platform_type_t::PLATFORM_TYPE_MOCK:
+        serdes_fn->serdes_cfg = &serdes_cfg_hw;
+        serdes_fn->serdes_signal_detect = &serdes_signal_detect_hw;
+        serdes_fn->serdes_rdy = &serdes_rdy_hw;
+        serdes_fn->serdes_output_enable = &serdes_output_enable_hw;
+        serdes_fn->serdes_tx_rx_enable = &serdes_tx_rx_enable_hw;
+        serdes_fn->serdes_reset = &serdes_reset_hw;
         break;
 
     default:
