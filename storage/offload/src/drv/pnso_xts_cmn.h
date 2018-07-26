83c3690cb4da564d71037911068022348caa81c8
Mohan Kumar <mohan@pensando.io>
2018-07-26 19:48:32 +0000

    pnso_xts: Add base service for encryption/decryption
    Signed-off-by: Mohan Kumar <mohan@pensando.io>

--- storage/offload/src/drv/pnso_xts_cmn.h
+++ storage/offload/src/drv/pnso_xts_cmn.h
@@ -3,11 +3,18 @@
 
 #include "pnso_chain.h"
 
+#ifdef NDEBUG
+#define XTS_PPRINT_DESC(d)
+#else
+#define XTS_PPRINT_DESC(d)						       \
+	do {								\
+		OSAL_LOG_INFO("%.*s", 30, "=========================================");\
+		xts_pprint_desc(d);					\
+		OSAL_LOG_INFO("%.*s", 30, "=========================================");\
+	} while (0)
+#endif
+
 /* XTS common/utility functions */
-pnso_error_t xts_common_chain(struct chain_entry *centry);
-
-void xts_common_teardown(void *desc);
-
 void xts_pprint_desc(const struct xts_desc *desc);
 
 #endif /* __PNSO_XTS_CMN_H__ */
