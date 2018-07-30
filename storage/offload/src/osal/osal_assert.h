eb4340caabc516374010209f36e20689b7e9bbeb
Mohan Kumar <mohan@pensando.io>
2018-07-30 14:35:02 +0000

    osal: Map OSAL_ASSERT to BUG_ON to abort in kernel
    Signed-off-by: Mohan Kumar <mohan@pensando.io>

--- storage/offload/src/osal/osal_assert.h
+++ storage/offload/src/osal/osal_assert.h
@@ -9,8 +9,12 @@
 #ifndef __KERNEL__
 #include <assert.h>
 #define OSAL_ASSERT(x) assert(x)
+#elif __KERNEL__
+#define OSAL_ASSERT(x) BUG_ON(x)
+#elif _KERNEL
+#define OSAL_ASSERT(x) BUG_ON(x)
 #else
-#define OSAL_ASSERT(x)
+#error "Invalid build!!"
 #endif
 
 #endif	/* __OSAL_ASSERT_H__ */
