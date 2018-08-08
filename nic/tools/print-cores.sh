3d7596bfb557fc569570a0bedaaf8bd8ee7dcbcc
sudhiaithal <sudhiaithal@gmail.com>
2018-08-08 08:01:52 -0700

    Fix core print script (#5682)

--- nic/tools/print-cores.sh
+++ nic/tools/print-cores.sh
@@ -5,6 +5,7 @@ for core_file in core.*; do
     [ -f "$core_file" ] || continue
     echo "********************CORE BEGIN************************" 
     exec_file="$(strings $core_file  | grep ^/ | tail -1)"
+    exec_file=`file $core_file  | grep -o -P '(?<=execfn:).*(?=,)' | tr -d "\'"`
     echo "Exec file : $exec_file"
     echo "Core file : $core_file"
     gdb -batch -ex bt $exec_file $core_file
