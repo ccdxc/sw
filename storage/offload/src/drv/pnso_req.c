7310715b8eaea7d5621c0b05c927adf3250d371a
Mohan Kumar <mohan@pensando.io>
2018-08-17 11:36:41 -0700

    pnso_req: Align to naming standard
    Signed-off-by: Mohan Kumar <mohan@pensando.io>

--- storage/offload/src/drv/pnso_req.c
+++ storage/offload/src/drv/pnso_req.c
@@ -10,13 +10,28 @@
 #include "pnso_req.h"
 #include "pnso_chain.h"
 
+/*
+ * TODO-req:
+ *	- print SHA/checksum tags in req_pprint_result()
+ *	- revisit validate_xxxx() and asserts
+ *
+ */
+
+#ifdef NDEBUG
+#define REQ_PPRINT_REQUEST(r)
+#define REQ_PPRINT_RESULT(r)
+#else
+#define REQ_PPRINT_REQUEST(r)	req_pprint_request(r)
+#define REQ_PPRINT_RESULT(r)	req_pprint_result(r)
+#endif
+
 typedef pnso_error_t (*validate_req_service_fn_t)(struct pnso_service *svc);
 
 typedef pnso_error_t (*validate_res_service_fn_t)(
 		struct pnso_service_status *svc);
 
-static void
-__pprint_crypto_desc(struct pnso_crypto_desc *desc)
+static void __attribute__((unused))
+pprint_crypto_desc(const struct pnso_crypto_desc *desc)
 {
 	if (!desc)
 		return;
@@ -29,8 +44,8 @@ __pprint_crypto_desc(struct pnso_crypto_desc *desc)
 	OSAL_LOG_INFO("%30s: %llx", "key_desc_idx", desc->iv_addr);
 }
 
-static void
-__pprint_cp_desc(struct pnso_compression_desc *desc)
+static void __attribute__((unused))
+pprint_cp_desc(const struct pnso_compression_desc *desc)
 {
 	if (!desc)
 		return;
@@ -44,8 +59,8 @@ __pprint_cp_desc(struct pnso_compression_desc *desc)
 	OSAL_LOG_INFO("%30s: %d", "hdr_fmt_idx", desc->hdr_fmt_idx);
 }
 
-static void
-__pprint_dc_desc(struct pnso_decompression_desc *desc)
+static void __attribute__((unused))
+pprint_dc_desc(const struct pnso_decompression_desc *desc)
 {
 	if (!desc)
 		return;
@@ -58,8 +73,8 @@ __pprint_dc_desc(struct pnso_decompression_desc *desc)
 	OSAL_LOG_INFO("%30s: %d", "rsvd", desc->rsvd);
 }
 
-static void
-__pprint_hash_desc(struct pnso_hash_desc *desc)
+static void __attribute__((unused))
+pprint_hash_desc(const struct pnso_hash_desc *desc)
 {
 	if (!desc)
 		return;
@@ -70,8 +85,8 @@ __pprint_hash_desc(struct pnso_hash_desc *desc)
 	OSAL_LOG_INFO("%30s: %d", "flags", desc->flags);
 }
 
-static void
-__pprint_chksum_desc(struct pnso_checksum_desc *desc)
+static void __attribute__((unused))
+pprint_chksum_desc(const struct pnso_checksum_desc *desc)
 {
 	if (!desc)
 		return;
@@ -82,8 +97,8 @@ __pprint_chksum_desc(struct pnso_checksum_desc *desc)
 	OSAL_LOG_INFO("%30s: %d", "flags", desc->flags);
 }
 
-static void
-__pprint_decompaction_desc(struct pnso_decompaction_desc *desc)
+static void __attribute__((unused))
+pprint_decompaction_desc(const struct pnso_decompaction_desc *desc)
 {
 	if (!desc)
 		return;
@@ -96,8 +111,8 @@ __pprint_decompaction_desc(struct pnso_decompaction_desc *desc)
 	OSAL_LOG_INFO("%30s: %d", "rsvd_2", desc->rsvd_2);
 }
 
-void
-req_pprint_request(struct pnso_service_request *req)
+void __attribute__((unused))
+req_pprint_request(const struct pnso_service_request *req)
 {
 	uint32_t i;
 
@@ -119,25 +134,25 @@ req_pprint_request(struct pnso_service_request *req)
 
 		switch (req->svc[i].svc_type) {
 		case PNSO_SVC_TYPE_ENCRYPT:
-			__pprint_crypto_desc(&req->svc[i].u.crypto_desc);
+			pprint_crypto_desc(&req->svc[i].u.crypto_desc);
 			break;
 		case PNSO_SVC_TYPE_DECRYPT:
-			__pprint_crypto_desc(&req->svc[i].u.crypto_desc);
+			pprint_crypto_desc(&req->svc[i].u.crypto_desc);
 			break;
 		case PNSO_SVC_TYPE_COMPRESS:
-			__pprint_cp_desc(&req->svc[i].u.cp_desc);
+			pprint_cp_desc(&req->svc[i].u.cp_desc);
 			break;
 		case PNSO_SVC_TYPE_DECOMPRESS:
-			__pprint_dc_desc(&req->svc[i].u.dc_desc);
+			pprint_dc_desc(&req->svc[i].u.dc_desc);
 			break;
 		case PNSO_SVC_TYPE_HASH:
-			__pprint_hash_desc(&req->svc[i].u.hash_desc);
+			pprint_hash_desc(&req->svc[i].u.hash_desc);
 			break;
 		case PNSO_SVC_TYPE_CHKSUM:
-			__pprint_chksum_desc(&req->svc[i].u.chksum_desc);
+			pprint_chksum_desc(&req->svc[i].u.chksum_desc);
 			break;
 		case PNSO_SVC_TYPE_DECOMPACT:
-			__pprint_decompaction_desc(
+			pprint_decompaction_desc(
 					&req->svc[i].u.decompact_desc);
 			break;
 		default:
@@ -147,8 +162,8 @@ req_pprint_request(struct pnso_service_request *req)
 	}
 }
 
-void
-req_pprint_result(struct pnso_service_result *res)
+void __attribute__((unused))
+req_pprint_result(const struct pnso_service_result *res)
 {
 	uint32_t i;
 
@@ -184,14 +199,12 @@ req_pprint_result(struct pnso_service_result *res)
 					res->svc[i].u.hash.num_tags);
 			OSAL_LOG_INFO("%30s: %d", "rsvd_2",
 					res->svc[i].u.hash.rsvd_2);
-			/* TODO: print SHA/tags */
 			break;
 		case PNSO_SVC_TYPE_CHKSUM:
 			OSAL_LOG_INFO("%30s: %d", "num_tags",
 					res->svc[i].u.chksum.num_tags);
 			OSAL_LOG_INFO("%30s: %d", "rsvd_3",
 					res->svc[i].u.chksum.rsvd_3);
-			/* TODO: print chksum/tags */
 			break;
 		default:
 			OSAL_ASSERT(0);
@@ -201,35 +214,35 @@ req_pprint_result(struct pnso_service_result *res)
 }
 
 static pnso_error_t
-__validate_req_encryption_service(struct pnso_service *svc)
+validate_req_dummy_service(struct pnso_service *svc)
 {
-	pnso_error_t err;
-
-	/* TODO: validate service input */
-	err = PNSO_OK;
-
-	return err;
+	OSAL_ASSERT(0);
+	return EINVAL;
 }
 
 static pnso_error_t
-__validate_req_decryption_service(struct pnso_service *svc)
+validate_req_encryption_service(struct pnso_service *svc)
 {
-	pnso_error_t err;
-
-	/* TODO: validate service input */
-	err = PNSO_OK;
-
-	return err;
+	OSAL_ASSERT(0);
+	return EINVAL;
 }
 
 static pnso_error_t
-__validate_req_compression_service(struct pnso_service *svc)
+validate_req_decryption_service(struct pnso_service *svc)
+{
+	OSAL_ASSERT(0);
+	return EINVAL;
+}
+
+static pnso_error_t
+validate_req_compression_service(struct pnso_service *svc)
 {
 	pnso_error_t err;
 
 	if (!svc) {
 		err = EINVAL;
-		OSAL_LOG_ERROR("invalid service! svc: 0x%llx err: %d", (u64) svc, err);
+		OSAL_LOG_ERROR("invalid service! svc: 0x%llx err: %d",
+				(u64) svc, err);
 		OSAL_ASSERT(0);
 		goto out;
 	}
@@ -242,7 +255,7 @@ __validate_req_compression_service(struct pnso_service *svc)
 		goto out;
 	}
 
-	/* TODO: validate threshold len, hdr format index */
+	/* TODO-req: validate threshold len, hdr format index */
 
 	err = PNSO_OK;
 out:
@@ -250,13 +263,14 @@ out:
 }
 
 static pnso_error_t
-__validate_req_decompression_service(struct pnso_service *svc)
+validate_req_decompression_service(struct pnso_service *svc)
 {
 	pnso_error_t err;
 
 	if (!svc) {
 		err = EINVAL;
-		OSAL_LOG_ERROR("invalid service! svc: 0x%llx %d", (u64) svc, err);
+		OSAL_LOG_ERROR("invalid service! svc: 0x%llx %d",
+				(u64) svc, err);
 		OSAL_ASSERT(0);
 		goto out;
 	}
@@ -269,7 +283,7 @@ __validate_req_decompression_service(struct pnso_service *svc)
 		goto out;
 	}
 
-	/* TODO: validate threshold len, hdr format index */
+	/* TODO-req: validate threshold len, hdr format index */
 
 	err = PNSO_OK;
 out:
@@ -277,13 +291,14 @@ out:
 }
 
 static pnso_error_t
-__validate_req_hash_service(struct pnso_service *svc)
+validate_req_hash_service(struct pnso_service *svc)
 {
 	pnso_error_t err;
 
 	if (!svc) {
 		err = EINVAL;
-		OSAL_LOG_ERROR("invalid service! svc: 0x%llx err: %d", (u64) svc, err);
+		OSAL_LOG_ERROR("invalid service! svc: 0x%llx err: %d",
+				(u64) svc, err);
 		OSAL_ASSERT(0);
 		goto out;
 	}
@@ -302,13 +317,14 @@ out:
 }
 
 static pnso_error_t
-__validate_req_chksum_service(struct pnso_service *svc)
+validate_req_chksum_service(struct pnso_service *svc)
 {
 	pnso_error_t err;
 
 	if (!svc) {
 		err = EINVAL;
-		OSAL_LOG_ERROR("invalid service! svc: 0x%llx err: %d", (u64) svc, err);
+		OSAL_LOG_ERROR("invalid service! svc: 0x%llx err: %d",
+				(u64) svc, err);
 		OSAL_ASSERT(0);
 		goto out;
 	}
@@ -327,28 +343,25 @@ out:
 }
 
 static pnso_error_t
-__validate_req_decompaction_service(struct pnso_service *svc)
+validate_req_decompaction_service(struct pnso_service *svc)
 {
-	pnso_error_t err;
-
-	/* TODO: validate service input */
-	err = PNSO_OK;
-
-	return err;
+	OSAL_ASSERT(0);
+	return EINVAL;
 }
 
 static validate_req_service_fn_t validate_req_service_fn[PNSO_SVC_TYPE_MAX] = {
-	__validate_req_encryption_service,
-	__validate_req_decryption_service,
-	__validate_req_compression_service,
-	__validate_req_decompression_service,
-	__validate_req_hash_service,
-	__validate_req_chksum_service,
-	__validate_req_decompaction_service,
+	[PNSO_SVC_TYPE_NONE] = validate_req_dummy_service,
+	[PNSO_SVC_TYPE_ENCRYPT] = validate_req_encryption_service,
+	[PNSO_SVC_TYPE_DECRYPT] = validate_req_decryption_service,
+	[PNSO_SVC_TYPE_COMPRESS] = validate_req_compression_service,
+	[PNSO_SVC_TYPE_DECOMPRESS] = validate_req_decompression_service,
+	[PNSO_SVC_TYPE_HASH] = validate_req_hash_service,
+	[PNSO_SVC_TYPE_CHKSUM] = validate_req_chksum_service,
+	[PNSO_SVC_TYPE_DECOMPACT] = validate_req_decompaction_service,
 };
 
 static bool __attribute__ ((unused))
-__is_service_type_valid(enum pnso_service_type svc_type)
+is_service_type_valid(enum pnso_service_type svc_type)
 {
 	switch (svc_type) {
 	case PNSO_SVC_TYPE_ENCRYPT:
@@ -368,7 +381,7 @@ __is_service_type_valid(enum pnso_service_type svc_type)
 }
 
 static pnso_error_t __attribute__ ((unused))
-__validate_res_service(struct pnso_service_status *status)
+validate_res_service(struct pnso_service_status *status)
 {
 	pnso_error_t err;
 
@@ -380,7 +393,7 @@ __validate_res_service(struct pnso_service_status *status)
 		goto out;
 	}
 
-	if (!__is_service_type_valid(status->svc_type)) {
+	if (!is_service_type_valid(status->svc_type)) {
 		err = EINVAL;
 		OSAL_LOG_ERROR("invalid service type specified! status: 0x%llx svc_type: %d err: %d",
 				(u64) status, status->svc_type, err);
@@ -395,28 +408,28 @@ out:
 }
 
 static pnso_error_t
-__validate_res_dummy_service(struct pnso_service_status *status)
+validate_res_dummy_service(struct pnso_service_status *status)
 {
 	OSAL_ASSERT(0);
 	return EINVAL;
 }
 
 static pnso_error_t
-__validate_res_encryption_service(struct pnso_service_status *status)
+validate_res_encryption_service(struct pnso_service_status *status)
 {
 	OSAL_ASSERT(0);
 	return EINVAL;
 }
 
 static pnso_error_t
-__validate_res_decryption_service(struct pnso_service_status *status)
+validate_res_decryption_service(struct pnso_service_status *status)
 {
 	OSAL_ASSERT(0);
 	return EINVAL;
 }
 
 static pnso_error_t
-__validate_res_compression_service(struct pnso_service_status *status)
+validate_res_compression_service(struct pnso_service_status *status)
 {
 	pnso_error_t err;
 
@@ -431,7 +444,7 @@ __validate_res_compression_service(struct pnso_service_status *status)
 }
 
 static pnso_error_t
-__validate_res_decompression_service(struct pnso_service_status *status)
+validate_res_decompression_service(struct pnso_service_status *status)
 {
 	pnso_error_t err;
 
@@ -446,7 +459,7 @@ __validate_res_decompression_service(struct pnso_service_status *status)
 }
 
 static pnso_error_t
-__validate_res_hash_service(struct pnso_service_status *status)
+validate_res_hash_service(struct pnso_service_status *status)
 {
 	pnso_error_t err;
 
@@ -470,7 +483,7 @@ out:
 }
 
 static pnso_error_t
-__validate_res_chksum_service(struct pnso_service_status *status)
+validate_res_chksum_service(struct pnso_service_status *status)
 {
 	pnso_error_t err;
 
@@ -494,29 +507,29 @@ out:
 }
 
 static pnso_error_t
-__validate_res_decompaction_service(struct pnso_service_status *status)
+validate_res_decompaction_service(struct pnso_service_status *status)
 {
 	OSAL_ASSERT(0);
 	return EINVAL;
 }
 
 static validate_res_service_fn_t validate_res_service_fn[PNSO_SVC_TYPE_MAX] = {
-	[PNSO_SVC_TYPE_NONE] = __validate_res_dummy_service,
-	[PNSO_SVC_TYPE_ENCRYPT] = __validate_res_encryption_service,
-	[PNSO_SVC_TYPE_DECRYPT] = __validate_res_decryption_service,
-	[PNSO_SVC_TYPE_COMPRESS] = __validate_res_compression_service,
-	[PNSO_SVC_TYPE_DECOMPRESS] = __validate_res_decompression_service,
-	[PNSO_SVC_TYPE_HASH] = __validate_res_hash_service,
-	[PNSO_SVC_TYPE_CHKSUM] = __validate_res_chksum_service,
-	[PNSO_SVC_TYPE_DECOMPACT] = __validate_res_decompaction_service,
+	[PNSO_SVC_TYPE_NONE] = validate_res_dummy_service,
+	[PNSO_SVC_TYPE_ENCRYPT] = validate_res_encryption_service,
+	[PNSO_SVC_TYPE_DECRYPT] = validate_res_decryption_service,
+	[PNSO_SVC_TYPE_COMPRESS] = validate_res_compression_service,
+	[PNSO_SVC_TYPE_DECOMPRESS] = validate_res_decompression_service,
+	[PNSO_SVC_TYPE_HASH] = validate_res_hash_service,
+	[PNSO_SVC_TYPE_CHKSUM] = validate_res_chksum_service,
+	[PNSO_SVC_TYPE_DECOMPACT] = validate_res_decompaction_service,
 };
 
 pnso_error_t
 validate_service_request(struct pnso_service_request *svc_req)
 {
-	pnso_error_t err;
-	uint32_t num_services;
-	uint32_t i;
+	pnso_error_t err = EINVAL;
+	enum pnso_service_type svc_type;
+	uint32_t num_services, i;
 
 	if (!svc_req)
 		return EINVAL;
@@ -534,11 +547,22 @@ validate_service_request(struct pnso_service_request *svc_req)
 		return EINVAL;
 
 	for (i = 0; i < num_services; i++) {
-		err = validate_req_service_fn[i](&svc_req->svc[i]);
+		svc_type = svc_req->svc[i].svc_type;
+		if (svc_type <= PNSO_SVC_TYPE_NONE ||
+				svc_type >= PNSO_SVC_TYPE_MAX) {
+			err = EINVAL;
+			OSAL_LOG_ERROR("invalid service type specified! service #: %d svc_type: %d err: %d",
+					i+1, svc_type, err);
+			OSAL_ASSERT(0);
+			goto out;
+		}
+
+		err = validate_req_service_fn[svc_type](&svc_req->svc[i]);
 		if (err)
 			return err;
 	}
 
+out:
 	return err;
 }
 
@@ -556,6 +580,11 @@ validate_service_result(struct pnso_service_result *svc_res)
 	}
 
 	num_services = svc_res->num_services;
+	if (num_services <= PNSO_SVC_TYPE_NONE ||
+	    num_services >= PNSO_SVC_TYPE_MAX) {
+		goto out;
+	}
+
 	for (i = 0; i < num_services; i++) {
 		svc_type = svc_res->svc[i].svc_type;
 
@@ -586,23 +615,31 @@ pnso_error_t pnso_submit_request(struct pnso_service_request *svc_req,
 {
 	pnso_error_t err;
 
-	OSAL_LOG_INFO("start ...");
+	OSAL_LOG_INFO("enter...");
+
+	REQ_PPRINT_REQUEST(svc_req);
+	REQ_PPRINT_RESULT(svc_res);
 
 	/* validate each service request */
 	err = validate_service_request(svc_req);
-	if (err)
+	if (err) {
+		OSAL_LOG_ERROR("invalid service request specified! err: %d",
+				err);
 		return err;
+	}
 
 	/* validate each service result */
 	err = validate_service_result(svc_res);
-	if (err)
+	if (err) {
+		OSAL_LOG_ERROR("invalid service result specified! err: %d",
+				err);
 		return err;
+	}
 
-	/* TODO: this one is hanging out ... */
 	if (svc_req->num_services != svc_res->num_services)
 		return EINVAL;
 
-	/* TODO:
+	/* TODO-req:
 	 *      sanitize cb, cb_ctx, etc. here and bail out from this layer
 	 *      before going deeper into other layers
 	 */
@@ -611,7 +648,7 @@ pnso_error_t pnso_submit_request(struct pnso_service_request *svc_req,
 	err = chn_build_chain(svc_req, svc_res, cb, cb_ctx,
 			pnso_poll_fn, pnso_poll_ctx);
 
-	OSAL_LOG_INFO("done");
+	OSAL_LOG_INFO("exit!");
 
 	return err;
 }
--- storage/offload/src/drv/pnso_req.h
+++ storage/offload/src/drv/pnso_req.h
@@ -10,6 +10,6 @@
 
 void req_pprint_request(const struct pnso_service_request *req);
 
-void req_pprint_result(struct pnso_service_result *res);
+void req_pprint_result(const struct pnso_service_result *res);
 
 #endif	/* __PNSO_REQ_H__ */
