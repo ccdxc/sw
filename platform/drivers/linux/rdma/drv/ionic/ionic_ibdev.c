8e523a5838fcec346544d932139de165cd8aef52
Allen Hubbe <allenbh@pensando.io>
2018-08-23 16:38:37 -0700

    ionic_rdma: fix leak of pgtbl resources related to a workaround

--- platform/drivers/linux/rdma/drv/ionic/ionic_ibdev.c
+++ platform/drivers/linux/rdma/drv/ionic/ionic_ibdev.c
@@ -435,6 +435,10 @@ static void ionic_put_mrid(struct ionic_ibdev *dev, u32 mrid)
 
 static void ionic_put_pgtbl(struct ionic_ibdev *dev, u32 pos, int order)
 {
+	/* XXX segment aligned in device pagetbl, not final */
+	if (order < 3)
+		order = 3;
+
 	mutex_lock(&dev->inuse_lock);
 	bitmap_release_region(dev->inuse_pgtbl, pos, order);
 	mutex_unlock(&dev->inuse_lock);
