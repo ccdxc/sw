f55ac07de5f28124bfb6590ab67c5e0cd28790ae
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2018-08-23 15:03:16 -0700

    workaround for not having curl on haps (#5983)

--- nic/tools/sysinit.sh
+++ nic/tools/sysinit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-NIC_DIR='/nic'
-HAL_CONFIG_PATH=$NIC_DIR/conf/
-LD_LIBRARY_PATH=$NIC_DIR/lib:$NIC_DIR/conf/sdk:$NIC_DIR/conf/linkmgr:$NIC_DIR/conf/sdk/external:/usr/local/lib:/usr/lib/aarch64-linux-gnu:/platform/lib:$LD_LIBRARY_PATH
-HAL_PBC_INIT_CONFIG="2x100_hbm"
+export NIC_DIR='/nic'
+export HAL_CONFIG_PATH=$NIC_DIR/conf/
+export LD_LIBRARY_PATH=$NIC_DIR/lib:$NIC_DIR/conf/sdk:$NIC_DIR/conf/linkmgr:$NIC_DIR/conf/sdk/external:/usr/local/lib:/usr/lib/aarch64-linux-gnu:/platform/lib:$LD_LIBRARY_PATH
+export HAL_PBC_INIT_CONFIG="2x100_hbm"
 ulimit -c unlimited
 
 # check for all the binaries
@@ -32,25 +32,30 @@ HAL_SERVER="localhost:$HAL_GRPC_PORT"
 HAL_UP=0
 
 # wait for HAL
-timeout=1
-echo "HAL WAIT BEGIN: `date +%x_%H:%M:%S:%N`"
-while [ "$HAL_UP" -eq 0 ]
-do
-  echo "Waiting for HAL GRPC server to be up. Sleeping for $timeout seconds..."
-  sleep "$timeout"
-  curl "$HAL_SERVER"
-  HAL_UP=$?
-  timeout=`expr $timeout \\* 2`
-  i=`expr $i + 1`
-  if [ $i -eq $MAX_RETRIES ]; then
-    echo "HAL server failed to come up"
-    exit 1
-  fi
-done
-echo "HAL WAIT END: `date +%x_%H:%M:%S:%N`"
+#timeout=1
+#echo "HAL WAIT BEGIN: `date +%x_%H:%M:%S:%N`"
+#while [ "$HAL_UP" -eq 0 ]
+#do
+  #echo "Waiting for HAL GRPC server to be up. Sleeping for $timeout seconds..."
+  #sleep "$timeout"
+  #curl "$HAL_SERVER"
+  #HAL_UP=$?
+  #timeout=`expr $timeout \\* 2`
+  #i=`expr $i + 1`
+  #if [ $i -eq $MAX_RETRIES ]; then
+    #echo "HAL server failed to come up"
+    #exit 1
+  #fi
+#done
+#echo "HAL WAIT END: `date +%x_%H:%M:%S:%N`"
+
+echo "Waiting for HAL to come up ..."
+sleep 600
 
 # start netagent
 $NIC_DIR/bin/netagent -datapath hal -logtofile /tmp/agent.log -hostif lo &
 
 # start nicmgr
 /platform/bin/nicmgrd > nicmgr.log 2>&1
+
+echo "All processes brought up, please check ..."
