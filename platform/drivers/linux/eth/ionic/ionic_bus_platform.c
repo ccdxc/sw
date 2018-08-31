410999a7146890da5cd07cbbc47d2f9f1e4fa5f5
Dhruval Shah <dpshah@pensando.io>
2018-08-31 11:48:42 -0700

    removed debug messages

--- platform/drivers/linux/eth/ionic/ionic_bus_platform.c
+++ platform/drivers/linux/eth/ionic/ionic_bus_platform.c
@@ -27,8 +27,6 @@
 #include "ionic_lif.h"
 #include "ionic_debugfs.h"
 
-#define FOR_TESTING_MNIC 1
-
 #define INTR_MSIXCFG_STRIDE     0x10
 #define INTR_FWCFG_STRIDE       0x8
 
@@ -75,7 +73,6 @@ void intr_fwcfg(const int intr)
     uint64_t data = 0;
 
     data = (uint64_t)((uint64_t)1 << 46)/*local_int=1*/ | ((uint64_t)4 << 32)/*lif=4*/;
-    //printk("DBG_FW_CFG: intr[%d]: writing %llx @%llx\n", intr, data, pa);
     writeq(data, (volatile void*)(pa));
 }
 
@@ -83,13 +80,8 @@ void intr_msixcfg(const int intr, const u_int64_t msgaddr, const u_int32_t msgda
 {
     u_int64_t pa = intr_msixcfg_addr(intr);
 
-    //printk("DBG: intr[%d]: writing %llx @%llx\n", intr, msgaddr, pa + offsetof(intr_msixcfg_t, msgaddr));
     writeq(msgaddr, (volatile void*)(pa + offsetof(intr_msixcfg_t, msgaddr)));
-
-    //printk("DBG: intr[%d]: writing %x @%llx\n", intr, msgdata, pa + offsetof(intr_msixcfg_t, msgdata));
     writel(msgdata, (volatile void*)(pa + offsetof(intr_msixcfg_t, msgdata)));
-    
-    //printk("DBG: intr[%d]: writing %x @%llx\n", intr, vctrl, pa + offsetof(intr_msixcfg_t, vector_ctrl));
     writel(vctrl, (volatile void*)(pa + offsetof(intr_msixcfg_t, vector_ctrl)));
 }
 
@@ -132,7 +124,6 @@ static void mnic_set_msi_msg(struct msi_desc *desc, struct msi_msg *msg)
                                 msg->address_hi, msg->address_lo, msg->data);
 
     intr_msixcfg(desc->platform.msi_index, (((uint64_t)msg->address_hi << 32) | msg->address_lo), msg->data, 0/*vctrl*/);
-
     intr_fwcfg(desc->platform.msi_index);
 }
 
@@ -141,7 +132,6 @@ int ionic_bus_alloc_irq_vectors(struct ionic *ionic, unsigned int nintrs)
 
     int err = 0;
 
-    //printk("Inside %s, nintrs = %d\n", __FUNCTION__, nintrs);
 	err =  platform_msi_domain_alloc_irqs(ionic->dev, nintrs, mnic_set_msi_msg);
     if (err)
         return err;
@@ -151,16 +141,8 @@ int ionic_bus_alloc_irq_vectors(struct ionic *ionic, unsigned int nintrs)
 
 void ionic_bus_free_irq_vectors(struct ionic *ionic)
 {
-    //printk("Inside %s\n", __FUNCTION__);
     platform_msi_domain_free_irqs(ionic->dev);
 }
-
-#ifdef  DPS_FASTMODEL
-static irqreturn_t mnic_msi_interrupt_handler(int irq, void *data)
-{
-    return IRQ_HANDLED;
-}
-#endif
 
 int ionic_mnic_dev_setup(struct ionic *ionic)
 {
@@ -179,13 +161,10 @@ int ionic_mnic_dev_setup(struct ionic *ionic)
     fwcfg_base_addr = ionic->bars[2].vaddr;
     msix_cfg_base_addr = ionic->bars[3].vaddr;
 
-    //printk("msix_cfg_base_addr = %p, phys_addr = %llx\n", msix_cfg_base_addr, ionic->bars[3].bus_addr);
-
 #ifdef HAPS
     idev->ident = ionic->bars[0].vaddr + 0x800;
 #endif
 
-    print_remove_hack_message(__LINE__, "signature read");
 #ifdef HAPS
     sig = ioread32(&idev->dev_cmd->signature);
     if (sig != DEV_CMD_SIGNATURE)
@@ -194,28 +173,6 @@ int ionic_mnic_dev_setup(struct ionic *ionic)
 
     idev->db_pages = ionic->bars[4].vaddr;
     idev->phy_db_pages = ionic->bars[4].bus_addr;
-
-#if 0
-    print_remove_hack_message(__LINE__, "msi_alloc_irq");
-    ret = platform_msi_domain_alloc_irqs(ionic->dev, 4, mnic_set_msi_msg);
-    if(ret) {
-        dev_err(ionic->dev, "platform_msi_domain_alloc_irqs failed: %d\n", ret);
-        return ret;
-    }
-
-    for_each_msi_entry(desc, ionic->dev) {
-        printk(KERN_INFO "msi_entry: %d.%d\n",
-                desc->platform.msi_index,
-                desc->irq);
-        ret = devm_request_irq(ionic->dev, desc->irq,
-                mnic_msi_interrupt_handler, 0,
-                dev_name(ionic->dev), ionic);
-        if (ret) {
-            printk(KERN_INFO "devm_request_irq failed\n");
-            return ret;
-        }
-    }
-#endif
 
 #ifndef ADMINQ
 	spin_lock_init(&ionic->cmd_lock);
@@ -248,7 +205,6 @@ static int ionic_map_bars(struct ionic *ionic)
 		bars[j].len = res->end - res->start + 1;
 		bars[j].vaddr = base;
 		bars[j].bus_addr = res->start;
-        //printk("DBG: Mapped res-%d, bars[j].bus_addr = %llx bars[j].vaddr = %p, bars[j].len = %lx", j, bars[j].bus_addr, bars[j].vaddr, bars[j].len);
 		ionic->num_bars++;
 		j++;
 	}
@@ -329,38 +285,17 @@ static int ionic_probe(struct platform_device *pfdev)
 
 	/* Allocate and init LIFs, creating a netdev per LIF
 	 */
-    //printk("Calling ionic_lif_size !!!\n");
 	err = ionic_lifs_size(ionic);
 	if (err) {
 		dev_err(dev, "Cannot size LIFs, aborting\n");
 		goto err_out_forget_identity;
 	}
 
-    //printk("Calling ionic_lif_alloc !!!\n");
 	err = ionic_lifs_alloc(ionic);
 	if (err) {
 		dev_err(dev, "Cannot allocate LIFs, aborting\n");
 		goto err_out_free_lifs;
 	}
-
-#ifdef DPS_FASTMODEL
-    for_each_msi_entry(desc, ionic->dev) {
-        printk(KERN_INFO "msi_entry: %d.%d\n",
-                desc->platform.msi_index,
-                desc->irq);
-        ret = devm_request_irq(ionic->dev, desc->irq,
-                mnic_msi_interrupt_handler, 0,
-                dev_name(ionic->dev), ionic);
-        if (ret) {
-            printk(KERN_INFO "devm_request_irq failed\n");
-            return ret;
-        }
-    }
-
-    print_remove_hack_message(__LINE__, "stopping probe here for ARM fastmodel");
-    return 0;
-
-#endif //DPS_FASTMODEL
 
 	err = ionic_lifs_init(ionic);
 	if (err) {
@@ -399,24 +334,13 @@ static int ionic_remove(struct platform_device *pfdev)
 
 	if (ionic) {
 
-        printk("DBG: executing: ionic_debugfs_del_dev...\n");
 		ionic_debugfs_del_dev(ionic);
-
-        printk("DBG: executing: ionic_lifs_unregister...\n");
 		ionic_lifs_unregister(ionic);
-        printk("DBG: executing: ionic_lifs_deinit...\n");
 		ionic_lifs_deinit(ionic);
-        printk("DBG: executing: ionic_lifs_free...\n");
 		ionic_lifs_free(ionic);
-        printk("DBG: executing: ionic_bus_free_irq_vectors...\n");
 		ionic_bus_free_irq_vectors(ionic);
-        printk("DBG: executing: ionic_forget_identity...\n");
 		ionic_forget_identity(ionic);
-        printk("DBG: executing: ionic_unmap_bars...\n");
 		ionic_unmap_bars(ionic);
-        
-        printk("DBG: Finished with ionic_remove\n");
-
 	}
 
 	return 0;
@@ -437,72 +361,12 @@ static struct platform_driver ionic_driver = {
 	},
 };
 
-
-
-
-#ifndef FOR_TESTING_MNIC
-static struct resource ionic_resources[] = {
-	{
-		.start = 0xfe800000,
-		.end   = 0xfe807fff,
-		.flags = IORESOURCE_MEM,
-		.name  = "ionic-mnic-BAR0",
-	},
-	{
-		.start = 0xfe808000,
-		.end   = 0xfe808fff,
-		.flags = IORESOURCE_MEM,
-		.name  = "ionic-mnic-BAR1",
-	},
-	{
-		.start = 32,
-		.end   = 32,
-		.flags = IORESOURCE_IRQ,
-		.name  = "ionic-mnic-irq0",
-	},
-	{
-		.start = 33,
-		.end   = 33,
-		.flags = IORESOURCE_IRQ,
-		.name  = "ionic-mnic-irq1",
-	},
-	{
-		.start = 34,
-		.end   = 34,
-		.flags = IORESOURCE_IRQ,
-		.name  = "ionic-mnic-irq2",
-	},
-};
-
-static u64 ionic_dma_mask = DMA_BIT_MASK(64);
-
-static struct platform_device ionic_device = {
-	.name          = "ionic-mnic",
-	.resource      = ionic_resources,
-	.num_resources = ARRAY_SIZE(ionic_resources),
-	.dev = {
-		.dma_mask = &ionic_dma_mask,
-		.coherent_dma_mask = DMA_BIT_MASK(64),
-	},
-};
-#endif
-
 int ionic_bus_register_driver(void)
 {
-#ifndef FOR_TESTING_MNIC
-	int err;
-
-	err = platform_device_register(&ionic_device);
-	if (err)
-		return err;
-#endif
 	return platform_driver_register(&ionic_driver);
 }
 
 void ionic_bus_unregister_driver(void)
 {
 	platform_driver_unregister(&ionic_driver);
-#ifndef FOR_TESTING_MNIC
-	platform_device_unregister(&ionic_device);
-#endif
 }
