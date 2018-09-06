b773422e170239dfae3176be120975e15a6850f0
Nitish Bhat <bhatnitish@gmail.com>
2018-09-05 23:34:29 -0700

    Move RDMA Create CQ to Dev Cmd interface temporarily (#6202)
    Admin Cmd interface is broken with nicmgr changes.
    Fix issues and move Dev Cmds to AdminQ.
    
    Signed-off-by: Nitish Bhat <bnitish@pensando.io>

--- platform/drivers/linux/rdma/drv/ionic/ionic_fw.h
+++ platform/drivers/linux/rdma/drv/ionic/ionic_fw.h
@@ -574,7 +574,6 @@ struct ionic_v1_admin_query_qp {
 /* admin queue v1 opcodes */
 enum ionic_v1_admin_op {
 	IONIC_V1_ADMIN_NOOP,
-	IONIC_V1_ADMIN_CREATE_CQ = 1,
     IONIC_V1_ADMIN_DEBUG = 12,
     
 	/* TODO: move ops up as they are assigned opcode numbers in fw */
@@ -583,6 +582,7 @@ enum ionic_v1_admin_op {
 	IONIC_V1_ADMIN_STATS_VALS,
 	IONIC_V1_ADMIN_CREATE_MR,
 	IONIC_V1_ADMIN_DESTROY_MR,
+	IONIC_V1_ADMIN_CREATE_CQ,
 	IONIC_V1_ADMIN_DESTROY_CQ,
 	IONIC_V1_ADMIN_CREATE_QP,
 	IONIC_V1_ADMIN_MODIFY_QP,
