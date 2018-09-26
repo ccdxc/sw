efd185f43080becbd15b02c56d210884c3e3dfdd
leslie-qiwa <34456213+leslie-qiwa@users.noreply.github.com>
2018-09-26 11:32:13 -0700

    update golang version from 1.9.5 to 1.10.3 for e2e test (#6541)
    Signed-off-by: Leslie Qi Wang <qiwa@pensando.io>

--- test/ci_targets/e2e/box.rb
+++ test/ci_targets/e2e/box.rb
@@ -5,6 +5,6 @@ env PATH: "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/loc
 
 run "apt update"
 run "apt install -y curl libltdl-dev"
-run "curl -sSL http://package-mirror.test.pensando.io/go1.9.5.linux-amd64.tar.gz | tar xz -C /usr/local"
+run "curl -sSL http://package-mirror.test.pensando.io/go1.10.3.linux-amd64.tar.gz | tar xz -C /usr/local"
 
 workdir "/go"
