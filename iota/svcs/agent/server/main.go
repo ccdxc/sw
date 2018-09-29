195dec61d72d7b612f05cdc4065f2dc5414a753e
sudhiaithal <sudhiaithal@gmail.com>
2018-09-28 19:13:10 -0700

    Remove iota agent stub (#7)

--- iota/svcs/agent/server/main.go
+++ iota/svcs/agent/server/main.go
@@ -1,24 +1,63 @@
-package server
+package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
-	//agent "github.com/pensando/sw/iota/svcs/agent"
+
+	pb "github.com/pensando/sw/iota/protos/gogen"
+	Globals "github.com/pensando/sw/iota/svcs/globals"
 )
 
 var grpcServer *grpc.Server
 
-type IotaAgent struct {
+type iotaStubAgent struct {
+}
+
+// AddNode brings up the node with the personality
+func (*iotaStubAgent) AddNode(context.Context, *pb.IotaNode) (*pb.IotaNode, error) {
+	return nil, nil
+}
+
+// DeleteNode, remove the personaltiy set
+func (*iotaStubAgent) DeleteNode(context.Context, *pb.Node) (*pb.Node, error) {
+
+	return nil, nil
+}
+
+// AddWorkloads brings up a workload type on a given node
+func (*iotaStubAgent) AddWorkload(context.Context, *pb.Workload) (*pb.Workload, error) {
+
+	return nil, nil
+}
+
+// DeleteWorkloads deletes a given workload
+func (*iotaStubAgent) DeleteWorkload(context.Context, *pb.Workload) (*pb.Workload, error) {
+	return nil, nil
+
+}
+
+// Trigger invokes the workload's trigger. It could be ping, start client/server etc..
+func (*iotaStubAgent) Trigger(context.Context, *pb.TriggerMsg) (*pb.TriggerMsg, error) {
+	return nil, nil
+
+}
+
+// CheckClusterHealth returns the cluster health
+func (*iotaStubAgent) CheckHealth(context.Context, *pb.NodeHealth) (*pb.NodeHealth, error) {
+	return nil, nil
+
 }
 
 //AgentInstance instance
-var iotaAgent *IotaAgent
+//var iotaAgent *iotaStubAgent
 
 //StartServer Start Grpc Server
-func StartServer(port int) {
+func StartServer(port int, iotaAgent pb.IotaAgentApiServer) {
 	//_ = os.Mkdir(agent.LogDir, 644)
 	// create a listener on TCP port 7777
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -28,6 +67,8 @@ func StartServer(port int) {
 	// create a gRPC server object
 	grpcServer = grpc.NewServer()
 	// attach the App service to the server
+	pb.RegisterIotaAgentApiServer(grpcServer, iotaAgent)
+
 	// start the server
 	log.Print("Starting Grpc server started on port :", port)
 	if err := grpcServer.Serve(lis); err == grpc.ErrServerStopped {
@@ -43,10 +84,27 @@ func StopServer() {
 	grpcServer.Stop()
 }
 
-func NewIotaAgent() *IotaAgent {
-	return &IotaAgent{}
+func newiotaStubAgent() *iotaStubAgent {
+	return &iotaStubAgent{}
 }
 
+func main() {
+	var iotaAgent pb.IotaAgentApiServer
+
+	if *args.stubmode {
+		iotaAgent = newiotaStubAgent()
+	} else {
+		iotaAgent = newiotaAgent()
+	}
+	StartServer(Globals.AgentPort, iotaAgent)
+}
+
+type cmdArgs struct {
+	stubmode *bool
+}
+
+var args = cmdArgs{}
+
 func init() {
-	iotaAgent = NewIotaAgent()
+	args.stubmode = flag.Bool("stubmode", true, "Stub mode")
 }
