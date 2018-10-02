5531576a24ac655a4c8321f3aaf10784b632ea92
Suresh Kumar Nalluru <nasuku@users.noreply.github.com>
2018-10-01 17:23:35 -0700

    changes to provide ova image to venice customer (#6602)
    * changes to give ova image to customers
    
    * install the content to /home/vm instead of /home/vagrant

---
+++ test/topos/gs/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /home/vm/venice
+docker load -i /home/vm/venice/pen-dind.tar
+docker load -i /home/vm/venice/pen-e2e.tar
+docker load -i /home/vm/venice/venice/tars/pen-netagent.tar
+
+./dind/do.py -configFile /home/vm/venice/testbed.json -custom_config_file /home/vm/venice/venice-conf.json -venice_image_dir /home/vm/venice/venice
+echo "Waiting to create auth policy and user......"
+sleep 120
+newman run /home/vm/venice/authbootstrap_postman_collection.json
---
+++ test/topos/gs/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/vm/venice
+./dind/do.py -configFile /home/vm/venice/testbed.json -delete
