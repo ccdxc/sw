f294f6e300491bfd580c5f57a676367fba8f90ef
Ranjith <rchirakk@users.noreply.github.com>
2018-10-06 17:52:53 -0700

    disk and network stats (#6714)
    * disk stats
    
    Signed-off-by: Ranjith <rchirakk@users.noreply.github.com>
    
    * addressed comments
    
    Signed-off-by: Ranjith <rchirakk@users.noreply.github.com>

--- venice/utils/nodewatcher/nodewatcher.go
+++ venice/utils/nodewatcher/nodewatcher.go
@@ -5,11 +5,13 @@ package nodewatcher
 import (
 	"context"
 	"fmt"
+	"math"
 	"time"
 
-	"github.com/minio/minio/cmd/logger"
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 
 	"github.com/pensando/sw/api"
 	"github.com/pensando/sw/venice/globals"
@@ -26,12 +28,18 @@ const (
 // Metrics reported for a Venice Node or NAPLES.
 // TODO: transition to using NodeMetrics schema once its defined.
 type nodeMetrics struct {
-	MemTotal       api.Gauge
-	MemAvailable   api.Gauge
-	MemFree        api.Gauge
-	MemUsed        api.Gauge
-	MemUsedPercent api.Gauge
-	CPUUsedPercent api.Gauge
+	MemTotal         api.Gauge
+	MemAvailable     api.Gauge
+	MemFree          api.Gauge
+	MemUsed          api.Gauge
+	MemUsedPercent   api.Gauge
+	CPUUsedPercent   api.Gauge
+	DiskFree         api.Gauge
+	DiskUsed         api.Gauge
+	DiskTotal        api.Gauge
+	InterfaceName    api.String
+	InterfaceRxBytes api.Gauge
+	InterfaceTxBytes api.Gauge
 }
 
 // nodewatcher monitors system resources. It can run on Venice Nodes or on NAPLES.
@@ -50,11 +58,11 @@ func NewNodeWatcher(ctx context.Context, obj runtime.Object, resolver resolver.I
 	}
 
 	if resolver == nil {
-		return fmt.Errorf("Need a resolver")
+		return fmt.Errorf("need a resolver")
 	}
 
 	if frequency < minFrequency {
-		return fmt.Errorf("Min frequency is %v", minFrequency)
+		return fmt.Errorf("minimum frequency is %v", minFrequency)
 	}
 
 	opts := &tsdb.Opts{
@@ -90,28 +98,79 @@ func (w *nodewatcher) periodicUpdate(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			logger.Info("Metrics watcher context cancelled, exiting")
+			w.logger.Info("Metrics watcher context cancelled, exiting")
 			return
 		case <-time.After(time.Duration(w.frequency) * time.Second):
 			t := time.Now()
+
+			// memory
 			vmstat, err := mem.VirtualMemory()
 			if err != nil {
 				w.logger.Errorf("Node Watcher: failed to read virtual memory info, error: %v", err)
 				continue
 			}
-			w.metricObj.MemAvailable.Set(float64(vmstat.Available), t)
-			w.metricObj.MemFree.Set(float64(vmstat.Free), t)
-			w.metricObj.MemUsed.Set(float64(vmstat.Used), t)
-			w.metricObj.MemTotal.Set(float64(vmstat.Total), t)
-			w.metricObj.MemUsedPercent.Set(float64(vmstat.UsedPercent), t)
+			w.metricObj.MemAvailable.Set(float64(vmstat.Available>>20), t)
+			w.metricObj.MemFree.Set(float64(vmstat.Free>>20), t)
+			w.metricObj.MemUsed.Set(float64(vmstat.Used>>20), t)
+			w.metricObj.MemTotal.Set(float64(vmstat.Total>>20), t)
+			w.metricObj.MemUsedPercent.Set(math.Floor(vmstat.UsedPercent*100)/100, t)
 
+			// cpu
 			cpuPercent, err := cpu.Percent(0, false)
 			if err != nil {
 				w.logger.Errorf("Node Watcher: failed to read cpu percent, error: %v", err)
 				continue
 			}
-			w.metricObj.CPUUsedPercent.Set(cpuPercent[0], t)
+			w.metricObj.CPUUsedPercent.Set(math.Ceil(cpuPercent[0]*100)/100, t)
 			w.logger.Debugf("Node Watcher: recording new metrics, mem used %v%%, cpu used %v%%", vmstat.UsedPercent, cpuPercent[0])
+
+			// disk
+			part, err := disk.PartitionsWithContext(ctx, false)
+			if err != nil {
+				w.logger.Errorf("Node Watcher: failed to read disk partitions, error: %v", err)
+				continue
+			}
+
+			diskFree := uint64(0)
+			diskUsed := uint64(0)
+			diskTotal := uint64(0)
+
+			for _, p := range part {
+				usage, err := disk.UsageWithContext(ctx, p.Mountpoint)
+				if err != nil {
+					w.logger.Errorf("Node Watcher: failed to read disk %+v, error: %v", p, err)
+					continue
+				}
+
+				diskFree += usage.Free
+				diskUsed += usage.Used
+				diskTotal += usage.Total
+			}
+			w.metricObj.DiskFree.Set(math.Floor(float64(diskFree>>20)), t)
+			w.metricObj.DiskUsed.Set(math.Ceil(float64(diskUsed>>20)), t)
+			w.metricObj.DiskTotal.Set(math.Floor(float64(diskTotal>>20)), t)
+
+			// network
+			stat, err := net.IOCountersWithContext(ctx, true)
+			if err != nil {
+				w.logger.Errorf("Node Watcher: failed to read net stats, error: %v", err)
+				continue
+			}
+
+			// report metrics for eth0, TODO: report multiple network interfaces
+			phyIntf := map[string]bool{
+				"eth0": true,
+			}
+
+			for _, s := range stat {
+				if _, ok := phyIntf[s.Name]; ok {
+					w.metricObj.InterfaceName.Set(s.Name, t)
+					w.metricObj.InterfaceRxBytes.Set(float64(s.BytesRecv), t)
+					w.metricObj.InterfaceTxBytes.Set(float64(s.BytesSent), t)
+					// fill metrics only for one interface
+					break
+				}
+			}
 		}
 	}
 }
