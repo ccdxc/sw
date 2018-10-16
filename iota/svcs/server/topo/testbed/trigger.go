b28ff033028429cdd1ae70f86fd326838a4caba4
sudhiaithal <sudhiaithal@gmail.com>
2018-10-15 17:57:28 -0700

    Trigger fix (#80)

--- iota/svcs/server/topo/testbed/trigger.go
+++ iota/svcs/server/topo/testbed/trigger.go
@@ -23,6 +23,6 @@ func (n *TestNode) Trigger(index int) error {
 		return fmt.Errorf("adding workload on node %v failed. Agent Returned non ok status: %v", n.Node.Name, resp.ApiResponse.ApiStatus)
 	}
 
-	n.TriggerResp[index] = resp
+	n.TriggerResp = append(n.TriggerResp, resp)
 	return nil
 }
