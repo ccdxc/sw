90e818f7e765c5626c01587c924c2d47f323c200
Abhijeet Mahagaonkar <31998629+abhijeet-mahagaonkar@users.noreply.github.com>
2018-10-18 17:44:48 -0700

    Add NMD to package. (#6776)
    Changes:
    	1. naples tar needs nmd to be built and packaged.
    	2. This change adds nmd to pack_haps and pack_sim

---
+++ nic/agent/cmd/nmd/module.mk
@@ -0,0 +1,8 @@
+# {C} Copyright 2018 Pensando Systems Inc. All rights reserved
+include ${MKDEFS}/pre.mk
+MODULE_TARGET   := nmd.gobin
+MODULE_PREREQS  := agent_halproto.submake
+MODULE_PIPELINE := iris
+MODULE_FLAGS    := -ldflags="-s -w"
+MODULE_DEPS     := $(shell find ${NICDIR}/agent/ -name '*.go')
+include ${MKDEFS}/post.mk
