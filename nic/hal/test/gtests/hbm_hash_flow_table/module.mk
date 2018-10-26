68020c0eba1d13139b291db462ab3355ddf887f7
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2018-10-26 16:25:45 -0700

    more gtest compilation optimization (#7127)

--- nic/hal/test/gtests/hbm_hash_flow_table/module.mk
+++ nic/hal/test/gtests/hbm_hash_flow_table/module.mk
@@ -2,6 +2,7 @@
 include ${MKDEFS}/pre.mk
 MODULE_TARGET   = hbm_hash_flow_table_test.gtest
 MODULE_PIPELINE = iris
+MODULE_ARCH     = x86_64
 MODULE_SOLIBS   = ${NIC_HAL_GTEST_SOLIBS}
 MODULE_LDLIBS   = ${NIC_HAL_GTEST_LDLIBS}
 include ${MKDEFS}/post.mk
