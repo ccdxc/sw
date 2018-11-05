fc313d91d29f641c87b064815abd993a2cc6964e
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2018-11-04 19:44:35 -0800

    moving hal logs to /data/ (#7342)

--- nic/tools/start-hal-haps.sh
+++ nic/tools/start-hal-haps.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
 SYSCONFIG=/sysconfig/config0
 TOOLS_DIR=`dirname $0`
 ABS_TOOLS_DIR=`readlink -f $TOOLS_DIR`
 export NIC_DIR=`dirname $ABS_TOOLS_DIR`
 export HAL_CONFIG_PATH=$NIC_DIR/conf/
+export HAL_LOG_DIR=/data/
 export HAL_LIBRARY_PATH=$NIC_DIR/lib:$NIC_DIR/../platform/lib:/usr/local/lib:/usr/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
 export HAL_PBC_INIT_CONFIG="2x100_hbm"
 
