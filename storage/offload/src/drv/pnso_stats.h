1fec49f02e30827516e5ceba70fa2121286701c3
Mohan Kumar <mohan@pensando.io>
2018-11-12 10:18:45 -0800

    pnso_stats: Park osal 32-bit atomics in stats counters
    This reverts commit acafc6aa357c4f45eb8819b3cc83fb8b09af8fe4.

--- storage/offload/src/drv/pnso_stats.h
+++ storage/offload/src/drv/pnso_stats.h
@@ -65,119 +65,87 @@ struct pnso_api_stats {
 };
 
 #define PAS_INC_NUM_REQUESTS(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_requests, 1)
+	(pcr->api_stats.pas_num_requests += 1)
 #define PAS_INC_NUM_SERVICES(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_services, 1)
+	(pcr->api_stats.pas_num_services += 1)
 #define PAS_INC_NUM_CHAINS(pcr)						\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_chains, 1)
+	(pcr->api_stats.pas_num_chains += 1)
 #define PAS_INC_NUM_BATCHES(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_batches, 1)
+	(pcr->api_stats.pas_num_batches += 1)
 
 #define PAS_INC_NUM_REQUEST_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_request_failures, 1)
+	(pcr->api_stats.pas_num_request_failures += 1)
 #define PAS_INC_NUM_SERVICE_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_service_failures, 1)
+	(pcr->api_stats.pas_num_service_failures += 1)
 #define PAS_INC_NUM_CHAIN_FAILURES(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_chain_failures, 1)
+	(pcr->api_stats.pas_num_chain_failures += 1)
 #define PAS_INC_NUM_BATCH_FAILURES(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_batch_failures, 1)
+	(pcr->api_stats.pas_num_batch_failures += 1)
 
 #define PAS_INC_NUM_ENC_REQUESTS(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_enc_requests, 1)
+	(pcr->api_stats.pas_num_enc_requests += 1)
 #define PAS_INC_NUM_DEC_REQUESTS(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dec_requests, 1)
+	(pcr->api_stats.pas_num_dec_requests += 1)
 #define PAS_INC_NUM_CP_REQUESTS(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_cp_requests, 1)
+	(pcr->api_stats.pas_num_cp_requests += 1)
 #define PAS_INC_NUM_DC_REQUESTS(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dc_requests, 1)
+	(pcr->api_stats.pas_num_dc_requests += 1)
 #define PAS_INC_NUM_HASH_REQUESTS(pcr)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_hash_requests, 1)
+	(pcr->api_stats.pas_num_hash_requests += 1)
 #define PAS_INC_NUM_CHKSUM_REQUESTS(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_chksum_requests, 1)
+	(pcr->api_stats.pas_num_chksum_requests += 1)
 
 #define PAS_INC_NUM_ENC_REQUEST_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_enc_request_failures, 1)
+	(pcr->api_stats.pas_num_enc_request_failures += 1)
 #define PAS_INC_NUM_DEC_REQUEST_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dec_request_failures, 1)
+	(pcr->api_stats.pas_num_dec_request_failures += 1)
 #define PAS_INC_NUM_CP_REQUEST_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_cp_request_failures, 1)
+	(pcr->api_stats.pas_num_cp_request_failures += 1)
 #define PAS_INC_NUM_DC_REQUEST_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dc_request_failures, 1)
+	(pcr->api_stats.pas_num_dc_request_failures += 1)
 #define PAS_INC_NUM_HASH_REQUEST_FAILURES(pcr)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_hash_request_failures, 1)
+	(pcr->api_stats.pas_num_hash_request_failures += 1)
 #define PAS_INC_NUM_CHKSUM_REQUEST_FAILURES(pcr)			\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_chksum_request_failures, 1)
+	(pcr->api_stats.pas_num_chksum_request_failures += 1)
 
 #define PAS_INC_NUM_ENC_BYTES(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_enc_bytes, bytes)
+	(pcr->api_stats.pas_num_enc_bytes += bytes)
 #define PAS_INC_NUM_DEC_BYTES(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dec_bytes, bytes)
+	(pcr->api_stats.pas_num_dec_bytes += bytes)
 
 #define PAS_INC_NUM_CP_BYTES_IN(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_cp_bytes_in, bytes)
+	(pcr->api_stats.pas_num_cp_bytes_in += bytes)
 #define PAS_INC_NUM_CP_BYTES_OUT(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_cp_bytes_out, bytes)
+	(pcr->api_stats.pas_num_cp_bytes_out += bytes)
 
 #define PAS_INC_NUM_DC_BYTES_IN(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dc_bytes_in, bytes)
+	(pcr->api_stats.pas_num_dc_bytes_in += bytes)
 #define PAS_INC_NUM_DC_BYTES_OUT(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_dc_bytes_out, bytes)
+	(pcr->api_stats.pas_num_dc_bytes_out += bytes)
 
 /* TODO-stats: Separate count for different algo?? */
 #define PAS_INC_NUM_HASH_BYTES_IN(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_hash_bytes_in,  bytes)
+	(pcr->api_stats.pas_num_hash_bytes_in += bytes)
 #define PAS_INC_NUM_HASHES(pcr, count)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_hashes, count)
+	(pcr->api_stats.pas_num_hashes += count)
 
 #define PAS_INC_NUM_CHKSUM_BYTES_IN(pcr, bytes)				\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_chksum_bytes_in, bytes)
+	(pcr->api_stats.pas_num_chksum_bytes_in += bytes)
 #define PAS_INC_NUM_CHKSUMS(pcr, count)					\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_num_chksums, count)
+	(pcr->api_stats.pas_num_chksums += count)
 
 /* for measuring the time spent in both software and hardware */
 #define PAS_DECL_PERF()		ktime_t s
 #define PAS_START_PERF()	s = ktime_get()
 #define PAS_END_PERF(pcr)						\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-			&pcr->api_stats.pas_total_latency,		\
-			ktime_us_delta(ktime_get(), s))
+	pcr->api_stats.pas_total_latency +=				\
+		ktime_us_delta(ktime_get(), s);
 
 /* for measuring the time spent just in hardware */
 #define PAS_DECL_HW_PERF()	ktime_t h
 #define PAS_START_HW_PERF()	h = ktime_get()
 #define PAS_END_HW_PERF(pcr)						\
-	osal_atomic_fetch_add((osal_atomic_int_t *)			\
-		&pcr->api_stats.pas_total_hw_latency, 			\
+	(pcr->api_stats.pas_total_hw_latency +=				\
 		ktime_us_delta(ktime_get(), h))
 
 #define PAS_SHOW_STATS(pcr)	pas_show_stats(&pcr->api_stats);
