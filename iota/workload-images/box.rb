8cc9b4053418b3c0fc23ac7feba4b1b326bdb302
pavramas <pavramas@pensando.io>
2018-11-14 10:56:13 -0800

    Add ALG Apps to IOTA container (#7536)

--- iota/workload-images/box.rb
+++ iota/workload-images/box.rb
@@ -4,6 +4,7 @@ run "yum -y install https://centos7.iuscommunity.org/ius-release.rpm"
 run "yum -y install python36u python36u-pip"
 run "ln -s /usr/bin/python3.6 /usr/bin/python3"
 run "ln -s /usr/bin/pip3.6 /usr/bin/pip3"
+run "yum -y install http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm"
 
 
 PIP2_PACKAGES = %w[
@@ -38,6 +39,9 @@ PACKAGES = %w[
   dhclient
   net-tools
   iperf
+  tftp
+  ftp
+  live555-tools
 ]
 
 run "yum install -y #{PACKAGES.join(" ")}"
