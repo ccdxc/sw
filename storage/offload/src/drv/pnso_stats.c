6177c25ee47ff76d2808f606904f974249f0dc39
Mohan Kumar <mohan@pensando.io>
2018-11-14 15:29:15 -0800

    pnso_stats: Use linuxkpi atomic64 and its macros
    Squashed:
    	pnso_stats: Add linux/atomic.h to compile in FreeBSD
    
    Signed-off-by: Mohan Kumar <mohan@pensando.io>

--- storage/offload/src/drv/pnso_stats.c
+++ storage/offload/src/drv/pnso_stats.c
@@ -17,84 +17,84 @@ pprint_pnso_stats(struct pnso_api_stats *stats)
 
 	OSAL_LOG_NOTICE("%30s: 0x" PRIx64, "=== stats", (uint64_t) stats);
 
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_requests",
-			stats->pas_num_requests);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_services",
-			stats->pas_num_services);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_chains",
-			stats->pas_num_chains);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_batches",
-			stats->pas_num_batches);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_requests",
+			stats->pas_num_requests.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_services",
+			stats->pas_num_services.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_chains",
+			stats->pas_num_chains.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_batches",
+			stats->pas_num_batches.counter);
 
 	OSAL_LOG_NOTICE("%30s:", "===");
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_request_failures",
-			stats->pas_num_request_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_service_failures",
-			stats->pas_num_service_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_chain_failures",
-			stats->pas_num_chain_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_batch_failures",
-			stats->pas_num_batch_failures);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_request_failures",
+			stats->pas_num_request_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_service_failures",
+			stats->pas_num_service_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_chain_failures",
+			stats->pas_num_chain_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_batch_failures",
+			stats->pas_num_batch_failures.counter);
 
 	OSAL_LOG_NOTICE("%30s:", "===");
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_enc_requests",
-			stats->pas_num_enc_requests);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dec_requests",
-			stats->pas_num_dec_requests);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_cp_requests",
-			stats->pas_num_cp_requests);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dc_requests",
-			stats->pas_num_dc_requests);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_hash_requests",
-			stats->pas_num_hash_requests);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_chksum_requests",
-			stats->pas_num_chksum_requests);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_enc_requests",
+			stats->pas_num_enc_requests.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dec_requests",
+			stats->pas_num_dec_requests.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_cp_requests",
+			stats->pas_num_cp_requests.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dc_requests",
+			stats->pas_num_dc_requests.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_hash_requests",
+			stats->pas_num_hash_requests.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_chksum_requests",
+			stats->pas_num_chksum_requests.counter);
 
 	OSAL_LOG_NOTICE("%30s:", "===");
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_enc_request_failures",
-			stats->pas_num_enc_request_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dec_request_failures",
-			stats->pas_num_dec_request_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_cp_request_failures",
-			stats->pas_num_cp_request_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dc_request_failures",
-			stats->pas_num_dc_request_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_hash_request_failures",
-			stats->pas_num_hash_request_failures);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_chksum_request_failures",
-			stats->pas_num_chksum_request_failures);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_enc_request_failures",
+			stats->pas_num_enc_request_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dec_request_failures",
+			stats->pas_num_dec_request_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_cp_request_failures",
+			stats->pas_num_cp_request_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dc_request_failures",
+			stats->pas_num_dc_request_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_hash_request_failures",
+			stats->pas_num_hash_request_failures.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_chksum_request_failures",
+			stats->pas_num_chksum_request_failures.counter);
 
 	OSAL_LOG_NOTICE("%30s:", "===");
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_enc_bytes",
-			stats->pas_num_enc_bytes);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dec_bytes",
-			stats->pas_num_dec_bytes);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_enc_bytes",
+			stats->pas_num_enc_bytes.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dec_bytes",
+			stats->pas_num_dec_bytes.counter);
 
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_cp_bytes_in",
-			stats->pas_num_cp_bytes_in);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_cp_bytes_out",
-			stats->pas_num_cp_bytes_out);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_cp_bytes_in",
+			stats->pas_num_cp_bytes_in.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_cp_bytes_out",
+			stats->pas_num_cp_bytes_out.counter);
 
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dc_bytes_in",
-			stats->pas_num_dc_bytes_in);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_dc_bytes_out",
-			stats->pas_num_dc_bytes_out);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dc_bytes_in",
+			stats->pas_num_dc_bytes_in.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_dc_bytes_out",
+			stats->pas_num_dc_bytes_out.counter);
 
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_hash_bytes_in",
-			stats->pas_num_hash_bytes_in);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_hashes",
-			stats->pas_num_hashes);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_hash_bytes_in",
+			stats->pas_num_hash_bytes_in.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_hashes",
+			stats->pas_num_hashes.counter);
 
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_chksum_bytes_in",
-			stats->pas_num_chksum_bytes_in);
-	OSAL_LOG_NOTICE("%30s: " PRIu64, "pas_num_chksums",
-			stats->pas_num_chksums);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_chksum_bytes_in",
+			stats->pas_num_chksum_bytes_in.counter);
+	OSAL_LOG_NOTICE("%30s: %ld", "pas_num_chksums",
+			stats->pas_num_chksums.counter);
 
 	OSAL_LOG_NOTICE("%30s:", "===");
-	OSAL_LOG_NOTICE("%30s: " PRIu64 " (%s)", "pas_total_latency",
-			stats->pas_total_latency, "us");
-	OSAL_LOG_NOTICE("%30s: " PRIu64 " (%s)", "pas_total_hw_latency",
-			stats->pas_total_hw_latency, "us");
+	OSAL_LOG_NOTICE("%30s: %ld (%s)", "pas_total_latency",
+			stats->pas_total_latency.counter, "us");
+	OSAL_LOG_NOTICE("%30s: %ld (%s)", "pas_total_hw_latency",
+			stats->pas_total_hw_latency.counter, "us");
 }
 
 void
--- storage/offload/src/drv/pnso_stats.h
+++ storage/offload/src/drv/pnso_stats.h
@@ -6,6 +6,7 @@
 #ifndef __PNSO_STATS_H__
 #define __PNSO_STATS_H__
 
+#include <linux/atomic.h>
 #include "pnso_api.h"
 
 #ifdef __cplusplus
@@ -21,132 +22,132 @@ extern "C" {
  *
  */
 struct pnso_api_stats {
-	uint64_t pas_num_requests;
-	uint64_t pas_num_services;
-	uint64_t pas_num_chains;
-	uint64_t pas_num_batches;
+	atomic64_t pas_num_requests;
+	atomic64_t pas_num_services;
+	atomic64_t pas_num_chains;
+	atomic64_t pas_num_batches;
 
-	uint64_t pas_num_request_failures;
-	uint64_t pas_num_service_failures;
-	uint64_t pas_num_chain_failures;
-	uint64_t pas_num_batch_failures;
+	atomic64_t pas_num_request_failures;
+	atomic64_t pas_num_service_failures;
+	atomic64_t pas_num_chain_failures;
+	atomic64_t pas_num_batch_failures;
 
-	uint64_t pas_num_enc_requests;
-	uint64_t pas_num_dec_requests;
-	uint64_t pas_num_cp_requests;
-	uint64_t pas_num_dc_requests;
-	uint64_t pas_num_hash_requests;
-	uint64_t pas_num_chksum_requests;
+	atomic64_t pas_num_enc_requests;
+	atomic64_t pas_num_dec_requests;
+	atomic64_t pas_num_cp_requests;
+	atomic64_t pas_num_dc_requests;
+	atomic64_t pas_num_hash_requests;
+	atomic64_t pas_num_chksum_requests;
 
-	uint64_t pas_num_enc_request_failures;
-	uint64_t pas_num_dec_request_failures;
-	uint64_t pas_num_cp_request_failures;
-	uint64_t pas_num_dc_request_failures;
-	uint64_t pas_num_hash_request_failures;
-	uint64_t pas_num_chksum_request_failures;
+	atomic64_t pas_num_enc_request_failures;
+	atomic64_t pas_num_dec_request_failures;
+	atomic64_t pas_num_cp_request_failures;
+	atomic64_t pas_num_dc_request_failures;
+	atomic64_t pas_num_hash_request_failures;
+	atomic64_t pas_num_chksum_request_failures;
 
-	uint64_t pas_num_enc_bytes;
-	uint64_t pas_num_dec_bytes;
+	atomic64_t pas_num_enc_bytes;
+	atomic64_t pas_num_dec_bytes;
 
-	uint64_t pas_num_cp_bytes_in;
-	uint64_t pas_num_cp_bytes_out;
+	atomic64_t pas_num_cp_bytes_in;
+	atomic64_t pas_num_cp_bytes_out;
 
-	uint64_t pas_num_dc_bytes_in;
-	uint64_t pas_num_dc_bytes_out;
+	atomic64_t pas_num_dc_bytes_in;
+	atomic64_t pas_num_dc_bytes_out;
 
-	uint64_t pas_num_hash_bytes_in;
-	uint64_t pas_num_hashes;
+	atomic64_t pas_num_hash_bytes_in;
+	atomic64_t pas_num_hashes;
 
-	uint64_t pas_num_chksum_bytes_in;
-	uint64_t pas_num_chksums;
+	atomic64_t pas_num_chksum_bytes_in;
+	atomic64_t pas_num_chksums;
 
-	uint64_t pas_total_latency;
-	uint64_t pas_total_hw_latency;
+	atomic64_t pas_total_latency;
+	atomic64_t pas_total_hw_latency;
 };
 
 #define PAS_INC_NUM_REQUESTS(pcr)					\
-	(pcr->api_stats.pas_num_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_requests)
 #define PAS_INC_NUM_SERVICES(pcr)					\
-	(pcr->api_stats.pas_num_services += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_services)
 #define PAS_INC_NUM_CHAINS(pcr)						\
-	(pcr->api_stats.pas_num_chains += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_chains)
 #define PAS_INC_NUM_BATCHES(pcr)					\
-	(pcr->api_stats.pas_num_batches += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_batches)
 
 #define PAS_INC_NUM_REQUEST_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_request_failures)
 #define PAS_INC_NUM_SERVICE_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_service_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_service_failures)
 #define PAS_INC_NUM_CHAIN_FAILURES(pcr)					\
-	(pcr->api_stats.pas_num_chain_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_chain_failures)
 #define PAS_INC_NUM_BATCH_FAILURES(pcr)					\
-	(pcr->api_stats.pas_num_batch_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_batch_failures)
 
 #define PAS_INC_NUM_ENC_REQUESTS(pcr)					\
-	(pcr->api_stats.pas_num_enc_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_enc_requests)
 #define PAS_INC_NUM_DEC_REQUESTS(pcr)					\
-	(pcr->api_stats.pas_num_dec_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_dec_requests)
 #define PAS_INC_NUM_CP_REQUESTS(pcr)					\
-	(pcr->api_stats.pas_num_cp_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_cp_requests)
 #define PAS_INC_NUM_DC_REQUESTS(pcr)					\
-	(pcr->api_stats.pas_num_dc_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_dc_requests)
 #define PAS_INC_NUM_HASH_REQUESTS(pcr)					\
-	(pcr->api_stats.pas_num_hash_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_hash_requests)
 #define PAS_INC_NUM_CHKSUM_REQUESTS(pcr)				\
-	(pcr->api_stats.pas_num_chksum_requests += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_chksum_requests)
 
 #define PAS_INC_NUM_ENC_REQUEST_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_enc_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_enc_request_failures)
 #define PAS_INC_NUM_DEC_REQUEST_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_dec_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_dec_request_failures)
 #define PAS_INC_NUM_CP_REQUEST_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_cp_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_cp_request_failures)
 #define PAS_INC_NUM_DC_REQUEST_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_dc_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_dc_request_failures)
 #define PAS_INC_NUM_HASH_REQUEST_FAILURES(pcr)				\
-	(pcr->api_stats.pas_num_hash_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_hash_request_failures)
 #define PAS_INC_NUM_CHKSUM_REQUEST_FAILURES(pcr)			\
-	(pcr->api_stats.pas_num_chksum_request_failures += 1)
+	atomic64_inc_return(&pcr->api_stats.pas_num_chksum_request_failures)
 
 #define PAS_INC_NUM_ENC_BYTES(pcr, bytes)				\
-	(pcr->api_stats.pas_num_enc_bytes += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_enc_bytes)
 #define PAS_INC_NUM_DEC_BYTES(pcr, bytes)				\
-	(pcr->api_stats.pas_num_dec_bytes += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_dec_bytes)
 
 #define PAS_INC_NUM_CP_BYTES_IN(pcr, bytes)				\
-	(pcr->api_stats.pas_num_cp_bytes_in += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_cp_bytes_in)
 #define PAS_INC_NUM_CP_BYTES_OUT(pcr, bytes)				\
-	(pcr->api_stats.pas_num_cp_bytes_out += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_cp_bytes_out)
 
 #define PAS_INC_NUM_DC_BYTES_IN(pcr, bytes)				\
-	(pcr->api_stats.pas_num_dc_bytes_in += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_dc_bytes_in)
 #define PAS_INC_NUM_DC_BYTES_OUT(pcr, bytes)				\
-	(pcr->api_stats.pas_num_dc_bytes_out += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_dc_bytes_out)
 
 /* TODO-stats: Separate count for different algo?? */
 #define PAS_INC_NUM_HASH_BYTES_IN(pcr, bytes)				\
-	(pcr->api_stats.pas_num_hash_bytes_in += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_hash_bytes_in)
 #define PAS_INC_NUM_HASHES(pcr, count)					\
-	(pcr->api_stats.pas_num_hashes += count)
+	atomic64_add(count, &pcr->api_stats.pas_num_hashes)
 
 #define PAS_INC_NUM_CHKSUM_BYTES_IN(pcr, bytes)				\
-	(pcr->api_stats.pas_num_chksum_bytes_in += bytes)
+	atomic64_add(bytes, &pcr->api_stats.pas_num_chksum_bytes_in)
 #define PAS_INC_NUM_CHKSUMS(pcr, count)					\
-	(pcr->api_stats.pas_num_chksums += count)
+	atomic64_add(count, &pcr->api_stats.pas_num_chksums)
 
 /* for measuring the time spent in both software and hardware */
 #define PAS_DECL_PERF()		ktime_t s
 #define PAS_START_PERF()	s = ktime_get()
 #define PAS_END_PERF(pcr)						\
-	pcr->api_stats.pas_total_latency +=				\
-		ktime_us_delta(ktime_get(), s);
+	atomic64_add(ktime_us_delta(ktime_get(), s),\
+			&pcr->api_stats.pas_total_latency)
 
 /* for measuring the time spent just in hardware */
 #define PAS_DECL_HW_PERF()	ktime_t h
 #define PAS_START_HW_PERF()	h = ktime_get()
 #define PAS_END_HW_PERF(pcr)						\
-	(pcr->api_stats.pas_total_hw_latency +=				\
-		ktime_us_delta(ktime_get(), h))
+	atomic64_add(ktime_us_delta(ktime_get(), h), 			\
+			&pcr->api_stats.pas_total_hw_latency)
 
 #define PAS_SHOW_STATS(pcr)	pas_show_stats(&pcr->api_stats);
 
