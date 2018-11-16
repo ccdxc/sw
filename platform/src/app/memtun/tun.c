1a99f9a10e5a73c39024db777105750ab2cd6d5a
anishgupta3 <40500113+anishgupta3@users.noreply.github.com>
2018-11-15 16:55:59 -0800

    memtun for FreeBSD (#7580)

--- platform/src/app/memtun/tun.c
+++ platform/src/app/memtun/tun.c
@@ -5,7 +5,9 @@
 
 #include <sys/ioctl.h>
 #include <net/if.h>
+#ifndef __FreeBSD__
 #include <linux/if_tun.h>
+#endif
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,15 +20,27 @@
 int
 tun_open(char **namp)
 {
+#ifndef __FreeBSD__
     struct ifreq ifr;
-    int fd, r;
+    int r;
+#endif
+    int fd;
+    char path[20];
 
-    if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
-        perror("/dev/net/tun");
+#ifdef __FreeBSD__
+    *namp = strdup("tun100");
+    snprintf(path, 20, "/dev/%s", *namp);
+#else
+    snprintf(path, 20, "/dev/net/tun");
+#endif
+
+    if ((fd = open(path, O_RDWR)) < 0) {
+        perror(path);
         return -1;
     }
     fcntl(fd, F_SETFL, O_NONBLOCK);
 
+#ifndef __FreeBSD__
     memset(&ifr, 0, sizeof(ifr));
     ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
     r = ioctl(fd, TUNSETIFF, &ifr);
@@ -36,6 +50,7 @@ tun_open(char **namp)
         return -1;
     }
     *namp = strdup(ifr.ifr_name);
+#endif
     return fd;
 }
 
@@ -72,6 +87,7 @@ if_up(int s, struct ifreq *ifr)
     return r;
 }
 
+#ifndef __FreeBSD__
 static int
 if_setxaddr(int s, struct ifreq *ifr, uint32_t cmd, char *cmds, uint32_t addr)
 {
@@ -94,6 +110,7 @@ if_setifdstaddr(int s, struct ifreq *ifr, uint32_t addr)
 {
     return if_setxaddr(s, ifr, SIOCSIFDSTADDR, "SIOCSIFDSTADDR", addr);
 }
+#endif
 
 int
 tun_setaddrs(const char *ifname, uint32_t local, uint32_t remote)
@@ -108,12 +125,14 @@ tun_setaddrs(const char *ifname, uint32_t local, uint32_t remote)
     if (if_down(s, &ifr) < 0) {
         goto out;
     }
+#ifndef __FreeBSD__
     if (if_setifaddr(s, &ifr, local) < 0) {
         goto out;
     }
     if (if_setifdstaddr(s, &ifr, remote) < 0) {
         goto out;
     }
+#endif
     r = if_up(s, &ifr);
 out:
     close(s);
