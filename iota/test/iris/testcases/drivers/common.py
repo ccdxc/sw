1bb0de1a988c6e2ae3252f781aec30636a4f54c1
sudhiaithal <sudhiaithal@gmail.com>
2018-11-17 09:25:33 -0800

    Generalize structure for driver testing (#7631)

--- iota/test/iris/testcases/drivers/common.py
+++ iota/test/iris/testcases/drivers/common.py
@@ -6,6 +6,100 @@ import iota.protos.pygen.types_pb2 as types_pb2
 import iota.test.iris.testcases.drivers.cmd_builder as cmd_builder
 import iota.test.iris.config.netagent.hw_push_config as cfg_api
 
+class InterfaceType:
+    HOST               = 1
+    HOST_INTERNAL      = 2
+    NAPLES_INT_MGMT    = 3
+    NAPLES_IB_100G     = 4
+    NAPLES_OOB_1G      = 5
+
+def GetHostInterfaces(node):
+    intfs = []
+    for intf in api.GetWorkloadNodeHostInterfaces(node):
+        intfObj = Interface(node, intf, InterfaceType.HOST)
+        intfs.append(intfObj)
+    return intfs
+
+class Interface:
+
+    __CMD_WRAPPER = {
+        InterfaceType.HOST             : api.Trigger_AddHostCommand,
+        InterfaceType.HOST_INTERNAL    : api.Trigger_AddHostCommand,
+        InterfaceType.NAPLES_INT_MGMT  : api.Trigger_AddNaplesCommand,
+        InterfaceType.NAPLES_IB_100G   : api.Trigger_AddNaplesCommand,
+        InterfaceType.NAPLES_OOB_1G    : api.Trigger_AddNaplesCommand,
+    }
+    def __init__(self, node, name, intfType):
+        self.__name = name
+        self.__node = node
+        self.__type = intfType
+        self.__ip   = None
+        self.__prefix = None
+        self.__workload = None
+
+    def Name(self):
+        return self.__name
+
+    def IntfType(self):
+        return self.__type
+
+    def SetIP(self, ip):
+        self.__ip = ip
+
+    def GetIP(self):
+        return self.__ip
+
+    def Node(self):
+        return self.__node
+
+    def AddCommand(self, req, cmd, background = False):
+        Interface.__CMD_WRAPPER[self.__type](req, self.__node, cmd, background = background)
+
+    def ConfigureInterface(self, ip, netmask = 24):
+        self.__ip = ip
+        self.__prefix = str(netmask)
+        return self.ReconfigureInterface()
+
+    def ReconfigureInterface(self):
+        req = api.Trigger_CreateExecuteCommandsRequest(serial = True)
+        ip = self.GetIP() + "/" + self.__prefix
+        ifconfig_cmd = "ifconfig " + self.Name() + " " + ip
+        self.AddCommand(req, ifconfig_cmd)
+        trig_resp = api.Trigger(req)
+        for cmd in trig_resp.commands:
+            if cmd.exit_code != 0:
+                return api.types.status.FAILURE
+
+
+        return api.types.status.SUCCESS
+
+
+class NodeInterface:
+    def __init__(self, node):
+        self._host_intfs             = GetHostInterfaces(node)
+        self._host_int_mgmt_intfs    = []
+        self._naples_int_mgmt_intfs  = []
+        self._oob_1g_intfs           = []
+        self._ib_100g_intfs          = []
+
+    def HostIntfs(self):
+        return self._host_intfs
+
+    def HostIntIntfs(self):
+        return self._host_int_mgmt_intfs
+
+    def NaplesIntMgmtIntfs(self):
+        return self._naples_int_mgmt_intfs
+
+    def Oob1GIntfs(self):
+        return self._oob_1g_intfs
+
+    def Inb100GIntfs(self):
+        return self._ib_100g_intfs
+
+def GetNodeInterface(node):
+    return NodeInterface(node)
+
 feature_cmd_map = {
     "tx_ring_size"       :
         {
@@ -39,12 +133,12 @@ feature_cmd_map = {
         },
     "tso_offload"        :
         {
-            "linux"    : { "cmd" : cmd_builder.ethtool_tso_offload }, 
+            "linux"    : { "cmd" : cmd_builder.ethtool_tso_offload },
             "freebsd"  : { "cmd" : cmd_builder.bsd_ethtool_tso_offload }
         },
     "sg_offload"         :
         {
-            "linux"    : { "cmd" : cmd_builder.ethtool_sg_offload }, 
+            "linux"    : { "cmd" : cmd_builder.ethtool_sg_offload },
             "freebsd"  : { "cmd" : cmd_builder.bsd_ethtool_sg_offload }
         },
     "filter_mcast"       :
@@ -74,12 +168,12 @@ feature_cmd_map = {
         },
     "rx_vlan"            :
         {
-            "linux"    : { "cmd" : cmd_builder.ethtool_rxvlan_offload }, 
+            "linux"    : { "cmd" : cmd_builder.ethtool_rxvlan_offload },
             "freebsd"  : { "cmd" : cmd_builder.bsd_ethtool_rxvlan_offload }
         },
     "tx_vlan"            :
         {
-            "linux"    : { "cmd" : cmd_builder.ethtool_txvlan_offload }, 
+            "linux"    : { "cmd" : cmd_builder.ethtool_txvlan_offload },
             "freebsd"  : { "cmd" : cmd_builder.bsd_ethtool_txvlan_offload }
         },
 }
@@ -87,10 +181,9 @@ feature_cmd_map = {
 def setup_features(tc):
     req = api.Trigger_CreateExecuteCommandsRequest(serial = True)
     driverReloaded = False
-    for n, host_intfs in tc.host_intfs.items():
-        if len(host_intfs) == 0:
-            api.Logger.error("No host interfaces for node :%s" %n)
-            return api.types.status.FAILURE
+    for intfObj in tc.test_intfs:
+        intf = intfObj.Name()
+        n = intfObj.Node()
         reloadDone = False
         for feature, cmdBuilderDict in feature_cmd_map.items():
             feature_value = getattr(tc.iterators, feature, None)
@@ -99,20 +192,19 @@ def setup_features(tc):
                 continue
             os_type = api.GetNodeOs(n)
             callback = cmdBuilderDict[os_type]["cmd"]
-            for intf in host_intfs:
-                cmds = callback(intf, feature_value)
-                if not isinstance(cmds, list):
-                    cmds = [cmds]
-                for cmd in cmds:
-                    api.Trigger_AddHostCommand(req, n, cmd)
-                if cmdBuilderDict[os_type].get("reloadCmd"):
-                    #Driver reload, just break as no need to setup for each interface.
-                    if reloadDone:
-                        api.Logger.error("Driver reload already added for feature, can't do it again")
-                        assert(0)
-                    reloadDone = True
-                    driverReloaded = True
-                    break
+            cmds = callback(intf, feature_value)
+            if not isinstance(cmds, list):
+                cmds = [cmds]
+            for cmd in cmds:
+                intfObj.AddCommand(req, cmd)
+            if cmdBuilderDict[os_type].get("reloadCmd"):
+                #Driver reload, just break as no need to setup for each interface.
+                if reloadDone:
+                    api.Logger.error("Driver reload already added for feature, can't do it again")
+                    assert(0)
+                reloadDone = True
+                driverReloaded = True
+                break
 
     resp = api.Trigger(req)
     for cmd in resp.commands:
@@ -122,10 +214,11 @@ def setup_features(tc):
             api.Logger.error("Std Err :  %s "% cmd.stdout)
             return api.types.status.FAILURE
         api.Logger.info("Success running cmd : %s" % cmd.command)
-    
+
     if driverReloaded:
-        #Repush the configuration as driver loaded
-        cfg_api.AddWorkloads()
+        for intfObj in tc.test_intfs:
+            intfObj.ReconfigureInterface()
+
     return api.types.status.SUCCESS
 
 def pcap_file_name(prefix):
@@ -135,12 +228,12 @@ def start_pcap_capture(tc):
     req = api.Trigger_CreateExecuteCommandsRequest(serial = True)
     nodes = api.GetWorkloadNodeHostnames()
     tc.pcap_cmds = []
-    for n in nodes:
-        host_intfs = api.GetWorkloadNodeHostInterfaces(n)
+    for n, host_intfs in tc.host_intfs.items():
         if len(host_intfs) == 0:
             api.Logger.error("No host interfaces for node :%s" %n)
             return api.types.status.FAILURE
-        for intf in host_intfs:
+        for intfObj in host_intfs:
+            intf = intfObj.Name()
             cmd = cmd_builder.tcpdump_cmd(intf, pcap_file_name(intf))
             api.Trigger_AddHostCommand(req, n, cmd, background = True)
 
@@ -160,12 +253,12 @@ def stop_pcap_capture(tc):
     api.Trigger_TerminateAllCommands(tc.pcap_trigger)
     nodes = api.GetWorkloadNodeHostnames()
     tc_dir = tc.GetLogsDir()
-    for n in nodes:
-        host_intfs = api.GetWorkloadNodeHostInterfaces(n)
+    for n, host_intfs in tc.host_intfs.items():
         if len(host_intfs) == 0:
             api.Logger.error("No host interfaces for node :%s" %n)
             return api.types.status.FAILURE
-        for intf in host_intfs:
+        for intfObj in host_intfs:
+            intf = intfObj.Name()
             resp = api.CopyFromHost(n, [pcap_file_name(intf)], tc_dir)
             if resp == None or resp.api_response.api_status != types_pb2.API_STATUS_OK:
                 api.Logger.error("Failed to copy from  to Node: %s" % n)
--- iota/test/iris/testcases/drivers/features.py
+++ iota/test/iris/testcases/drivers/features.py
@@ -5,18 +5,92 @@ import iota.test.iris.testcases.drivers.common as common
 import iota.test.iris.testcases.drivers.cmd_builder as cmd_builder
 import iota.test.iris.testcases.drivers.verify as verify
 
-tc_count = 0
+DRIVER_TEST_TYPE_OOB_1G_INTF       = "oob-1g"
+DRIVER_TEST_TYPE_IB_100G_INTF      = "ib-100g"
+DRIVER_TEST_TYPE_INT_MGMT_INTF     = "int-mgmt"
+DRIVER_TEST_TYPE_HOST_INTF         = "host"
+
+ip_prefix = 24
+
+ip_map =  {
+    DRIVER_TEST_TYPE_HOST_INTF     : ("1.2.2.2", "1.2.2.3"),
+    DRIVER_TEST_TYPE_OOB_1G_INTF   : ("2.2.2.2", "2.2.2.3"),
+    DRIVER_TEST_TYPE_INT_MGMT_INTF : ("2.2.2.2", "2.2.2.3"),
+    DRIVER_TEST_TYPE_IB_100G_INTF  : ("2.2.2.2", "2.2.2.3"),
+}
+
+def __configure_interfaces(tc, tc_type):
+    ip1 = ip_map[tc_type][0]
+    ip2 = ip_map[tc_type][1]
+    ret = tc.intf1.ConfigureInterface(ip1, ip_prefix)
+    if ret != api.types.status.SUCCESS:
+        api.Logger.error("Configure interface failed")
+        return api.types.status.FAILURE
+    ret = tc.intf2.ConfigureInterface(ip2, ip_prefix)
+    if ret != api.types.status.SUCCESS:
+        api.Logger.error("Configure interface failed")
+        return api.types.status.FAILURE
+    tc.intf1.SetIP(ip1)
+    tc.intf2.SetIP(ip2)
+
+    return api.types.status.SUCCESS
+
+def __setup_host_interface_test(tc):
+    host_intfs = tc.node_intfs[tc.nodes[0]].HostIntfs()
+    host_intfs1 = tc.node_intfs[tc.nodes[1]].HostIntfs()
+    tc.intf1 = host_intfs[0]
+    tc.intf2 = host_intfs1[0]
+    return __configure_interfaces(tc, DRIVER_TEST_TYPE_HOST_INTF)
+
+def __setup_int_mgmt_interface_test(tc):
+    host_intfs = tc.node_intfs[tc.nodes[0]].HostIntIntfs()
+    naples_intfs = tc.node_intfs[tc.nodes[0]].NaplesIntMgmtIntfs()
+    tc.intf1 = host_intfs[0]
+    tc.intf2 = naples_intfs[1]
+    return __configure_interfaces(tc, DRIVER_TEST_TYPE_INT_MGMT_INTF)
+
+def __setup_oob_1g_interface_test(tc):
+    intfs = tc.node_intfs[tc.nodes[0]].Oob1GIntfs()
+    intfs1 = tc.node_intfs[tc.nodes[1]].Oob1GIntfs()
+    tc.intf1 = intfs[0]
+    tc.intf2 = intfs1[0]
+    return __configure_interfaces(tc, DRIVER_TEST_TYPE_OOB_1G_INTF)
+
+def __setup_inb_100g_inteface_test(tc):
+    intfs = tc.node_intfs[tc.nodes[0]].Inb100GIntfs()
+    intfs1 = tc.node_intfs[tc.nodes[1]].Inb100GIntfs()
+    tc.intf1 = intfs[0]
+    tc.intf2 = intfs1[1]
+    return __configure_interfaces(tc, DRIVER_TEST_TYPE_OOB_1G_INTF)
+
+def __set_interfaces(tc, test_type):
+    if test_type == DRIVER_TEST_TYPE_HOST_INTF:
+        ret = __setup_host_interface_test(tc)
+    elif test_type == DRIVER_TEST_TYPE_INT_MGMT_INTF:
+        ret = __setup_int_mgmt_interface_test(tc)
+    elif test_type == DRIVER_TEST_TYPE_OOB_1G_INTF:
+        ret = __setup_oob_1g_interface_test(tc)
+    elif test_type == DRIVER_TEST_TYPE_IB_100G_INTF:
+        ret = __setup_inb_100g_inteface_test(tc)
+    else:
+        api.Logger.error("Invalid test type : ", test_type)
+        return api.types.status.FAILURE
+
+    tc.test_intfs = [tc.intf1, tc.intf2]
+
+    return ret
 
 def Setup(tc):
-    if tc.args.type == 'local_only':
-        tc.workload_pairs = api.GetLocalWorkloadPairs()
-    else:
-        tc.workload_pairs = api.GetRemoteWorkloadPairs()
-
     tc.nodes = api.GetWorkloadNodeHostnames()
-    tc.host_intfs = {}
+    tc.node_intfs = {}
     for node in tc.nodes:
-        tc.host_intfs[node] = api.GetWorkloadNodeHostInterfaces(node)
+        tc.node_intfs[node] = common.GetNodeInterface(node)
+
+    test_type = getattr(tc.args, "test-type", DRIVER_TEST_TYPE_HOST_INTF)
+    ret = __set_interfaces(tc, test_type)
+    if ret != api.types.status.SUCCESS:
+        api.Logger.error("Set interface failed")
+        return api.types.status.FAILURE
 
     api.Logger.info("Setting driver features")
     if common.setup_features(tc) != api.types.status.SUCCESS:
@@ -31,24 +105,21 @@ def Setup(tc):
     return api.types.status.SUCCESS
 
 def Trigger(tc):
-    w1 = tc.workload_pairs[0][0]
-    w2 = tc.workload_pairs[0][1]
 
     req = api.Trigger_CreateExecuteCommandsRequest(serial = True)
     tc.cmd_descr = "Server: %s(%s) <--> Client: %s(%s)" %\
-                   (w1.workload_name, w1.ip_address, w2.workload_name, w2.ip_address)
+                   (tc.intf1.Name(), tc.intf1.GetIP(), tc.intf2.Name(), tc.intf2.GetIP())
     api.Logger.info("Starting Iperf test from %s" % (tc.cmd_descr))
 
     iperf_server_cmd = cmd_builder.iperf_server_cmd()
-    api.Trigger_AddCommand(req, w1.node_name, w1.workload_name,
-                           iperf_server_cmd, background = True)
+    tc.intf1.AddCommand(req, iperf_server_cmd, background = True)
     proto = getattr(tc.iterators, "proto", 'tcp')
     pktsize = getattr(tc.iterators, "pktsize", 512)
     ipproto = getattr(tc.iterators, "ipproto", 'v4')
-    iperf_client_cmd = cmd_builder.iperf_client_cmd(server_ip = w1.ip_address,
+    iperf_client_cmd = cmd_builder.iperf_client_cmd(server_ip = tc.intf1.GetIP(),
                             proto=proto, pktsize=pktsize, ipproto=ipproto)
-    api.Trigger_AddCommand(req, w2.node_name, w2.workload_name,
-                           iperf_client_cmd)
+    tc.intf2.AddCommand(req, iperf_client_cmd)
+
 
     trig_resp = api.Trigger(req)
     term_resp = api.Trigger_TerminateAllCommands(trig_resp)
