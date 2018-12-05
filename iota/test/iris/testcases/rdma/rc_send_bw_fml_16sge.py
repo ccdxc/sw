6ccc949661fc15dcf9d43f443c180b58ac20c9e2
sanshanb <38573165+sanshanb@users.noreply.github.com>
2018-12-04 18:39:21 -0800

    Support for send_bw and send_lat with 16 SGE, and corresponding IOTA tests. (#8017)

---
+++ iota/test/iris/testcases/rdma/rc_send_bw_fml_16sge.py
@@ -0,0 +1,115 @@
+#! /usr/bin/python3
+import iota.harness.api as api
+
+def Setup(tc):
+ 
+    tc.desc = '''
+    Test   :  ib_send_bw
+    Opcode :  Send First, Middle, Last
+    Num QP :  1, 2
+    Pad    :  No
+    Inline :  No
+    Num SGE:  16
+    modes  :  workload1 as server, workload2 as client
+              workload2 as server, workload1 as client
+    '''
+
+    tc.iota_path = api.GetTestsuiteAttr("driver_path")
+
+    pairs = api.GetRemoteWorkloadPairs()
+    # get workloads from each node
+    tc.w = []
+    tc.w.append(pairs[0][0])
+    tc.w.append(pairs[0][1])
+
+    tc.devices = []
+    tc.gid = []
+    tc.ib_prefix = []
+    for i in range(2):
+        tc.devices.append(api.GetTestsuiteAttr(tc.w[i].ip_address+'_device'))
+        tc.gid.append(api.GetTestsuiteAttr(tc.w[i].ip_address+'_gid'))
+        if tc.w[i].IsNaples():
+            tc.ib_prefix.append('cd ' + tc.iota_path + ' && ./run_rdma.sh  ')
+        else:
+            tc.ib_prefix.append('')
+
+    return api.types.status.SUCCESS
+
+def Trigger(tc):
+
+    #==============================================================
+    # trigger the commands
+    #==============================================================
+    req = api.Trigger_CreateExecuteCommandsRequest(serial = True)
+
+    i = 0
+    tc.cmd_descr = ''
+    while (i < 2):
+        j = (i + 1) % 2
+        w1 = tc.w[i]
+        w2 = tc.w[j]
+
+        tc.cmd_descr += "Server: %s(%s) <--> Client: %s(%s)\n" %\
+                       (w1.workload_name, w1.ip_address, w2.workload_name, w2.ip_address)
+
+        # Set the msg size to 64512, so that size of each SGE is 4032
+        # Each packet is 4096 bytes, which means each packet will need 
+        # to use 2 SGE's
+
+        # cmd for server
+        cmd = "ib_send_bw -d " + tc.devices[i] + " -n 10 -F -x " + tc.gid[i] + " -m 4096 -s 64512 -q " + str(tc.iterators.num_qp) + " --report_gbits"
+        # append num_sge's option only if node is naples
+        if w1.IsNaples():
+            cmd += ' -W 16'
+
+        api.Trigger_AddCommand(req, 
+                               w1.node_name, 
+                               w1.workload_name,
+                               tc.ib_prefix[i] + cmd,
+                               background = True)
+
+        # On Naples-Mellanox setups, with Mellanox as server, it takes a few seconds before the server
+        # starts listening. So sleep for a few seconds before trying to start the client
+        cmd = 'sleep 2'
+        api.Trigger_AddCommand(req,
+                               w1.node_name,
+                               w1.workload_name,
+                               cmd)
+
+        # cmd for client
+        cmd = "ib_send_bw -d " + tc.devices[j] + " -n 10 -F -x " + tc.gid[j] + " -m 4096 -s 64512 -q " + str(tc.iterators.num_qp) + " --report_gbits " + w1.ip_address
+        # append num_sge's option only if node is naples
+        if w2.IsNaples():
+            cmd += ' -W 16'
+
+        api.Trigger_AddCommand(req, 
+                               w2.node_name, 
+                               w2.workload_name,
+                               tc.ib_prefix[j] + cmd)
+
+        i = i + 1
+    # end while
+
+    api.Logger.info("Starting ib_send_bw test from \n%s" % (tc.cmd_descr))
+    # trigger the request
+    trig_resp = api.Trigger(req)
+    term_resp = api.Trigger_TerminateAllCommands(trig_resp)
+
+    tc.resp = api.Trigger_AggregateCommandsResponse(trig_resp, term_resp)
+    return api.types.status.SUCCESS
+
+def Verify(tc):
+    if tc.resp is None:
+        return api.types.status.FAILURE
+
+    result = api.types.status.SUCCESS
+
+    api.Logger.info("ib_send_bw results for \n%s" % (tc.cmd_descr))
+    for cmd in tc.resp.commands:
+        api.PrintCommandResults(cmd)
+        if cmd.exit_code != 0 and not api.Trigger_IsBackgroundCommand(cmd):
+            result = api.types.status.FAILURE
+    return result
+
+def Teardown(tc):
+    return api.types.status.SUCCESS
---
+++ iota/test/iris/testcases/rdma/rc_send_lat_16sge.py
@@ -0,0 +1,108 @@
+#! /usr/bin/python3
+import iota.harness.api as api
+import iota.protos.pygen.topo_svc_pb2 as topo_svc_pb2
+import iota.test.iris.verif.utils.rdma_utils as rdma
+
+def Setup(tc):
+ 
+    tc.desc = '''
+    Test   :  ib_send_lat
+    Opcode :  Send FML
+    Num QP :  1
+    Pad    :  No
+    Inline :  No
+    Num SGE:  1
+    modes  :  inherently bidirectional
+    '''
+
+    tc.iota_path = api.GetTestsuiteAttr("driver_path")
+
+    pairs = api.GetRemoteWorkloadPairs()
+    # get workloads from each node
+    tc.w = []
+    tc.w.append(pairs[0][0])
+    tc.w.append(pairs[0][1])
+
+    tc.devices = []
+    tc.gid = []
+    tc.ib_prefix = []
+    for i in range(2):
+        tc.devices.append(api.GetTestsuiteAttr(tc.w[i].ip_address+'_device'))
+        tc.gid.append(api.GetTestsuiteAttr(tc.w[i].ip_address+'_gid'))
+        if tc.w[i].IsNaples():
+            tc.ib_prefix.append('cd ' + tc.iota_path + ' && ./run_rdma.sh  ')
+        else:
+            tc.ib_prefix.append('')
+
+    return api.types.status.SUCCESS
+
+def Trigger(tc):
+
+    i = 0
+    j = i + 1
+    w1 = tc.w[i]
+    w2 = tc.w[j]
+
+    #==============================================================
+    # trigger the commands
+    #==============================================================
+    req = api.Trigger_CreateExecuteCommandsRequest(serial = True)
+
+    tc.cmd_descr = "Server: %s(%s) <--> Client: %s(%s)\n" %\
+                   (w1.workload_name, w1.ip_address, w2.workload_name, w2.ip_address)
+
+    api.Logger.info("Starting ib_send_lat test from %s" % (tc.cmd_descr))
+
+    # cmd for server
+    cmd = "ib_send_lat -d " + tc.devices[i] + " -n 10 -F -x " + tc.gid[i] + " -s 65536 --report_gbits"
+    # append num_sge's option only if node is naples
+    if w1.IsNaples():
+        cmd += ' -W 16'
+
+    api.Trigger_AddCommand(req, 
+                           w1.node_name, 
+                           w1.workload_name,
+                           tc.ib_prefix[i] + cmd,
+                           background = True)
+
+    # On Naples-Mellanox setups, with Mellanox as server, it takes a few seconds before the server
+    # starts listening. So sleep for a few seconds before trying to start the client
+    cmd = 'sleep 2'
+    api.Trigger_AddCommand(req,
+                           w1.node_name,
+                           w1.workload_name,
+                           cmd)
+
+    # cmd for client
+    cmd = "ib_send_lat -d " + tc.devices[j] + " -n 10 -F -x " + tc.gid[j] + " -s 65536 --report_gbits " + w1.ip_address
+    # append num_sge's option only if node is naples
+    if w2.IsNaples():
+        cmd += ' -W 16'
+
+    api.Trigger_AddCommand(req, 
+                           w2.node_name, 
+                           w2.workload_name,
+                           tc.ib_prefix[j] + cmd)
+
+    # trigger the request
+    trig_resp = api.Trigger(req)
+    term_resp = api.Trigger_TerminateAllCommands(trig_resp)
+
+    tc.resp = api.Trigger_AggregateCommandsResponse(trig_resp, term_resp)
+    return api.types.status.SUCCESS
+
+def Verify(tc):
+    if tc.resp is None:
+        return api.types.status.FAILURE
+
+    result = api.types.status.SUCCESS
+
+    api.Logger.info("ib_send_lat results for %s" % (tc.cmd_descr))
+    for cmd in tc.resp.commands:
+        api.PrintCommandResults(cmd)
+        if cmd.exit_code != 0 and not api.Trigger_IsBackgroundCommand(cmd):
+            result = api.types.status.FAILURE
+    return result
+
+def Teardown(tc):
+    return api.types.status.SUCCESS
