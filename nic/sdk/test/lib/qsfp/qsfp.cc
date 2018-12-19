1b99400cb3b0f5a9a34523e03328d61f63b6a6fe
rahshekh <rahulshekhar@pensando.io>
2018-12-19 09:11:19 -0800

    Chaning i2c write struct messages
    Fixing i2c write and qsfp read

--- nic/sdk/test/lib/qsfp/qsfp.cc
+++ nic/sdk/test/lib/qsfp/qsfp.cc
@@ -5,6 +5,7 @@
 //------------------------------------------------------------------------------
 
 #include <gtest/gtest.h>
+#include <inttypes.h>
 #include "nic/sdk/include/sdk/pal.hpp"
 
 using namespace std;
@@ -28,12 +29,80 @@ protected:
 
 };
 
+static void
+test_qsfp (int port)
+{
+    uint8_t qsfp_buffer[QSFP_READ_SIZE];
+    sdk::lib::qsfp_page_t lowpage = sdk::lib::QSFP_PAGE_LOW;
+    uint32_t readoffset;
+    int i;
+
+    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
+    
+    printf("Enter a offset to read:");
+    scanf("%" SCNx8, &readoffset);
+    printf("offset to read: 0x%x\n", readoffset);
+
+    printf("Enter a page to read:");
+    scanf("%" SCNx32, &lowpage);
+    printf("page to read: %d\n", lowpage);
+
+    ASSERT_EQ(sdk::lib::pal_qsfp_read(
+        qsfp_buffer, QSFP_READ_SIZE, readoffset, lowpage, MAX_QSFP_RETRIES, port), 0);
+
+    printf("\nAfter read\n");
+    for (i = 0; i < QSFP_READ_SIZE; i++)
+        printf("%x", qsfp_buffer[i]);
+    
+    printf("\nreading done for port: %d\n", port);
+}
+
+static void
+test_qsfp_write (int port)
+{
+    uint8_t qsfp_buffer[QSFP_READ_SIZE];
+    sdk::lib::qsfp_page_t lowpage = sdk::lib::QSFP_PAGE_LOW;
+    uint32_t writeoffset;
+    uint8_t qsfp_wbuffer;
+    int i;
+
+    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
+
+    printf("Enter a offset to write:");
+    scanf("%" SCNx8, &writeoffset);
+    printf("offset to read: 0x%x\n", writeoffset);
+
+    printf("Enter a page to read:");
+    scanf("%" SCNx32, &lowpage);
+    printf("page to read: %d\n", lowpage);
+
+    ASSERT_EQ(sdk::lib::pal_qsfp_read(
+        qsfp_buffer, QSFP_READ_SIZE, writeoffset, lowpage, MAX_QSFP_RETRIES, port), 0);
+
+    printf("\nAfter read\n");
+    for (i = 0; i < QSFP_READ_SIZE; i++)
+        printf("%x", qsfp_buffer[i]);
+
+    printf("Enter a value to write:");
+    scanf("%" SCNx8, &qsfp_wbuffer);
+    printf("value to write : %x\n", qsfp_wbuffer);
+
+    ASSERT_EQ(sdk::lib::pal_qsfp_write(&qsfp_wbuffer, 1, writeoffset, lowpage, MAX_QSFP_RETRIES, port), 0);
+    printf("writing done\n");
+
+    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
+    ASSERT_EQ(sdk::lib::pal_qsfp_read(
+        qsfp_buffer, QSFP_READ_SIZE, writeoffset, lowpage, MAX_QSFP_RETRIES, port), 0);
+
+    printf("\nAfter read\n");
+    for (i = 0; i < QSFP_READ_SIZE; i++)
+        printf("%x", qsfp_buffer[i]);
+
+    printf("\nwriting done for port: %d\n", port);
+}
+
 // test qsfp_write
 TEST_F(qsfp_test, pal_hw_qsfp_write) {
-    uint8_t *qsfp_buffer = NULL;
-    uint8_t qsfp_wbuffer;
-    sdk::lib::qsfp_page_t lowpage = sdk::lib::QSFP_PAGE_LOW;
-    uint8_t i;
     // initialize the pal
 #ifdef __x86_64__
     assert(sdk::lib::pal_init(sdk::types::platform_type_t::PLATFORM_TYPE_SIM) == sdk::lib::PAL_RET_OK);
@@ -41,75 +110,20 @@ TEST_F(qsfp_test, pal_hw_qsfp_write) {
     assert(sdk::lib::pal_init(sdk::types::platform_type_t::PLATFORM_TYPE_HAPS) == sdk::lib::PAL_RET_OK);
 #endif
 
-    qsfp_buffer = (uint8_t*)malloc(QSFP_READ_SIZE);
-    if (qsfp_buffer == NULL) {
-        printf("failed to allocate memory.\n");
-    }
-    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
-    ASSERT_EQ(sdk::lib::pal_qsfp_read(qsfp_buffer, QSFP_READ_SIZE, 0x54, lowpage, MAX_QSFP_RETRIES, 2), 0);
-    printf("after read\n");
-    for (i = 0; i < QSFP_READ_SIZE; i++)
-        printf("%x", qsfp_buffer[i]);
-    printf("\nreading done\n");
-
-    printf("Enter a value to write:");
-    scanf("%c",&qsfp_wbuffer);
-    ASSERT_EQ(sdk::lib::pal_qsfp_write(&qsfp_wbuffer, 1, 0x56, lowpage, MAX_QSFP_RETRIES, 2), 0);
-    printf("writing done\n");
-
-    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
-    ASSERT_EQ(sdk::lib::pal_qsfp_read(qsfp_buffer, QSFP_READ_SIZE, 0x54, lowpage, MAX_QSFP_RETRIES, 2), 0);
-    printf("after read\n");
-    for (i = 0; i < QSFP_READ_SIZE; i++)
-        printf("%x", qsfp_buffer[i]);
-    printf("\nreading done\n");
-
-
-
-    qsfp_buffer = (uint8_t*)malloc(QSFP_READ_SIZE);
-    if (qsfp_buffer == NULL) {
-        printf("failed to allocate memory.\n");
-    }
-    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
-    ASSERT_EQ(sdk::lib::pal_qsfp_read(qsfp_buffer, QSFP_READ_SIZE, 0x54, lowpage, MAX_QSFP_RETRIES, 1), 0);
-    printf("after read\n");
-    for (i = 0; i < QSFP_READ_SIZE; i++)
-        printf("%x", qsfp_buffer[i]);
-    printf("\nreading done\n");
-
-    ASSERT_EQ(sdk::lib::pal_qsfp_write(&qsfp_wbuffer, 1, 0x56, lowpage, MAX_QSFP_RETRIES, 1), 0);
-    printf("writing done\n");
-
-    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
-    ASSERT_EQ(sdk::lib::pal_qsfp_read(qsfp_buffer, QSFP_READ_SIZE, 0x54, lowpage, MAX_QSFP_RETRIES, 1), 0);
-    printf("after read\n");
-    for (i = 0; i < QSFP_READ_SIZE; i++)
-        printf("%x", qsfp_buffer[i]);
-    printf("\nreading done\n");
-
+    test_qsfp_write (1);
+    test_qsfp_write (2);
 }
 
 // test qsfp_read
 TEST_F(qsfp_test, pal_hw_qsfp_read) {
-    uint8_t *qsfp_buffer = NULL;
-    sdk::lib::qsfp_page_t lowpage = sdk::lib::QSFP_PAGE_LOW;
-    uint8_t i;
     // initialize the pal
 #ifdef __x86_64__
     assert(sdk::lib::pal_init(sdk::types::platform_type_t::PLATFORM_TYPE_SIM) == sdk::lib::PAL_RET_OK);
 #elif __aarch64__
     assert(sdk::lib::pal_init(sdk::types::platform_type_t::PLATFORM_TYPE_HAPS) == sdk::lib::PAL_RET_OK);
 #endif
-    qsfp_buffer = (uint8_t*)malloc(QSFP_READ_SIZE);
-    if (qsfp_buffer == NULL) {
-        printf("failed to allocate memory.\n");
-    }
-    memset(qsfp_buffer, 0, QSFP_READ_SIZE);
-    ASSERT_EQ(sdk::lib::pal_qsfp_read(qsfp_buffer, QSFP_READ_SIZE, 0x54, lowpage, MAX_QSFP_RETRIES, 2), 0);
-    printf("after read\n");		
-    for (i = 0; i < QSFP_READ_SIZE; i++)		
-        printf("%x", qsfp_buffer[i]);		
-    printf("\nreading done\n");
+    test_qsfp (1);
+    test_qsfp (2);
 }
 
 int main(int argc, char **argv) {
