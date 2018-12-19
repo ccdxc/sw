19a48b0c66f1adc0c325f9c24e1fe02b5d7f079f
Srikanth Rajagopalan <srikanth@pensando.io>
2018-12-19 12:27:56 -0800

    Remove print.cc and move functions to correct files

--- nic/sdk/include/sdk/ip.hpp
+++ nic/sdk/include/sdk/ip.hpp
@@ -163,10 +163,92 @@ typedef struct ip_prefix_s {
     uint8_t            len;     // prefix length
 } __PACK__ ip_prefix_t;
 
-extern char *ipv4addr2str(ipv4_addr_t v4_addr);
-extern char *ipv6addr2str(ipv6_addr_t v6_addr);
-extern char *ipaddr2str(const ip_addr_t *ip_addr);
-extern char *ippfx2str(const ip_prefix_t *ip_pfx);
+//------------------------------------------------------------------------------
+// thread safe helper to stringify IPv4 address
+// NOTE: IP address is expected to be in host order
+//------------------------------------------------------------------------------
+static inline char *
+ipv4addr2str (ipv4_addr_t v4_addr)
+{
+    static thread_local char       ipaddr_str[4][16];
+    static thread_local uint8_t    ipaddr_str_next = 0;
+    char                           *buf;
+
+    buf = ipaddr_str[ipaddr_str_next++ & 0x3];
+    snprintf(buf, 16, "%d.%d.%d.%d",
+             ((v4_addr >> 24) & 0xff), ((v4_addr >> 16) & 0xff),
+             ((v4_addr >> 8) & 0xff), (v4_addr & 0xff));
+    return buf;
+}
+
+//------------------------------------------------------------------------------
+// thread safe helper to stringify IPv6 address
+//------------------------------------------------------------------------------
+static inline char *
+ipv6addr2str (ipv6_addr_t v6_addr)
+{
+    static thread_local char       ipaddr_str[4][40];
+    static thread_local uint8_t    ipaddr_str_next = 0;
+    char                           *buf;
+
+    buf = ipaddr_str[ipaddr_str_next++ & 0x3];
+    sprintf(buf, "%02x%02x:%02x%02x:%02x%02x:%02x%02x:"
+                 "%02x%02x:%02x%02x:%02x%02x:%02x%02x",
+                 v6_addr.addr8[0], v6_addr.addr8[1],
+                 v6_addr.addr8[2], v6_addr.addr8[3],
+                 v6_addr.addr8[4], v6_addr.addr8[5],
+                 v6_addr.addr8[6], v6_addr.addr8[7],
+                 v6_addr.addr8[8], v6_addr.addr8[9],
+                 v6_addr.addr8[10], v6_addr.addr8[11],
+                 v6_addr.addr8[12], v6_addr.addr8[13],
+                 v6_addr.addr8[14], v6_addr.addr8[15]);
+
+    return buf;
+}
+
+//------------------------------------------------------------------------------
+// thread safe helper to stringify IP address
+//------------------------------------------------------------------------------
+static inline char *
+ipaddr2str (const ip_addr_t *ip_addr)
+{
+    static thread_local char       ipaddr_str[4][40];
+    static thread_local uint8_t    ipaddr_str_next = 0;
+    char                           *buf;
+
+    switch (ip_addr->af) {
+    case IP_AF_IPV4:
+        return ipv4addr2str(ip_addr->addr.v4_addr);
+        break;
+
+    case IP_AF_IPV6:
+        return ipv6addr2str(ip_addr->addr.v6_addr);
+        break;
+
+    default:
+        buf = ipaddr_str[ipaddr_str_next++ &0x3];
+        strcpy(buf, "NULL");
+        return buf;
+    }
+}
+
+//------------------------------------------------------------------------------
+// thread safe helper to stringify IP prefix
+//------------------------------------------------------------------------------
+static inline char *
+ippfx2str (const ip_prefix_t *ip_pfx)
+{
+    static thread_local char       ippfx_str[4][44];
+    static thread_local uint8_t    ippfx_str_next = 0;
+    char                           *buf, *ip_addr_buf;
+
+    buf = ippfx_str[ippfx_str_next++ & 0x3];
+
+    ip_addr_buf = ipaddr2str(&ip_pfx->addr);
+    sprintf(buf, "%s/%d", ip_addr_buf, ip_pfx->len);
+
+    return buf;
+}
 
 // spdlog formatter for ip_addr_t
 static inline std::ostream& operator<<(std::ostream& os, const ip_addr_t& ip) {
--- nic/utils/print/module.mk
+++
@@ -1,4 +0,0 @@
-# {C} Copyright 2018 Pensando Systems Inc. All rights reserved
-include ${MKDEFS}/pre.mk
-MODULE_TARGET = libprint.so
-include ${MKDEFS}/post.mk
