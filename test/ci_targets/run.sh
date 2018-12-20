12c001621ca116eccbf81da2ef2f6f3e61a89667
Suresh Kumar Nalluru <nasuku@users.noreply.github.com>
2018-12-20 15:23:26 -0800

    fix log collection (#8409)

--- test/ci_targets/run.sh
+++ test/ci_targets/run.sh
@@ -4,9 +4,8 @@ export E2E_CONFIG="test/e2e/cluster/tb_config_ci.json"
 cd $GOPATH/src/github.com/pensando/sw
 make $1
 if [ $? -ne 0 ]; then
-  mkdir -p /tmp/ClusterLogs/$node/events
   for node in $(docker ps -f label=pens-dind --format '{{.Names}}'); do
-    mkdir -p /tmp/ClusterLogs/$node
+    mkdir -p /tmp/ClusterLogs/$node/events
     docker cp  $node:/var/log/pensando /tmp/ClusterLogs/$node/logs
     docker exec $node tar Ccf /var/lib/pensando/events - . | tar Cxf /tmp/ClusterLogs/$node/events -
     docker exec  $node journalctl -x --no-pager > /tmp/ClusterLogs/$node/logs/journalctl
