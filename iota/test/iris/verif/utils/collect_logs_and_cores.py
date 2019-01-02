1caf0f1fb04f5b8a2eec4756c971f34acf314c53
bharatb226 <bharat@pensando.io>
2019-01-01 16:01:39 -0800

    Adding more traces

--- iota/test/iris/verif/utils/collect_logs_and_cores.py
+++ iota/test/iris/verif/utils/collect_logs_and_cores.py
@@ -14,7 +14,10 @@ def Main(tc):
     for n in nodes:
         api.Logger.info("Techsupport fron node: %s" % n)
         common.AddPenctlCommand(req, n, "system tech-support")
-        api.Trigger(req)
+        resp = api.Trigger(req)
+        if resp != api.types.status.SUCCESS:
+            api.Logger.error("Failed to execute penctl system tech-support. err: %d" % resp)
+            return api.types.status.FAILURE
         # Copy tech support tar out
         resp = api.CopyFromHost(n, [common.DEF_TECH_SUPPORT_FILE], "%s/%s" % (tc_dir, n))
         if resp == None or resp.api_response.api_status != types_pb2.API_STATUS_OK:
