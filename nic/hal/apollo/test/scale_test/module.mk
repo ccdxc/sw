0bdd88b0eeac0990d25df6aabfefa74197d87250
Sarat Kamisetty <saratk@gmail.com>
2019-01-17 11:28:26 -0800

    review comments + new file to package for apollo

--- nic/hal/apollo/test/scale_test/module.mk
+++ nic/hal/apollo/test/scale_test/module.mk
@@ -7,8 +7,7 @@ MODULE_SOLIBS   = pal ociframework ocicore ociapi ociapi_impl ocitestutils \
                   ${NIC_${PIPELINE}_P4PD_SOLIBS} \
                   ${NIC_SDK_SOLIBS} \
                   sdkp4 sdkp4utils sdkcapri_csrint sdkcapri_asicrw_if sdkcapri \
-				  sdkplatformutils sdkxcvrdriver sdkasicpd lpmitree \
-                  p4pd_common_p4plus_rxdma p4pd_common_p4plus_txdma \
+                  sdkplatformutils sdkxcvrdriver sdkasicpd lpmitree \
                   bm_allocator mtrack sdklinkmgr sdklinkmgrcsr memhash
 MODULE_LDLIBS   =  ${NIC_COMMON_LDLIBS} \
 				   ${NIC_CAPSIM_LDLIBS} \
