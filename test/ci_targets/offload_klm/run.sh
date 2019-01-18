7115971b421db77cf53fc2955ac289e3913fbc33
Pirabhu Raman <piraman@pensando.io>
2019-01-18 11:47:06 -0800

    Enforce that sonic loads fine when there is no naples and pnso_init returns expected error

--- test/ci_targets/offload_klm/run.sh
+++ test/ci_targets/offload_klm/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /tmp/git/pensando/sw/storage/offload && OS_DIR=/usr/src/freebsd ./freebsd_build.sh 
+cd /tmp/git/pensando/sw/storage/offload && OS_DIR=/usr/src/freebsd ./freebsd_build.sh && sudo kldload ./sonic.ko && sudo kldload ./pencake.ko && dmesg | grep "pnso_init failed with rc = 35" 
