aea85ffa9e0af964906b13f5c5285b9cc539e299
bharatb226 <bharat@pensando.io>
2019-01-19 20:48:23 -0800

    fixing arm builds

--- nic/entrypoint.sh
+++ nic/entrypoint.sh
@@ -19,6 +19,6 @@ mount -o bind /sw ${dir}
 sysctl -w vm.max_map_count=262144
 cp -R ../vendor/github.com/gogo/protobuf/proto/* /usr/local/include/
 echo 1>&2 building tools...
-cd $dir && make ws-tools && go install ./asset-build/... && make pull-assets && cd /sw/nic
+cd $dir && make ws-tools && go install ./asset-build/... && make pull-assets && cd /usr/src/github.com/pensando/sw/nic
 
 exec "$@"
