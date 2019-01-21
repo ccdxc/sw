3f8d9e3eb167c365b3c21578e236e941a653f3d7
Vikas B Dharwadkar <vikasbd@users.noreply.github.com>
2019-01-21 11:18:02 -0800

    Remove nic/utils/crc_fast, after moving to sdk

--- nic/hal/pd/utils/hbm_hash_test/module.mk
+++ nic/hal/pd/utils/hbm_hash_test/module.mk
@@ -4,7 +4,7 @@ include ${MKDEFS}/pre.mk
 MODULE_TARGET   = hbmhash_test.gtest
 MODULE_PIPELINE = iris
 MODULE_SOLIBS   = ${NIC_HAL_GTEST_SOLIBS} hbmhash \
-                  crc_fast jenkins_spooky hbmhashp4pd_mock
+                  jenkins_spooky hbmhashp4pd_mock
 MODULE_LDLIBS   = ${NIC_HAL_GTEST_LDLIBS}
 MODULE_ARCH     = x86_64
 include ${MKDEFS}/post.mk
--- nic/utils/crc_fast/module.mk
+++
@@ -1,5 +0,0 @@
-# {C} Copyright 2018 Pensando Systems Inc. All rights reserved
-include ${MKDEFS}/pre.mk
-MODULE_PIPELINE = iris gft
-MODULE_TARGET = libcrc_fast.so
-include ${MKDEFS}/post.mk
