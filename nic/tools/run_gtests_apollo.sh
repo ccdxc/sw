3828a26cb45fcd511d79c04f14d534c1f6277d99
Sarat Kamisetty <saratk@gmail.com>
2019-01-21 16:47:19 -0800

    cleaning up duplicate apollo gtest script

--- nic/tools/run_gtests_apollo.sh
+++ nic/tools/run_gtests_apollo.sh
@@ -23,7 +23,6 @@ function finish {
 }
 trap finish EXIT
 
-rm -rf *.log core.*
 export PATH=${PATH}:${BUILD_DIR}/bin
 $GDB apollo_scale_test -c hal.json -i ${NICDIR}/hal/apollo/test/scale_test/scale_cfg.json --gtest_output="xml:${GEN_TEST_RESULTS_DIR}/apollo_scale_test.xml"
 #valgrind --track-origins=yes --xml=yes --xml-file=out.xml apollo_scale_test -c hal.json -i ${NICDIR}/hal/apollo/test/scale_test/scale_cfg.json
