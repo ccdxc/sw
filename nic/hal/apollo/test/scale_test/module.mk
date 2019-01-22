2408079ed23d54ceff9899eb929e8089ee55db1e
Sarat Kamisetty <saratk@gmail.com>
2019-01-22 12:40:09 -0800

    switchport mac cfg bug fix

--- nic/hal/apollo/test/scale_test/module.mk
+++ nic/hal/apollo/test/scale_test/module.mk
@@ -5,7 +5,7 @@ MODULE_TARGET   = apollo_scale_test.gtest
 MODULE_PIPELINE = apollo
 MODULE_SOLIBS   = pal ociframework ocicore ociapi ociapi_impl ocitestutils \
                   ${NIC_${PIPELINE}_P4PD_SOLIBS} \
-                  ${NIC_SDK_SOLIBS} \
+                  ${NIC_SDK_SOLIBS} ${NIC_HAL_PD_SOLIBS_${ARCH}} \
                   sdkp4 sdkp4utils sdkcapri_csrint sdkcapri_asicrw_if sdkcapri \
                   sdkplatformutils sdkxcvrdriver sdkasicpd lpmitree \
                   bm_allocator mtrack sdklinkmgr sdklinkmgrcsr memhash
--- nic/tools/start-apollo-model.sh
+++ nic/tools/start-apollo-model.sh
@@ -6,4 +6,4 @@ export ZMQ_SOC_DIR=${NIC_DIR}
 ${NIC_DIR}/tools/merge_model_debug.py --p4 apollo --rxdma apollo_rxdma --txdma apollo_txdma
 $GDB $NIC_DIR/build/x86_64/apollo/bin/cap_model \
     +PLOG_MAX_QUIT_COUNT=0 +plog=info \
-    +model_debug=$NIC_DIR/build/x86_64/apollo/gen/p4gen//apollo/dbg_out/combined_model_debug.json 2>&1 | tee $NIC_DIR/model.log
+    +model_debug=$NIC_DIR/build/x86_64/apollo/gen/p4gen//apollo/dbg_out/combined_model_debug.json 2>&1 > $NIC_DIR/model.log
