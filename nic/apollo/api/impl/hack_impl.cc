075c10a8b076104b588b9889082c1b1bd959ce00
Ramesh Ponnapalli <rameshp@pensando.io>
2019-01-31 14:36:37 -0800

    fixing the ring_size0 in qstate

--- nic/apollo/api/impl/hack_impl.cc
+++ nic/apollo/api/impl/hack_impl.cc
@@ -101,14 +101,14 @@ init_service_lif (void)
 
     lif_qstate_t lif_qstate = { 0 };
     lif_qstate.ring0_base = api::g_oci_state.mempartition()->start_addr(MEM_REGION_RXDMA_TO_TXDMA_BUF_NAME);
-    lif_qstate.ring0_size = log2(api::g_oci_state.mempartition()->size(MEM_REGION_RXDMA_TO_TXDMA_BUF_NAME) / 10);
+    lif_qstate.ring0_size = log2((api::g_oci_state.mempartition()->size(MEM_REGION_RXDMA_TO_TXDMA_BUF_NAME) >> 10) / 10);
     lif_qstate.total_rings = 1;
     sdk::platform::capri::write_qstate(qstate.hbm_address, (uint8_t *)&lif_qstate, sizeof(lif_qstate));
 
     txdma_qstate_t txdma_qstate = { 0 };
     txdma_qstate.rxdma_cindex_addr = qstate.hbm_address + offsetof(lif_qstate_t, sw_cindex);
     txdma_qstate.ring_base = api::g_oci_state.mempartition()->start_addr(MEM_REGION_RXDMA_TO_TXDMA_BUF_NAME);
-    txdma_qstate.ring_size = log2(api::g_oci_state.mempartition()->size(MEM_REGION_RXDMA_TO_TXDMA_BUF_NAME) / 10);
+    txdma_qstate.ring_size = log2((api::g_oci_state.mempartition()->size(MEM_REGION_RXDMA_TO_TXDMA_BUF_NAME) >> 10) / 10);
     txdma_qstate.total_rings = 1;
     sdk::platform::capri::write_qstate(qstate.hbm_address + sizeof(lif_qstate_t),
                  (uint8_t *)&txdma_qstate, sizeof(txdma_qstate));
