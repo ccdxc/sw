be31fe0634a6fc35a8f6788336cf4c8de82239d4
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2019-02-20 18:09:49 -0800

    new scripts to run scale test on sim and hw (#9775)
    packaging hw script for customer use

---
+++ nic/apollo/test/scale/run_scale_test_hw.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -e
+export NIC_DIR=/nic/
+export HAL_CONFIG_PATH=$NIC_DIR/conf/
+export HAL_LOG_DIR=/var/log/pensando/
+export HAL_LIBRARY_PATH=$NIC_DIR/lib:$NIC_DIR/../platform/lib:/usr/local/lib:/usr/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
+export HAL_PBC_INIT_CONFIG="2x100_hbm"
+export HAL_PLATFORM_HW=1
+$NIC_DIR/bin/apollo_scale_test -d -c hal_hw.json -i /nic/conf/scale_cfg.json > /data/logs.txt &
---
+++ nic/apollo/test/scale/run_scale_test_sim.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+export NICDIR=`pwd`
+export HAL_LOG_DIR=${NICDIR}
+export ZMQ_SOC_DIR=${NICDIR}
+export CAPRI_MOCK_MODE=1
+export CAPRI_MOCK_MEMORY_MODE=1
+export SKIP_VERIFY=1
+export BUILD_DIR=${NICDIR}/build/x86_64/apollo/
+export GEN_TEST_RESULTS_DIR=${BUILD_DIR}/gtest_results
+export HAL_CONFIG_PATH=${NICDIR}/conf
+#export GDB='gdb --args'
+
+export PATH=${PATH}:${BUILD_DIR}/bin
+$GDB apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg.json --gtest_output="xml:${GEN_TEST_RESULTS_DIR}/apollo_scale_test.xml"
