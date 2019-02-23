9aeddcbc443c0c3df67281127952e8cd028d5c8b
stavrospen <38233196+stavrospen@users.noreply.github.com>
2019-02-23 15:28:25 -0800

    Add version to firmware filename (#9824)

--- nic/mkdefs/image.mk
+++ nic/mkdefs/image.mk
@@ -72,4 +72,6 @@ build-image: build-squashfs
 
 .PHONY: firmware
 firmware: build-image
-	cp ${NICDIR}/buildroot/output/images/naples_fw.tar ${NICDIR}/naples_fw.tar
+	cp ${NICDIR}/buildroot/output/images/naples_fw.tar \
+		${NICDIR}/naples_fw_${SW_VERSION}.tar
+	ln -rs ${NICDIR}/naples_fw_${SW_VERSION}.tar ${NICDIR}/naples_fw.tar
