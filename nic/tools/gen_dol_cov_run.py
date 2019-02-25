4f3717d593aff053e2b123a475a2bed2723d269f
sudhiaithal <sudhiaithal@gmail.com>
2019-02-25 11:09:09 -0800

    Enhance coverage to take multiple job,yml as input (#9879)

--- nic/tools/gen_dol_cov_run.py
+++ nic/tools/gen_dol_cov_run.py
@@ -12,6 +12,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--skip-targets', dest='skip_targets',
                         nargs='*', type=str,
                         default=[], help='Skip targets')
+parser.add_argument('--job-files', dest='job_files',
+                        nargs='*', type=str,
+                        default=None, help='List of Job ymls')
+
 args = parser.parse_args()
 
 DOL_JOB_PREFIX = "jobd"
@@ -36,26 +40,35 @@ model_logging_enabled = os.environ.get("MODEL_LOGGING_ENABLED")
 os.chdir(nic_dir)
 
 try:
-    with open(".job.yml") as f:
-        job = yaml.load(f)
-except:
-    print "Error loading .job.yml"
-    sys.exit(1)
-
-try:
     file = coverage_output_path + "/" + OUTPUT_FILE
     output_file = open(file, "w+")
 except:
     print "Error creating output file"
     sys.exit(1)
 
+
+
+
+
 job_cfg = {}
-for target in job["targets"]:
-    if target in args.skip_targets:
-        continue
-    job_info = job["targets"][target]["commands"]
-    cov_cmd = "JOB_ID=1 " + " ".join(job_info) + " COVERAGE=1"
-    job_cfg["_".join(target.split("/")[-2:])] = cov_cmd
+
+def __update_job_cfg(job_file):
+    try:
+        with open(job_file) as f:
+            job = yaml.load(f)
+    except:
+        print "Error loading : " + job_file
+        sys.exit(1)
+
+    for target in job["targets"]:
+        if target in args.skip_targets:
+            continue
+        job_info = job["targets"][target]["commands"]
+        cov_cmd = "JOB_ID=1 " + " ".join(job_info) + " COVERAGE=1"
+        job_cfg["_".join(target.split("/")[-2:])] = cov_cmd
+
+for job_file in args.job_files:
+    __update_job_cfg(job_file)
 
 json.dump(job_cfg, output_file, indent=4)
 output_file.close()
