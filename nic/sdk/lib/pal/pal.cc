a7447ea654cf8250230a8735ca06c0b21870b3a7
sai sashankh donkena <sai@pensando.io>
2019-02-27 10:16:44 +0530

    Suppressing this message in captrace (#9933)
    captrace conf /nic/conf/captrace/pipeline-all.json
    [pal_init:31] Initializing PAL in HW mode

--- nic/sdk/lib/pal/pal.cc
+++ nic/sdk/lib/pal/pal.cc
@@ -28,7 +28,7 @@ pal_init (platform_type_t platform_type)
     switch(platform_type) {
     case platform_type_t::PLATFORM_TYPE_HW:
     case platform_type_t::PLATFORM_TYPE_HAPS:
-        SDK_TRACE_DEBUG("Initializing PAL in HW mode");
+        SDK_TRACE_VERBOSE("Initializing PAL in HW mode");
         return pal_hw_init();
 
     case platform_type_t::PLATFORM_TYPE_SIM:
