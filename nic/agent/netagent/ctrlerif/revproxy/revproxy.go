8792fabd37dc73eec8a11a7f31ae2e00eaa3c284
rathorea <32275334+rathorea@users.noreply.github.com>
2019-03-05 23:50:28 -0800

    audit logs in revproxy (#10145)

--- nic/agent/netagent/ctrlerif/revproxy/revproxy.go
+++ nic/agent/netagent/ctrlerif/revproxy/revproxy.go
@@ -2,6 +2,7 @@ package revproxy
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -17,6 +18,34 @@ import (
 // Server holds information about the reverse proxy http server
 type Server struct {
 	httpServer *http.Server // HTTP server
+}
+
+const (
+	kiB = 1024 // kilobyte
+)
+const twoKB int64 = 2 * kiB
+
+func dumpHTTPRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ContentLength > twoKB {
+			log.Infof("RevProxy: ContentLength: %d", r.ContentLength)
+			dump, err := httputil.DumpRequest(r, false)
+			if err != nil {
+				log.Infof("RevProxy: Could not dump request: %s", fmt.Sprint(err))
+			} else {
+				log.Infof("RevProxy: Client Request: %s Forms %v", dump, r.Header)
+			}
+		} else {
+			dump, err := httputil.DumpRequest(r, true)
+			if err != nil {
+				log.Infof("RevProxy: Could not dump request: %s", fmt.Sprint(err))
+			} else {
+				log.Infof("RevProxy: Client Request: %s", dump)
+			}
+		}
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
 }
 
 // NewRevProxyRouter creates a new reverse proxy router
@@ -56,7 +85,7 @@ func NewRevProxyRouter(listenURL string, proxyConfig map[string]string) (*Server
 	muxRtr.HandleFunc("/stop", proxyServer.handleStopRequest)
 
 	// update http handler
-	httpServer.Handler = muxRtr
+	httpServer.Handler = dumpHTTPRequest(muxRtr)
 
 	go httpServer.Serve(listener)
 
