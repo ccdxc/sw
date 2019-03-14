34b9f6cea7348434c1ad26926483440a10ca1a65
sudsampa <47798161+sudsampa@users.noreply.github.com>
2019-03-14 07:50:09 -0700

    Adding new suite for classic networking only (#10408)
    * Adding new suite for classic networking only
    
    * PXE Infra changes
    1. created helpers for needed activity
    2. followed flat file structure for init of nodes
    3. followed json and cleaned up pxe server details
    More changes for PXE infra to be independent and load more images as
    needed for Dev-Test activity

---
+++ iota/scripts/sanitize/helpers.py
@@ -0,0 +1,172 @@
+#! /usr/bin/python3
+import re
+import json
+import time
+import os
+
+TIMEOUT = 60
+NODE_TYPE_HOST = 'host'
+NODE_TYPE_PXE  = 'pxe'
+PXE_SVR_FILENAME = './pxe_server.json'
+from helpers import *
+
+def JsonToDict(filename=PXE_SVR_FILENAME):
+    with open(filename) as fp:
+        return(json.load(fp))
+
+def SendlineExpect(line, expect, hdl, timeout = TIMEOUT):
+    hdl.sendline(line)
+    return hdl.expect_exact(expect, timeout)
+
+class PxeServerHelper:
+    server_info = None
+    PXE_OP_CREATE = 'create'
+    PXE_OP_DELETE = 'delete'
+
+    @staticmethod
+    def SetupPxeServer(server, operation, macs, osname):
+        for mac in macs:
+            mac = '01-' + re.sub(':', '-', mac)
+            if operation == PxeServerHelper.PXE_OP_CREATE:
+                ssh_cmd = 'cd /export/tftpboot/pxelinux.cfg && ln -sf %s %s' % (PxeServerHelper.GetOsPath(osname), mac.lower())
+            if operation == PxeServerHelper.PXE_OP_DELETE:
+                ssh_cmd = 'cd /export/tftpboot/pxelinux.cfg && rm -f %s' % (mac.lower())
+            server.RunSshCmd(ssh_cmd)
+    
+    @staticmethod
+    def InitServerInfo():
+        if PxeServerHelper.server_info == None:
+            PxeServerHelper.server_info = JsonToDict()
+        return True 
+
+    @staticmethod
+    def GetServerIp():
+        return PxeServerHelper.server_info['ipaddr']
+    
+    @staticmethod
+    def GetOsPath(osname):
+        if osname not in PxeServerHelper.server_info['images'].keys():
+            raise Exception("[%s] Not a valid OS for PXE server" % osname)
+        return PxeServerHelper.server_info['images'][osname]
+
+class ServerHelper():
+    @staticmethod
+    def GetUserNameByType(devtype):
+        if devtype == NODE_TYPE_PXE:
+            return 'root'
+        if devtype == NODE_TYPE_HOST:
+            return 'root'
+        raise Exception("Device type unknown")
+
+    @staticmethod
+    def GetPasswordByType(devtype):
+        if devtype == NODE_TYPE_PXE:
+            return 'N0isystem$'
+        if devtype == NODE_TYPE_HOST:
+            return 'docker'
+        raise Exception("Device type unknown")
+
+    @staticmethod
+    def GetPromptByType(devtype, osname):
+        if devtype == NODE_TYPE_PXE:
+            return ']#'
+        if devtype == NODE_TYPE_HOST:
+            return '#'
+        raise Exception("Device type unknown")
+
+class HostHelper():
+    
+    @staticmethod
+    def RunIpmi(host, command):
+        cmd = "ipmitool -I lanplus -H %s -U %s -P %s %s" % (host.cimc, host.cimc_username, host.cimc_password, command)
+        print ("%s: %s" % (str(host),command))
+        return  os.system(cmd)
+
+    @staticmethod
+    def SetUserPassword(host, username, password):
+        hdl = host.GetHandle()
+        SendlineExpect("sudo passwd %s" % username, "password:",  hdl)
+        SendlineExpect("%s" % password, "password:" , hdl)
+        SendlineExpect("%s" % password, ServerHelper.GetPromptByType(NODE_TYPE_HOST),  hdl)
+        return True
+
+    @staticmethod
+    def ShowVersion(host, cmd=None):
+        if cmd == None:
+            cmd = "cat /etc/os-release"
+        hdl = host.GetHandle()
+        SendlineExpect(cmd, ServerHelper.GetPromptByType(NODE_TYPE_HOST), hdl)
+        op = hdl.before
+        print ("%s: %s" % (str(host), op))
+        return True
+    
+    @staticmethod
+    def WaitForReachability(host, msg):
+        i = 0 
+        while i < 100:
+            print ("%s: %s" % (str(host),msg))
+            if host.IsPingable():
+                break
+            time.sleep(5)
+            i += 1
+        if i == 100:
+            raise Exception("Node not booting after [%s] seconds -- Debug" % str(100*5))
+        return True
+        
+    @staticmethod
+    def WaitForReboot(host, msg):
+        i = 0 
+        while i < 100:
+            print ("%s: %s" % (str(host), msg))
+            if not host.IsPingable():
+                break
+            time.sleep(5)
+            i += 1
+        if i == 100:
+            raise Exception("Node not booting after [%s] seconds -- Debug" % str(100*5))
+        return True
+        
+    @staticmethod
+    def SetHostName(host):
+        cmd = "hostname %s" % host.ipaddr
+        host.RunSshCmd(cmd)
+
+class CimcHelper():
+    @staticmethod
+    def GetCimcPrompt():
+        # Best to get from json
+        return "#"
+    
+    @staticmethod
+    def GetCimcUsername():
+        # Best to get from json
+        return "admin"
+
+    @staticmethod
+    def GetCimcPassword():
+        # Best to get from json
+        return "N0isystem$"
+
+    @staticmethod
+    def GetMacList(buf):
+        reg_exp = "[0-9A-F]{2}[:-][0-9A-F]{2}[:-][0-9A-F]{2}[:-][0-9A-F]{2}[:-][0-9A-F]{2}[:-][0-9A-F]{2}"
+        return re.findall(reg_exp, buf.upper())
+    
+    @staticmethod
+    def GetOobMacFromCimc(host):
+        cimc = host.GetCimcSshHandle()
+        SendlineExpect("scope chassis", CimcHelper.GetCimcPrompt(),  cimc)
+        SendlineExpect("scope network-adapter L", CimcHelper.GetCimcPrompt(),  cimc)
+        SendlineExpect("show mac-list", CimcHelper.GetCimcPrompt(),  cimc)
+        op = cimc.before
+        return CimcHelper.GetMacList(op)
+
+    @staticmethod
+    def SetBootOrder(host, boot_order):
+        cimc = host.GetCimcSshHandle()
+        SendlineExpect("scope bios", CimcHelper.GetCimcPrompt(),  cimc)
+        SendlineExpect("set boot-order %s" % boot_order, CimcHelper.GetCimcPrompt(),  cimc)
+        SendlineExpect("commit", "y|N]",  cimc)
+        SendlineExpect("y", CimcHelper.GetCimcPrompt(),  cimc)
+        return
+
--- iota/scripts/sanitize/pxe_boot.py
+++ iota/scripts/sanitize/pxe_boot.py
@@ -10,7 +10,8 @@ from datetime import timedelta
 top_dir = os.path.dirname(sys.argv[0])
 sys.path.insert(0,'%s' % top_dir)
 sys.path.insert(0,'../../')
-import defines
+import helpers
+from helpers import *
 from server import Server
 
 import harness.infra.utils.parser as par
@@ -21,11 +22,12 @@ parser = argparse.ArgumentParser(description='Sanitize Host in test bed file')
 # Mandatory parameters
 parser.add_argument('--testbed', dest='file_name', required = True, default=None, help='Testbed JSON filename')
 parser.add_argument('--os', dest='os', required = True, default=None, help='which OS to boot to')
-parser.add_argument('--os-ver', dest='os_ver', required = False, default=None, help='which Version to boot')
 
 GlobalOptions = parser.parse_args()
 
-def __get_date_string(osname):
+INITFILE = "./init.config"
+
+def __get_date_command(osname):
     x = datetime.now()
     x = x + timedelta(minutes=2)
     year  =  str(x.year)[2:]
@@ -33,99 +35,67 @@ def __get_date_string(osname):
     day   = "{0:02d}".format(x.day)
     hour   = "{0:02d}".format(x.hour)
     minute = "{0:02d}".format(x.minute)
-    if osname == 'freebsd':
-        ret = "%s%s%s%s%s" % (str(year),str(month),str(day),str(hour),str(minute))
-    if osname == 'linux':
-        ret = ("%s%s%s" % (str(year),str(month),str(day)), "%s%s" % (str(hour),str(minute)))
-    return ret
+    if 'freebsd' in osname.lower():
+        d = "%s%s%s%s%s" % (str(year),str(month),str(day),str(hour),str(minute))
+        cmd  = "date %s" % (d)
+    if 'linux' in osname.lower():
+        d = ("%s%s%s" % (str(year),str(month),str(day)), "%s%s" % (str(hour),str(minute)))
+        cmd  = "date %s -s %s && date %s -s %s" % ('%y%m%d', d[0], '%H%M', d[1] ) 
+    return cmd
 
-def __init_linux(host):
-    cmds = []
-    #host.SetUserPassword('vm', 'vm')
-    cmds.append("cp /usr/share/zoneinfo/America/Los_Angeles /etc/localtime")
-    d = __get_date_string('linux')
-    cmds.append("date %s -s %s && date %s -s %s" % ('%y%m%d', d[0], '%H%M', d[1] ))
-    cmds.append("hostnamectl  set-hostname %s" % host.ipaddr)
-    cmds.append("rm -f /root/.ssh/known_hosts")
-    cmds.append("mkdir -p /pensando")
-    cmds.append("chown vm:vm /pensando")
-    #cmds.append("cat /root/.bashrc | sed s/\'$ \'/\'%s \'/ > /tmp/prompt.txt && cp /tmp/prompt.txt /root/.bashrc && cat /root/.bashrc" % "#")
-    cmds.append("echo PS1=\\'\\\\\u@\\\\\H:\\\\\w#\\'  >> /root/.bashrc && cat /root/.bashrc")
-    for cmd in cmds:
-        host.RunSshCmd(cmd, True)
-    pass
+def __get_prompt_settings_command(prompt):
+    return "echo PS1=\\'\\\\\u@\\\\\H:\\\\\w#\\'  >> /root/.bashrc && cat /root/.bashrc"
 
-
-def __init_freebsd(host):
-    cmds = []
-    cmds.append("cp /usr/share/zoneinfo/America/Los_Angeles /etc/localtime")
-    cmds.append("date %s" % __get_date_string('freebsd'))
-    cmds.append("cat /etc/rc.conf | sed s/FreeBSD/%s/ > /tmp/hostname.txt && cp /tmp/hostname.txt /etc/rc.conf && cat /etc/rc.conf" % host.ipaddr)
-    cmds.append("rm -f /root/.ssh/known_hosts")
-    cmds.append("mkdir -p /pensando")
-    cmds.append("chown vm:vm /pensando")
-    for cmd in cmds:
-        host.RunSshCmd(cmd, True)
-    pass
+def __get_hostname_command(osname, hostname):
+    if 'freebsd' in osname.lower():
+        return ("cat /etc/rc.conf | sed s/FreeBSD/%s/ > /tmp/hostname.txt && cp /tmp/hostname.txt /etc/rc.conf && cat /etc/rc.conf" % hostname)
+    if 'linux' in osname.lower():
+        return ("hostnamectl  set-hostname %s" % hostname)
 
 def __init_node(host, osname):
-    if osname == 'freebsd':
-        __init_freebsd(host)
-    if osname == 'linux':
-        __init_linux(host)
-    if osname == 'ubuntu':
-        __init_linux(host)
+    #read from file and operate per os params later 
+    config  = helpers.JsonToDict(INITFILE)
+    cmds = config['cmds']
+    for key, value in config.items():
+        if key == 'cmds':
+            continue
+        if key == 'hostname':
+            cmds.append(__get_hostname_command(osname, host.ipaddr))
+        if key == "prompt":
+            cmds.append(__get_prompt_settings_command(osname))
+        if key == "date":
+            cmds.append(__get_date_command(osname))
+    for cmd in cmds:
+        host.RunSshCmd(cmd)
 
 def __print_msg(msg):
     print (msg)
 
-PXE_OP_CREATE = 'create'
-PXE_OP_DELETE = 'delete'
-
-def __setup_pxe_server(server, operation, macs, osname):
-    for mac in macs:
-        mac = '01-' + re.sub(':', '-', mac)
-        if operation == PXE_OP_CREATE:
-            ssh_cmd = 'cd /export/tftpboot/pxelinux.cfg && ln -sf %s %s' % (defines.GetOsPath(osname), mac.lower())
-        if operation == PXE_OP_DELETE:
-            ssh_cmd = 'cd /export/tftpboot/pxelinux.cfg && rm -f %s' % (mac.lower())
-
-        server.RunSshCmd(ssh_cmd)
-    
-def __get_defines_osname(osname):
-    if osname == 'linux':
-        osname = osname + '_' + GlobalOptions.os_ver
-    if not defines.CheckPxeSupport(osname):
-        raise Exception("Requested OS[%s] Not supported for pxe yet" % osname)
-    return osname 
-
 def __pxe_boot(host_spec):
-    osname = __get_defines_osname(host_spec.NodeOs)
+    osname = GlobalOptions.os
     
-    server = Server(defines.NODE_TYPE_PXE, defines.GetPxeServerIp())
-    host   = Server(defines.NODE_TYPE_HOST, host_spec.NodeMgmtIP, host_spec.NodeCimcIP)
-    macs = host.GetOobMacFromCimc()
-    __setup_pxe_server(server, PXE_OP_CREATE, macs, osname)
-    
+    server = Server(helpers.NODE_TYPE_PXE, PxeServerHelper.GetServerIp())
+    host   = Server(helpers.NODE_TYPE_HOST, host_spec.NodeMgmtIP, host_spec.NodeCimcIP)
+    macs = CimcHelper.GetOobMacFromCimc(host)
+    PxeServerHelper.SetupPxeServer(server, PxeServerHelper.PXE_OP_CREATE, macs, osname)
+   
     __print_msg('[%s]: Setting boot device to pxe' % host.ipaddr)
-    host.RunIpmi( "chassis bootdev pxe optsions=persistant" )
+    HostHelper.RunIpmi(host, "chassis bootdev pxe optsions=persistant" )
     __print_msg('[%s]: restarting the host to pxe boot' % host.ipaddr)
-    host.RunIpmi( "chassis power reset")
+    HostHelper.RunIpmi(host, "chassis power reset")
     time.sleep(10)
-    host.WaitForReachability("Waiting for host to PXE boot")
+    HostHelper.WaitForReachability(host, "Waiting for host to PXE boot")
     #Change boot dev to local disck
     __print_msg('[%s]: Setting boot device to disk' % host.ipaddr)
-    host.RunIpmi( "chassis bootdev disk optsions=persistant" )
-    host.WaitForReboot("Waiting for host to Install")
-    host.WaitForReachability("Waiting for host to boot from disk after installation")
+    HostHelper.RunIpmi(host, "chassis bootdev disk optsions=persistant" )
+    HostHelper.WaitForReboot(host, "Waiting for host to Install")
+    HostHelper.WaitForReachability(host, "Waiting for host to boot from disk after installation")
     __print_msg('[%s]: Setting boot device to disk' % host.ipaddr)
-    host.RunIpmi( "chassis bootdev disk optsions=persistant" )
-    
-    __setup_pxe_server(server, PXE_OP_DELETE, macs, osname)
-
-    host.SetBootOrder("hdd")
-    host.WaitForReboot("bios setting changed to hdd: waiting to reboot")
-    host.WaitForReachability("bios setting changed to hdd: rebooting")
+    HostHelper.RunIpmi(host, "chassis bootdev disk optsions=persistant" )
+    PxeServerHelper.SetupPxeServer(server, PxeServerHelper.PXE_OP_DELETE, macs, osname)
+    CimcHelper.SetBootOrder(host, "hdd")
+    HostHelper.WaitForReboot(host, "bios setting changed to hdd: waiting to reboot")
+    HostHelper.WaitForReachability(host, "bios setting changed to hdd: rebooting")
     time.sleep(5)
     # Initialize few things on node for iota run
     __init_node(host, host_spec.NodeOs)
@@ -143,6 +113,7 @@ def Main():
     Bringup all the hosts with os in argument via PXE boot.
     Run iota on this TB for basic connectivity check and conclude its good to go.
     '''
+    PxeServerHelper.InitServerInfo()
     hosts = __get_hosts(GlobalOptions.file_name)
     #PXE Boot the servers and make sure this are up
     run_parallel = True
