5e2b18fd20693b9ddc907bfa6474c2ae544d4a8a
dpshah0612 <dpshah.0612@gmail.com>
2019-03-14 17:39:34 -0700

    added file dependency for building mnci and mnet driver (#10445)

--- platform/drivers/linux/module.mk
+++ platform/drivers/linux/module.mk
@@ -2,5 +2,7 @@
 include ${MKDEFS}/pre.mk
 MODULE_ARCH     := aarch64
 MODULE_TARGET   := mnic.submake
+MODULE_DEPS      = $(wildcard ${TOPDIR}/platform/drivers/linux/eth/ionic/*) \
+                   $(wildcard ${TOPDIR}/platform/drivers/linux/mnet/*)
 export AARCH64_LINUX_HEADERS	:= ${NICDIR}/buildroot/output/linux-headers
 include ${MKDEFS}/post.mk
