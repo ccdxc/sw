8aea6bb330f941b1c94ce30f740813cbf3fdb8dc
Sukhesh Halemane <shaleman@users.noreply.github.com>
2019-03-25 09:33:42 -0700

    Fix a build error in shm iterator (#10735)

--- nic/delphi/shm/test/golang_iterate/module.mk
+++ nic/delphi/shm/test/golang_iterate/module.mk
@@ -5,5 +5,5 @@ MODULE_PREREQS  := libdelphishm.a
 MODULE_PIPELINE := iris
 MODULE_ARCH     := x86_64 
 MODULE_FLAGS    := -ldflags="-s -w"
-MODULE_DEPS     := $(shell find ${NICDIR}/delphi/shm/test/iterate -name '*.go')
+MODULE_DEPS     := $(shell find ${NICDIR}/delphi/shm/test/golang_iterate -name '*.go')
 include ${MKDEFS}/post.mk
