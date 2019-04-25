bd94903e9b5a07b55f6b527e8d7a2b5945a0f4c5
Rohan Sikdar <rsikdar@berkeley.edu>
2019-04-24 23:31:04 -0700

    reverting offline changes (#11636)

--- venice/ui/ui-build.sh
+++ venice/ui/ui-build.sh
@@ -2,11 +2,11 @@
 node -v
 cd ~/src/github.com/pensando/sw/venice/ui
 cd web-app-framework 
-npm install --offline
+npm install --prefer-offline
 npm run pack
 cd ../webapp 
-npm install  ../web-app-framework/dist/web-app-framework-0.0.0.tgz --offline
-npm install --offline
+npm install  ../web-app-framework/dist/web-app-framework-0.0.0.tgz --prefer-offline
+npm install --prefer-offline
 ng build 
 cd dist
 zip -r -X "../veniceui.zip" *
