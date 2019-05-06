8906d20d26dc698eeb4ffffe388e6464fcce8a79
Suresh Kumar Nalluru <nasuku@users.noreply.github.com>
2019-05-06 10:54:51 -0700

    add cat /warmd.json (#11875)

--- iota/test/venice/start_iota.sh
+++ iota/test/venice/start_iota.sh
@@ -7,5 +7,9 @@ fi
 
 pkill iota_server
 cd $GOPATH/src/github.com/pensando/sw/iota
+if [ -f /warmd.json ]; then
+    echo Starting IOTA server with the following contents of warmd.json
+    cat /warmd.json
+fi
 VENICE_DEV=1 bin/server/iota_server > server.log 2>&1 &
 echo "Started IOTA Server: in " $PWD " with log file server.log"
