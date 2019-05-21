07b479b3dec7373d82a86ff5d42c93ec007ae830
Akshaya Nadahalli <akshaya@pensando.io>
2019-05-21 19:52:21 +0530

    Add missing libs to packaging (#12280)
    Fix lib dependency for VPP flow plugin

--- nic/apollo/vpp/flow_plugin/module.mk
+++ nic/apollo/vpp/flow_plugin/module.mk
@@ -5,18 +5,13 @@ MODULE_TARGET   = flow_plugin.so
 MODULE_PIPELINE = apollo
 MODULE_ARCH     = aarch64
 #MODULE_PREREQS  = vpp_build.export
-MODULE_SOLIBS   = pal pdsframework pdscore pdsapi pdsapi_impl pdsapi_apollo_impl pdstestutils  \
-                  ${NIC_${PIPELINE}_P4PD_SOLIBS}                            \
-                  ${NIC_SDK_SOLIBS} ${NIC_HAL_PD_SOLIBS_${ARCH}}            \
-                  sdkp4 sdkp4utils sdkcapri_asicrw_if sdkcapri              \
-                  sdkplatformutils sdkxcvrdriver sdkasicpd lpmitree         \
-                  bm_allocator sdklinkmgr sdklinkmgrcsr memhash utils ftl   \
-                  ${NIC_APOLLO_NICMGR_LIBS}
-MODULE_LDLIBS   =  ${NIC_COMMON_LDLIBS}                                     \
-                   ${NIC_CAPSIM_LDLIBS}                                     \
-                   ${SDK_THIRDPARTY_CAPRI_LDLIBS}                           \
-                   ${SDK_THIRD_PARTY_VPP_LIBS}                              \
-                   AAPL edit ncurses
+MODULE_SOLIBS   = ftl ftlv4 logger sdkpal sdkcapri                          \
+                  sdkp4 sdkp4utils sdkasicpd bm_allocator                   \
+                  ${NIC_SDK_SOLIBS} ${NIC_${PIPELINE}_P4PD_SOLIBS}          \
+                  ${NIC_SDK_SOLIBS} ${NIC_HAL_PD_SOLIBS_${ARCH}}
+MODULE_LDLIBS   = ${SDK_THIRDPARTY_CAPRI_LDLIBS}                            \
+                  ${NIC_COMMON_LDLIBS} ${NIC_CAPSIM_LDLIBS} dl              \
+                  ${SDK_THIRD_PARTY_VPP_LIBS}
 MODULE_INCS     = ${NIC_CSR_INCS}                                           \
                   ${SDK_THIRD_PARTY_VPP_INCLUDES}
 MODULE_LDPATHS  = ${SDK_THIRD_PARTY_VPP_LDPATH}
