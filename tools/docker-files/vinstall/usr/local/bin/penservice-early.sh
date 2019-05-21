cb07be8708887f345510b6d25010d45c3d97fb05
Suresh Kumar Nalluru <nasuku@users.noreply.github.com>
2019-05-20 19:13:05 -0700

    persist /var/log on appliance. Dont add grub entry if one exists already. Linter fixes to use double-bracket (#12264)

--- tools/docker-files/vinstall/usr/local/bin/penservice-early.sh
+++ tools/docker-files/vinstall/usr/local/bin/penservice-early.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-if [ -z "`grep pen.venice /proc/cmdline `" ]
+if [[ -z "`grep pen.venice /proc/cmdline `" ]]
 then
     exit 0
 fi
@@ -13,12 +13,15 @@ mount -L PENDATA /data
 mount -L PENCFG /config
 mkdir -p /data/docker
 mkdir -p /data/lib/etcd
+mkdir -p /data/var/log
 mkdir -p /config/etc
 mkdir -p /config/etcd
 mkdir -p /var/lib/docker
 mkdir -p /var/lib/pensando
+mkdir -p /var/log
 mkdir -p /etc/pensando
 
+mount --bind /data/var/log /var/log
 mount --bind /run/initramfs/live/boot/ /boot
 mount --bind /data/docker /var/lib/docker
 mount --bind /data/lib /var/lib/pensando
@@ -27,7 +30,7 @@ mount --bind /config/etcd /var/lib/pensando/etcd # make sure this is after /var/
 
 
 # All scripts are run from here
-if [ -d /config/rc.d ]
+if [[ -d /config/rc.d ]]
 then
 	for i in /config/rc.d/*
 	do
@@ -48,7 +51,7 @@ mkdir -p ${BINDMNT_ROOT}
 for i in /var/lib/NetworkManager
 do
 	# first time read from dvd. After that keep bind mounting
-	if [ ! -r ${BINDMNT_ROOT}$i ]
+	if [[ ! -r ${BINDMNT_ROOT}$i ]]
 	then
 		cp --parents -a $i ${BINDMNT_ROOT}
 	fi
@@ -58,4 +61,4 @@ done
 
 
 
-[ -x /sbin/restorecon ] && /sbin/restorecon /var/lib/docker
+[[ -x /sbin/restorecon ]] && /sbin/restorecon /var/lib/docker
--- tools/scripts/venice_appl_GrubEntry.sh
+++ tools/scripts/venice_appl_GrubEntry.sh
@@ -11,7 +11,7 @@ MV=/bin/mv
 GRUBFILE=/run/initramfs/live/boot/grub2/grub.cfg
 
 while getopts ":oAdv:g:p:u:" arg; do
-  case $arg in
+  case ${arg} in
     A) OP="add";;   # add a given version in the grubfile
     u) OP="doupgrade";   # Given a directory as its argument, do the upgrade of image (except reboot)
         SRCPATH=$OPTARG;;
@@ -24,24 +24,24 @@ while getopts ":oAdv:g:p:u:" arg; do
   esac
 done
 
-if [ ! -w "$GRUBFILE" -o ! -f $GRUBFILE ]
+if [[ ! -w "$GRUBFILE" || ! -f ${GRUBFILE} ]]
 then
-    echo $GRUBFILE is not writable
+    echo ${GRUBFILE} is not writable
     exit 1
 fi
 
-if [ "${OP}" == "del" -a -z "$VERSION" ]
+if [[ "${OP}" == "del" && -z "$VERSION" ]]
 then
     echo VERSION  must be specified with -d option
     exit 2
 fi
-if [ "${OP}" == "add" -a -z "$VERSION" ]
+if [[ "${OP}" == "add" && -z "$VERSION" ]]
 then
     echo VERSION  must be specified with -A option
     exit 3
 fi
 
-if [ -z "$OP" ]
+if [[ -z "$OP" ]]
 then
     echo Operation  must be specified
     exit 4
@@ -49,7 +49,7 @@ fi
 
 function getBootUUID() {
     UUID=$(blkid -s UUID -t LABEL=PENBOOT -o export | grep UUID | head -n 1 | cut -f2 -d=)
-    if [ -z "$UUID" ]
+    if [[ -z "$UUID" ]]
     then
         echo Boot partition not found or labelled correctly. Please check installation
         exit 5
@@ -60,20 +60,28 @@ function addMenuEntry() {
     local VER=$1
     local GRUBFILE=$2
     getBootUUID
+
+    entryNum=$(grep menuentry ${GRUBFILE} | grep -n ${VER} | head -n 1 | cut -d: -f1)
+
+    # add an entry of one does not exist already
+    if [[ "${entryNum}" == "" ]]
+    then
+
     cat >> ${GRUBFILE} <<END
-menuentry $VER {
+menuentry ${VER} {
     linux16 /OS-${VER}/vmlinuz0 rw rd.fstab=0 root=live:UUID=${UUID} rd.live.dir=/OS-${VER} rd.live.squashimg=squashfs.img console=ttyS0 console=tty0 rd.live.image rd.luks=0 rd.md=0 rd.dm=0  enforcing=0 LANG=en_US.utf8 rd.writable.fsimg=1 pen.venice=OS-${VER}/venice.tgz pen.naples=OS-${VER}/naples_fw.tar
     initrd16 /OS-${VER}/initrd0.img
 }
-
 END
+    fi
+
 }
 
 function setDefaultBootEntry() {
     local VER=$1
     local GRUBFILE=$2
     entryNum=$(grep menuentry ${GRUBFILE} | grep -n ${VER} | head -n 1 | cut -d: -f1)
-    if [ "${entryNum}" == "" ]
+    if [[ "${entryNum}" == "" ]]
     then
         echo Version must be specified.
         exit 6
@@ -88,7 +96,7 @@ function setDefaultBootEntry() {
 function getCurBootVersion() {
     local GRUBFILE=$1
     entry=$(grep 'default=' ${GRUBFILE} | cut -d= -f2)
-    if [ -z "$entry" ]
+    if [[ -z "$entry" ]]
     then
         echo "Default entry not found in grub file. Exiting"
         exit 7
@@ -100,7 +108,7 @@ function getCurBootVersion() {
         echo "unable to determine the currently booted version in grub"
         exit 8
     fi
-    echo $version
+    echo ${version}
 }
 
 # set the default to entry #0
@@ -112,24 +120,24 @@ function setDefaultFirstEntry() {
 function doAddImage() {
     local VER=$1
     local GRUBFILE=$2
-    if [ ! -d /run/initramfs/live/OS-${VER} ]
+    if [[ ! -d /run/initramfs/live/OS-${VER} ]]
     then
-        echo OS version $VERis not installed.
+        echo OS version ${VER} is not installed.
         exit 9
     fi
-    addMenuEntry $VER $GRUBFILE
-    setDefaultBootEntry $VER $GRUBFILE
+    addMenuEntry ${VER} ${GRUBFILE}
+    setDefaultBootEntry ${VER} ${GRUBFILE}
 }
 
 function delMenuEntryVersion() {
     local VER=$1
     local GRUBFILE=$2
-    if [ -z "$(grep menuentry $GRUBFILE | grep $VER)" ]
+    if [[ -z "$(grep menuentry ${GRUBFILE} | grep ${VER})" ]]
     then
-        echo Version $VER not found in file $GRUBFILE
+        echo Version ${VER} not found in file ${GRUBFILE}
         exit 10
     fi
-    sed -i "/${VER}/,/}/d" $GRUBFILE
+    sed -i "/${VER}/,/}/d" ${GRUBFILE}
 }
 
 function doDelImage() {
@@ -138,54 +146,54 @@ function doDelImage() {
 
     # lets operate on a temp file first
     TMPFILE=`mktemp /tmp/veniceGrubTool.XXXXXX` || exit 10
-    cp $GRUBFILE $TMPFILE
+    cp ${GRUBFILE} ${TMPFILE}
 
-    curBootVersion=$(getCurBootVersion $GRUBFILE)
+    curBootVersion=$(getCurBootVersion ${GRUBFILE})
 
-    delMenuEntryVersion $VER $TMPFILE
+    delMenuEntryVersion ${VER} ${TMPFILE}
 
     remainingEntries=$(grep menuentry ${TMPFILE} | wc -l)
-    if [ $remainingEntries -le 0 ]
+    if [[ ${remainingEntries} -le 0 ]]
     then
         echo "Cant delete the last bootable image"
         exit 11
     fi
 
-    if  [ "$curBootVersion" == "$VER" ]
+    if  [[ "$curBootVersion" == "$VER" ]]
     then
         setDefaultFirstEntry ${TMPFILE}
     else
-        setDefaultBootEntry $curBootVersion ${TMPFILE}
+        setDefaultBootEntry ${curBootVersion} ${TMPFILE}
     fi
 
 
-    $MV -f ${TMPFILE} ${GRUBFILE}
-    $RM -fr /run/initramfs/live/OS-${VER}
+    ${MV} -f ${TMPFILE} ${GRUBFILE}
+    ${RM} -fr /run/initramfs/live/OS-${VER}
 }
 
 function keepOnlyCurrentDefaultImage() {
-    curBootVersion=$(getCurBootVersion $GRUBFILE)
+    curBootVersion=$(getCurBootVersion ${GRUBFILE})
     for v in $(grep menuentry ${GRUBFILE}  | grep -v ${curBootVersion} | awk '{print $2}')
     do
-        echo doDelImage $v $GRUBFILE
-        doDelImage $v $GRUBFILE
+        echo doDelImage ${v} ${GRUBFILE}
+        doDelImage ${v} ${GRUBFILE}
     done
 }
 
 function doPreUpgrade() {
-    if [ ! -d "${SRCPATH}" ]
+    if [[ ! -d "${SRCPATH}" ]]
     then
         echo "Cant read directory ${SRCPATH}"
         exit 14
     fi
     cd ${SRCPATH}
-    if [ ! -f venice.tgz -o ! -f naples_fw.tar -o ! -f venice_appl_os.tgz -o ! -f metadata.json ]
+    if [[ ! -f venice.tgz || ! -f naples_fw.tar || ! -f venice_appl_os.tgz || ! -f metadata.json ]]
     then
         echo "Cant find all the files required. Only see $(ls)"
         exit 15
     fi
     VER=$(eval echo $(cat metadata.json | jq '.Bundle.Version'))
-    if [ -z "$VER" ]
+    if [[ -z "$VER" ]]
     then
         echo "Cant find bundle version in metadata"
         exit 16
@@ -195,13 +203,13 @@ function doPreUpgrade() {
 
     diskused=$(du -kxc initrd0.img naples_fw.tar squashfs.img vmlinuz0 venice.tgz | grep total| cut -f1)
     diskavail=$(df -kP /run/initramfs/live  | grep  /run/initramfs/live |  awk '{print $4};')
-    if [ -z "$diskavail" -o -z "$diskused" ]
+    if [[ -z "$diskavail" || -z "$diskused" ]]
     then
         echo "Unable to determine free space available or disk space used"
         exit 17
     fi
 
-    if [ $diskavail -lt $((diskused + 10)) ]
+    if [[ ${diskavail} -lt $((diskused + 10)) ]]
     then
         echo "Not enough free disk space to install image"
         exit 18
@@ -209,21 +217,22 @@ function doPreUpgrade() {
 }
 
 function doUpgrade() {
-    DEST=/run/initramfs/live/OS-$VER
-    mkdir -p $DEST
-    cd $SRCPATH
+    DEST=/run/initramfs/live/OS-${VER}
+    mkdir -p ${DEST}
+    cd ${SRCPATH}
     tar zxvf venice_appl_os.tgz
     for i in initrd0.img naples_fw.tar squashfs.img vmlinuz0 venice.tgz
     do
-        cp $SRCPATH/$i $DEST
+        cp ${SRCPATH}/${i} ${DEST}
     done
-    doAddImage $VER $GRUBFILE
+    doAddImage ${VER} ${GRUBFILE}
+    sync
 }
 
 
-case $OP in
-"add") doAddImage $VERSION $GRUBFILE;;
-"del") doDelImage $VERSION $GRUBFILE;;
+case ${OP} in
+"add") doAddImage ${VERSION} ${GRUBFILE};;
+"del") doDelImage ${VERSION} ${GRUBFILE};;
 "only") keepOnlyCurrentDefaultImage;;
 "preupgrade") doPreUpgrade ;;
 "doupgrade") doPreUpgrade; keepOnlyCurrentDefaultImage; doUpgrade;;
