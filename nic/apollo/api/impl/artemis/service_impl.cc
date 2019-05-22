2b90eb5cf96a6bb203f479989283f3d974422527
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2019-05-21 21:24:20 -0700

    service impl pd template (#12315)

---
+++ nic/apollo/api/impl/artemis/service_impl.cc
@@ -0,0 +1,105 @@
+//
+// Copyright (c) 2019 Pensando Systems, Inc.
+//
+//----------------------------------------------------------------------------
+///
+/// \file
+/// datapath implementation of service mapping
+///
+//----------------------------------------------------------------------------
+
+#include "nic/apollo/core/mem.hpp"
+#include "nic/apollo/framework/api_engine.hpp"
+#include "nic/apollo/api/service.hpp"
+#include "nic/apollo/core/trace.hpp"
+#include "nic/apollo/api/pds_state.hpp"
+#include "nic/apollo/api/impl/artemis/pds_impl_state.hpp"
+#include "nic/apollo/p4/include/defines.h"
+#include "nic/sdk/lib/p4/p4_api.hpp"
+#include "nic/sdk/lib/table/memhash/mem_hash.hpp"
+#include "nic/sdk/include/sdk/table.hpp"
+#include "nic/sdk/lib/utils/utils.hpp"
+
+using sdk::table::sdk_table_api_params_t;
+
+namespace api {
+namespace impl {
+
+/// \defgroup PDS_SVC_MAPPING_IMPL - service mapping datapath implementation
+/// \ingroup PDS_SERVICE
+/// @{
+
+svc_mapping_impl *
+svc_mapping_impl::factory(pds_svc_mapping_spec_t *mapping) {
+    svc_mapping_impl    *impl;
+
+    impl = svc_mapping_impl_db()->alloc();
+    if (unlikely(impl == NULL)) {
+        return NULL;
+    }
+    return impl;
+}
+
+void
+svc_mapping_impl::soft_delete(svc_mapping_impl *impl) {
+    impl->~svc_mapping_impl();
+    svc_mapping_impl_db()->free(impl);
+}
+
+void
+svc_mapping_impl::destroy(svc_mapping_impl *impl) {
+    svc_mapping_impl::soft_delete(impl);
+}
+
+svc_mapping_impl *
+svc_mapping_impl::build(pds_svc_mapping_key_t *key) {
+    return NULL;
+}
+
+sdk_ret_t
+svc_mapping_impl::reserve_resources(api_base *orig_obj, obj_ctxt_t *obj_ctxt) {
+    return SDK_RET_INVALID_OP;
+}
+
+sdk_ret_t
+svc_mapping_impl::nuke_resources(api_base *api_obj) {
+    return SDK_RET_INVALID_OP;
+}
+
+sdk_ret_t
+svc_mapping_impl::release_resources(api_base *api_obj) {
+    return SDK_RET_OK;
+}
+
+sdk_ret_t
+svc_mapping_impl::program_hw(api_base *api_obj, obj_ctxt_t *obj_ctxt) {
+    return SDK_RET_INVALID_OP;
+}
+
+sdk_ret_t
+svc_mapping_impl::cleanup_hw(api_base *api_obj, obj_ctxt_t *obj_ctxt) {
+    return SDK_RET_INVALID_OP;
+}
+
+sdk_ret_t
+svc_mapping_impl::update_hw(api_base *curr_obj, api_base *prev_obj,
+                        obj_ctxt_t *obj_ctxt) {
+    return SDK_RET_INVALID_OP;
+}
+
+sdk_ret_t
+svc_mapping_impl::activate_hw(api_base *api_obj, pds_epoch_t epoch,
+                          api_op_t api_op, obj_ctxt_t *obj_ctxt) {
+    return SDK_RET_INVALID_OP;
+}
+
+void
+sdk_ret_t
+svc_mapping_impl::read_hw(obj_key_t *key, obj_info_t *info, void *arg) {
+    return SDK_RET_INVALID_OP;
+}
+
+/// \@}    // end of PDS_SVC_MAPPING_IMPL
+
+}    // namespace impl
+}    // namespace api
