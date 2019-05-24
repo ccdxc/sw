6f3c68f089878abdd2c396a04a29839a14b6a528
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2019-05-23 19:10:31 -0700

    pxe build issue (#12420)
    more packaging changes to artemis

--- platform/drivers/pxe/module.mk
+++ platform/drivers/pxe/module.mk
@@ -1,4 +1,6 @@
 # {C} Copyright 2019 Pensando Systems Inc. All rights reserved
 include ${MKDEFS}/pre.mk
 MODULE_TARGET   := ipxe.submake
+MODULE_ARCH     := aarch64
+MODULE_PIPELINE := iris
 include ${MKDEFS}/post.mk
