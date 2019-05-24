43d0f2d90fe51b65d7e6f54f91ff9cf1051510ce
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2019-05-23 20:27:56 -0700

    merged v4 & v6 lpm regions as max cache regions exceeded hw limit (#12423)
    TODO: meter & tag regions to follow the same approach

--- nic/apollo/api/impl/artemis/route_impl_state.cc
+++ nic/apollo/api/impl/artemis/route_impl_state.cc
@@ -14,6 +14,16 @@
 namespace api {
 namespace impl {
 
+// 1K routes, 3 stages, 16-way tree => 64 + 16 * 64 + 16 * 16 * 64 = 17472 bytes
+#define PDS_ROUTE_LPM_IPV4_BLOCK_SIZE     17472
+// region size = (64 + 1) * PDS_ROUTE_LPM_IPV4_BLOCK_SIZE
+#define PDS_ROUTE_LPM_IPV4_REGION_SIZE   (1 << 21)
+// 1K routes, 4 stages, 8-way tree =>
+// 64 + 8 * 64 + 8 * 8 * 64 + 8 * 8 * 8 * 64 = 37440 bytes
+#define PDS_ROUTE_LPM_IPV6_BLOCK_SIZE    37440
+// region size = (64 + 1) * PDS_ROUTE_LPM_IPV6_BLOCK_SIZE
+#define PDS_ROUTE_LPM_IPV6_REGION_SIZE    (3 * (1 << 20))
+
 route_table_impl_state::route_table_impl_state(pds_state *state) {
     /**
      * we need max + 1 blocks, extra 1 block for processing updates for
@@ -24,14 +34,13 @@ route_table_impl_state::route_table_impl_state(pds_state *state) {
     SDK_ASSERT(v4_idxr_ != NULL);
     v6_idxr_ = indexer::factory(PDS_MAX_ROUTE_TABLE + 1);
     SDK_ASSERT(v6_idxr_ != NULL);
-    v4_region_addr_ = state->mempartition()->start_addr("lpm_v4");
+    v4_region_addr_ = state->mempartition()->start_addr("lpm");
     SDK_ASSERT(v4_region_addr_ != INVALID_MEM_ADDRESS);
-    v4_table_size_ = state->mempartition()->block_size("lpm_v4");
-    v4_max_routes_ = state->mempartition()->max_elements("lpm_v4") - 1;
-    v6_region_addr_ = state->mempartition()->start_addr("lpm_v6");
-    SDK_ASSERT(v6_region_addr_ != INVALID_MEM_ADDRESS);
-    v6_table_size_ = state->mempartition()->block_size("lpm_v6");
-    v6_max_routes_ = state->mempartition()->max_elements("lpm_v6") - 1;
+    v4_table_size_ = PDS_ROUTE_LPM_IPV4_BLOCK_SIZE;
+    v4_max_routes_ = state->mempartition()->max_elements("lpm") - 1;
+    v6_region_addr_ = v4_region_addr_ + PDS_ROUTE_LPM_IPV4_REGION_SIZE;
+    v6_table_size_ = PDS_ROUTE_LPM_IPV6_BLOCK_SIZE;
+    v6_max_routes_ = v4_max_routes_;
 }
 
 route_table_impl_state::~route_table_impl_state() {
