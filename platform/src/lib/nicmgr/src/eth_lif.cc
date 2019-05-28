ccdxc/sw

commit 39235f5e47a1b66034b0cbec592fd5664f051d90

artemis compilation (#12404)

platform/src/lib/nicmgr/src/eth_lif.cc
@@ -19,7 +19,7 @@
 #include "nic/sdk/lib/thread/thread.hpp"
 #include "nic/p4/common/defines.h"
 
-#ifndef APOLLO
+#if !defined(APOLLO) && !defined(ARTEMIS)
 #include "gen/proto/nicmgr/metrics.delphi.hpp"
 #include "platform/src/app/nicmgrd/src/delphic.hpp"
 #endif
@@ -213,7 +213,7 @@ EthLif::EthLif(devapi *dev_api,
     NIC_LOG_INFO("{}: lif_stats_addr: {:#x}",
         hal_lif_info_.name, lif_stats_addr);
 
-#ifndef APOLLO
+#if !defined(APOLLO) && !defined(ARTEMIS)
     auto lif_stats =
         delphi::objects::LifMetrics::NewLifMetrics(hal_lif_info_.lif_id, lif_stats_addr);
     if (lif_stats == NULL) {
@@ -1370,7 +1370,7 @@ EthLif::NotifyQInit(void *req, void *req_data, void *resp, void *resp_data)
      * worst case we see the third attempt going through. Along with the original
      * notify block update we retry 3 times.
      */
-#ifdef APOLLO
+#if !defined(APOLLO) && !defined(ARTEMIS)
     uint8_t count = 3;
     while(count--) {
         port_status_t retry_status = {0};

nic/apollo/agent/core/module.mk
@@ -2,7 +2,7 @@
 
 include ${MKDEFS}/pre.mk
 MODULE_TARGET   = libpdsagentcore.so
-MODULE_PIPELINE = apollo artermis
+MODULE_PIPELINE = apollo artemis
 MODULE_INCS     = ${MODULE_GEN_DIR}
 MODULE_SOLIBS   = pdsapi
 include ${MKDEFS}/post.mk

nic/apollo/agent/module.mk
@@ -5,7 +5,7 @@ MODULE_TARGET   = agent.bin
 MODULE_PIPELINE = apollo artemis
 MODULE_INCS     = ${MODULE_GEN_DIR}
 MODULE_SOLIBS   = pdsproto trace svc pdsapi memhash sltcam ftl ftlv4 \
-				  pdsagentcore \
+				  pdsagentcore slhash \
                   ${NIC_${PIPELINE}_NICMGR_LIBS}
 MODULE_LDLIBS   = ${NIC_THIRDPARTY_GOOGLE_LDLIBS} \
                   ${NIC_COMMON_LDLIBS} edit ncurses

nic/apollo/agent/svc/module.mk
@@ -8,7 +8,7 @@ MODULE_SOLIBS   = pal pdsframework pdscore pdsapi pdsapi_impl \
                   ${NIC_${PIPELINE}_P4PD_SOLIBS} \
                   ${NIC_SDK_SOLIBS} ${NIC_HAL_PD_SOLIBS_${ARCH}} \
                   sdkp4 sdkp4utils sdkcapri_asicrw_if sdkcapri \
-                  sdkplatformutils sdkxcvrdriver sdkasicpd lpmitree \
+                  sdkplatformutils sdkxcvrdriver sdkasicpd \
                   bm_allocator sdklinkmgr sdklinkmgrcsr
 MODULE_LDLIBS   = ${NIC_CAPSIM_LDLIBS} \
                   ${SDK_THIRDPARTY_CAPRI_LDLIBS} \

nic/apollo/api/impl/artemis/meter_impl.cc
@@ -134,11 +134,12 @@ meter_impl::program_hw(api_base *api_obj, obj_ctxt_t *obj_ctxt) {
             n++;
         }
     }
-    ret = lpm_tree_create(rtable,
-                          (spec->af == IP_AF_IPV4) ? ITREE_TYPE_METER_V4 :
+#if 0
+    ret = lpm_tree_create(rtable, (spec->af == IP_AF_IPV4) ? ITREE_TYPE_METER_V4 :
                                                      ITREE_TYPE_METER_V6,
                           lpm_root_addr_,
                           meter_impl_db()->table_size(spec->af));
+#endif
     if (ret != SDK_RET_OK) {
         PDS_TRACE_ERR("Failed to build LPM meter table, err : %u", ret);
     }

nic/apollo/api/impl/artemis/pipeline_impl_base.cc
@@ -1,0 +1,36 @@
+//
+// {C} Copyright 2018 Pensando Systems Inc. All rights reserved
+//
+//----------------------------------------------------------------------------
+///
+/// \file
+/// Implementation of pipeline impl methods
+///
+//----------------------------------------------------------------------------
+
+#include "nic/apollo/framework/pipeline_impl_base.hpp"
+#include "nic/apollo/api/impl/artemis/artemis_impl.hpp"
+
+namespace api {
+namespace impl {
+
+/// \defgroup PDS_PIPELINE_IMPL Pipeline wrapper implementation
+/// @{
+
+pipeline_impl_base *
+pipeline_impl_base::factory(pipeline_cfg_t *pipeline_cfg) {
+    pipeline_impl_base *impl;
+    impl = artemis_impl::factory(pipeline_cfg);
+    impl->pipeline_cfg_ = *pipeline_cfg;
+    return impl;
+}
+
+void
+pipeline_impl_base::destroy(pipeline_impl_base *impl) {
+    artemis_impl::destroy(static_cast<artemis_impl*>(impl));
+}
+
+/// \@}
+
+}    // namespace impl
+}    // namespace api

nic/apollo/api/impl/artemis/route_impl.cc
@@ -161,6 +161,7 @@ route_table_impl::program_hw(api_base *api_obj, obj_ctxt_t *obj_ctxt) {
             break;
         }
     }
+#if 0
     ret = lpm_tree_create(rtable,
                           (spec->af == IP_AF_IPV4) ? ITREE_TYPE_IPV4 :
                                                      ITREE_TYPE_IPV6,
@@ -168,6 +169,7 @@ route_table_impl::program_hw(api_base *api_obj, obj_ctxt_t *obj_ctxt) {
                           (spec->af == IP_AF_IPV4) ?
                           route_table_impl_db()->v4_table_size() :
                           route_table_impl_db()->v6_table_size());
+#endif
     if (ret != SDK_RET_OK) {
         PDS_TRACE_ERR("Failed to build LPM route table, err : %u", ret);
     }

commit dfce94a0a4e6e9a82f58f0143c08d9459ce9bb8f

eth: remove incorrect link status update introduced by PR #12404. (#12496)

platform/src/lib/nicmgr/src/eth_lif.cc
@@ -1383,28 +1383,6 @@ EthLif::NotifyQInit(void *req, void *req_data, void *resp, void *resp_data)
     // Enable notifications
     notify_enabled = 1;
 
-    /*
-     * EDMA is not going through. We tried to write a well-known pattern during lif
-     * initialization in driver and thats being not updated without this hack.
-     * This hack tries to update the notify block multiple times and in the
-     * worst case we see the third attempt going through. Along with the original
-     * notify block update we retry 3 times.
-     */
-#if !defined(APOLLO) && !defined(ARTEMIS)
-    uint8_t count = 3;
-    while(count--) {
-        port_status_t retry_status = {0};
-        retry_status.status = lif_status->link_status;
-        retry_status.speed = lif_status->link_speed;
-        retry_status.id = spec->uplink_port_num;
-        NIC_LOG_INFO("{}: Retrying link event status: {}, speed: {}, port_num: {}",
-                     hal_lif_info_.name, retry_status.status, retry_status.speed,
-                     retry_status.id);
-        LinkEventHandler(&retry_status);
-        usleep(1000 * 1000);
-    }
-#endif
-
     return (IONIC_RC_SUCCESS);
 }
 