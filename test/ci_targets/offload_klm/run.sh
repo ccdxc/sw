d7fca940b9d9df7a24be7dfd7c2bb7d23a3819db
pirabhur <pirabhur@users.noreply.github.com>
2019-06-04 12:00:22 -0700

    Unload sonic and pencake modules unconditionally to start test with (#12779)
    clean state

--- test/ci_targets/offload_klm/run.sh
+++ test/ci_targets/offload_klm/run.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-cd /tmp/git/pensando/sw/storage/offload && OS_DIR=/usr/src/freebsd ./freebsd_build.sh && sudo kldload ./sonic.ko && sudo kldload ./pencake.ko && dmesg | grep "pnso_init failed with rc = 35" 
+sudo dmesg -c > /dev/null
+sudo kldunload pencake
+sudo kldunload sonic
+cd /tmp/git/pensando/sw/storage/offload && OS_DIR=/usr/src/freebsd ./freebsd_build.sh && sudo kldload ./sonic.ko && sudo kldload ./pencake.ko && sudo dmesg | grep "pnso_init failed with rc = 35" && sudo kldunload pencake && sudo kldunload sonic
+errcode=$?
+echo "errcode is $errcode"
+exit $errcode
