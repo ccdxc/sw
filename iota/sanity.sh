fff9bb8f4b97972ba87afebefa91cfee59af7a20
sudhiaithal <sudhiaithal@pensando.io>
2019-06-07 07:12:50 -0700

    Fix iota-infra tests (#12855)

--- iota/sanity.sh
+++ iota/sanity.sh
@@ -4,6 +4,8 @@ export DIR_GO_IOTA=$GOPATH/src/github.com/pensando/sw/iota
 
 cd $DIR_IOTA
 
+cp  ../iota.tgz . && tar -zxf iota.tgz -C .
+
 function ErrorCheckNExit() {
     if [ "$1" = "0" ];then
         return
@@ -13,10 +15,12 @@ function ErrorCheckNExit() {
     exit 1
 }
 
-make
-ErrorCheckNExit $? "make"
+#make
+#ErrorCheckNExit $? "make"
 
-cd $DIR_GO_IOTA && mkdir -p /pensando/iota && go  test -v -failfast -p=1 ./svcs/agent/
+cd $DIR_GO_IOTA 
+mkdir -p /pensando/iota
+sh -c "cd svcs/agent/ && ../../bin/agent.test"
 ErrorCheckNExit $? "Agent unit tests failed"
 
 infra_sanity="./iota.py  --testbed testbeds/sample.json --testsuite hw_naples_config  --debug --skip-firmware-upgrade --dryrun"
