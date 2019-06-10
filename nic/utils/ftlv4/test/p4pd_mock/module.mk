1e1ca2e39bb6328371200d60da903bd116b68aea
Vikas B Dharwadkar <vikasbd@users.noreply.github.com>
2019-06-10 16:23:59 -0700

    Remove FTLv4 copy (#12951)

--- nic/utils/ftlv4/test/p4pd_mock/module.mk
+++
@@ -1,8 +0,0 @@
-# {C} Copyright 2019 Pensando Systems Inc. All rights reserved
-include ${MKDEFS}/pre.mk
-MODULE_TARGET   = libftlp4pd_mock_ipv4.so
-MODULE_ARCH     = x86_64
-MODULE_FLAGS    = -fno-permissive
-MODULE_FLAGS    = -O3
-MODULE_PIPELINE = apollo iris
-include ${MKDEFS}/post.mk
