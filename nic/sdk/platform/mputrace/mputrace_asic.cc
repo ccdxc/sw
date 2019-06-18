6b3878b35b992a7355e65026a3cc3943f3aded57
sai sashankh donkena <sai@pensando.io>
2019-06-19 04:28:47 +0530

    captrace reset trace buffer (#13252)

--- nic/sdk/platform/mputrace/mputrace_asic.cc
+++ nic/sdk/platform/mputrace/mputrace_asic.cc
@@ -295,7 +295,9 @@ mputrace_reset_buffer_erase (cap_mpu_csr_trace_t trace)
     if (mputrace_util_is_enabled(trace)) {
         trace_addr = mputrace_util_base_addr_get(trace);
         trace_size = mputrace_util_size_get(trace);
-        sdk::lib::pal_mem_set(trace_addr, 0, trace_size);
+        trace_addr = (mem_addr_t)sdk::lib::pal_mem_map(trace_addr, trace_size);
+        memset((void*)trace_addr, 0, trace_size);
+        sdk::lib::pal_mem_unmap((void*)trace_addr);
     }
 }
 
