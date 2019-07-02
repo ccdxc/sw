e8deca769b39d9a04cc454504edf0968db9af2fc
ajeerpudiyapura <ajeer@pensando.io>
2019-07-01 19:31:40 -0700

    Fixing sacl DIP LPM the right table. (#13725)

--- nic/apollo/api/impl/artemis/lpm/lpm_ipv4_acl.cc
+++ nic/apollo/api/impl/artemis/lpm/lpm_ipv4_acl.cc
@@ -17,53 +17,53 @@ sdk_ret_t
 lpm_ipv4_acl_add_key_to_stage (uint8_t *bytes, uint32_t idx,
                                lpm_inode_t *lpm_inode)
 {
-    auto table = (rxlpm1_actiondata_t *)bytes;
+    auto table = (rxlpm2_actiondata_t *)bytes;
 
     switch (idx) {
     case 0:
-        table->action_u.rxlpm1_match1_32b.key0 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key0 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 1:
-        table->action_u.rxlpm1_match1_32b.key1 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key1 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 2:
-        table->action_u.rxlpm1_match1_32b.key2 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key2 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 3:
-        table->action_u.rxlpm1_match1_32b.key3 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key3 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 4:
-        table->action_u.rxlpm1_match1_32b.key4 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key4 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 5:
-        table->action_u.rxlpm1_match1_32b.key5 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key5 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 6:
-        table->action_u.rxlpm1_match1_32b.key6 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key6 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 7:
-        table->action_u.rxlpm1_match1_32b.key7 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key7 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 8:
-        table->action_u.rxlpm1_match1_32b.key8 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key8 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 9:
-        table->action_u.rxlpm1_match1_32b.key9 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key9 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 10:
-        table->action_u.rxlpm1_match1_32b.key10 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key10 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 11:
-        table->action_u.rxlpm1_match1_32b.key11 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key11 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 12:
-        table->action_u.rxlpm1_match1_32b.key12 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key12 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 13:
-        table->action_u.rxlpm1_match1_32b.key13 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key13 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     case 14:
-        table->action_u.rxlpm1_match1_32b.key14 = lpm_inode->ipaddr.addr.v4_addr;
+        table->action_u.rxlpm2_match2_32b.key14 = lpm_inode->ipaddr.addr.v4_addr;
         break;
     default:
         break;
@@ -75,49 +75,49 @@ sdk_ret_t
 lpm_ipv4_acl_add_key_to_last_stage (uint8_t *bytes, uint32_t idx,
                                     lpm_inode_t *lpm_inode)
 {
-    auto table = (rxlpm1_actiondata_t *)bytes;
+    auto table = (rxlpm2_actiondata_t *)bytes;
 
     switch (idx) {
     case 0:
-        table->action_u.rxlpm1_match1_32b_retrieve.key0 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key0 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data0 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data0 =
             (uint16_t)lpm_inode->data;
         break;
     case 1:
-        table->action_u.rxlpm1_match1_32b_retrieve.key1 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key1 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data1 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data1 =
             (uint16_t)lpm_inode->data;
         break;
     case 2:
-        table->action_u.rxlpm1_match1_32b_retrieve.key2 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key2 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data2 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data2 =
             (uint16_t)lpm_inode->data;
         break;
     case 3:
-        table->action_u.rxlpm1_match1_32b_retrieve.key3 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key3 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data3 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data3 =
             (uint16_t)lpm_inode->data;
         break;
     case 4:
-        table->action_u.rxlpm1_match1_32b_retrieve.key4 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key4 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data4 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data4 =
             (uint16_t)lpm_inode->data;
         break;
     case 5:
-        table->action_u.rxlpm1_match1_32b_retrieve.key5 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key5 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data5 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data5 =
             (uint16_t)lpm_inode->data;
         break;
     case 6:
-        table->action_u.rxlpm1_match1_32b_retrieve.key6 =
+        table->action_u.rxlpm2_match2_32b_retrieve.key6 =
             lpm_inode->ipaddr.addr.v4_addr;
-        table->action_u.rxlpm1_match1_32b_retrieve.data6 =
+        table->action_u.rxlpm2_match2_32b_retrieve.data6 =
             (uint16_t)lpm_inode->data;
         break;
     default:
@@ -129,8 +129,8 @@ lpm_ipv4_acl_add_key_to_last_stage (uint8_t *bytes, uint32_t idx,
 sdk_ret_t
 lpm_ipv4_acl_set_default_data (uint8_t *bytes, uint32_t default_data)
 {
-    auto table = (rxlpm1_actiondata_t *) bytes;
-    table->action_u.rxlpm1_match1_32b_retrieve.data_ =
+    auto table = (rxlpm2_actiondata_t *) bytes;
+    table->action_u.rxlpm2_match2_32b_retrieve.data_ =
             (uint16_t)default_data;
     return SDK_RET_OK;
 }
@@ -139,16 +139,16 @@ sdk_ret_t
 lpm_ipv4_acl_write_stage_table (mem_addr_t addr, uint8_t *bytes)
 {
     return impl_base::pipeline_impl()->write_to_rxdma_table(addr,
-               P4_ARTEMIS_RXDMA_TBL_ID_RXLPM1,
-               RXLPM1_MATCH1_32B_ID, bytes);
+               P4_ARTEMIS_RXDMA_TBL_ID_RXLPM2,
+               RXLPM2_MATCH2_32B_ID, bytes);
 }
 
 sdk_ret_t
 lpm_ipv4_acl_write_last_stage_table (mem_addr_t addr, uint8_t *bytes)
 {
     return impl_base::pipeline_impl()->write_to_rxdma_table(addr,
-               P4_ARTEMIS_RXDMA_TBL_ID_RXLPM1,
-               RXLPM1_MATCH1_32B_RETRIEVE_ID, bytes);
+               P4_ARTEMIS_RXDMA_TBL_ID_RXLPM2,
+               RXLPM2_MATCH2_32B_RETRIEVE_ID, bytes);
 }
 
 /**
