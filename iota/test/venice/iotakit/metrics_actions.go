42d6e92b4e6f6e45cbc97fcce3c0a8bc0f410601
Ranjith <rchirakk@users.noreply.github.com>
2019-07-15 10:25:53 -0700

    check stats reported by all naples (#14127)
    Signed-off-by: Ranjith <rchirakk@users.noreply.github.com>

--- iota/test/venice/iotakit/metrics_actions.go
+++ iota/test/venice/iotakit/metrics_actions.go
@@ -72,7 +72,7 @@ func (act *ActionCtx) VerifyRuleStats(timestr string, spc *SGPolicyCollection, m
 
 		// walk each rule
 		for idx, rule := range sts.RuleStatus {
-			res, err := act.model.tb.QueryMetrics("RuleMetrics", rule.RuleHash, timestr, 1)
+			res, err := act.model.tb.QueryMetrics("RuleMetrics", rule.RuleHash, timestr, int32(len(act.model.naples)))
 			if err != nil {
 				log.Errorf("Error during metrics query %v", err)
 			}
