1e09f2b261c9bf3039792754c50862baa9b64a39
Parameshwaran B <param@pensando.io>
2019-08-07 17:28:47 +0530

    Fix iperf cases for apollo IOTA (#14383)

--- iota/test/apollo/testcases/networking/ping.py
+++ iota/test/apollo/testcases/networking/ping.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python3
-import pdb
-
 import iota.harness.api as api
 import iota.test.apollo.config.api as config_api
+import iota.test.utils.traffic as traffic_utils
 
 def Setup(tc):
-    tc.skip = False
-
     if tc.args.type == 'local_only':
         tc.workload_pairs = config_api.GetPingableWorkloadPairs(
             wl_pair_type = config_api.WORKLOAD_PAIR_TYPE_LOCAL_ONLY)
@@ -15,61 +12,17 @@ def Setup(tc):
             wl_pair_type = config_api.WORKLOAD_PAIR_TYPE_REMOTE_ONLY)
 
     if len(tc.workload_pairs) == 0:
-        api.Logger.info("Skipping Testcase due to no workload pairs.")
-        tc.skip = True
+        api.Logger.error("Skipping Testcase due to no workload pairs.")
+        return api.types.status.FAILURE
 
     return api.types.status.SUCCESS
 
 def Trigger(tc):
-    if tc.skip: return api.types.status.SUCCESS
-
-    req = None
-    count = "5"
-    interval = "0.2"
-    if not api.IsSimulation():
-        req = api.Trigger_CreateAllParallelCommandsRequest()
-    else:
-        req = api.Trigger_CreateExecuteCommandsRequest(serial = False)
-        interval = "3"
-    tc.cmd_cookies = []
-
-    ping_base_cmd = "ping -A -W 1 -c %s -i %s -s %s " % (count, interval, tc.iterators.pktsize)
-    ping6_base_cmd = "ping6 -A -W 1 -c %s -i %s -s %s " % (count, interval, tc.iterators.pktsize)
-    for pair in tc.workload_pairs:
-        w1 = pair[0]
-        w2 = pair[1]
-        if tc.iterators.ipaf == 'ipv6':
-            cmd_cookie = "%s(%s) --> %s(%s)" %\
-                         (w1.workload_name, w1.ipv6_address, w2.workload_name, w2.ipv6_address)
-            ping_cmd = ping6_base_cmd + w2.ipv6_address
-        else:
-            cmd_cookie = "%s(%s) --> %s(%s)" %\
-                         (w1.workload_name, w1.ip_address, w2.workload_name, w2.ip_address)
-            ping_cmd = ping_base_cmd + w2.ip_address
-        api.Trigger_AddCommand(req, w1.node_name, w1.workload_name, ping_cmd)
-        api.Logger.info("Ping test from %s" % (cmd_cookie))
-        tc.cmd_cookies.append(cmd_cookie)
-
-    tc.resp = api.Trigger(req)
-
-
+    tc.cmd_cookies, tc.resp = traffic_utils.pingWorkloads(tc.workload_pairs, tc.iterators.ipaf, tc.iterators.pktsize)
     return api.types.status.SUCCESS
 
 def Verify(tc):
-    if tc.skip: return api.types.status.SUCCESS
-    if tc.resp is None:
-        return api.types.status.FAILURE
-
-    result = api.types.status.SUCCESS
-    cookie_idx = 0
-
-    for cmd in tc.resp.commands:
-        api.Logger.info("Ping Results for %s" % (tc.cmd_cookies[cookie_idx]))
-        api.PrintCommandResults(cmd)
-        if cmd.exit_code != 0:
-            result = api.types.status.FAILURE
-        cookie_idx += 1
-    return result
+    return traffic_utils.verifyPing(tc.cmd_cookies, tc.resp)
 
 def Teardown(tc):
     return api.types.status.SUCCESS
---
+++ iota/test/utils/traffic.py
@@ -0,0 +1,152 @@
+#! /usr/bin/python3
+import time
+import iota.harness.api as api
+import iota.test.iris.utils.iperf as iperf
+
+__PING_CMD = "ping"
+__PING6_CMD = "ping6"
+
+def __sleep(timeout):
+    if api.GlobalOptions.dryrun:
+        return
+    time.sleep(timeout)
+    return
+
+def __is_ipv4(af):
+    return af == "ipv4"
+
+def __get_ipproto(af):
+    if __is_ipv4(af):
+        return "v4"
+    return "v6"
+
+def __get_workload_address(workload, af):
+    if __is_ipv4(af):
+        return workload.ip_address
+    return workload.ipv6_address
+
+def __ping_addr_substitution(ping_base_cmd, addr):
+    ping_addr_options = " %s" %(addr)
+    ping_cmd = ping_base_cmd + ping_addr_options
+    return ping_cmd
+
+def __get_ping_base_cmd(af, packet_size, count, interval):
+    ping_cmd = __PING_CMD if __is_ipv4(af) else __PING6_CMD
+    ping_options = " -A -W 1 -c %d -i %f -s %d " %(count, interval, packet_size)
+    ping_base_cmd = ping_cmd + ping_options
+    return ping_base_cmd
+
+def pingWorkloads(workload_pairs, af="ipv4", packet_size=64, count=3, interval=0.2):
+    cmd_cookies = []
+    ping_base_cmd = __get_ping_base_cmd(af, packet_size, count, interval)
+
+    if not api.IsSimulation():
+        req = api.Trigger_CreateAllParallelCommandsRequest()
+    else:
+        req = api.Trigger_CreateExecuteCommandsRequest(serial = False)
+    
+    for pair in workload_pairs:
+        w1 = pair[0]
+        w2 = pair[1]
+        addr = __get_workload_address(w2, af)
+        ping_cmd = __ping_addr_substitution(ping_base_cmd, addr)
+        api.Logger.verbose(" Ping cmd %s " % (ping_cmd))
+        api.Trigger_AddCommand(req, w1.node_name, w1.workload_name, ping_cmd)
+        cmd_cookies.append(ping_cmd)
+
+    resp = api.Trigger(req)
+    return cmd_cookies, resp
+
+def verifyPing(cmd_cookies, response, exit_code=0):
+    result = api.types.status.SUCCESS
+    if response is None:
+        api.Logger.error("verifyPing failed - no response")
+        return api.types.status.FAILURE
+    commands = response.commands
+    cookie_idx = 0
+    for cmd in commands:
+        if cmd.exit_code != exit_code:
+            api.Logger.error("verifyPing failed for %s" % (cmd_cookies[cookie_idx]))
+            api.PrintCommandResults(cmd)
+            result = api.types.status.FAILURE
+        cookie_idx += 1
+    return result
+
+def iperfWorkloads(workload_pairs, af="ipv4", proto="tcp", packet_size=64):
+    serverCmds = []
+    clientCmds = []
+    cmdDesc = []
+    num_sessions = 1
+    ipproto = __get_ipproto(af)
+    
+    if not api.IsSimulation():
+        serverReq = api.Trigger_CreateAllParallelCommandsRequest()
+        clientReq = api.Trigger_CreateAllParallelCommandsRequest()
+    else:
+        serverReq = api.Trigger_CreateExecuteCommandsRequest(serial = False)
+        clientReq = api.Trigger_CreateExecuteCommandsRequest(serial = False)
+
+    for idx, pairs in enumerate(workload_pairs):
+        client = pairs[0]
+        server = pairs[1]
+        server_addr = __get_workload_address(server, af)
+        client_addr = __get_workload_address(client, af)
+        port = api.AllocateUdpPort() if proto == 'udp' else api.AllocateTcpPort()
+        serverCmd = iperf.ServerCmd(port)
+        clientCmd = iperf.ClientCmd(server_addr, port, 1, packet_size, proto, None, ipproto, num_sessions, jsonOut=True)
+
+        cmd_cookie = "Server: %s(%s:%s:%d) <--> Client: %s(%s)" %\
+                     (server.workload_name, server_addr, proto, port,\
+                      client.workload_name, client_addr)
+        api.Logger.verbose("Starting Iperf test %s num-sessions %d" % (cmd_cookie, num_sessions))
+        serverCmds.append(serverCmd)
+        clientCmds.append(clientCmd)
+        cmdDesc.append(cmd_cookie)
+
+        api.Trigger_AddCommand(serverReq, server.node_name, server.workload_name, serverCmd, background = True)
+        api.Trigger_AddCommand(clientReq, client.node_name, client.workload_name, clientCmd)
+
+    server_resp = api.Trigger(serverReq)
+    #Sleep for some time as bg may not have been started.
+    __sleep(30)
+    client_resp = api.Trigger(clientReq)
+    __sleep(3)
+    api.Trigger_TerminateAllCommands(server_resp)
+    return [cmdDesc, serverCmds, clientCmds], client_resp
+
+def verifyIPerf(cmd_cookies, response, exit_code=0):
+    result = api.types.status.SUCCESS
+    conn_timedout = 0
+    control_socker_err = 0
+    cmdDesc = cmd_cookies[0]
+    serverCmds = cmd_cookies[1]
+    clientCmds = cmd_cookies[2]
+    for idx, cmd in enumerate(response.commands):
+        api.Logger.verbose("Iperf Result for %s" % (cmdDesc[idx]))
+        api.Logger.verbose("Iperf Server cmd %s" % (serverCmds[idx]))
+        api.Logger.verbose("Iperf Client cmd %s" % (clientCmds[idx]))
+        if cmd.exit_code != exit_code:
+            api.Logger.error("Iperf client exited with error")
+            api.PrintCommandResults(cmd)
+            if iperf.ConnectionTimedout(cmd.stdout):
+                api.Logger.error("Connection timeout, ignoring for now")
+                conn_timedout = conn_timedout + 1
+                continue
+            if iperf.ControlSocketClosed(cmd.stdout):
+                api.Logger.error("Control socket cloned, ignoring for now")
+                control_socker_err = control_socker_err + 1
+                continue
+            if iperf.ServerTerminated(cmd.stdout):
+                api.Logger.error("Iperf server terminated")
+                result = api.types.status.FAILURE
+            if not iperf.Success(cmd.stdout):
+                api.Logger.error("Iperf failed", iperf.Error(cmd.stdout))
+                result = api.types.status.FAILURE
+        elif not api.GlobalOptions.dryrun:
+            api.Logger.verbose("Iperf Send Rate in Gbps ", iperf.GetSentGbps(cmd.stdout))
+            api.Logger.verbose("Iperf Receive Rate in Gbps ", iperf.GetReceivedGbps(cmd.stdout))
+
+    api.Logger.verbose("Iperf test successfull")
+    api.Logger.verbose("Number of connection timeouts : {}".format(conn_timedout))
+    api.Logger.verbose("Number of control socket errors : {}".format(control_socker_err))
+    return result
