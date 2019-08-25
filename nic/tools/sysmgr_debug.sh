508aa314f9de20361fced42f503a64272978c4b1
stavrospen <38233196+stavrospen@users.noreply.github.com>
2019-08-24 18:05:23 -0700

    sysmgr: less logging on non-volatile memory (#15044)

--- nic/tools/sysmgr_debug.sh
+++ nic/tools/sysmgr_debug.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
 NOW=`date +"%Y%m%d%H%M%S"`
-LOG_LOCATION=/data/debug-${NOW}-$1/
+TEMP_LOCATION=/data/debug-${NOW}-$1
+FINAL_LOCATION=/data/lastlog
 
-mkdir -p ${LOG_LOCATION}
+mkdir -p ${TEMP_LOCATION}
 
 # Check CPU load
-top -b -n 1 -d 1 > ${LOG_LOCATION}/top_start.txt
+top -b -n 1 -d 1 > ${TEMP_LOCATION}/top_start.txt
 
-# Copy everything under `/log` to `/data/debug-${NOW}/log`
-cp -a /var/log ${LOG_LOCATION}/log
+# Copy everything under `/log`
+cp -a /var/log ${TEMP_LOCATION}/log
 
 # One final CPU load check
-top -b -n 1 -d 1 > ${LOG_LOCATION}/top_end.txt
+top -b -n 1 -d 1 > ${TEMP_LOCATION}/top_end.txt
+
+mv ${FINAL_LOCATION} ${FINAL_LOCATION}.old
+rm -rf ${FINAL_LOCATION}.old
+mv ${TEMP_LOCATION} ${FINAL_LOCATION}
+# in case `mv` fails
+rm -rf ${TEMP_LOCATION}
