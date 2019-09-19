8807849847383340fdd421f1b4143a0cae007ba7
stavrospen <38233196+stavrospen@users.noreply.github.com>
2019-09-19 10:42:32 -0700

    Ingore sysmond in obfl_check (#15745)
    * Ingore sysmond in obfl_check
    
    * Ingore sysmond in obfl_check

--- iota/test/iris/testcases/system/obfl_check.py
+++ iota/test/iris/testcases/system/obfl_check.py
@@ -4,6 +4,8 @@ import iota.harness.api as api
 import string
 import time
 
+INGORE_FILES = [ 'sysmond' ]
+
 def parse_ls(output):
     files = {}
     lines = output.split('\n')
@@ -15,6 +17,14 @@ def parse_ls(output):
         files[fields[8]] = int(fields[4])
     return files
 
+def clean_log_files(tc):
+    req = api.Trigger_CreateExecuteCommandsRequest()
+    n = tc.Nodes[0]
+    api.Trigger_AddNaplesCommand(req, tc.Nodes[0], "rm -f /obfl/*.log.[0-9]")
+    res = api.Trigger(req)
+    api.Logger.info(res)
+    api.PrintCommandResults(res.commands[0])
+
 def load_log_files(tc):
     req = api.Trigger_CreateExecuteCommandsRequest()
     n = tc.Nodes[0]
@@ -24,20 +34,31 @@ def load_log_files(tc):
     api.PrintCommandResults(res.commands[0])
     return parse_ls(res.commands[0].stdout)
 
+def is_ignored(filename):
+    for ignored_file in INGORE_FILES:
+        if ignored_file in filename:
+            return True
+    return False
+
 def compare_files(before, after):
     success = True
-    for f, s in after.items():
-        if f not in before:
-            success = False
-            api.Logger.error("New file '%s' in obfl" % (f))
+    for filename, new_size in after.items():
+        if is_ignored(filename):
+            api.Logger.info("Ignoring '%s'" % (filename))
             continue
-        ns = before[f]
-        if ns > s:
+        if filename not in before:
+            success = False
+            api.Logger.error("New file '%s' in obfl" % (filename))
+            continue
+        old_size = before[filename]
+        if new_size != old_size:
             success = False
             api.Logger.error(
-                "File '%s' in obfl increased from %d to %d" % (f, s, ns))
+                "File '%s' in obfl increased from %d to %d" %
+                (filename, old_size, new_size))
             continue
-        api.Logger.info("File '%s' in obfl was %d is %d" % (f, s, ns))
+        api.Logger.info("File '%s' in obfl was %d is %d" %
+                        (filename, old_size, new_size))
     return success
 
 def Setup(tc):
@@ -45,6 +66,7 @@ def Setup(tc):
     return api.types.status.SUCCESS
 
 def Trigger(tc):
+    clean_log_files(tc)
     before = load_log_files(tc)
     time.sleep(5 * 60)
     after = load_log_files(tc)
