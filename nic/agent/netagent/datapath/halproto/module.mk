3ea6b43809f9b0edfc1e5788619852a65212f74f
rathorea <32275334+rathorea@users.noreply.github.com>
2019-09-25 08:35:01 -0700

    dependency fix (#15926)

--- nic/agent/netagent/datapath/halproto/module.mk
+++ nic/agent/netagent/datapath/halproto/module.mk
@@ -1,6 +1,7 @@
 # {C} Copyright 2018 Pensando Systems Inc. All rights reserved
 include ${MKDEFS}/pre.mk
 MODULE_TARGET   = agent_halproto.submake
+MODULE_PREREQS  = nmd_halproto.submake
 MODULE_PIPELINE = iris gft apollo
 MODULE_DIR      := ${GOPATH}/src/github.com/pensando/sw/nic/${MODULE_DIR}
 MODULE_DEPS     := $(wildcard ${NICDIR}/proto/hal/*.proto)
