86cca064296654c6247a0be8bd4faa053b571481
sai sashankh donkena <sai@pensando.io>
2019-09-26 20:51:58 +0530

    DOL apollo : rename the mlist file and the module names (#15914)

--- dol/apollo/test/networking/networking.py
+++ dol/apollo/test/networking/networking.py
@@ -6,7 +6,7 @@ import apollo.config.objects.dropstats as dropstats
 import apollo.config.utils as utils
 
 def Setup(infra, module):
-    if module.name == 'WORKFLOW_START':
+    if 'WORKFLOW_START' in module.name:
         utils.CachedObjs.select_objs = True
         utils.CachedObjs.setMaxLimits(module.testspec.selectors.maxlimits)
     modcbs.Setup(infra, module)
@@ -45,10 +45,10 @@ def TestCaseStepTeardown(tc, step):
     return True
 
 def TestCaseVerify(tc):
-    if tc.module.name == 'WORKFLOW_START':
+    if 'WORKFLOW_START' in tc.module.name:
         utils.CachedObjs.select_objs = False
         utils.CachedObjs.use_selected_objs = True
-    elif tc.module.name == 'WORKFLOW_END':
+    elif 'WORKFLOW_END' in tc.module.name:
         utils.CachedObjs.reset()
     if hasattr(tc.pvtdata, 'verify_drop_stats') and tc.pvtdata.verify_drop_stats\
        and hasattr(tc.pvtdata, 'dropstats') and tc.pvtdata.dropstats.Verify(tc) == False:
