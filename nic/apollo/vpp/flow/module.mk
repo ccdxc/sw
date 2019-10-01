b73f74273d97b956d50d499d6d2afd89f50eec64
Hareesh Ramachandran <hareesh@pensando.io>
2019-10-01 21:54:41 +0530

    Apollo, vpp module linking fix because of pds_init (#16017)

--- nic/apollo/vpp/flow/module.mk
+++ nic/apollo/vpp/flow/module.mk
@@ -5,16 +5,18 @@ MODULE_TARGET   = flow_plugin.so
 MODULE_PIPELINE = apollo artemis
 MODULE_ARCH     = aarch64 x86_64
 MODULE_PREREQS  = vpp_pkg.export
-MODULE_SOLIBS   = ftlv6 ftlv4 logger sdkpal sdkcapri                        \
-                  sdkp4 sdkp4utils sdkasicpd bm_allocator                   \
-                  ${NIC_SDK_SOLIBS} ${NIC_${PIPELINE}_P4PD_SOLIBS}          \
-                  ${NIC_SDK_SOLIBS} ${NIC_HAL_PD_SOLIBS_${ARCH}}
+MODULE_SOLIBS   = pal pdsframework pdscore pdsapi pdsapi_impl               \
+                  ${NIC_${PIPELINE}_P4PD_SOLIBS} ${NIC_SDK_SOLIBS}          \
+                  sdkp4 sdkp4utils sdkcapri_asicrw_if sdkcapri              \
+                  sdkplatformutils sdkxcvrdriver sdkasicpd                  \
+                  rfc_${PIPELINE} pdsrfc                                    \
+                  bm_allocator sdklinkmgr sdklinkmgrcsr ftlv6 ftlv4 utils   \
+                  sltcam slhash ${NIC_${PIPELINE}_NICMGR_LIBS}
 MODULE_LDLIBS   = ${SDK_THIRDPARTY_CAPRI_LDLIBS}                            \
                   ${NIC_COMMON_LDLIBS} ${NIC_CAPSIM_LDLIBS} dl              \
-                  ${SDK_THIRD_PARTY_VPP_LIBS}
+                  ${SDK_THIRD_PARTY_VPP_LIBS} AAPL
 MODULE_INCS     = ${NIC_CSR_INCS}                                           \
                   ${SDK_THIRD_PARTY_VPP_INCLUDES}
-#MODULE_LDPATHS  = ${SDK_THIRD_PARTY_VPP_LDPATH}
 MODULE_FLAGS    = ${NIC_CSR_FLAGS}
 ifeq ($(ARCH),x86_64)
     MODULE_DEFS     = -O0 -DCLIB_DEBUG -DCAPRI_SW ${NIC_CSR_DEFINES} -DCLIB_LOG2_CACHE_LINE_BYTES=6
