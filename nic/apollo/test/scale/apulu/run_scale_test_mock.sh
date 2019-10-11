9725f06f4f6d72323770a7dcd5a16c06f593f23c
akoradha <32655671+akoradha@users.noreply.github.com>
2019-10-11 16:00:00 -0700

    apulu scale test (#16275)

---
+++ nic/apollo/test/scale/apulu/run_scale_test_mock.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+set -e
+export NICDIR=`pwd`
+export NON_PERSISTENT_LOGDIR=${NICDIR}
+export ZMQ_SOC_DIR=${NICDIR}
+export CAPRI_MOCK_MODE=1
+export CAPRI_MOCK_MEMORY_MODE=1
+export SKIP_VERIFY=1
+export BUILD_DIR=${NICDIR}/build/x86_64/apulu/
+export GEN_TEST_RESULTS_DIR=${BUILD_DIR}/gtest_results
+export HAL_CONFIG_PATH=${NICDIR}/conf
+#export GDB='gdb --args'
+
+cfgfile=scale_cfg.json
+if [[ "$1" ==  --cfg ]]; then
+    cfgfile=$2
+fi
+
+export PATH=${PATH}:${BUILD_DIR}/bin
+$GDB apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/$cfgfile -f apulu --gtest_output="xml:${GEN_TEST_RESULTS_DIR}/apulu_scale_test.xml"
+if [ $? -eq 0 ]
+then
+    rm -f apulu_scale_test.log
+else
+    tail -100 apulu_scale_test.log
+fi
+#$GDB apollo_scale_test -p p1 -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg_p1.json --gtest_output="xml:${GEN_TEST_RESULTS_DIR}/apulu_scale_test.xml"
+#$GDB apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg_v4_only.json --gtest_output="xml:${GEN_TEST_RESULTS_DIR}/apulu_scale_test.xml"
+#valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all --gen-suppressions=all --verbose --error-limit=no --log-file=valgrind-out.txt apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg_v4_only.json
---
+++ nic/apollo/test/scale/apulu/run_scale_test_sim.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+set -e
+export NICDIR=`pwd`
+export NON_PERSISTENT_LOGDIR=${NICDIR}
+export ZMQ_SOC_DIR=${NICDIR}
+export CONFIG_PATH=${NICDIR}/conf/
+#export CAPRI_MOCK_MODE=1
+#export CAPRI_MOCK_MEMORY_MODE=1
+#export SKIP_VERIFY=1
+export BUILD_DIR=${NICDIR}/build/x86_64/apulu/
+export GEN_TEST_RESULTS_DIR=${BUILD_DIR}/gtest_results
+export HAL_CONFIG_PATH=${NICDIR}/conf
+#export GDB='gdb --args'
+#export TCID="1"
+
+export PATH=${PATH}:${BUILD_DIR}/bin
+$GDB apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg_sim.json -f apulu --gtest_output="xml:${GEN_TEST_RESULTS_DIR}/apulu_scale_test.xml"
+
+# Valgrind with XML output
+#valgrind --track-origins=yes --xml=yes --xml-file=out.xml apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg.json
+
+# Valgrind with text output
+#valgrind --track-origins=yes apollo_scale_test -c hal.json -i ${NICDIR}/apollo/test/scale/scale_cfg.json
