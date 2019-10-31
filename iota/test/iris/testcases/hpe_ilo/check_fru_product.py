03368907c12a8a9e1b882cf242bfcac34ffccca5
rahshekh <40581610+rahshekh@users.noreply.github.com>
2019-10-30 22:22:09 -0700

    Adding iota test for reading fru and cpld via HPE ilo (#16496)

---
+++ iota/test/iris/testcases/hpe_ilo/check_fru_product.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python3
+import time
+import pexpect
+import re
+import iota.harness.api as api
+import iota.harness.infra.utils.parser as parser
+from iota.harness.infra.glopts import GlobalOptions
+
+def Setup(tc):
+    return api.types.status.SUCCESS
+
+def Trigger(tc):
+    tbspec = parser.JsonParse(GlobalOptions.testbed_json)
+    for instance in tbspec.Instances:
+        #Check if the node is HPE or not
+        if hasattr(instance, 'NodeServer'):
+            if instance.NodeServer != "hpe":
+                api.Logger.info("Node is not an HPE server")
+                result = api.types.status.SUCCESS
+                continue
+        else:
+            api.Logger.info("CIMC ip is {} - ucs server".format(instance.NodeCimcIP))
+            result = api.types.status.SUCCESS
+            continue
+
+        api.Logger.info("Node server ip is {}".format(instance.NodeCimcIP))
+        child = pexpect.spawn("telnet %s 8" % instance.NodeCimcIP)
+        try :
+            child.expect(">")
+            child.sendline("plat_inv dump tbl 0")
+            child.expect(">\r\n>")
+            plat_inv_dump = child.before
+            plat_inv_dump_str = plat_inv_dump.decode("utf-8")
+            plat_inv_dump_str = plat_inv_dump_str.replace(" ", "")
+            newlist = plat_inv_dump_str.splitlines(True)
+            for i in range(len(newlist)):
+                if (newlist[i].find("pci_vendor_id:0x1dd8") != -1):
+                    i2c_seg_str = newlist[i-5].split(":")[1]
+                    break
+            i2c_seg = int(i2c_seg_str, 16)
+            child.sendline("i2c s %d" % i2c_seg)
+            child.expect(">\r\n>")
+            child.sendline("i2c s")
+            child.expect(">\r\n>")
+            child.sendline("i2c a 0xa0 w 0x25 r 9")
+            child.expect(">\r\n>")
+            fru_product = child.before
+            fru_product_data = fru_product.decode("utf-8")
+            fru_product_str = fru_product_data.split('Data length 9:')[1]
+            fru_product_str = fru_product_str.replace("\r\n", "")
+            fru_product_str = fru_product_str.replace("\x1b[0m", "")
+            fru_product_str = fru_product_str.replace("\t", "")
+            fru_product_str = fru_product_str.strip()
+            manu_list = fru_product_str.split(" ")
+            res = ""
+            for i in range(len(manu_list)):
+                res = res + chr(int(manu_list[i], 16))
+            api.Logger.info("Product string from fru is {}".format(res))
+            if (res == "NAPLES 25"):
+                result = api.types.status.SUCCESS
+            else:
+                result = api.types.status.FAILURE
+        except pexpect.TIMEOUT:
+            api.Logger.error("Time out encountered exiting")
+            result = api.types.status.FAILURE
+
+        child.close()
+    return result
+
+def Verify(tc):
+    return api.types.status.SUCCESS
+
+def Teardown(tc):
+    return api.types.status.SUCCESS
---
+++ iota/test/iris/testcases/hpe_ilo/check_temp_core.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python3
+import time
+import pexpect
+import re
+import iota.harness.api as api
+import iota.harness.infra.utils.parser as parser
+from iota.harness.infra.glopts import GlobalOptions
+
+def Setup(tc):
+    return api.types.status.SUCCESS
+
+def Trigger(tc):
+    tbspec = parser.JsonParse(GlobalOptions.testbed_json)
+    for instance in tbspec.Instances:
+        #Check if the node is HPE or not
+        if hasattr(instance, 'NodeServer'):
+            if instance.NodeServer != "hpe":
+                api.Logger.info("Node is not an HPE server")
+                result = api.types.status.SUCCESS
+                continue
+        else:
+            api.Logger.info("CIMC ip is {} - ucs server".format(instance.NodeCimcIP))
+            result = api.types.status.SUCCESS
+            continue
+
+        api.Logger.info("Node server ip is {}".format(instance.NodeCimcIP))
+        child = pexpect.spawn("telnet %s 8" % instance.NodeCimcIP)
+        try :
+            child.expect(">")
+            child.sendline("plat_inv dump tbl 0")
+            child.expect(">\r\n>")
+            plat_inv_dump = child.before
+            plat_inv_dump_str = plat_inv_dump.decode("utf-8")
+            plat_inv_dump_str = plat_inv_dump_str.replace(" ", "")
+            newlist = plat_inv_dump_str.splitlines(True)
+            for i in range(len(newlist)):
+                if (newlist[i].find("pci_vendor_id:0x1dd8") != -1):
+                    i2c_seg_str = newlist[i-5].split(":")[1]
+                    break
+            i2c_seg = int(i2c_seg_str, 16)
+            child.sendline("i2c s %d" % i2c_seg)
+            child.expect(">\r\n>")
+            child.sendline("i2c s")
+            child.expect(">\r\n>")
+            child.sendline("i2c a 0x94 w 0x16 r 1")
+            child.expect(">\r\n>")
+            cpld_temp_core = child.before
+            cpld_temp_core_data = cpld_temp_core.decode("utf-8")
+            cpld_temp_core_str = cpld_temp_core_data.split('Response Data length 1:')[1]
+            cpld_temp_core_str = cpld_temp_core_str.replace("\r\n", "")
+            cpld_temp_core_str = cpld_temp_core_str.replace("\x1b[0m", "")
+            cpld_temp_core_str = cpld_temp_core_str.replace("\t", "")
+            cpld_temp_core_str = cpld_temp_core_str.strip()
+            res = int(cpld_temp_core_str, 16)
+            api.Logger.info("Core temperature is {}".format(res))
+            if (res != 0):
+                result = api.types.status.SUCCESS
+            else:
+                result = api.types.status.FAILURE
+        except pexpect.TIMEOUT:
+            api.Logger.error("Time out encountered exiting")
+            result = api.types.status.FAILURE
+
+        child.close()
+    return result
+
+def Verify(tc):
+    return api.types.status.SUCCESS
+
+def Teardown(tc):
+    return api.types.status.SUCCESS
