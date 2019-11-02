02ff9967071bbb6e1871bcb34c229e01e0b0c599
vijasrin <8528272+vijasrin@users.noreply.github.com>
2019-11-01 17:52:23 -0700

    Add module arch filter (#16639)
    - Only build for x86 platform

--- nic/metaswitch/stubs/common/module.mk
+++ nic/metaswitch/stubs/common/module.mk
@@ -5,6 +5,7 @@ include $(TOPDIR)/nic/metaswitch/pre.mk
 MODULE_TARGET   = libpdsacommon.so
 MODULE_PREREQS  = metaswitch.submake
 MODULE_PIPELINE = apulu
+MODULE_ARCH     = x86_64
 MODULE_INCS 	= $(addprefix $(MS_ROOT)/,$(MS_INCLPATH))
 MODULE_FLAGS	= $(addprefix -D,$(MS_COMPILATION_SWITCH))
 include ${MKDEFS}/post.mk
