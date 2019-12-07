77f1f5b9086d34e4b5e88a841b4fb5c2ded607ea
dpshah0612 <dpshah.0612@gmail.com>
2019-12-07 10:26:52 -0800

    adding hw catalog support for Naples25 SWM and Naples25 OCP cards (#17311)
    enslaving uplinks to bond0 upon boot
    
    removing unneeded lib from nic/lib
    
    fixed review comment and added 59M size limit for goldfw to notify people early enough start taking actions
    
    removing goldfw size change as I had to merge it on priority to master with another PR
    
    updating buildroot
    
    reverting buildrot version as Rahul's PR has another buildroot which includes the changes related to this PR
    
    reverting buildroot version in submodule

--- nic/tools/bringup_mgmt_ifs.sh
+++ nic/tools/bringup_mgmt_ifs.sh
@@ -5,11 +5,16 @@ int_mnic_up=0
 oob_mnic_up=0
 inb_mnic0_up=0
 inb_mnic1_up=0
+inb_mnic0_enslaved=0
+inb_mnic1_enslaved=0
 
-echo "Waiting for mgmt interfaces(int_mnic0 and oob_mnic0) to show up"
+#setup the bonding device for inband management
+ip link s dev bond0 type bond mode active-backup miimon 100
 
-#Wait for 5 minutes for int_mnic0 nad oob_mnic0 to show up
-while [ $counter -gt 0 ]
+echo "Waiting for mgmt interfaces to show up"
+
+#Wait for mnic interfaces to show up
+while true
 do
     if [ -d "/sys/class/net/int_mnic0" ] && [ $int_mnic_up -eq 0 ] ; then
         ifup int_mnic0
@@ -33,6 +38,13 @@ do
     fi
 
     if [ -d "/sys/class/net/inb_mnic0" ] && [ $inb_mnic0_up -eq 0 ] ; then
+
+        if [ $inb_mnic0_enslaved -eq 0 ]; then
+            echo "adding inb_mnic0 (inband mgmt0) interface to bond0"
+            ifconfig bond0 up && ifenslave bond0 inb_mnic0
+            [[ $? -eq 0 ]] && inb_mnic0_enslaved=1
+        fi
+
         irq_number=`find /proc/irq  -name *inb_mnic0* | awk -F/ '{ print $4 }'`
         if [[ ! -z $irq_number ]]; then
             echo d > /proc/irq/$irq_number/smp_affinity
@@ -43,6 +55,12 @@ do
     fi
 
     if [ -d "/sys/class/net/inb_mnic1" ] && [ $inb_mnic1_up -eq 0 ] ; then
+        if [ $inb_mnic1_enslaved -eq 0 ]; then
+            echo "adding inb_mnic1 (inband mgmt1) interface to bond0"
+            ifconfig bond0 up && ifenslave bond0 inb_mnic1
+            [[ $? -eq 0 ]] && inb_mnic1_enslaved=1
+        fi
+
         irq_number=`find /proc/irq  -name *inb_mnic1* | awk -F/ '{ print $4 }'`
         if [[ ! -z $irq_number ]]; then
             echo d > /proc/irq/$irq_number/smp_affinity
@@ -58,10 +76,3 @@ do
 
 done
 
-echo ""
-
-if [ $int_mnic_up -eq 1 ]; then
-    echo "Brought up int_mnic0(169.254.0.1) interface for Naples management"
-else
-    echo "int_mnic0 (mgmt) interface didn't show up for 5 minutes!!!"
-fi
