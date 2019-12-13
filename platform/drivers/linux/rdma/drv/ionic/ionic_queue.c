17f99febc30963b153d55e7e5d5ea0320f12c069
Andrew Boyer <aboyer@pensando.io>
2019-12-13 16:04:25 -0500

    Revert "ionic_rdma: Pad RQ and SQ allocations to avoid page boundaries [PS-2116] (#16957)" (#17641)
    This reverts commit 6dae1faead4bf01380bd95299ebe1bd5c7600eec.
    
    The FW has been updated so that WQE reads will not cross page
    boundaries. This workaround is no longer needed.

--- platform/drivers/linux/rdma/drv/ionic/ionic_queue.c
+++ platform/drivers/linux/rdma/drv/ionic/ionic_queue.c
@@ -37,9 +37,6 @@ int ionic_queue_init(struct ionic_queue *q, struct device *dma_dev,
 	q->size = BIT_ULL(q->depth_log2 + q->stride_log2);
 	q->mask = BIT(q->depth_log2) - 1;
 
-	/* Temporary pad for SGL/page boundary issues [PS-2116] */
-	q->size += 2 * PAGE_SIZE;
-
 	q->ptr = dma_alloc_coherent(dma_dev, q->size, &q->dma, GFP_KERNEL);
 	if (!q->ptr)
 		return -ENOMEM;
@@ -50,11 +47,6 @@ int ionic_queue_init(struct ionic_queue *q, struct device *dma_dev,
 		return -ENOMEM;
 	}
 
-	/* Temporary pad for SGL/page boundary issues [PS-2116] */
-	q->ptr = (void *)((u64)q->ptr + PAGE_SIZE);
-	q->dma += PAGE_SIZE;
-	q->size -= 2 * PAGE_SIZE;
-
 	q->prod = 0;
 	q->cons = 0;
 	q->dbell = 0;
@@ -64,10 +56,5 @@ int ionic_queue_init(struct ionic_queue *q, struct device *dma_dev,
 
 void ionic_queue_destroy(struct ionic_queue *q, struct device *dma_dev)
 {
-	/* Temporary pad for SGL/page boundary issues [PS-2116] */
-	q->ptr = (void *)((u64)q->ptr - PAGE_SIZE);
-	q->dma -= PAGE_SIZE;
-	q->size += 2 * PAGE_SIZE;
-
 	dma_free_coherent(dma_dev, q->size, q->ptr, q->dma);
 }
