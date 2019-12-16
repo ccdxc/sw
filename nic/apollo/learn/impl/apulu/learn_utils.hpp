c39ed0c8039d975b34d26543e3c1490bca4a447e
Akshaya Nadahalli <akshaya@pensando.io>
2019-12-16 12:27:32 +0530

    DOL-DPDK SIM fixes (#17667)
    1. Change base address for descriptors
    2. For all dev/adminq commands allocate memory from hbm wherever
       physical address is reffered.
    3. Enable learn lif

--- nic/apollo/learn/impl/apulu/learn_utils.hpp
+++ nic/apollo/learn/impl/apulu/learn_utils.hpp
@@ -5,7 +5,6 @@
 #ifndef __LEARN_APULU_LEARN_UTILS_HPP__
 #define __LEARN_APULU_LEARN_UTILS_HPP__
 
-#define LEARN_LIF_NAME ""
-//#define LEARN_LIF_NAME  "net_ionic1"
+#define LEARN_LIF_NAME  "net_ionic1"
 
 #endif  //__LEARN_APULU_LEARN_UTILS_HPP__
