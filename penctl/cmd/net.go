577429de7b425a1bd8180a5998f7b18d70a5dce2
rathorea <32275334+rathorea@users.noreply.github.com>
2019-12-16 23:57:03 -0800

    setting naplesURL if nothing else works (#17734)

--- penctl/cmd/net.go
+++ penctl/cmd/net.go
@@ -67,10 +67,10 @@ func pickNetwork(cmd *cobra.Command, args []string) error {
 				}
 			}
 		}
+		// should never be set to this. but trying this ip as a last resort anyways
+		// also needed for backwards compatibility
+		naplesURL = "http://169.254.0.1"
 	}
-	// should never be set to this. but trying this ip as a last resort anyways
-	// also needed for backwards compatibility
-	naplesURL = "http://169.254.0.1"
 	naplesIP = strings.TrimPrefix(naplesURL, "http://")
 	revProxyPort = globals.AgentProxyPort
 	naplesURL += ":" + revProxyPort + "/"
