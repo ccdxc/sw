62e3613a9d79535d554f00b1bb489e1c658d89a2
Yan <yan@pensando.io>
2020-01-02 16:29:21 -0800

    Add test case for mirror session schedule time (#17967)

--- test/e2e/cluster/mirror_test.go
+++ test/e2e/cluster/mirror_test.go
@@ -3,7 +3,9 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/gogo/protobuf/types"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -74,6 +76,107 @@ var _ = Describe("mirror session tests", func() {
 			}
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(mirrorRestIf).ShouldNot(Equal(nil))
+		})
+
+		It("Should run mirror sessions after shcedule time", func() {
+			// create mirror session with exp duration of 5s
+			// check that it starts running and then stops after 5s
+			// delete mirror session
+			scheduledTestCase := monitoring.MirrorSession{
+				ObjectMeta: api.ObjectMeta{
+					Name:   "TestMirrorSession2",
+					Tenant: "default",
+				},
+				TypeMeta: api.TypeMeta{
+					Kind:       "MirrorSession",
+					APIVersion: "v1",
+				},
+				Spec: monitoring.MirrorSessionSpec{
+					PacketSize:    128,
+					PacketFilters: []string{monitoring.MirrorSessionSpec_ALL_PKTS.String()},
+					Collectors: []monitoring.MirrorCollector{
+						{
+							Type: "ERSPAN",
+							ExportCfg: &monitoring.MirrorExportConfig{
+								Destination: "100.1.1.1",
+							},
+						},
+					},
+					MatchRules: []monitoring.MatchRule{
+						{
+							Src: &monitoring.MatchSelector{
+								IPAddresses: []string{"10.1.1.10"},
+							},
+							AppProtoSel: &monitoring.AppProtoSelector{
+								ProtoPorts: []string{"TCP/5555"},
+							},
+						},
+						{
+							Src: &monitoring.MatchSelector{
+								IPAddresses: []string{"10.2.2.20"},
+							},
+							AppProtoSel: &monitoring.AppProtoSelector{
+								ProtoPorts: []string{"UDP/5555"},
+							},
+						},
+					},
+					// for this test case
+					// add schedule time after 30 seconds
+					// test mirror session state
+					StartConditions: monitoring.MirrorStartConditions{
+						ScheduleTime: &api.Timestamp{
+							Timestamp: types.Timestamp{
+								Seconds: time.Now().Unix() + 30,
+							},
+						},
+					},
+				},
+			}
+
+			ctx := ts.tu.MustGetLoggedInContext(context.Background())
+			ms := scheduledTestCase
+			By("Creating MirrorSession ------")
+			_, err := mirrorRestIf.Create(ctx, &ms)
+			Expect(err).ShouldNot(HaveOccurred())
+			time.Sleep(5 * time.Second)
+
+			By("Checking that MirrorSession has started after scheduled time exactly------")
+			tms, err := mirrorRestIf.Get(ctx, &ms.ObjectMeta)
+			if err != nil {
+				By(fmt.Sprintf("GET err:%s", err))
+			}
+			Expect(err).ShouldNot(HaveOccurred())
+
+			// raise error if session start before scheduled time
+			// the expected state should be SCHEDULED
+			if tms.Status.State != monitoring.MirrorSessionState_SCHEDULED.String() {
+				By(fmt.Sprintf("mirror state: %v before scheduled time", tms.Status.State))
+			}
+			Expect(tms.Status.State).To(Equal(monitoring.MirrorSessionState_SCHEDULED.String()))
+
+			Eventually(func() bool {
+				// get the new state
+				tms, err = mirrorRestIf.Get(ctx, &ms.ObjectMeta)
+				if err != nil {
+					By(fmt.Sprintf("GET err:%s", err))
+					return false
+				}
+
+				// raise error if session doesn't start after scheduled time
+				// the expected state should be RUNNING
+				if tms.Status.State != monitoring.MirrorSessionState_RUNNING.String() {
+					By(fmt.Sprintf("mirror state: %v after scheduled time", tms.Status.State))
+					return false
+				}
+
+				By("Deleting MirrorSession ------")
+				_, err = mirrorRestIf.Delete(ctx, &ms.ObjectMeta)
+				if err != nil {
+					By(fmt.Sprintf("GET err:%s", err))
+					return false
+				}
+				return true
+			}, 90, 30).Should(BeTrue(), fmt.Sprintf("Failed to get mirror session running state after scheduled time %s", ms.Name))
 		})
 
 		It("Check max mirror sessions", func() {
