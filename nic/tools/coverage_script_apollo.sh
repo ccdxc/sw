5b32b817d1b71c986146f43ffd23df4083172fc7
sudhiaithal <sudhiaithal@pensando.io>
2020-01-03 08:20:35 +0530

    Fix coverage with new container (#18105)

--- nic/tools/coverage_script_apollo.sh
+++ nic/tools/coverage_script_apollo.sh
@@ -2,7 +2,7 @@ export ASIC_TOOLS_DIR=/home/asic/tools
 mkdir -p $ASIC_TOOLS_DIR
 mount -o bind /asictools $ASIC_TOOLS_DIR
 
-cd /sw && make pull-assets && cd nic
+cd /usr/src/github.com/pensando/sw && make pull-assets && cd nic
 /home/asic/tools/eda/bullseye/bin/cov01 -1
 export PATH=$PATH:/home/asic/tools/eda/bullseye/bin
 tools/run-coverage --run-time 120 -s artemis/aarch64_build  -s artemis/valgrind -s apollo/aarch64_container -s apollo/x86_64_container -j apollo/.job.yml -c apollo_coverage.json -i --skip-asm-ins-stats
