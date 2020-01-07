90aa8cf3eba41b2cdde8ecd4add353517b885de2
vijasrin <8528272+vijasrin@users.noreply.github.com>
2020-01-06 19:22:18 -0800

    Fix json parsing in test app (#18214)

--- nic/metaswitch/stubs/test/common/test_config.cc
+++ nic/metaswitch/stubs/test/common/test_config.cc
@@ -63,38 +63,38 @@ parse_json_config (test_config_t *conf, uint8_t node) {
     g_node_id = test_config;
 
     try {
-    value                   = pt.get <std::string>("local.ip","");
+    value                   = pt.get <std::string>("local.ip");
     conf->local_ip_addr     = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("local.lo-ip","");
+    value                   = pt.get <std::string>("local.lo-ip");
     conf->local_lo_ip_addr  = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("local.gwip","");
+    value                   = pt.get <std::string>("local.gwip");
     conf->local_gwip_addr   = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("local.ac-ip","");
+    value                   = pt.get <std::string>("local.ac-ip");
     conf->local_mai_ip      = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("remote.ip","");
+    value                   = pt.get <std::string>("remote.ip");
     conf->remote_ip_addr    = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("remote.lo-ip","");
+    value                   = pt.get <std::string>("remote.lo-ip");
     conf->remote_lo_ip_addr = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("eth-if-index","");
+    value                   = pt.get <std::string>("eth-if-index");
     conf->eth_if_index      = strtol (value.c_str(),NULL, 0);
-    value                   = pt.get <std::string>("lif-if-index","");
+    value                   = pt.get <std::string>("lif-if-index");
     conf->lif_if_index      = strtol (value.c_str(),NULL, 0);
-    value                   = pt.get <std::string>("local.as","");
+    value                   = pt.get <std::string>("local.as");
     conf->local_asn         = strtol (value.c_str(),NULL, 0);
-    value                   = pt.get <std::string>("remote.as","");
+    value                   = pt.get <std::string>("remote.as");
     conf->remote_asn        = strtol (value.c_str(),NULL, 0);
-    value                   = pt.get <std::string>("vni","");
+    value                   = pt.get <std::string>("vni");
     conf->vni               = strtol (value.c_str(),NULL, 0);
-    value                   = pt.get <std::string>("route.dest_ip","");
+    value                   = pt.get <std::string>("route.dest_ip");
     conf->route_dest_ip     = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("route.nh_ip","");
+    value                   = pt.get <std::string>("route.nh_ip");
     conf->route_nh_ip       = inet_addr (value.c_str());
-    value                   = pt.get <std::string>("route.prefix","");
+    value                   = pt.get <std::string>("route.prefix");
     conf->route_prefix_len  = strtol (value.c_str(), NULL, 0);
 
     // RD and RT config parsing
-    conf->manual_rd         = pt.get <uint8_t>("manual-rd",0);
-    conf->manual_rt         = pt.get <uint8_t>("manual-rt",0);
+    conf->manual_rd         = pt.get <uint8_t>("manual-rd");
+    conf->manual_rt         = pt.get <uint8_t>("manual-rt");
     if (conf->manual_rd) {
         uint8_t i = 0;
         for (ptree::value_type &rd : pt.get_child("route-distinguisher")) {
@@ -112,6 +112,7 @@ parse_json_config (test_config_t *conf, uint8_t node) {
     } catch (std::exception const&  ex) {
         fprintf(stderr, "Config file %s doesn't have required fields\n",
                 file.c_str());
+        exit(0);
     }
 
     return 0;
