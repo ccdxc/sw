ccdxc/sw

commit afbff629a8aca89cc8100b6f17ef4ac3647330d1

ionic_rdma: Remove fallback code (#18240)
* ionic_rdma: Add comment tags to end of headers

* ionic_rdma: Remove userspace fallback code

platform/drivers/linux/rdma/lib/ionic/ionic_verbs.c
@@ -63,6 +63,133 @@
 		pthread_spin_unlock(lock);		\
 } while (0)
 
+static int ionic_query_device(struct ibv_context *ibctx,
+			      struct ibv_device_attr *dev_attr)
+{
+	struct ibv_query_device req = {};
+	uint64_t fw_ver;
+	int rc;
+
+	rc = ibv_cmd_query_device(ibctx, dev_attr, &fw_ver,
+				  &req, sizeof(req));
+	if (rc)
+		return rc;
+
+	rc = ibv_read_sysfs_file(ibctx->device->ibdev_path, "fw_ver",
+				 dev_attr->fw_ver, sizeof(dev_attr->fw_ver));
+	if (rc < 0)
+		dev_attr->fw_ver[0] = 0;
+
+	return 0;
+}
+
+static int ionic_query_port(struct ibv_context *ibctx, uint8_t port,
+			    struct ibv_port_attr *port_attr)
+{
+	struct ibv_query_port req = {};
+
+	return ibv_cmd_query_port(ibctx, port, port_attr,
+				  &req, sizeof(req));
+}
+
+static struct ibv_pd *ionic_alloc_pd(struct ibv_context *ibctx)
+{
+	struct ibv_pd *ibpd;
+	struct ibv_alloc_pd req = {};
+	struct ib_uverbs_alloc_pd_resp resp = {};
+	int rc;
+
+	ibpd = calloc(1, sizeof(*ibpd));
+	if (!ibpd) {
+		rc = errno;
+		goto err_pd;
+	}
+
+	rc = ibv_cmd_alloc_pd(ibctx, ibpd,
+			      &req, sizeof(req),
+			      &resp, sizeof(resp));
+	if (rc)
+		goto err_cmd;
+
+	return ibpd;
+
+err_cmd:
+	free(ibpd);
+err_pd:
+	errno = rc;
+	return NULL;
+}
+
+static int ionic_dealloc_pd(struct ibv_pd *ibpd)
+{
+	int rc;
+
+	rc = ibv_cmd_dealloc_pd(ibpd);
+	if (rc)
+		return rc;
+
+	free(ibpd);
+
+	return 0;
+}
+
+static struct ibv_mr *ionic_reg_mr(struct ibv_pd *ibpd,
+				   void *addr, size_t len, int access)
+{
+	struct verbs_mr *vmr;
+	struct ibv_reg_mr req = {};
+	struct ib_uverbs_reg_mr_resp resp = {};
+	int rc;
+
+	vmr = calloc(1, sizeof(*vmr));
+	if (!vmr) {
+		rc = errno;
+		goto err_mr;
+	}
+
+	rc = ibv_cmd_reg_mr(ibpd, addr, len, (uintptr_t)addr, access, vmr,
+			    &req, sizeof(req),
+			    &resp, sizeof(resp));
+	if (rc)
+		goto err_cmd;
+
+	return &vmr->ibv_mr;
+
+err_cmd:
+	free(vmr);
+err_mr:
+	errno = rc;
+	return NULL;
+}
+
+static int ionic_rereg_mr(struct verbs_mr *vmr, int flags, struct ibv_pd *pd,
+			  void *addr, size_t length, int access)
+{
+	struct ibv_rereg_mr cmd;
+	struct ib_uverbs_rereg_mr_resp resp;
+
+	if (flags & IBV_REREG_MR_KEEP_VALID)
+		return ENOTSUP;
+
+	return ibv_cmd_rereg_mr(vmr, flags, addr, length,
+				(uintptr_t)addr, access, pd,
+				&cmd, sizeof(cmd),
+				&resp, sizeof(resp));
+}
+
+static int ionic_dereg_mr(struct verbs_mr *vmr)
+{
+	int rc;
+
+	rc = ibv_cmd_dereg_mr(vmr);
+	if (rc)
+		return rc;
+
+	free(vmr);
+
+	return 0;
+}
+
 static struct ibv_cq *ionic_create_cq(struct ibv_context *ibctx, int ncqe,
 				      struct ibv_comp_channel *channel,
 				      int vec)
@@ -146,6 +273,26 @@ static int ionic_destroy_cq(struct ibv_cq *ibcq)
 	free(cq);
 
 	return 0;
+}
+
+static struct ibv_srq *ionic_create_srq(struct ibv_pd *ibpd,
+					struct ibv_srq_init_attr *attr)
+{
+	struct verbs_context *vctx =
+		container_of(ibpd->context, struct verbs_context, context);
+	struct ibv_srq_init_attr_ex ex = {
+		.srq_context = attr->srq_context,
+		.attr = attr->attr,
+		.srq_type = IBV_SRQT_BASIC,
+		.pd = ibpd,
+	};
+	struct ibv_srq *ibsrq;
+
+	ibsrq = vctx->create_srq_ex(&vctx->context, &ex);
+
+	attr->attr = ex.attr;
+
+	return ibsrq;
 }
 
 static int ionic_flush_recv(struct ionic_qp *qp, struct ibv_wc *wc)
@@ -2307,6 +2454,31 @@ static int ionic_post_srq_recv(struct ibv_srq *ibsrq, struct ibv_recv_wr *wr,
 	return ionic_post_recv_common(ctx, cq, qp, wr, bad);
 }
 
+static struct ibv_qp *ionic_create_qp(struct ibv_pd *ibpd,
+				      struct ibv_qp_init_attr *attr)
+{
+	struct verbs_context *vctx =
+		container_of(ibpd->context, struct verbs_context, context);
+	struct ibv_qp_init_attr_ex ex = {
+		.qp_context = attr->qp_context,
+		.send_cq = attr->send_cq,
+		.recv_cq = attr->recv_cq,
+		.srq = attr->srq,
+		.cap = attr->cap,
+		.qp_type = attr->qp_type,
+		.sq_sig_all = attr->sq_sig_all,
+		.comp_mask = IBV_QP_INIT_ATTR_PD,
+		.pd = ibpd,
+	};
+	struct ibv_qp *ibqp;
+
+	ibqp = vctx->create_qp_ex(&vctx->context, &ex);
+
+	attr->cap = ex.cap;
+
+	return ibqp;
+}
+
 static struct ibv_ah *ionic_create_ah(struct ibv_pd *ibpd,
 				      struct ibv_ah_attr *attr)
 {
@@ -2350,6 +2522,35 @@ static int ionic_destroy_ah(struct ibv_ah *ibah)
 	return 0;
 }
 
+static struct ibv_mw *ionic_alloc_mw(struct ibv_pd *ibpd,
+				     enum ibv_mw_type type)
+{
+	struct ibv_mw *ibmw;
+	struct ibv_alloc_mw cmd;
+	struct ib_uverbs_alloc_mw_resp resp;
+	int rc;
+
+	ibmw = calloc(1, sizeof(*ibmw));
+	if (!ibmw) {
+		rc = errno;
+		goto err_mw;
+	}
+
+	rc = ibv_cmd_alloc_mw(ibpd, type, ibmw,
+			      &cmd, sizeof(cmd),
+			      &resp, sizeof(resp));
+	if (rc)
+		goto err_cmd;
+
+	return ibmw;
+
+err_cmd:
+	free(ibmw);
+err_mw:
+	errno = rc;
+	return NULL;
+}
+
 static int ionic_bind_mw(struct ibv_qp *ibqp, struct ibv_mw *ibmw,
 			 struct ibv_mw_bind *bind)
 {
@@ -2379,18 +2580,40 @@ static int ionic_bind_mw(struct ibv_qp *ibqp, struct ibv_mw *ibmw,
 	return rc;
 }
 
+static int ionic_dealloc_mw(struct ibv_mw *ibmw)
+{
+	int rc;
+
+	rc = ibv_cmd_dealloc_mw(ibmw);
+	if (rc)
+		return rc;
+
+	free(ibmw);
+
+	return 0;
+}
+
 const struct verbs_context_ops ionic_ctx_ops = {
+	.query_device		= ionic_query_device,
+	.query_port		= ionic_query_port,
+	.alloc_pd		= ionic_alloc_pd,
+	.dealloc_pd		= ionic_dealloc_pd,
+	.reg_mr			= ionic_reg_mr,
+	.rereg_mr		= ionic_rereg_mr,
+	.dereg_mr		= ionic_dereg_mr,
 	.create_cq		= ionic_create_cq,
 	.poll_cq		= ionic_poll_cq,
 	.req_notify_cq		= ionic_req_notify_cq,
 	.resize_cq		= ionic_resize_cq,
 	.destroy_cq		= ionic_destroy_cq,
+	.create_srq		= ionic_create_srq,
 	.create_srq_ex		= ionic_create_srq_ex,
 	.get_srq_num		= ionic_get_srq_num,
 	.modify_srq		= ionic_modify_srq,
 	.query_srq		= ionic_query_srq,
 	.destroy_srq		= ionic_destroy_srq,
 	.post_srq_recv		= ionic_post_srq_recv,
+	.create_qp		= ionic_create_qp,
 	.create_qp_ex		= ionic_create_qp_ex,
 	.query_qp		= ionic_query_qp,
 	.modify_qp		= ionic_modify_qp,
@@ -2399,5 +2622,7 @@ const struct verbs_context_ops ionic_ctx_ops = {
 	.post_recv		= ionic_post_recv,
 	.create_ah		= ionic_create_ah,
 	.destroy_ah		= ionic_destroy_ah,
+	.alloc_mw		= ionic_alloc_mw,
 	.bind_mw		= ionic_bind_mw,
+	.dealloc_mw		= ionic_dealloc_mw,
 };

platform/drivers/freebsd/usr/src/contrib/ofed/libionic/ionic_verbs.c
@@ -63,6 +63,133 @@
 		pthread_spin_unlock(lock);		\
 } while (0)
 
+static int ionic_query_device(struct ibv_context *ibctx,
+			      struct ibv_device_attr *dev_attr)
+{
+	struct ibv_query_device req = {};
+	uint64_t fw_ver;
+	int rc;
+
+	rc = ibv_cmd_query_device(ibctx, dev_attr, &fw_ver,
+				  &req, sizeof(req));
+	if (rc)
+		return rc;
+
+	rc = ibv_read_sysfs_file(ibctx->device->ibdev_path, "fw_ver",
+				 dev_attr->fw_ver, sizeof(dev_attr->fw_ver));
+	if (rc < 0)
+		dev_attr->fw_ver[0] = 0;
+
+	return 0;
+}
+
+static int ionic_query_port(struct ibv_context *ibctx, uint8_t port,
+			    struct ibv_port_attr *port_attr)
+{
+	struct ibv_query_port req = {};
+
+	return ibv_cmd_query_port(ibctx, port, port_attr,
+				  &req, sizeof(req));
+}
+
+static struct ibv_pd *ionic_alloc_pd(struct ibv_context *ibctx)
+{
+	struct ibv_pd *ibpd;
+	struct ibv_alloc_pd req = {};
+	struct ibv_alloc_pd_resp resp = {};
+	int rc;
+
+	ibpd = calloc(1, sizeof(*ibpd));
+	if (!ibpd) {
+		rc = errno;
+		goto err_pd;
+	}
+
+	rc = ibv_cmd_alloc_pd(ibctx, ibpd,
+			      &req, sizeof(req),
+			      &resp, sizeof(resp));
+	if (rc)
+		goto err_cmd;
+
+	return ibpd;
+
+err_cmd:
+	free(ibpd);
+err_pd:
+	errno = rc;
+	return NULL;
+}
+
+static int ionic_dealloc_pd(struct ibv_pd *ibpd)
+{
+	int rc;
+
+	rc = ibv_cmd_dealloc_pd(ibpd);
+	if (rc)
+		return rc;
+
+	free(ibpd);
+
+	return 0;
+}
+
+static struct ibv_mr *ionic_reg_mr(struct ibv_pd *ibpd,
+				   void *addr, size_t len, int access)
+{
+	struct ibv_mr *ibmr;
+	struct ibv_reg_mr req = {};
+	struct ibv_reg_mr_resp resp = {};
+	int rc;
+
+	ibmr = calloc(1, sizeof(*ibmr));
+	if (!ibmr) {
+		rc = errno;
+		goto err_mr;
+	}
+
+	rc = ibv_cmd_reg_mr(ibpd, addr, len, (uintptr_t)addr, access, ibmr,
+			    &req, sizeof(req),
+			    &resp, sizeof(resp));
+	if (rc)
+		goto err_cmd;
+
+	return ibmr;
+
+err_cmd:
+	free(ibmr);
+err_mr:
+	errno = rc;
+	return NULL;
+}
+
+static int ionic_rereg_mr(struct ibv_mr *ibmr, int flags, struct ibv_pd *pd,
+			  void *addr, size_t length, int access)
+{
+	struct ibv_rereg_mr cmd;
+	struct ibv_rereg_mr_resp resp;
+
+	if (flags & IBV_REREG_MR_KEEP_VALID)
+		return ENOTSUP;
+
+	return ibv_cmd_rereg_mr(ibmr, flags, addr, length,
+				(uintptr_t)addr, access, pd,
+				&cmd, sizeof(cmd),
+				&resp, sizeof(resp));
+}
+
+static int ionic_dereg_mr(struct ibv_mr *ibmr)
+{
+	int rc;
+
+	rc = ibv_cmd_dereg_mr(ibmr);
+	if (rc)
+		return rc;
+
+	free(ibmr);
+
+	return 0;
+}
+
 static struct ibv_cq *ionic_create_cq(struct ibv_context *ibctx, int ncqe,
 				      struct ibv_comp_channel *channel,
 				      int vec)
@@ -146,6 +273,31 @@ static int ionic_destroy_cq(struct ibv_cq *ibcq)
 	free(cq);
 
 	return 0;
+}
+
+static struct ibv_srq *ionic_create_srq(struct ibv_pd *ibpd,
+					struct ibv_srq_init_attr *attr)
+{
+	struct ibv_srq_init_attr_ex ex = {
+		.srq_context = attr->srq_context,
+		.attr = attr->attr,
+		.srq_type = IBV_SRQT_BASIC,
+		.pd = ibpd,
+	};
+	struct verbs_context *vctx;
+	struct ibv_srq *ibsrq;
+
+	vctx = verbs_get_ctx_op(ibpd->context, create_srq_ex);
+	if (!vctx) {
+		errno = -ENOSYS;
+		return NULL;
+	}
+
+	ibsrq = vctx->create_srq_ex(&vctx->context, &ex);
+
+	attr->attr = ex.attr;
+
+	return ibsrq;
 }
 
 static int ionic_flush_recv(struct ionic_qp *qp, struct ibv_wc *wc)
@@ -2310,6 +2462,36 @@ static int ionic_post_srq_recv(struct ibv_srq *ibsrq, struct ibv_recv_wr *wr,
 	return ionic_post_recv_common(ctx, cq, qp, wr, bad);
 }
 
+static struct ibv_qp *ionic_create_qp(struct ibv_pd *ibpd,
+				      struct ibv_qp_init_attr *attr)
+{
+	struct ibv_qp_init_attr_ex ex = {
+		.qp_context = attr->qp_context,
+		.send_cq = attr->send_cq,
+		.recv_cq = attr->recv_cq,
+		.srq = attr->srq,
+		.cap = attr->cap,
+		.qp_type = attr->qp_type,
+		.sq_sig_all = attr->sq_sig_all,
+		.comp_mask = IBV_QP_INIT_ATTR_PD,
+		.pd = ibpd,
+	};
+	struct verbs_context *vctx;
+	struct ibv_qp *ibqp;
+
+	vctx = verbs_get_ctx_op(ibpd->context, create_qp_ex);
+	if (!vctx) {
+		errno = -ENOSYS;
+		return NULL;
+	}
+
+	ibqp = vctx->create_qp_ex(&vctx->context, &ex);
+
+	attr->cap = ex.cap;
+
+	return ibqp;
+}
+
 static struct ibv_ah *ionic_create_ah(struct ibv_pd *ibpd,
 				      struct ibv_ah_attr *attr)
 {
@@ -2353,6 +2535,35 @@ static int ionic_destroy_ah(struct ibv_ah *ibah)
 	return 0;
 }
 
+static struct ibv_mw *ionic_alloc_mw(struct ibv_pd *ibpd,
+				     enum ibv_mw_type type)
+{
+	struct ibv_mw *ibmw;
+	struct ibv_alloc_mw cmd;
+	struct ibv_alloc_mw_resp resp;
+	int rc;
+
+	ibmw = calloc(1, sizeof(*ibmw));
+	if (!ibmw) {
+		rc = errno;
+		goto err_mw;
+	}
+
+	rc = ibv_cmd_alloc_mw(ibpd, type, ibmw,
+			      &cmd, sizeof(cmd),
+			      &resp, sizeof(resp));
+	if (rc)
+		goto err_cmd;
+
+	return ibmw;
+
+err_cmd:
+	free(ibmw);
+err_mw:
+	errno = rc;
+	return NULL;
+}
+
 static int ionic_bind_mw(struct ibv_qp *ibqp, struct ibv_mw *ibmw,
 			 struct ibv_mw_bind *bind)
 {
@@ -2382,56 +2593,39 @@ static int ionic_bind_mw(struct ibv_qp *ibqp, struct ibv_mw *ibmw,
 	return rc;
 }
 
-struct ibv_qp *fallback_create_qp(struct ibv_pd *ibpd,
-				  struct ibv_qp_init_attr *attr);
+static int ionic_dealloc_mw(struct ibv_mw *ibmw)
+{
+	struct ibv_dealloc_mw cmd;
+	int rc;
 
-struct ibv_srq *fallback_create_srq(struct ibv_pd *ibpd,
-				    struct ibv_srq_init_attr *attr);
+	rc = ibv_cmd_dealloc_mw(ibmw, &cmd, sizeof(cmd));
+	if (rc)
+		return rc;
 
-int fallback_query_device(struct ibv_context *ibctx,
-			  struct ibv_device_attr *dev_attr);
+	free(ibmw);
 
-int fallback_query_port(struct ibv_context *ibctx, uint8_t port,
-			struct ibv_port_attr *port_attr);
-
-struct ibv_pd *fallback_alloc_pd(struct ibv_context *ibctx);
-
-int fallback_free_pd(struct ibv_pd *ibpd);
-
-struct ibv_mr *fallback_reg_mr(struct ibv_pd *ibpd,
-			       void *addr,
-			       size_t len,
-			       int access);
-
-int fallback_rereg_mr(struct ibv_mr *ibmr, int flags, struct ibv_pd *pd,
-		      void *addr, size_t length, int access);
-
-int fallback_dereg_mr(struct ibv_mr *ibmr);
-
-struct ibv_mw *fallback_alloc_mw(struct ibv_pd *ibpd,
-                                 enum ibv_mw_type type);
-
-int fallback_dealloc_mw(struct ibv_mw *mw);
+	return 0;
+}
 
 static const struct ibv_context_ops ionic_ctx_ops = {
-	.query_device		= fallback_query_device,
-	.query_port		= fallback_query_port,
-	.alloc_pd		= fallback_alloc_pd,
-	.dealloc_pd		= fallback_free_pd,
-	.reg_mr			= fallback_reg_mr,
-	.rereg_mr		= fallback_rereg_mr,
-	.dereg_mr		= fallback_dereg_mr,
+	.query_device		= ionic_query_device,
+	.query_port		= ionic_query_port,
+	.alloc_pd		= ionic_alloc_pd,
+	.dealloc_pd		= ionic_dealloc_pd,
+	.reg_mr			= ionic_reg_mr,
+	.rereg_mr		= ionic_rereg_mr,
+	.dereg_mr		= ionic_dereg_mr,
 	.create_cq		= ionic_create_cq,
 	.poll_cq		= ionic_poll_cq,
 	.req_notify_cq		= ionic_req_notify_cq,
 	.resize_cq		= ionic_resize_cq,
 	.destroy_cq		= ionic_destroy_cq,
-	.create_srq		= fallback_create_srq,
+	.create_srq		= ionic_create_srq,
 	.modify_srq		= ionic_modify_srq,
 	.query_srq		= ionic_query_srq,
 	.destroy_srq		= ionic_destroy_srq,
 	.post_srq_recv		= ionic_post_srq_recv,
-	.create_qp		= fallback_create_qp,
+	.create_qp		= ionic_create_qp,
 	.query_qp		= ionic_query_qp,
 	.modify_qp		= ionic_modify_qp,
 	.destroy_qp		= ionic_destroy_qp,
@@ -2439,9 +2633,9 @@ static const struct ibv_context_ops ionic_ctx_ops = {
 	.post_recv		= ionic_post_recv,
 	.create_ah		= ionic_create_ah,
 	.destroy_ah		= ionic_destroy_ah,
-	.alloc_mw		= fallback_alloc_mw,
+	.alloc_mw		= ionic_alloc_mw,
 	.bind_mw		= ionic_bind_mw,
-	.dealloc_mw		= fallback_dealloc_mw,
+	.dealloc_mw		= ionic_dealloc_mw,
 };
 
 void ionic_set_ops(struct ibv_context *ibctx)

platform/drivers/freebsd/usr/src/contrib/ofed/libionic/Makefile
@@ -7,7 +7,6 @@ MK_PROFILE=	no
 
 SRCS= \
 	ionic.c \
-	ionic_fallback.c \
 	ionic_memory.c \
 	ionic_queue.c \
 	ionic_verbs.c

platform/drivers/freebsd/usr/src/contrib/ofed/libionic/ionic-abi.h
@@ -40,13 +40,12 @@
 
 struct uionic_ctx {
 	struct ibv_get_context ibv_cmd;
-	__u32 fallback;
-	__u32 rsvd;
+	__u32 rsvd[2];
 };
 
 struct uionic_ctx_resp {
 	struct ibv_get_context_resp ibv_resp;
-	__u32 fallback;
+	__u32 rsvd2;
 	__u32 page_shift;
 
 	__u64 dbell_offset;
@@ -121,4 +120,4 @@ struct uionic_srq_resp {
 	__u64 rq_cmb_offset;
 };
 
-#endif
+#endif /* __IONIC_ABI_H__ */

platform/drivers/freebsd/usr/src/contrib/ofed/libionic/ionic.c
@@ -37,7 +37,6 @@
 #include "ionic_dbg.h"
 #include "ionic_stats.h"
 
-extern void ionic_set_fallback_ops(struct ibv_context *ibctx);
 extern void ionic_set_ops(struct ibv_context *ibctx);
 
 FILE *IONIC_DEBUG_FILE;
@@ -88,11 +87,6 @@ static int ionic_env_val(const char *name)
 	return ionic_env_val_def(name, 0);
 }
 
-static int ionic_env_fallback(void)
-{
-	return ionic_env_val("IONIC_FALLBACK");
-}
-
 static int ionic_env_debug(void)
 {
 	if (!(IONIC_DEBUG))
@@ -139,8 +133,6 @@ static int ionic_init_context(struct verbs_device *vdev,
 
 	ionic_debug_file_init();
 
-	req.fallback = ionic_env_fallback();
-
 	ibctx->cmd_fd = cmd_fd;
 
 	rc = ibv_cmd_get_context(ibctx, &req.ibv_cmd, sizeof(req),
@@ -148,60 +140,53 @@ static int ionic_init_context(struct verbs_device *vdev,
 	if (rc)
 		goto out;
 
-	ionic_set_fallback_ops(ibctx);
-
-	ctx->fallback = resp.fallback != 0;
 	ctx->pg_shift = resp.page_shift;
 
-	if (!ctx->fallback) {
-		version = resp.version;
-
-		if (version < IONIC_MIN_RDMA_VERSION) {
-			fprintf(stderr, "ionic: Firmware RDMA Version %u\n",
-				version);
-			fprintf(stderr, "ionic: Driver Min RDMA Version %u\n",
-				IONIC_MIN_RDMA_VERSION);
-			rc = EINVAL;
-			goto out;
-		}
-
-		if (version > IONIC_MAX_RDMA_VERSION) {
-			fprintf(stderr, "ionic: Firmware RDMA Version %u\n",
-				version);
-			fprintf(stderr, "ionic: Driver Max RDMA Version %u\n",
-				IONIC_MAX_RDMA_VERSION);
-			rc = EINVAL;
-			goto out;
-		}
-
-		ctx->version = version;
-		ctx->opcodes = resp.qp_opcodes;
-
-		if (ctx->opcodes <= IONIC_V1_OP_BIND_MW) {
-			fprintf(stderr, "ionic: qp opcodes %d want min %d\n",
-				ctx->opcodes, IONIC_V1_OP_BIND_MW + 1);
-			rc = EINVAL;
-			goto out;
-		}
-
-		ctx->sq_qtype = resp.sq_qtype;
-		ctx->rq_qtype = resp.rq_qtype;
-		ctx->cq_qtype = resp.cq_qtype;
-
-		ctx->max_stride = resp.max_stride;
-
-		ctx->dbpage = ionic_map_device(1u << ctx->pg_shift, cmd_fd,
-					       resp.dbell_offset);
-		if (!ctx->dbpage) {
-			rc = errno;
-			goto out;
-		}
-
-		pthread_mutex_init(&ctx->mut, NULL);
-		ionic_tbl_init(&ctx->qp_tbl);
-
-		ionic_set_ops(ibctx);
+	version = resp.version;
+	if (version < IONIC_MIN_RDMA_VERSION) {
+		fprintf(stderr, "ionic: Firmware RDMA Version %u\n",
+			version);
+		fprintf(stderr, "ionic: Driver Min RDMA Version %u\n",
+			IONIC_MIN_RDMA_VERSION);
+		rc = EINVAL;
+		goto out;
 	}
+
+	if (version > IONIC_MAX_RDMA_VERSION) {
+		fprintf(stderr, "ionic: Firmware RDMA Version %u\n",
+			version);
+		fprintf(stderr, "ionic: Driver Max RDMA Version %u\n",
+			IONIC_MAX_RDMA_VERSION);
+		rc = EINVAL;
+		goto out;
+	}
+
+	ctx->version = version;
+	ctx->opcodes = resp.qp_opcodes;
+	if (ctx->opcodes <= IONIC_V1_OP_BIND_MW) {
+		fprintf(stderr, "ionic: qp opcodes %d want min %d\n",
+			ctx->opcodes, IONIC_V1_OP_BIND_MW + 1);
+		rc = EINVAL;
+		goto out;
+	}
+
+	ctx->sq_qtype = resp.sq_qtype;
+	ctx->rq_qtype = resp.rq_qtype;
+	ctx->cq_qtype = resp.cq_qtype;
+
+	ctx->max_stride = resp.max_stride;
+
+	ctx->dbpage = ionic_map_device(1u << ctx->pg_shift, cmd_fd,
+				       resp.dbell_offset);
+	if (!ctx->dbpage) {
+		rc = errno;
+		goto out;
+	}
+
+	pthread_mutex_init(&ctx->mut, NULL);
+	ionic_tbl_init(&ctx->qp_tbl);
+
+	ionic_set_ops(ibctx);
 
 	ctx->lockfree = ionic_env_lockfree();
 

platform/drivers/freebsd/usr/src/contrib/ofed/libionic/ionic.h
@@ -63,7 +63,6 @@
 struct ionic_ctx {
 	struct ibv_context	ibctx;
 
-	bool			fallback;
 	bool			lockfree;
 	int			spec;
 	uint32_t		pg_shift;

commit 805c3751d1643aac5090b721bae59d9007078c65

ionic_rdma: Fix linux userspace by fixing ionic_reg_mr() (#18311)
This was dropped when I removed the fallback code.

See: 4601b6931ac5 ("ionic_rdma: Update to rdma-core v27")
Fixes: afbff629a8ac ("ionic_rdma: Remove fallback code")

platform/drivers/linux/rdma/lib/ionic/ionic_verbs.c
@@ -134,7 +134,10 @@ static int ionic_dealloc_pd(struct ibv_pd *ibpd)
 }
 
 static struct ibv_mr *ionic_reg_mr(struct ibv_pd *ibpd,
-				   void *addr, size_t len, int access)
+				   void *addr,
+				   size_t len,
+				   uint64_t hca_va,
+				   int access)
 {
 	struct verbs_mr *vmr;
 	struct ibv_reg_mr req = {};
@@ -147,7 +150,7 @@ static struct ibv_mr *ionic_reg_mr(struct ibv_pd *ibpd,
 		goto err_mr;
 	}
 
-	rc = ibv_cmd_reg_mr(ibpd, addr, len, (uintptr_t)addr, access, vmr,
+	rc = ibv_cmd_reg_mr(ibpd, addr, len, hca_va, access, vmr,
 			    &req, sizeof(req),
 			    &resp, sizeof(resp));
 	if (rc)