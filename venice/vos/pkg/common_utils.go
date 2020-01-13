855fb4cd4c7a398c7b7e606fb8d2f67ecdcc6721
Sanjay Thyamagundalu <tsanjay@gmail.com>
2020-01-13 10:02:12 -0800

    Fix creation and mod-time in VOS events (#18458)

--- venice/vos/pkg/common_utils.go
+++ venice/vos/pkg/common_utils.go
@@ -14,25 +14,36 @@ import (
 func updateObjectMeta(info *minio.ObjectInfo, ometa *api.ObjectMeta) {
 	log.Infof("Updating from UserMeta[%+v]", info)
 	key := metaPrefix + metaCreationTime
+	updCrTime, updModTime := true, true
 	ct, err := time.Parse(time.RFC3339Nano, info.Metadata.Get(key))
 	if err == nil {
 		ts, err := types.TimestampProto(ct)
 		if err == nil {
+			updCrTime = false
 			ometa.CreationTime.Timestamp = *ts
 		}
-	} else {
+	}
+
+	// Minio does not provide the creation and mod times sometimes in events. Check and workaround
+	if updCrTime {
 		log.Errorf("[%v]failed to parse creation time [%s](%s)", ometa.Name, info.Metadata.Get(key), err)
+		// Override the creation time
+		ts, err := types.TimestampProto(time.Now())
+		if err == nil {
+			ometa.CreationTime.Timestamp = *ts
+		}
 	}
 	if !info.LastModified.IsZero() {
 		ts, err := types.TimestampProto(info.LastModified)
 		if err == nil {
+			updModTime = false
 			ometa.ModTime.Timestamp = *ts
 		}
-	} else {
+	}
+	if updModTime {
 		// minio failed to give the mod time. Log error and do the next best thing to mark mod time as creationtime
 		log.Errorf("failed to parse modification time [%s](%s)", info.LastModified, err)
 		ometa.ModTime.Timestamp = ometa.CreationTime.Timestamp
-
 	}
 	ometa.Labels = make(map[string]string)
 	for k, v := range info.Metadata {
