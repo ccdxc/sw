dd25415d0553240d62ed4f875b64728aa385605d
Yan <yan@pensando.io>
2020-01-20 20:07:18 -0800

    Add deep equal test for downloaded mirror session (#18789)

--- test/e2e/cluster/mirror_test.go
+++ test/e2e/cluster/mirror_test.go
@@ -3,18 +3,17 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"time"
 
 	"github.com/gogo/protobuf/types"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/pensando/sw/venice/ctrler/tsm/statemgr"
-
 	"github.com/pensando/sw/api"
-
 	"github.com/pensando/sw/api/generated/apiclient"
 	"github.com/pensando/sw/api/generated/monitoring"
+	"github.com/pensando/sw/venice/ctrler/tsm/statemgr"
 	"github.com/pensando/sw/venice/globals"
 )
 
@@ -35,7 +34,7 @@ var _ = Describe("mirror session tests", func() {
 					PacketFilters: []string{monitoring.MirrorSessionSpec_ALL_PKTS.String()},
 					Collectors: []monitoring.MirrorCollector{
 						{
-							Type: "ERSPAN",
+							Type: "erspan",
 							ExportCfg: &monitoring.MirrorExportConfig{
 								Destination: "100.1.1.1",
 							},
@@ -78,15 +77,33 @@ var _ = Describe("mirror session tests", func() {
 			Expect(mirrorRestIf).ShouldNot(Equal(nil))
 		})
 
+		AfterEach(func() {
+			ctx := ts.tu.MustGetLoggedInContext(context.Background())
+			s, err := mirrorRestIf.List(ctx, &api.ListWatchOptions{})
+			Expect(err).ShouldNot(HaveOccurred())
+			Eventually(func() bool {
+				By("Deleting MirrorSession ------")
+				for _, i := range s {
+					_, err := mirrorRestIf.Delete(ctx, i.GetObjectMeta())
+					if err != nil {
+						return false
+					}
+				}
+				return true
+			}, 30, 5).Should(BeTrue(), fmt.Sprintf("Failed to delete mirror session after testing process"))
+		})
+
 		It("Check max mirror sessions", func() {
 			ctx := ts.tu.MustGetLoggedInContext(context.Background())
 			ms := testMirrorSessions[0]
+			initMirrorMap := make(map[string]monitoring.MirrorSessionSpec)
+			listMirrorMap := make(map[string]monitoring.MirrorSessionSpec)
 
 			for i := 0; i < statemgr.MaxMirrorSessions+1; i++ {
 				ms.Name = fmt.Sprintf("max-mirror-%d", i+1)
 				ms.Spec.Collectors = []monitoring.MirrorCollector{
 					{
-						Type: "ERSPAN",
+						Type: "erspan",
 						ExportCfg: &monitoring.MirrorExportConfig{
 							Destination: fmt.Sprintf("192.168.30.1"),
 						},
@@ -97,16 +114,19 @@ var _ = Describe("mirror session tests", func() {
 				_, err := mirrorRestIf.Create(ctx, &ms)
 				if i < statemgr.MaxMirrorSessions {
 					Expect(err).ShouldNot(HaveOccurred())
+					initMirrorMap[ms.GetName()] = ms.GetSpec()
 				} else {
 					Expect(err).Should(HaveOccurred())
 				}
 			}
 			s, err := mirrorRestIf.List(ctx, &api.ListWatchOptions{})
 			Expect(err).ShouldNot(HaveOccurred())
-			for _, i := range s {
-				_, err := mirrorRestIf.Delete(ctx, i.GetObjectMeta())
-				Expect(err).ShouldNot(HaveOccurred())
+
+			By("Deep check obtained mirror session content------")
+			for _, sess := range s {
+				listMirrorMap[sess.GetName()] = sess.GetSpec()
 			}
+			Expect(reflect.DeepEqual(initMirrorMap, listMirrorMap)).To(Equal(true))
 		})
 
 		It("Should run mirror sessions Test 1", func() {
@@ -116,9 +136,13 @@ var _ = Describe("mirror session tests", func() {
 
 			ctx := ts.tu.MustGetLoggedInContext(context.Background())
 			ms := testMirrorSessions[0]
+			initMirrorMap := make(map[string]monitoring.MirrorSessionSpec)
+			listMirrorMap := make(map[string]monitoring.MirrorSessionSpec)
+
 			By("Creating MirrorSession ------")
 			_, err := mirrorRestIf.Create(ctx, &ms)
 			Expect(err).ShouldNot(HaveOccurred())
+			initMirrorMap[ms.GetName()] = ms.GetSpec()
 
 			By("Checking that MirrorSession has started------")
 			Eventually(func() bool {
@@ -127,8 +151,14 @@ var _ = Describe("mirror session tests", func() {
 					By(fmt.Sprintf("GET err:%s", err))
 					return false
 				}
+				listMirrorMap[tms.GetName()] = tms.GetSpec()
 				if tms.Status.ScheduleState != monitoring.MirrorSessionState_ACTIVE.String() {
 					By(fmt.Sprintf("mirror state: %v", tms.Status.ScheduleState))
+					return false
+				}
+				By("Deep check obtained mirror session content------")
+				if !reflect.DeepEqual(initMirrorMap, listMirrorMap) {
+					By(fmt.Sprintf("get inconsistent mirror session %v after initialization", ms.Name))
 					return false
 				}
 				return true
@@ -148,9 +178,6 @@ var _ = Describe("mirror session tests", func() {
 				}
 				return true
 			}, 10, 1).Should(BeTrue(), fmt.Sprintf("Failed to stop %s", ms.Name)) */
-			By("Deleting MirrorSession ------")
-			_, err = mirrorRestIf.Delete(ctx, &ms.ObjectMeta)
-			Expect(err).ShouldNot(HaveOccurred())
 		})
 
 		It("Should run mirror sessions after shcedule time", func() {
@@ -171,7 +198,7 @@ var _ = Describe("mirror session tests", func() {
 					PacketFilters: []string{monitoring.MirrorSessionSpec_ALL_PKTS.String()},
 					Collectors: []monitoring.MirrorCollector{
 						{
-							Type: "ERSPAN",
+							Type: "erspan",
 							ExportCfg: &monitoring.MirrorExportConfig{
 								Destination: "100.1.1.1",
 							},
@@ -210,13 +237,18 @@ var _ = Describe("mirror session tests", func() {
 
 			ctx := ts.tu.MustGetLoggedInContext(context.Background())
 			ms := scheduledTestCase
+			initMirrorMap := make(map[string]monitoring.MirrorSessionSpec)
+			listMirrorMap := make(map[string]monitoring.MirrorSessionSpec)
+
 			By("Creating MirrorSession ------")
+			initMirrorMap[ms.GetName()] = ms.GetSpec()
 			_, err := mirrorRestIf.Create(ctx, &ms)
 			Expect(err).ShouldNot(HaveOccurred())
 
 			By("Checking whether MirrorSession has started after scheduled time or not------")
 			Eventually(func() bool {
 				tms, err := mirrorRestIf.Get(ctx, &ms.ObjectMeta)
+				listMirrorMap[tms.GetName()] = tms.GetSpec()
 				if err != nil {
 					By(fmt.Sprintf("GET err:%s", err))
 					return false
@@ -228,6 +260,13 @@ var _ = Describe("mirror session tests", func() {
 					By(fmt.Sprintf("unexpected mirror session state: %v before scheduled time", tms.Status.ScheduleState))
 					return false
 				}
+
+				By("Deep check obtained mirror session content------")
+				if !reflect.DeepEqual(initMirrorMap, listMirrorMap) {
+					By(fmt.Sprintf("get inconsistent mirror session %v after initialization", ms.Name))
+					return false
+				}
+
 				return true
 			}, 30, 5).Should(BeTrue(), fmt.Sprintf("Failed to get SCHEDULED state for scheduled mirror session %s", ms.Name))
 
@@ -247,17 +286,6 @@ var _ = Describe("mirror session tests", func() {
 				}
 				return true
 			}, 180, 30).Should(BeTrue(), fmt.Sprintf("Failed to get mirror session active state after scheduled time %s", ms.Name))
-
-			// Delete mirror session anyway to avoid the aftermath on the other tests
-			By("Deleting MirrorSession ------")
-			Eventually(func() bool {
-				_, err = mirrorRestIf.Delete(ctx, &ms.ObjectMeta)
-				if err != nil {
-					By(fmt.Sprintf("GET err:%s", err))
-					return false
-				}
-				return true
-			}, 30, 5).Should(BeTrue(), fmt.Sprintf("Failed to delete mirror session %s after testing process", ms.Name))
 		})
 	})
 })
