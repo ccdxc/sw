253379a7232edbebf60e3378489c098bed47096b
Akshaya Nadahalli <akshaya@pensando.io>
2020-01-22 00:26:13 +0530

    DOL - Replace gdb with pstack to collect process backtrace (#18813)

--- nic/vpp/tools/stop-vpp-sim.sh
+++ nic/vpp/tools/stop-vpp-sim.sh
@@ -6,5 +6,6 @@ VPP_PKG_DIR=$NICDIR/sdk/third-party/vpp-pkg/x86_64
 
 #LD_LIBRARY_PATH=$NICDIR/build/x86_64/$PIPELINE/lib:$VPP_PKG_DIR/lib $VPP_PKG_DIR/bin/vppctl show trace
 #LD_LIBRARY_PATH=$NICDIR/build/x86_64/$PIPELINE/lib:$VPP_PKG_DIR/lib $VPP_PKG_DIR/bin/vppctl show logging
+pstack `pgrep vpp_main` &> $NICDIR/vpp_bt.log
 pkill -9 vpp_main
 rm -f $NICDIR/conf/vpp_startup.conf
