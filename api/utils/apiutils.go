df334f69f417bd064cc2742bc6b10a6209606915
Sanjay Thyamagundalu <tsanjay@gmail.com>
2020-01-21 12:02:29 -0800

    Exclude Alert kind from Save/Restore (#18810)

--- api/utils/apiutils.go
+++ api/utils/apiutils.go
@@ -35,7 +35,7 @@ const (
 )
 
 var (
-	doNotSaveKinds = []string{"Cluster", "Module", "SnapshotRestore", "Node", "NetworkInterface", "Version", "AuditEvent"}
+	doNotSaveKinds = []string{"Cluster", "Module", "SnapshotRestore", "Node", "NetworkInterface", "Version", "AuditEvent", "Alert"}
 )
 
 type requirementSetMarker struct {
