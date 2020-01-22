84b601b7c9d611b615a9e4c52f9f435722b5c9c7
Abhijeet Mahagaonkar <31998629+amahagaonkar@users.noreply.github.com>
2020-01-21 22:45:29 -0800

    x86 build failure fixes. (#18846)
    Changes:
    	1. Adds appropriate deps for nmd's submake
    	2. Fixes up irisproto to precompile types and kb pb.go files.

--- nic/agent/cmd/nmd/module.mk
+++ nic/agent/cmd/nmd/module.mk
@@ -2,7 +2,7 @@
 
 include ${MKDEFS}/pre.mk
 MODULE_TARGET   := nmd.gobin
-MODULE_PREREQS  := nmd_halproto.submake
+MODULE_PREREQS  := agent_halproto.submake nmd_halproto.submake
 MODULE_PIPELINE := iris
 MODULE_FLAGS    := -ldflags="-s -w"
 MODULE_DEPS     := $(shell find ${NICDIR}/agent/nmd -name '*.go')
