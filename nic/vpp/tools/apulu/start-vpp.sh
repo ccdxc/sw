445949be26ed328c60c824f495bb51209194529b
Akshaya Nadahalli <akshaya@pensando.io>
2020-01-26 08:15:22 +0530

    Apulu: Run VPP with 2 workers/cores (#18876)
    This has following changes:
        - Run VPP workers on core 2 and 3.
        - Reduce VPP-DPDK packet buffers.
        - Reduce huge page memory inline with packet buffers/rx-tx queues.

---
+++ nic/vpp/tools/apulu/start-vpp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#Huge-pages for DPDK
+echo 64 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+mkdir /dev/hugepages
+mount -t hugetlbfs nodev /dev/hugepages
+
+#VPP Partial init env variables
+export NIC_DIR=/nic/
+export HAL_CONFIG_PATH=$NIC_DIR/conf/
+
+ulimit -c unlimited
+
+# FIX ME AND REMOVE ME
+sleep 20
+
+exec $NIC_DIR/bin/vpp -c $HAL_CONFIG_PATH/vpp/vpp_2_workers.conf
