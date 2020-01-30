110c6e2d8b11bb9893a5e488237261c199434087
ssrisiva <48565140+ssrisiva@users.noreply.github.com>
2020-01-30 11:41:11 +0530

    CRUD APIs for Athena connection tracking (#19248)

---
+++ nic/apollo/api/impl/athena/pds_conntrack.cc
@@ -0,0 +1,122 @@
+//
+// {C} Copyright 2019 Pensando Systems Inc. All rights reserved
+//
+//----------------------------------------------------------------------------
+///
+/// \file
+/// athena connection tracking implementation
+///
+//----------------------------------------------------------------------------
+
+#include "nic/sdk/include/sdk/base.hpp"
+#include "nic/sdk/lib/p4/p4_api.hpp"
+#include "nic/sdk/lib/p4/p4_utils.hpp"
+#include "nic/apollo/core/trace.hpp"
+#include "nic/apollo/api/include/athena/pds_conntrack.h"
+#include "gen/p4gen/athena/include/p4pd.h"
+
+using namespace sdk;
+
+extern "C" {
+
+sdk_ret_t
+pds_conntrack_state_create (pds_conntrack_spec_t *spec)
+{
+    p4pd_error_t p4pd_ret;
+    uint32_t conntrack_id;
+    conntrack_actiondata_t conntrack_actiondata = { 0 };
+
+    if (!spec) {
+        PDS_TRACE_ERR("spec is null");
+        return SDK_RET_INVALID_ARG;
+    }
+    conntrack_id = spec->key.conntrack_id;
+    if (conntrack_id >= PDS_CONNTRACK_ID_MAX) {
+        PDS_TRACE_ERR("conntrack id %u is beyond range", conntrack_id);
+        return SDK_RET_INVALID_ARG;
+    }
+
+    conntrack_actiondata.action_id = CONNTRACK_CONNTRACK_ID;
+    conntrack_actiondata.action_u.conntrack_conntrack.flow_type =
+        spec->data.flow_type;
+    conntrack_actiondata.action_u.conntrack_conntrack.flow_state =
+        spec->data.flow_state;
+    p4pd_ret = p4pd_global_entry_write(P4TBL_ID_CONNTRACK,
+                                       conntrack_id, NULL, NULL,
+                                       &conntrack_actiondata);
+    if (p4pd_ret != P4PD_SUCCESS) {
+        PDS_TRACE_ERR("Failed to write conntrack table at index %u",
+                      conntrack_id);
+        return SDK_RET_HW_PROGRAM_ERR;
+    }
+    return SDK_RET_OK;
+}
+
+sdk_ret_t
+pds_conntrack_state_read (pds_conntrack_key_t *key,
+                          pds_conntrack_info_t *info)
+{
+    p4pd_error_t p4pd_ret;
+    uint32_t conntrack_id;
+    conntrack_actiondata_t conntrack_actiondata = { 0 };
+
+    if (!key || !info) {
+        PDS_TRACE_ERR("key/info is null");
+        return SDK_RET_INVALID_ARG;
+    }
+    conntrack_id = key->conntrack_id;
+    if (conntrack_id >= PDS_CONNTRACK_ID_MAX) {
+        PDS_TRACE_ERR("conntrack id %u is beyond range", conntrack_id);
+        return SDK_RET_INVALID_ARG;
+    }
+
+    p4pd_ret = p4pd_global_entry_read(P4TBL_ID_CONNTRACK,
+                                      conntrack_id, NULL, NULL,
+                                      &conntrack_actiondata);
+    if (p4pd_ret != P4PD_SUCCESS) {
+        PDS_TRACE_ERR("Failed to read conntrack table at index %u",
+                      conntrack_id);
+        return SDK_RET_HW_READ_ERR;
+    }
+    info->spec.data.flow_type =
+        (pds_flow_type_t)conntrack_actiondata.action_u.conntrack_conntrack.flow_type;
+    info->spec.data.flow_state =
+        (pds_flow_state_t)conntrack_actiondata.action_u.conntrack_conntrack.flow_state;
+    return SDK_RET_OK;
+}
+
+sdk_ret_t
+pds_conntrack_state_update (pds_conntrack_spec_t *spec)
+{
+    return pds_conntrack_state_create(spec);
+}
+
+sdk_ret_t
+pds_conntrack_state_delete (pds_conntrack_key_t *key)
+{
+    p4pd_error_t p4pd_ret;
+    uint32_t conntrack_id;
+    conntrack_actiondata_t conntrack_actiondata = { 0 };
+
+    if (!key) {
+        PDS_TRACE_ERR("key is null");
+        return SDK_RET_INVALID_ARG;
+    }
+    conntrack_id = key->conntrack_id;
+    if (conntrack_id >= PDS_CONNTRACK_ID_MAX) {
+        PDS_TRACE_ERR("conntrack id %u is beyond range", conntrack_id);
+        return SDK_RET_INVALID_ARG;
+    }
+
+    p4pd_ret = p4pd_global_entry_write(P4TBL_ID_CONNTRACK,
+                                       conntrack_id, NULL, NULL,
+                                       &conntrack_actiondata);
+    if (p4pd_ret != P4PD_SUCCESS) {
+        PDS_TRACE_ERR("Failed to clear conntrack table at index %u",
+                      conntrack_id);
+        return SDK_RET_HW_PROGRAM_ERR;
+    }
+    return SDK_RET_OK;
+}
+
+}
---
+++ nic/apollo/api/include/athena/pds_conntrack.h
@@ -0,0 +1,106 @@
+//
+// {C} Copyright 2019 Pensando Systems Inc. All rights reserved
+//
+//----------------------------------------------------------------------------
+///
+/// \file
+/// This file defines Connection track API
+///
+//----------------------------------------------------------------------------
+
+
+#ifndef __PDS_CONNTRACK_H__
+#define __PDS_CONNTRACK_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// \defgroup PDS_CONNTRACK
+/// @{
+
+#define    PDS_CONNTRACK_ID_MAX    (3774874)
+
+/// \brief Connection Tracking Flow Types
+typedef enum pds_flow_type_e {
+    PDS_FLOW_TYPE_TCP = 0,    ///< TCP flow
+    PDS_FLOW_TYPE_UDP,        ///< UDP flow
+    PDS_FLOW_TYPE_ICMP,       ///< ICMP flow
+    PDS_FLOW_TYPE_OTHERS      ///< Other flow
+} pds_flow_type_t;
+
+/// \brief Connection Tracking Flow States
+typedef enum pds_flow_state_e {
+    UNESTABLISHED = 0,             ///< Connection unestablished
+    PDS_FLOW_STATE_SYN_SENT,       ///< TCP SYN sent
+    PDS_FLOW_STATE_SYN_RECV,       ///< TCP SYN received
+    PDS_FLOW_STATE_SYNACK_SENT,    ///< TCP SYN ACK sent
+    PDS_FLOW_STATE_SYNACK_RECV,    ///< TCP SYN ACK received
+    ESTABLISHED,                   ///< Established
+    FIN_SENT,                      ///< FIN Sent
+    FIN_RECV,                      ///< FIN received
+    TIME_WAIT,                     ///< Wait
+    RST_CLOSE,                     ///< RST close
+    REMOVED                        ///< Connection removed
+} pds_flow_state_t;
+
+/// \brief Connection tracking data
+typedef struct pds_conntrack_data_s {
+    pds_flow_type_t     flow_type;     ///< Flow Type
+    pds_flow_state_t    flow_state;    ///< Flow State
+} __PACK__ pds_conntrack_data_t;
+
+/// \brief Connection tracking key
+typedef struct pds_conntrack_key_s {
+    uint32_t    conntrack_id;    ///< Connection Tracking State ID
+} __PACK__ pds_conntrack_key_t;
+
+/// \brief Connection tracking specification
+typedef struct pds_conntrack_spec_s {
+    pds_conntrack_key_t     key;    ///< Connection tracking key
+    pds_conntrack_data_t    data;  ///< Connection tracking data
+} __PACK__ pds_conntrack_spec_t;
+
+/// \brief Connection tracking status
+typedef struct pds_conntrack_status_s {
+} __PACK__ pds_conntrack_status_t;
+
+/// \brief Epoch info
+typedef struct pds_conntrack_info_s {
+    pds_conntrack_spec_t      spec;      ///< Specification
+    pds_conntrack_status_t    status;    ///< Status
+} __PACK__ pds_conntrack_info_t;
+
+/// \brief     create conntrack state entry
+/// \param[in] spec conntrack specification
+/// \return    #SDK_RET_OK on success, failure status code on error
+/// \remark    A valid flow type and conntrack id should be passed
+sdk_ret_t pds_conntrack_state_create(pds_conntrack_spec_t *spec);
+
+/// \brief     read conntrack state entry
+/// \param[in] key conntrack key
+//  \param[in] info conntrack info
+/// \return    #SDK_RET_OK on success, failure status code on error
+/// \remark    A valid conntrack id should be passed
+sdk_ret_t pds_conntrack_state_read(pds_conntrack_key_t *key,
+                                   pds_conntrack_info_t *info);
+
+/// \brief     update conntrack state entry
+/// \param[in] spec conntrack specification
+/// \return    #SDK_RET_OK on success, failure status code on error
+/// \remark    A valid flow type and conntrack id should be passed
+sdk_ret_t pds_conntrack_state_update(pds_conntrack_spec_t *spec);
+
+/// \brief     delete conntrack state entry
+/// \param[in] key conntrack key
+/// \return    #SDK_RET_OK on success, failure status code on error
+/// \remark    A valid conntrack id should be passed
+sdk_ret_t pds_conntrack_state_delete(pds_conntrack_key_t *key);
+
+/// @}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // __PDS_CONNTRACK_H__
