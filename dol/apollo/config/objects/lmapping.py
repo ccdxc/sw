ccdxc/sw

commit 2107c980b02dcc8fedb7673e67039c965ec940d4

proto changes for mapping create/update APIs (#19718)

dol/apollo/config/objects/lmapping.py
@@ -93,15 +93,19 @@ class LocalMappingObject(base.ConfigObjectBase):
         return super().IsFilterMatch(selectors.flow.filters)
 
     def PopulateKey(self, grpcmsg):
-        key = grpcmsg.Id.add()
-        key.IPKey.VPCId = self.VNIC.SUBNET.VPC.GetKey()
-        utils.GetRpcIPAddr(self.IPAddr, key.IPKey.IPAddr)
+        if grpcmsg.__class__.__name__ == 'MappingDeleteRequest' or grpcmsg.__class__.__name__ == 'MappingGetRequest':
+            key = grpcmsg.Id.add()
+            key.IPKey.VPCId = self.VNIC.SUBNET.VPC.GetKey()
+            utils.GetRpcIPAddr(self.IPAddr, key.IPKey.IPAddr)
+        else:
+            grpcmsg.Id.append(self.GetKey())
         return
 
     def PopulateSpec(self, grpcmsg):
         spec = grpcmsg.Request.add()
-        spec.Id.IPKey.VPCId = self.VNIC.SUBNET.VPC.GetKey()
-        utils.GetRpcIPAddr(self.IPAddr, spec.Id.IPKey.IPAddr)
+        spec.Id = self.GetKey()
+        spec.IPKey.VPCId = self.VNIC.SUBNET.VPC.GetKey()
+        utils.GetRpcIPAddr(self.IPAddr, spec.IPKey.IPAddr)
         spec.SubnetId = self.VNIC.SUBNET.GetKey()
         spec.VnicId = self.VNIC.GetKey()
         spec.MACAddr = self.VNIC.MACAddr.getnum()
@@ -116,7 +120,7 @@ class LocalMappingObject(base.ConfigObjectBase):
     def ValidateSpec(self, spec):
         if spec.Id.IPKey.VPCId != self.VNIC.SUBNET.VPC.GetKey():
             return False
-        if not utils.ValidateRpcIPAddr(self.IPAddr, spec.Id.IPKey.IPAddr):
+        if not utils.ValidateRpcIPAddr(self.IPAddr, spec.IPKey.IPAddr):
             return False
         return True
 

dol/apollo/config/objects/rmapping.py
@@ -25,6 +25,7 @@ class RemoteMappingObject(base.ConfigObjectBase):
         else:
             self.MappingId = next(ResmgrClient[node].RemoteMappingIdAllocator)
         self.GID('RemoteMapping%d'%self.MappingId)
+        self.UUID = utils.PdsUuid(self.MappingId)
         self.SUBNET = parent
         if (hasattr(spec, 'rmacaddr')):
             self.MACAddr = spec.rmacaddr
@@ -81,15 +82,19 @@ class RemoteMappingObject(base.ConfigObjectBase):
         return super().IsFilterMatch(selectors.flow.filters)
 
     def PopulateKey(self, grpcmsg):
-        key = grpcmsg.Id.add()
-        key.IPKey.VPCId = self.SUBNET.VPC.GetKey()
-        utils.GetRpcIPAddr(self.IPAddr, key.IPKey.IPAddr)
+        if grpcmsg.__class__.__name__ == 'MappingDeleteRequest' or grpcmsg.__class__.__name__ == 'MappingGetRequest':
+            key = grpcmsg.Id.add()
+            key.IPKey.VPCId = self.SUBNET.VPC.GetKey()
+            utils.GetRpcIPAddr(self.IPAddr, key.IPKey.IPAddr)
+        else:
+            grpcmsg.Id.append(self.GetKey())
         return
 
     def PopulateSpec(self, grpcmsg):
         spec = grpcmsg.Request.add()
-        spec.Id.IPKey.VPCId = self.SUBNET.VPC.GetKey()
-        utils.GetRpcIPAddr(self.IPAddr, spec.Id.IPKey.IPAddr)
+        spec.Id = self.GetKey()
+        spec.IPKey.VPCId = self.SUBNET.VPC.GetKey()
+        utils.GetRpcIPAddr(self.IPAddr, spec.IPKey.IPAddr)
         spec.SubnetId = self.SUBNET.GetKey()
         spec.TunnelId = self.TUNNEL.GetKey()
         spec.MACAddr = self.MACAddr.getnum()
@@ -101,7 +106,7 @@ class RemoteMappingObject(base.ConfigObjectBase):
     def ValidateSpec(self, spec):
         if spec.Id.IPKey.VPCId != self.SUBNET.VPC.GetKey():
             return False
-        if not utils.ValidateRpcIPAddr(self.IPAddr, spec.Id.IPKey.IPAddr):
+        if not utils.ValidateRpcIPAddr(self.IPAddr, spec.IPKey.IPAddr):
             return False
         if spec.MACAddr != self.MACAddr.getnum():
             return False

nic/apollo/agent/protos/mapping.proto
@@ -32,11 +32,11 @@ message L2MappingKey {
   uint64 MACAddr  = 2;
 }
 
-// MappingKey uniquley identifies the IP in the entire fabric
+// MappingKey uniquely identifies the IP in the entire fabric
 message MappingKey {
   oneof keyinfo {
     // IP mapping key
-    L3MappingKey IPKey = 1;
+    L3MappingKey IPKey  = 1;
     // MAC mapping key
     L2MappingKey MACKey = 2;
   }
@@ -45,33 +45,43 @@ message MappingKey {
 // MappingSpec captures configuration of each mapping
 // NOTE: Mapping is either on the local host/TEP or remote TEP
 message MappingSpec {
-  // key to identify the mapping
-  MappingKey          Id             = 1;
+  // unique mapping id
+  bytes           Id             = 1 [(gogoproto.moretags) = "meta:mandatory,immutable"];
+  oneof mac_or_ip {
+    // IP mapping key
+    L3MappingKey  IPKey          = 2 [(gogoproto.moretags) = "meta:mandatory,immutable"];
+    // MAC mapping key
+    L2MappingKey  MACKey         = 3 [(gogoproto.moretags) = "meta:mandatory,immutable"];
+  }
   // subnet this mapping is in
-  bytes               SubnetId       = 2 [(gogoproto.moretags) = "meta:mandatory"];
+  bytes           SubnetId       = 4 [(gogoproto.moretags) = "meta:mandatory"];
   // dstinfo is expected to empty for local mappings
   oneof dstinfo {
       // if IP is that of local vnic, corresponding vnic id
-      bytes           VnicId         = 3;
+      // this is mandatory attribute for local IP mappings
+      bytes       VnicId         = 5;
       // Tunnel ID of the remote TEP for remote mapping
-      bytes           TunnelId       = 4;
+      // this is mandatory attribute for remote MAC/IP mappings for
+      // non-ECMP cases
+      bytes       TunnelId       = 6;
       // overlay nexthop/TEP group for remote mappings
-      bytes           NexthopGroupId = 5;
+      // this is mandatory attribute for remote MAC/IP mappings for ECMP cases
+      bytes       NexthopGroupId = 7;
   }
   // overlay MAC address of this mapping
-  uint64              MACAddr        = 6 [(gogoproto.moretags) = "meta:mandatory"];
+  uint64          MACAddr        = 8 [(gogoproto.moretags) = "meta:mandatory"];
   // fabric encap information specific to this mapping, if any
-  types.Encap         Encap          = 7 [(gogoproto.moretags) = "meta:mandatory"];
+  types.Encap    Encap           = 9 [(gogoproto.moretags) = "meta:mandatory"];
   // public IP, if overlay IP has corresponding public IP
-  types.IPAddress     PublicIP       = 8;
+  types.IPAddress PublicIP       = 10;
   // provider/underlay IP corresponding to the overlay IP, if any
-  types.IPAddress     ProviderIp     = 9;
+  types.IPAddress ProviderIp     = 11;
   // tag/label/security group of this IP mapping, these tags/labels/SGs can be
   // used// in defining security policy rules
   // NOTE:
   // 1. service tag 0 is invalid
   // 2. a maximum of 5 tags are supported
-  repeated uint32     Tags           = 10;
+  repeated uint32 Tags           = 12;
 }
 
 // operational status of a mapping

nic/apollo/agent/svc/specs.hpp
@@ -3629,14 +3629,14 @@ pds_local_mapping_api_spec_to_proto (pds::MappingSpec *proto_spec,
     switch (local_spec->key.type) {
     case PDS_MAPPING_TYPE_L2:
         {
-            auto key = proto_spec->mutable_id()->mutable_mackey();
+            auto key = proto_spec->mutable_mackey();
             key->set_macaddr(MAC_TO_UINT64(local_spec->key.mac_addr));
             key->set_subnetid(local_spec->key.subnet.id, PDS_MAX_KEY_LEN);
         }
         break;
     case PDS_MAPPING_TYPE_L3:
         {
-            auto key = proto_spec->mutable_id()->mutable_ipkey();
+            auto key = proto_spec->mutable_ipkey();
             ipaddr_api_spec_to_proto_spec(key->mutable_ipaddr(),
                                           &local_spec->key.ip_addr);
             key->set_vpcid(local_spec->key.vpc.id, PDS_MAX_KEY_LEN);
@@ -3669,24 +3669,21 @@ static inline sdk_ret_t
 pds_local_mapping_proto_to_api_spec (pds_local_mapping_spec_t *local_spec,
                                      const pds::MappingSpec &proto_spec)
 {
-    pds::MappingKey key;
-
-    key = proto_spec.id();
-    switch (key.keyinfo_case()) {
-    case pds::MappingKey::kIPKey:
+    switch (proto_spec.mac_or_ip_case()) {
+    case pds::MappingSpec::kIPKey:
         local_spec->key.type = PDS_MAPPING_TYPE_L3;
         pds_obj_key_proto_to_api_spec(&local_spec->key.vpc,
-                                      key.ipkey().vpcid());
+                                      proto_spec.ipkey().vpcid());
         ipaddr_proto_spec_to_api_spec(&local_spec->key.ip_addr,
-                                      key.ipkey().ipaddr());
+                                      proto_spec.ipkey().ipaddr());
         pds_obj_key_proto_to_api_spec(&local_spec->subnet,
                                       proto_spec.subnetid());
         break;
 
-    case pds::MappingKey::kMACKey:
+    case pds::MappingSpec::kMACKey:
     default:
         PDS_TRACE_ERR("Unsupported local mapping key type %u, local mappings "
-                      "can only be L3 mappings", key.keyinfo_case());
+                      "can only be L3 mappings", proto_spec.mac_or_ip_case());
         return SDK_RET_INVALID_ARG;
     }
     if (proto_spec.dstinfo_case() != pds::MappingSpec::kVnicId) {
@@ -3736,14 +3733,14 @@ pds_remote_mapping_api_spec_to_proto (pds::MappingSpec *proto_spec,
     switch (remote_spec->key.type) {
     case PDS_MAPPING_TYPE_L2:
         {
-            auto key = proto_spec->mutable_id()->mutable_mackey();
+            auto key = proto_spec->mutable_mackey();
             key->set_macaddr(MAC_TO_UINT64(remote_spec->key.mac_addr));
             key->set_subnetid(remote_spec->key.subnet.id, PDS_MAX_KEY_LEN);
         }
         break;
     case PDS_MAPPING_TYPE_L3:
         {
-            auto key = proto_spec->mutable_id()->mutable_ipkey();
+            auto key = proto_spec->mutable_ipkey();
             ipaddr_api_spec_to_proto_spec(key->mutable_ipaddr(),
                                           &remote_spec->key.ip_addr);
             key->set_vpcid(remote_spec->key.vpc.id, PDS_MAX_KEY_LEN);
@@ -3826,25 +3823,22 @@ static inline sdk_ret_t
 pds_remote_mapping_proto_to_api_spec (pds_remote_mapping_spec_t *remote_spec,
                                       const pds::MappingSpec &proto_spec)
 {
-    pds::MappingKey key;
-
-    key = proto_spec.id();
-    switch (key.keyinfo_case()) {
-    case pds::MappingKey::kMACKey:
+    switch (proto_spec.mac_or_ip_case()) {
+    case pds::MappingSpec::kMACKey:
         remote_spec->key.type = PDS_MAPPING_TYPE_L2;
         pds_obj_key_proto_to_api_spec(&remote_spec->key.subnet,
-                                      key.mackey().subnetid());
+                                      proto_spec.mackey().subnetid());
         MAC_UINT64_TO_ADDR(remote_spec->key.mac_addr,
-                           key.mackey().macaddr());
+                           proto_spec.mackey().macaddr());
         remote_spec->subnet = remote_spec->key.subnet;
         break;
 
-    case pds::MappingKey::kIPKey:
+    case pds::MappingSpec::kIPKey:
         remote_spec->key.type = PDS_MAPPING_TYPE_L3;
         pds_obj_key_proto_to_api_spec(&remote_spec->key.vpc,
-                                      key.ipkey().vpcid());
+                                      proto_spec.ipkey().vpcid());
         ipaddr_proto_spec_to_api_spec(&remote_spec->key.ip_addr,
-                                      key.ipkey().ipaddr());
+                                      proto_spec.ipkey().ipaddr());
         pds_obj_key_proto_to_api_spec(&remote_spec->subnet,
                                       proto_spec.subnetid());
         break;

nic/apollo/framework/api_base.cc
@@ -82,8 +82,8 @@ api_base::factory(api_ctxt_t *api_ctxt) {
         return security_profile::factory(&api_ctxt->api_params->security_profile_spec);
 
     default:
-        PDS_TRACE_ERR("factory method not implemented for obj id %u\n",
-                                                     api_ctxt->obj_id);
+        PDS_TRACE_ERR("Method not implemented for obj id %u\n",
+                      api_ctxt->obj_id);
         break;
     }
     return NULL;
@@ -155,8 +155,8 @@ api_base::build(api_ctxt_t *api_ctxt) {
         return security_profile::build(&api_ctxt->api_params->security_profile_spec.key);
 
     default:
-        PDS_TRACE_ERR("build method not implemented for obj id %u\n",
-                                                   api_ctxt->obj_id);
+        PDS_TRACE_ERR("Method not implemented for obj id %u\n",
+                      api_ctxt->obj_id);
         break;
     }
     return NULL;
@@ -270,7 +270,7 @@ api_base::free(obj_id_t obj_id, api_base *api_obj) {
         return security_profile::free((security_profile *)api_obj);
 
     default:
-        PDS_TRACE_ERR("free method not implemented for obj id %u\n", obj_id);
+        PDS_TRACE_ERR("Method not implemented for obj id %u\n", obj_id);
         break;
     }
     return SDK_RET_INVALID_OP;
@@ -389,8 +389,8 @@ api_base::find_obj(api_ctxt_t *api_ctxt) {
         return NULL;
 
     default:
-        PDS_TRACE_ERR("find_obj api_ctx method not implemented for obj id %u\n",
-                                                              api_ctxt->obj_id);
+        PDS_TRACE_ERR("Method not implemented for obj id %u\n",
+                      api_ctxt->obj_id);
         break;
     }
     return NULL;
@@ -483,8 +483,7 @@ api_base::find_obj(obj_id_t obj_id, void *key) {
         break;
 
     default:
-        PDS_TRACE_ERR("find_obj obj_id method not implemented for obj id %u\n",
-                                                                       obj_id);
+        PDS_TRACE_ERR("Method not implemented for obj id %u\n", obj_id);
         break;
     }
 

commit 8184d55a4710a1175f154ed93444b8684b8b4e19

Fixing compilation error, introduced by PR #19718 (#19771)

dol/apollo/config/objects/lmapping.py
@@ -118,7 +118,7 @@ class LocalMappingObject(base.ConfigObjectBase):
         return
 
     def ValidateSpec(self, spec):
-        if spec.Id.IPKey.VPCId != self.VNIC.SUBNET.VPC.GetKey():
+        if spec.IPKey.VPCId != self.VNIC.SUBNET.VPC.GetKey():
             return False
         if not utils.ValidateRpcIPAddr(self.IPAddr, spec.IPKey.IPAddr):
             return False

dol/apollo/config/objects/rmapping.py
@@ -104,7 +104,7 @@ class RemoteMappingObject(base.ConfigObjectBase):
         return
 
     def ValidateSpec(self, spec):
-        if spec.Id.IPKey.VPCId != self.SUBNET.VPC.GetKey():
+        if spec.IPKey.VPCId != self.SUBNET.VPC.GetKey():
             return False
         if not utils.ValidateRpcIPAddr(self.IPAddr, spec.IPKey.IPAddr):
             return False