7b092aaa79a9969d3c3b13408d8fd40964ec972a
rathorea <32275334+rathorea@users.noreply.github.com>
2020-02-11 22:10:11 -0800

    pegctl to perseus communication bug fix (#19760)

--- nic/metaswitch/pegctl/utils/client.go
+++ nic/metaswitch/pegctl/utils/client.go
@@ -7,11 +7,12 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/pensando/sw/venice/utils/log"
+	"github.com/pensando/sw/venice/globals"
 )
 
 const (
 	pegasusGRPCDefaultPort    = "50057"
-	pegasusGRPCDefaultBaseURL = "localhost"
+	pegasusGRPCDefaultBaseURL = globals.Localhost
 )
 
 // CreateNewGRPCClient creates a grpc connection to HAL
@@ -25,7 +26,6 @@ func CreateNewGRPCClient() (*grpc.ClientConn, error) {
 	grpcOpts = append(grpcOpts, grpc.WithMaxMsgSize(math.MaxInt32-1))
 	grpcOpts = append(grpcOpts, grpc.WithInsecure())
 	rpcClient, err := grpc.Dial(srvURL, grpcOpts...)
-
 	if err != nil {
 		log.Errorf("Creating gRPC Client failed. Server URL: %s", srvURL)
 		return nil, err
