a3f3afffe22b1ebc07eb0e32ede2a5f263750001
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2020-02-25 12:01:33 -0800

    workaround - script changes to get oob IP faster in apulu (#20203)

--- nic/apollo/tools/apulu/start-nmd.sh
+++ nic/apollo/tools/apulu/start-nmd.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
+# wait for 5 minutes for interfaces to show up
+counter=300
+oob_up=0
+dsc0_up=0
+dsc1_up=0
+while [ $counter -gt 0 ]
+do
+    echo $dsc0_up
+    echo $dsc1_up
+    if [ -d "/sys/class/net/oob_mnic0" ] && [ $oob_up -eq 0 ] ; then
+        echo "OOB up"
+        oob_up=1
+    fi
+    if [ -d "/sys/class/net/dsc0" ] && [ $dsc0_up -eq 0 ] ; then
+        echo "dsc0 up"
+        dsc0_up=1
+    fi
+    if [ -d "/sys/class/net/dsc1" ] && [ $dsc1_up -eq 0 ] ; then
+        echo "dsc1 up"
+        dsc1_up=1
+    fi
+
+    if [ $oob_up -eq 1 ] && [ $dsc0_up -eq 1 ] && [ $dsc1_up -eq 1 ]; then
+        break
+    else
+        echo "one of the interfaces didn't come up"
+        echo "oob status $oob_up"
+        echo "dsc0 status $dsc0"
+        echo "dsc1 status $dsc1"
+        sleep 1
+        counter=$(( $counter - 1 ))
+    fi
+done
+
 exec env TMPDIR=/data NAPLES_PIPELINE=apollo /nic/bin/nmd
