6f58db152b3cb73dfbd952cfa8c79b9ba48a81e8
Dmitry <dbarsky@pensando.io>
2020-03-04 14:52:49 -0800

    Double commit compat changes 1.3 -> Master (#20486)
    * add OfflineException (#19794)
    
    * add uptime, plus small changes (#19831)
    
    * Fix pci command (#19973)
    
    Signed-off-by: Dmitry Barsky <dbarsky@pensando.io>
    
    * Compat - more logging (node, power methond) (#20242)
    
    Signed-off-by: Dmitry Barsky <dbarsky@pensando.io>
    
    * changes to the compat test (#20268)
    
    * Veify more with lspci
    
    Signed-off-by: Dmitry Barsky <dbarsky@pensando.io>
    
    * minor cleanup (spaces, etc.) in reboot.py
    
    Signed-off-by: Dmitry Barsky <dbarsky@pensando.io>
    
    * rearange verify_pci, remove dup code
    
    Signed-off-by: Dmitry Barsky <dbarsky@pensando.io>

--- iota/test/iris/testcases/server/reboot.py
+++ iota/test/iris/testcases/server/reboot.py
@@ -1,11 +1,10 @@
 import iota.harness.api as api
 import iota.test.utils.naples_host as host
 import iota.test.iris.testcases.server.verify_pci as verify_pci
-import pdb
-
+from iota.harness.infra.exceptions import *
 
 def Setup (tc):
-    api.Logger.info("Validate flow control")
+    api.Logger.info("Server Compatiblity Reboot: APC, IPMI, OS")
 
     tc.nodes = api.GetNaplesHostnames()
     tc.os = api.GetNodeOs(tc.nodes[0])
@@ -18,28 +17,29 @@ def Setup (tc):
 
 
 def Trigger (tc):
-    naples_nodes =[]
+    naples_nodes = []
     #for every node in the setup
     for node in tc.nodes:
         if api.IsNaplesNode(node):
-            naples_nodes.append (node)
+            naples_nodes.append(node)
             api.Logger.info(f"Found Naples Node: [{node}]")
 
     if len(naples_nodes) == 0:
         api.Logger.error(f"Failed to find a Naples Node!")
         return api.types.status.FAILURE
 
-    for reboot in range(tc.args.reboots): 
-        api.Logger.info (f"Starting Reboot Loop # {reboot}")
-        # Reboot Node.  
+    for reboot in range(tc.args.reboots):
+        # Reboot Node.
         # Reboot method (APC, IPMI, OS Reboot) is passed as a testcase parameter
-        api.RestartNodes([node], tc.iterators.reboot_method)
+        for node in naples_nodes:
+            api.Logger.info(f"Starting Reboot Loop # {reboot} on {node}. Reboot method: {tc.iterators.reboot_method}")
+            if api.RestartNodes([node], tc.iterators.reboot_method) == api.types.status.FAILURE:
+                raise OfflineTestbedException
 
-        for node in naples_nodes:   
-            api.Logger.info (f"Verifying PCI on [{node}]")
-            if verify_pci.verify_errors_lspci (node, tc.os) != api.types.status.SUCCESS:
-               api.Logger.error (f"PCIe Failure detected on {node}")
-               return api.types.status.FAILURE
+            api.Logger.info(f"Verifying PCI on [{node}]: ")
+            if verify_pci.verify_errors_lspci(node, tc.os) != api.types.status.SUCCESS:
+                api.Logger.error(f"PCIe Failure detected on {node}")
+                raise OfflineTestbedException
 
     return api.types.status.SUCCESS
 
--- iota/test/iris/testcases/server/verify_pci.py
+++ iota/test/iris/testcases/server/verify_pci.py
@@ -2,43 +2,114 @@ import re
 import iota.harness.api as api
 import iota.test.utils.naples_host as host
 
-def verify_errors_lspci (node, os_type):
+def run_command(command, node):
+    req = api.Trigger_CreateExecuteCommandsRequest()
+    api.Trigger_AddHostCommand(req, node, command)
 
-    pcie_dev_list = [ '1dd8:1000' ]
-    
-    #                  '1dd8:1001', \
-    #                  '1dd8:1002', \
-    #                  '1dd8:1004'  \
-    #                  '1dd8:1007'
+    resp = api.Trigger(req)
+    if resp is None:
+        api.Logger.error(f"Failed to run command on host: {node}, command: {command}")
+
+    return resp
+
+def log_lspci_err(node, device, std_err, std_out, command):
+
+    api.Logger.error(f"lspci exited with error on {node}:{device}")
+    api.Logger.info(f"{command}")
+    api.Logger.info(f"stderr: {std_err}")
+    api.Logger.info(f"stdout: {std_out}")
+
+def uptime(node):
+    req = api.Trigger_CreateExecuteCommandsRequest()
+    api.Trigger_AddHostCommand(req, node, "uptime")
+    resp = api.Trigger(req)
+
+    if resp is None:
+        api.Logger.error("uptime command failed")
+        return
+
+    cmd = resp.commands.pop()
+    api.Logger.info(f"{node} uptime: {cmd.stdout}")
+
+    return
+
+def verify_errors_lspci(node, os_type):
+
+    pci_eth_device = '1dd8:1002'
+    pci_root_device = '1dd8:1000'
+
+    uptime(node)
 
     if os_type == host.OS_TYPE_ESX:
         return api.types.status.SUCCESS
 
-    for pci_device in pcie_dev_list:
-        lspci_cmd = f"lspci -nnkvvv -d {pci_device} | grep Sta"
-        req = api.Trigger_CreateExecuteCommandsRequest()
-        api.Trigger_AddHostCommand(req, node, lspci_cmd)
-        resp = api.Trigger(req)
-
-        if resp is None:
-            api.Logger.error(f"Failed to run lspci on {node}:{pci_device}" )
-            return api.types.status.FAILURE
-
+    # Verify errors on the bridge (1dd8:1000)
+    pci_verif_cmd = f"lspci -nnkvvv -d {pci_root_device} | grep Sta"
+    resp = run_command(pci_verif_cmd, node)
+    if resp:
         cmd = resp.commands.pop()
+        if cmd.exit_code != 0 or cmd.stdout == "":
+            log_lspci_err(node, pci_root_device, cmd.stderr, cmd.stdout, pci_verif_cmd)
+            return api.types.status.FAILURE
+        api.Logger.info(f"     {pci_verif_cmd}")
 
-        if cmd.exit_code != 0 or cmd.stdout == "": 
-            api.Logger.error(f"lspci exited with error on {node}:{pci_device}" )
-            api.Logger.info (cmd.stderr)
+        if re.search('UncorrErr\+|FatalErr\+|TrErr\+|Train\+|RxErr\+|SDES\+|ECRC\+', cmd.stdout, re.I):
+            api.Logger.error(f'Errors on PCI Device {node}:{pci_root_device}')
+            api.Logger.error(cmd.stdout)
             return api.types.status.FAILURE
 
-        lspci_out = cmd.stdout
+        api.Logger.info(f"     No Errors on {pci_root_device}")
 
-        #if re.search( 'CorrErr\+|UncorrErr\+|FatalErr\+|TrErr\+|Train\+|RxErr\+|SDES\+|ECRC\+', lspci_out, re.I ):
-        if re.search( 'UncorrErr\+|FatalErr\+|TrErr\+|Train\+|RxErr\+|SDES\+|ECRC\+', lspci_out, re.I ):
-            api.Logger.error(f'Errors on PCI Device {node}:{pci_device}')
-            api.Logger.error(lspci_out)
+    # get a second oppinion, use grep instead of python search
+    pci_verif_cmd = f"lspci -nnkvvv -d {pci_root_device} | grep Sta: | grep Err+ | grep -v CorrErr+"
+    resp = run_command(pci_verif_cmd, node)
+    if resp:
+        cmd = resp.commands.pop()
+        if cmd.exit_code == 0:
+            log_lspci_err(node, pci_root_device, cmd.stderr, cmd.stdout, pci_verif_cmd)
             return api.types.status.FAILURE
+
+        api.Logger.info(f"     {pci_verif_cmd}")
+        api.Logger.info(f"     {cmd.stdout}")
+
+    # Verify PCIe Width came up correctly
+    # 1: how many x8 capable interfaces
+    pci_verif_cmd = f"lspci -nnkvvv -d {pci_eth_device} | grep LnkCap: | grep -c x8"
+    resp = run_command(pci_verif_cmd, node)
+
+    if resp:
+        cmd = resp.commands.pop()
+        if cmd.exit_code != 0 or cmd.stdout == "0":
+            log_lspci_err(node, pci_eth_device, cmd.stderr, cmd.stdout, pci_verif_cmd)
+            return api.types.status.FAILURE
+
+        capability_x8 = int(cmd.stdout)
+        api.Logger.info(f"     x8 Capable: {capability_x8}")
+
+    # 2: how many x8 status interface
+    pci_verif_cmd = f"lspci -nnkvvv -d {pci_eth_device} | grep LnkSta: | grep -c x8"
+    resp = run_command(pci_verif_cmd, node)
+    if resp:
+        cmd = resp.commands.pop()
+        if cmd.exit_code != 0 or cmd.stdout == "0":
+            log_lspci_err(node, pci_eth_device, cmd.stderr, cmd.stdout, pci_verif_cmd)
+            return api.types.status.FAILURE
+
+        status_x8 = int(cmd.stdout)
+
+    if capability_x8 != status_x8:
+        api.Logger.error(f"     ERROR: Less configured x8 than capable")
+        #print detailed status
+        pci_verif_cmd = f"lspci -nnkvvv -d {pci_eth_device} | grep LnkSta:"
+        if run_command(pci_verif_cmd, node):
+            cmd = resp.commands.pop()
+            if cmd.exit_code != 0 or cmd.stdout == "0":
+                log_lspci_err(node, pci_eth_device, cmd.stderr, cmd.stdout, pci_verif_cmd)
+                return api.types.status.FAILURE
+
+        api.Logger.info(f"{cmd.std.out}")
+        return api.types.status.FAILURE
+
+    api.Logger.info(f"     x8 Status: {status_x8}")
 
     return api.types.status.SUCCESS
-
-
