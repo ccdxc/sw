212a331e835dc1aa9aef2de0836cbcb8a6e287eb
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2020-03-21 18:55:31 -0700

    allocating larger NACL region to support 2 DHCP relay (#21137)
    servers per subnet

--- nic/apollo/api/impl/apulu/apulu_impl.hpp
+++ nic/apollo/api/impl/apulu/apulu_impl.hpp
@@ -59,12 +59,12 @@
 
 // NACL table partitioning
 #define PDS_IMPL_NACL_BLOCK_HIGH_PRIO_DYNAMIC_MIN    0
-#define PDS_IMPL_NACL_BLOCK_HIGH_PRIO_DYNAMIC_MAX    15
-#define PDS_IMPL_NACL_BLOCK_GLOBAL_MIN               16
-#define PDS_IMPL_NACL_BLOCK_GLOBAL_MAX               23
-#define PDS_IMPL_NACL_BLOCK_LEARN_MIN                24
-#define PDS_IMPL_NACL_BLOCK_LEARN_MAX                31
-#define PDS_IMPL_NACL_BLOCK_GENERIC_MIN              32
+#define PDS_IMPL_NACL_BLOCK_HIGH_PRIO_DYNAMIC_MAX    31
+#define PDS_IMPL_NACL_BLOCK_GLOBAL_MIN               32
+#define PDS_IMPL_NACL_BLOCK_GLOBAL_MAX               39
+#define PDS_IMPL_NACL_BLOCK_LEARN_MIN                40
+#define PDS_IMPL_NACL_BLOCK_LEARN_MAX                47
+#define PDS_IMPL_NACL_BLOCK_GENERIC_MIN              48
 
 // Inband control interface name
 #define PDS_IMPL_VENDOR_L3_IF_NAME                   "dscv0"
