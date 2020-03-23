b5521d8bd7977eeefe8987fdf42f8ae6d120f234
csalvo9 <55756953+csalvo9@users.noreply.github.com>
2020-03-23 13:48:40 -0700

    added ifconfig (#20976)

--- iota/test/iris/testcases/networking/ping.py
+++ iota/test/iris/testcases/networking/ping.py
@@ -1,11 +1,63 @@
 #! /usr/bin/python3
 import pdb
-
+import re
 import iota.harness.api as api
 import iota.protos.pygen.topo_svc_pb2 as topo_svc_pb2
 from iota.harness.infra.exceptions import *
 from iota.test.iris.utils import vmotion_utils
 
+def parseEthtool(text, intName):
+    found = re.search("Link detected: ([\S]+)", text, re.M)
+    if not found:
+        api.Logger.debug("failed to find link state in output of ethtool: {0}".format(text))
+        return None
+    else:
+        api.Logger.debug("link state for {0} was {1}".format(intName, found.group(1)))
+        res = True if found.group(1)=="yes" else False
+        return (intName,res)
+
+def parseArp(text, ip):
+    found = re.search(".*\(("+ip+")\) at ([\S]+) ([\S]+) on ([\S]+)\n", text, re.M)
+    if not found:
+        api.Logger.debug("no arp entry for ip {0}".format(ip))
+    else:
+        api.Logger.debug("found arp entry {0}".format(found.groups()))
+
+def runCmd(nodeName, cmd):
+    api.Logger.debug("sending command {0} to node {1}".format(cmd, nodeName))
+    req = api.Trigger_CreateExecuteCommandsRequest(serial=True)
+    api.Trigger_AddHostCommand(req, nodeName, cmd)
+    return api.Trigger(req)
+
+def checkArp(nodeName, ip):
+    cmd = "arp -na | grep {0}".format(ip)
+    output = runCmd(nodename, cmd)
+    return parseArp(output, ip)
+
+def checkLink(nodeName, intName):
+    cmd = "ethtool " + intName
+    output = runCmd(nodename, cmd)
+    return parseEthtool(output, intName)
+        
+def runIfconfig(nodeName, intName):
+    cmd = "ifconfig " + intName
+    output = runCmd(nodename, cmd)
+    api.Logger.debug(output)
+
+def debugPing(cmd, intName):
+    runIfconfig(cmd.node_name, intName)
+    foundIp = re.search(".* ([\S]+$)",cmd.command)
+    if foundIp:
+        ip = foundIp.group(1)
+        if re.search("[\d]+\.[\d]+\.[\d]+\.[\d]+", ip):
+            checkArp(cmd.node_name, ip)
+        else:
+            api.Logger.debug("skipping arp check for v6 address {0}".format(ip))
+    else:
+        api.Logger.debug("failed to determine ip address from ping command {0}".format(cmd.command))
+    return checkLink(cmd.node_name, intName)
+
+ 
 def Setup(tc):
     tc.skip = False
 
@@ -83,6 +135,14 @@ def Verify(tc):
             if api.GetNicMode() == 'hostpin' and tc.iterators.pktsize > 1024:
                 result = api.types.status.SUCCESS
             else:
+                try:
+                    found = re.search(cmd.node_name+"_(.*)_subif_(.*$)",cmd.entity_name)
+                    if found:
+                        debugPing(cmd, found.group(1))
+                    else:
+                        api.Logger.debug("failed to determine interface name from {0}".format(cmd.entity_name))
+                except:
+                    api.Logger.debug("failed to debug {0} ping issue".format(cmd.entity_name))
                 result = api.types.status.FAILURE
             #for future use
             #elif tc.args.type == 'local_only':
