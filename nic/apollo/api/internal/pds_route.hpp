f2164ee94f1458f44b9b38a08feb1072fbdf9e95
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2020-03-26 12:46:18 -0700

    internal route APIs (#21366)

---
+++ nic/apollo/api/internal/pds_route.hpp
@@ -0,0 +1,27 @@
+//
+// {C} Copyright 2020 Pensando Systems Inc. All rights reserved
+//
+//----------------------------------------------------------------------------
+///
+/// \file
+/// This file defines route APIs for internal module interactions
+///
+//----------------------------------------------------------------------------
+
+#ifndef __INTERNAL_PDS_ROUTE_HPP__
+#define __INTERNAL_PDS_ROUTE_HPP__
+
+#include "nic/sdk/include/sdk/base.hpp"
+#include "nic/sdk/include/sdk/types.hpp"
+#include "nic/apollo/api/include/pds.hpp"
+
+namespace api {
+
+sdk_ret_t pds_underlay_route_create(_In_ pds_route_spec_t *spec);
+sdk_ret_t pds_underlay_route_update(_In_ pds_route_spec_t *spec);
+sdk_ret_t pds_underlay_route_delete(_In_ pds_obj_key_t *key);
+pds_obj_key_t pds_underlay_nexthop(_In_ ip_addr_t ip_addr);
+
+}    // namespace api
+
+#endif    // __INTERNAL_PDS_ROUTE_HPP__
--- nic/apollo/api/internal/route_api.cc
+++ nic/apollo/api/internal/route_api.cc
@@ -10,6 +10,8 @@
 
 #include "nic/apollo/core/trace.hpp"
 #include "nic/apollo/api/include/pds_route.hpp"
+#include "nic/apollo/api/internal/pds_route.hpp"
+#include "nic/apollo/api/pds_state.hpp"
 
 void pds_route_table_spec_s::deepcopy_(const pds_route_table_spec_t& route_table) {
     // self-assignment guard
@@ -68,3 +70,73 @@ void pds_route_table_spec_s::move_(pds_route_table_spec_t&& route_table) {
     // transfer the ownership of the memory
     route_table.route_info = NULL;
 }
+
+namespace api {
+
+#define PDS_MAX_UNDERLAY_ROUTES    1
+uint32_t g_num_routes = 0;
+typedef struct route_entry_s {
+    uint8_t valid:1;
+    uint8_t rsvd:7;
+    pds_route_spec_t spec;
+} route_entry_t;
+static route_entry_t g_route_db[PDS_MAX_UNDERLAY_ROUTES];
+
+sdk_ret_t
+pds_underlay_route_create (_In_ pds_route_spec_t *spec)
+{
+    if (g_num_routes >= PDS_MAX_UNDERLAY_ROUTES) {
+        PDS_TRACE_ERR("Failed to create route %s, underlay route table is full",
+                      spec->key.str());
+        return SDK_RET_NO_RESOURCE;
+    }
+    if (route_table_db()->find(&spec->route_table) == NULL) {
+        PDS_TRACE_ERR("Failed to create route %s, route table %s not found",
+                      spec->key.str(), spec->route_table.str());
+        return SDK_RET_INVALID_ARG;
+    }
+    g_route_db[g_num_routes].valid = TRUE;
+    g_num_routes++;
+
+    return SDK_RET_OK;
+}
+
+sdk_ret_t
+pds_underlay_route_update (_In_ pds_route_spec_t *spec)
+{
+    for (uint32_t i = 0; i < g_num_routes; i++) {
+        if (g_route_db[i].valid && (g_route_db[i].spec.key == spec->key)) {
+            g_route_db[i].spec = *spec;
+            // TODO: do TEP walk !!
+            return SDK_RET_OK;
+        }
+    }
+    PDS_TRACE_ERR("Failed to update route %s, route not found",
+                  spec->key.str());
+    return SDK_RET_ENTRY_NOT_FOUND;
+}
+
+sdk_ret_t
+pds_underlay_route_delete (_In_ pds_obj_key_t *key)
+{
+    for (uint32_t i = 0; i < g_num_routes; i++) {
+        if (g_route_db[i].valid && (g_route_db[i].spec.key == *key)) {
+            // replace this with the last valid route
+            g_route_db[i] = g_route_db[g_num_routes - 1];
+            g_route_db[g_num_routes - 1].valid = FALSE;
+            g_num_routes--;
+            return SDK_RET_OK;
+        }
+    }
+    PDS_TRACE_ERR("Failed to delete route %s, route not found", key->str());
+    return SDK_RET_INVALID_OP;
+}
+
+pds_obj_key_t
+pds_underlay_nexthop (_In_ ip_addr_t ip_addr)
+{
+    // TODO
+    return k_pds_obj_key_invalid;
+}
+
+}    // namespace api
