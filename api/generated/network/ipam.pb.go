ccdxc/sw

commit 68adbdf8b7e0cb0eb152a7adf143f43ce809a3d1

Add all restapi endpoints for ipam objects (#21176)
Co-authored-by: Ashish Yadav <ashish@sw-dev4.pensando.io>

api/generated/network/ipam.pb.go
@@ -7,8 +7,8 @@
 	It is generated from these files:
 		ipam.proto
 		lb.proto
-		networkinterface.proto
 		network.proto
+		networkinterface.proto
 		route.proto
 		service.proto
 		svc_network.proto
@@ -24,6 +24,10 @@
 		LbPolicy
 		LbPolicySpec
 		LbPolicyStatus
+		Network
+		NetworkSpec
+		NetworkStatus
+		OrchestratorInfo
 		NetworkInterface
 		NetworkInterfaceHostStatus
 		NetworkInterfaceSpec
@@ -31,10 +35,6 @@
 		NetworkInterfaceUplinkStatus
 		PauseSpec
 		TransceiverStatus
-		Network
-		NetworkSpec
-		NetworkStatus
-		OrchestratorInfo
 		BGPConfig
 		BGPNeighbor
 		EVPNConfig

api/generated/pkgmanifest
@@ -8,7 +8,7 @@ true diagnostics module.proto svc_diagnostics.proto
 true events events.proto svc_events.proto 
 true fwlog fwlog.proto 
 true monitoring alerts.proto archive.proto auditpolicy.proto eventpolicy.proto export.proto mirror.proto svc_monitoring.proto techsupport.proto telemetry.proto troubleshooting.proto 
-true network ipam.proto lb.proto networkinterface.proto network.proto route.proto service.proto svc_network.proto vrf.proto 
+true network ipam.proto lb.proto network.proto networkinterface.proto route.proto service.proto svc_network.proto vrf.proto
 true objstore objstore.proto svc_objstore.proto 
 true orchestration orchestration.proto svc_orchestration.proto 
 true rollout rollout.proto svc_rollout.proto 

nic/agent/protos/netproto/ipam.pb.go
@@ -1707,56 +1707,58 @@ var (
 func init() { proto.RegisterFile("ipam.proto", fileDescriptorIpam) }
 
 var fileDescriptorIpam = []byte{
-	// 814 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x54, 0xc1, 0x6e, 0xf3, 0x44,
-	0x10, 0x8e, 0xf3, 0x47, 0x6d, 0xb2, 0x69, 0x93, 0xb0, 0x6d, 0xc1, 0x31, 0x6d, 0x53, 0x59, 0x20,
-	0x05, 0x54, 0xdb, 0x34, 0x48, 0xbd, 0x81, 0xa8, 0x49, 0x50, 0x4b, 0x5b, 0x1a, 0x92, 0x52, 0x2e,
-	0x08, 0xb4, 0x71, 0x96, 0x64, 0x91, 0x63, 0xaf, 0xec, 0x75, 0x50, 0x84, 0x7a, 0x42, 0xea, 0x13,
-	0xf0, 0x14, 0x95, 0xe0, 0xc0, 0x53, 0x54, 0xe2, 0xd2, 0x2b, 0x97, 0x08, 0xf5, 0x98, 0xa7, 0x40,
-	0xbb, 0x4e, 0xe2, 0x75, 0x9b, 0x4a, 0xff, 0xcd, 0xfb, 0xcd, 0x37, 0xdf, 0xce, 0x7c, 0x33, 0x6b,
-	0x00, 0x08, 0x45, 0x23, 0x93, 0x06, 0x3e, 0xf3, 0x61, 0xde, 0xc3, 0x4c, 0x7c, 0x69, 0xbb, 0x03,
-	0xdf, 0x1f, 0xb8, 0xd8, 0x42, 0x94, 0x58, 0xc8, 0xf3, 0x7c, 0x86, 0x18, 0xf1, 0xbd, 0x30, 0xe6,
-	0x69, 0xad, 0x01, 0x61, 0xc3, 0xa8, 0x67, 0x3a, 0xfe, 0xc8, 0xa2, 0xd8, 0x0b, 0x91, 0xd7, 0xf7,
-	0xad, 0xf0, 0x57, 0x6b, 0x8c, 0x3d, 0xe2, 0x60, 0x2b, 0x62, 0xc4, 0x0d, 0x79, 0xea, 0x00, 0x7b,
-	0x72, 0xb6, 0x45, 0x3c, 0xc7, 0x8d, 0xfa, 0x78, 0x21, 0x63, 0x48, 0x32, 0x03, 0x7f, 0xe0, 0x5b,
-	0x02, 0xee, 0x45, 0x3f, 0x8b, 0x93, 0x38, 0x88, 0xaf, 0x39, 0xfd, 0xc3, 0x57, 0x6e, 0xe5, 0x35,
-	0x8e, 0x30, 0x43, 0x31, 0x4d, 0xff, 0x43, 0x01, 0xa0, 0x79, 0xfa, 0x65, 0xbb, 0x8b, 0x83, 0x31,
-	0x0e, 0xa0, 0x0d, 0x0a, 0x67, 0xed, 0x93, 0x7e, 0x3f, 0xc0, 0x61, 0xa8, 0x2a, 0x07, 0x4a, 0xbd,
-	0x60, 0x7f, 0x70, 0x7f, 0x57, 0xcd, 0xc7, 0x60, 0xfd, 0xa3, 0xd9, 0xb4, 0xb6, 0x4d, 0xa8, 0x81,
-	0x62, 0xc6, 0xa1, 0x3f, 0x22, 0x0c, 0x8f, 0x28, 0x9b, 0x74, 0x92, 0x34, 0x68, 0x83, 0xcd, 0x1b,
-	0x12, 0xb0, 0x08, 0xb9, 0x1d, 0x3f, 0x62, 0x38, 0x50, 0xb3, 0x42, 0x67, 0x77, 0x36, 0xad, 0xa9,
-	0xe3, 0x38, 0x60, 0x04, 0x22, 0x22, 0xe5, 0xa7, 0x53, 0xf4, 0x1f, 0x41, 0x99, 0x57, 0xd5, 0xc1,
-	0x2e, 0x9a, 0xb4, 0x7d, 0x97, 0x38, 0x13, 0x78, 0x0e, 0xd6, 0xe3, 0x22, 0x79, 0x61, 0x6f, 0xea,
-	0xc5, 0xc6, 0xb6, 0xb9, 0x18, 0x80, 0x99, 0x74, 0x60, 0xbf, 0x3f, 0x9b, 0xd6, 0xde, 0x0b, 0x78,
-	0x9e, 0x11, 0xc6, 0x74, 0xe9, 0x96, 0x85, 0x82, 0xfe, 0x8f, 0x02, 0x4a, 0x67, 0xed, 0x93, 0xcb,
-	0x58, 0xbb, 0x4b, 0xb1, 0x03, 0xbf, 0x06, 0xb9, 0xeb, 0x09, 0xc5, 0xf3, 0xae, 0x8f, 0xef, 0xef,
-	0xaa, 0x07, 0x5d, 0x16, 0xb4, 0xbc, 0x68, 0x54, 0x4f, 0x33, 0x4d, 0x7e, 0xe4, 0x54, 0xee, 0x46,
-	0x89, 0x4d, 0x28, 0x96, 0x6e, 0x10, 0x1a, 0xf0, 0x5b, 0x50, 0x58, 0x96, 0x2f, 0xda, 0x2f, 0x36,
-	0xaa, 0xe9, 0x6a, 0xa5, 0xce, 0x6c, 0x95, 0xbb, 0xda, 0x1f, 0x3a, 0xd4, 0x10, 0x75, 0xcb, 0xae,
-	0x2e, 0xa9, 0xba, 0x06, 0xf2, 0x8b, 0x7b, 0x61, 0x29, 0x9e, 0xd9, 0x4f, 0x22, 0x52, 0xc9, 0xe8,
-	0xd7, 0xa0, 0x22, 0x95, 0xc8, 0x10, 0x8b, 0x42, 0xf8, 0x05, 0xd8, 0x48, 0xb0, 0xb3, 0xa6, 0x68,
-	0x2b, 0x17, 0x0f, 0x81, 0xef, 0xb0, 0x41, 0x45, 0xc0, 0x20, 0x7d, 0xe9, 0xba, 0x54, 0x86, 0xfe,
-	0x97, 0x02, 0xca, 0x09, 0xd0, 0x1a, 0x63, 0x8f, 0xc1, 0xaf, 0x40, 0x41, 0x7c, 0x2c, 0x9d, 0x2a,
-	0x35, 0x4a, 0x26, 0xa2, 0xc4, 0x5c, 0xa2, 0x71, 0x37, 0x98, 0x1f, 0x8d, 0x67, 0xde, 0x24, 0xa9,
-	0xb0, 0x0b, 0x40, 0x22, 0x3d, 0x77, 0x48, 0x9a, 0x67, 0x12, 0xb3, 0xf7, 0x1e, 0xa6, 0xb5, 0xcc,
-	0x6c, 0x5a, 0xdb, 0x91, 0xaa, 0x96, 0x34, 0x25, 0x19, 0xfd, 0x07, 0xb0, 0xf5, 0xac, 0xde, 0x0b,
-	0x12, 0x32, 0xd8, 0x02, 0x15, 0x92, 0x86, 0x17, 0x1b, 0x54, 0x5d, 0x75, 0xa3, 0x60, 0x74, 0x5e,
-	0xa4, 0xe8, 0xa7, 0xf2, 0xc6, 0x08, 0xe1, 0x63, 0x50, 0x4c, 0x58, 0x2b, 0xb6, 0x32, 0xa1, 0x77,
-	0x64, 0xa2, 0xfe, 0x67, 0x56, 0xee, 0x1e, 0x7e, 0x0e, 0xf2, 0xdc, 0x93, 0x4b, 0xcc, 0x90, 0xb0,
-	0xb4, 0xd8, 0xd8, 0x14, 0x96, 0x2e, 0x40, 0x7b, 0x8b, 0x5b, 0xf0, 0x38, 0xad, 0x29, 0xb3, 0x69,
-	0x6d, 0xfd, 0x90, 0x78, 0x2e, 0xf1, 0x70, 0x67, 0x99, 0x03, 0xcf, 0x01, 0xb8, 0xea, 0xfd, 0x82,
-	0x1d, 0x26, 0x14, 0x62, 0x2f, 0xcb, 0x42, 0x21, 0x81, 0x6d, 0x4d, 0xd2, 0x28, 0xf1, 0xf7, 0x2f,
-	0x7b, 0x98, 0xf0, 0x60, 0x13, 0xe4, 0xf8, 0x8e, 0xab, 0x6f, 0x84, 0x8c, 0xba, 0xaa, 0x19, 0x1e,
-	0xb7, 0xdf, 0x9d, 0x8f, 0xa5, 0x14, 0x52, 0xec, 0xc8, 0xfb, 0x2f, 0xde, 0xd2, 0x05, 0x58, 0x8b,
-	0xd7, 0x50, 0xcd, 0x09, 0x1d, 0x6d, 0xa5, 0x8e, 0x60, 0xd8, 0xea, 0x5c, 0xa9, 0x12, 0x8a, 0xb3,
-	0xa4, 0x35, 0xd7, 0x68, 0xfc, 0x9b, 0x95, 0x17, 0xf1, 0x84, 0x92, 0x9b, 0x23, 0xd8, 0x04, 0x65,
-	0x3e, 0x83, 0x04, 0x0e, 0xe1, 0x8e, 0xe8, 0x99, 0xa3, 0xdf, 0x23, 0xe6, 0x0c, 0xaf, 0xa8, 0xf8,
-	0x8d, 0x6a, 0x2b, 0x7b, 0xe0, 0x2c, 0x3d, 0x03, 0x2f, 0x40, 0x45, 0x70, 0xdf, 0x42, 0x66, 0xef,
-	0xd5, 0x5d, 0x89, 0xb5, 0x3e, 0x51, 0x60, 0x13, 0x6c, 0x27, 0xa1, 0x2b, 0x8a, 0x83, 0xef, 0x68,
-	0x1f, 0x31, 0x0c, 0x5f, 0x5f, 0x33, 0x2d, 0x3d, 0x69, 0x3d, 0x53, 0x57, 0x34, 0xf7, 0xef, 0xdf,
-	0xab, 0xc3, 0xd4, 0x82, 0xe4, 0x5c, 0x12, 0x32, 0xed, 0x33, 0xeb, 0xb7, 0x64, 0x4e, 0xe6, 0x35,
-	0xf6, 0x90, 0xc7, 0x6e, 0x53, 0xd8, 0x37, 0x68, 0x84, 0x43, 0x8a, 0x1c, 0xfc, 0x12, 0xbe, 0xd5,
-	0xdf, 0xe1, 0x3f, 0xfe, 0xe4, 0x0d, 0x11, 0x1c, 0x7e, 0x9c, 0xbd, 0x39, 0xb2, 0x37, 0x1e, 0x9e,
-	0xf6, 0x95, 0xc7, 0xa7, 0x7d, 0xe5, 0xbf, 0xa7, 0x7d, 0xa5, 0xad, 0xf4, 0xd6, 0x44, 0x9d, 0x9f,
-	0xfe, 0x1f, 0x00, 0x00, 0xff, 0xff, 0x5c, 0x10, 0x11, 0x0d, 0xe9, 0x06, 0x00, 0x00,
+	// 838 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x55, 0xd1, 0x8e, 0xdb, 0x44,
+	0x14, 0x5d, 0x27, 0xd1, 0x36, 0x99, 0xb4, 0x49, 0x98, 0x6e, 0xc1, 0x31, 0xed, 0xa6, 0xb2, 0x40,
+	0x0a, 0xa8, 0xb6, 0x69, 0x90, 0xfa, 0x06, 0x62, 0x4d, 0x82, 0xba, 0x74, 0xcb, 0x86, 0x64, 0x59,
+	0x5e, 0x10, 0x68, 0x62, 0x5f, 0xb2, 0x83, 0x1c, 0x7b, 0x64, 0x8f, 0x83, 0x22, 0x54, 0x09, 0x09,
+	0xd1, 0x2f, 0xe0, 0x2b, 0x2a, 0xc1, 0x03, 0x5f, 0x51, 0x89, 0x97, 0x7e, 0x41, 0x84, 0xf6, 0x31,
+	0xaf, 0xfc, 0x00, 0x9a, 0x71, 0x12, 0x8f, 0xbb, 0x59, 0xa9, 0x6f, 0x9e, 0x7b, 0xcf, 0x3d, 0x73,
+	0xef, 0xb9, 0x67, 0x12, 0x84, 0x28, 0x23, 0x33, 0x9b, 0xc5, 0x11, 0x8f, 0x70, 0x35, 0x04, 0x2e,
+	0xbf, 0x8c, 0xbb, 0xd3, 0x28, 0x9a, 0x06, 0xe0, 0x10, 0x46, 0x1d, 0x12, 0x86, 0x11, 0x27, 0x9c,
+	0x46, 0x61, 0x92, 0xe1, 0x8c, 0xc1, 0x94, 0xf2, 0x8b, 0x74, 0x62, 0x7b, 0xd1, 0xcc, 0x61, 0x10,
+	0x26, 0x24, 0xf4, 0x23, 0x27, 0xf9, 0xd9, 0x99, 0x43, 0x48, 0x3d, 0x70, 0x52, 0x4e, 0x83, 0x44,
+	0x94, 0x4e, 0x21, 0x54, 0xab, 0x1d, 0x1a, 0x7a, 0x41, 0xea, 0xc3, 0x86, 0xc6, 0x52, 0x68, 0xa6,
+	0xd1, 0x34, 0x72, 0x64, 0x78, 0x92, 0xfe, 0x28, 0x4f, 0xf2, 0x20, 0xbf, 0xd6, 0xf0, 0xf7, 0xaf,
+	0xb9, 0x55, 0xf4, 0x38, 0x03, 0x4e, 0x32, 0x98, 0xf9, 0x87, 0x86, 0x50, 0xff, 0xf1, 0xe7, 0xc3,
+	0x31, 0xc4, 0x73, 0x88, 0xb1, 0x8b, 0x6a, 0xc7, 0xc3, 0x23, 0xdf, 0x8f, 0x21, 0x49, 0x74, 0xed,
+	0xbe, 0xd6, 0xad, 0xb9, 0xef, 0xbd, 0x78, 0xde, 0xae, 0x66, 0xc1, 0xee, 0x07, 0xab, 0x65, 0xe7,
+	0x80, 0x32, 0x8b, 0x64, 0x88, 0x07, 0xd1, 0x8c, 0x72, 0x98, 0x31, 0xbe, 0x18, 0xe5, 0x65, 0xd8,
+	0x45, 0xb7, 0xce, 0x69, 0xcc, 0x53, 0x12, 0x8c, 0xa2, 0x94, 0x43, 0xac, 0x97, 0x24, 0xcf, 0xdd,
+	0xd5, 0xb2, 0xa3, 0xcf, 0xb3, 0x84, 0x15, 0xcb, 0x8c, 0x52, 0x5f, 0x2c, 0x31, 0xbf, 0x47, 0x4d,
+	0xd1, 0xd5, 0x08, 0x02, 0xb2, 0x18, 0x46, 0x01, 0xf5, 0x16, 0xf8, 0x09, 0xba, 0x91, 0x35, 0x29,
+	0x1a, 0x2b, 0x77, 0xeb, 0xbd, 0x03, 0x7b, 0xb3, 0x00, 0x3b, 0x9f, 0xc0, 0x7d, 0x77, 0xb5, 0xec,
+	0xbc, 0x13, 0x8b, 0x3a, 0x2b, 0xc9, 0xe0, 0xca, 0x2d, 0x1b, 0x06, 0xf3, 0x1f, 0x0d, 0x35, 0x8e,
+	0x87, 0x47, 0x4f, 0x33, 0xee, 0x31, 0x03, 0x0f, 0x7f, 0x89, 0x2a, 0x67, 0x0b, 0x06, 0xeb, 0xa9,
+	0x1f, 0xbd, 0x78, 0xde, 0xbe, 0x3f, 0xe6, 0xf1, 0x20, 0x4c, 0x67, 0xdd, 0x22, 0xd2, 0x16, 0x47,
+	0x01, 0x15, 0x6a, 0x34, 0xf8, 0x82, 0x81, 0x72, 0x83, 0xe4, 0xc0, 0x5f, 0xa3, 0xda, 0xb6, 0x7d,
+	0x39, 0x7e, 0xbd, 0xd7, 0x2e, 0x76, 0xab, 0x4c, 0xe6, 0xea, 0x42, 0x55, 0xff, 0xc2, 0x63, 0x96,
+	0xec, 0x5b, 0x55, 0x75, 0x0b, 0x35, 0x0d, 0x54, 0xdd, 0xdc, 0x8b, 0x1b, 0xd9, 0xce, 0x7e, 0x90,
+	0x99, 0xd6, 0x9e, 0x79, 0x86, 0x5a, 0x4a, 0x8b, 0x9c, 0xf0, 0x34, 0xc1, 0x9f, 0xa1, 0x9b, 0x79,
+	0xec, 0xb8, 0x2f, 0xc7, 0xaa, 0x64, 0x4b, 0x10, 0x1e, 0xb6, 0x98, 0x4c, 0x58, 0xd4, 0x57, 0xae,
+	0x2b, 0x54, 0x98, 0x7f, 0x69, 0xa8, 0x99, 0x07, 0x06, 0x73, 0x08, 0x39, 0xfe, 0x02, 0xd5, 0xe4,
+	0xc7, 0x56, 0xa9, 0x46, 0xaf, 0x61, 0x13, 0x46, 0xed, 0x6d, 0x34, 0x9b, 0x06, 0xc4, 0xd1, 0x7a,
+	0x4d, 0x9b, 0xbc, 0x14, 0x8f, 0x11, 0xca, 0xa9, 0xd7, 0x0a, 0x29, 0xfb, 0xcc, 0x73, 0xee, 0xbd,
+	0x97, 0xcb, 0xce, 0xde, 0x6a, 0xd9, 0xb9, 0xa3, 0x74, 0xad, 0x70, 0x2a, 0x34, 0xe6, 0x77, 0xe8,
+	0xf6, 0x6b, 0xfd, 0x9e, 0xd0, 0x84, 0xe3, 0x01, 0x6a, 0xd1, 0x62, 0x78, 0xe3, 0xa0, 0xf6, 0xae,
+	0x1b, 0x25, 0x62, 0x74, 0xa5, 0xc4, 0x7c, 0xac, 0x3a, 0x46, 0x12, 0x3f, 0x42, 0xf5, 0x1c, 0xb5,
+	0xc3, 0x95, 0x39, 0x7c, 0xa4, 0x02, 0xcd, 0x3f, 0x4b, 0xea, 0xf4, 0xf8, 0x53, 0x54, 0x15, 0x9a,
+	0x3c, 0x05, 0x4e, 0xa4, 0xa4, 0xf5, 0xde, 0x2d, 0x29, 0xe9, 0x26, 0xe8, 0xde, 0x16, 0x12, 0xbc,
+	0x5a, 0x76, 0xb4, 0xd5, 0xb2, 0x73, 0xe3, 0x01, 0x0d, 0x03, 0x1a, 0xc2, 0x68, 0x5b, 0x83, 0x9f,
+	0x20, 0x74, 0x3a, 0xf9, 0x09, 0x3c, 0x2e, 0x19, 0x32, 0x2d, 0x9b, 0x92, 0x21, 0x0f, 0xbb, 0x86,
+	0xc2, 0xd1, 0x10, 0xef, 0x5f, 0xd5, 0x30, 0xc7, 0xe1, 0x3e, 0xaa, 0x08, 0x8f, 0xeb, 0x65, 0x49,
+	0xa3, 0xef, 0x1a, 0x46, 0xe4, 0xdd, 0xb7, 0xd7, 0x6b, 0x69, 0x24, 0x0c, 0x3c, 0xd5, 0xff, 0xf2,
+	0x2d, 0x9d, 0xa0, 0xfd, 0xcc, 0x86, 0x7a, 0x45, 0xf2, 0x18, 0x3b, 0x79, 0x24, 0xc2, 0xd5, 0xd7,
+	0x4c, 0xad, 0x44, 0x9e, 0x15, 0xae, 0x35, 0x47, 0xef, 0xbf, 0x92, 0x6a, 0xc4, 0x23, 0x46, 0xcf,
+	0x1f, 0xe2, 0x3e, 0x6a, 0x8a, 0x1d, 0xe4, 0xe1, 0x04, 0xdf, 0x91, 0x33, 0x8b, 0xe8, 0xb7, 0x84,
+	0x7b, 0x17, 0xa7, 0x4c, 0xfe, 0x8c, 0x1a, 0x3b, 0x67, 0x10, 0x28, 0x73, 0x0f, 0x9f, 0xa0, 0x96,
+	0xc4, 0xbe, 0x01, 0xcd, 0xbd, 0x6b, 0xbd, 0x92, 0x71, 0x7d, 0xa4, 0xe1, 0x3e, 0x3a, 0xc8, 0x53,
+	0xa7, 0x0c, 0xe2, 0x6f, 0x98, 0x4f, 0x38, 0xe0, 0xeb, 0x6d, 0x66, 0x14, 0x37, 0x6d, 0xee, 0x75,
+	0x35, 0xe3, 0x77, 0xed, 0xef, 0xdf, 0xda, 0xbf, 0x6a, 0x05, 0x8b, 0x54, 0x02, 0xe1, 0xb7, 0xf2,
+	0x14, 0x38, 0xae, 0xb0, 0x28, 0xe1, 0x78, 0xdf, 0x87, 0x00, 0x38, 0xe0, 0x32, 0x4b, 0xb9, 0xf1,
+	0x89, 0xf3, 0x4b, 0xbe, 0x41, 0xfb, 0x0c, 0x42, 0x12, 0xf2, 0x67, 0x85, 0xd8, 0x57, 0x64, 0x06,
+	0x09, 0x23, 0x1e, 0x5c, 0x0d, 0x3f, 0x33, 0xdf, 0x12, 0x7f, 0x09, 0xf9, 0xeb, 0xa2, 0x90, 0x7c,
+	0x58, 0x3a, 0x7f, 0xe8, 0xde, 0x7c, 0x79, 0x79, 0xa8, 0xbd, 0xba, 0x3c, 0xd4, 0xfe, 0xbd, 0x3c,
+	0xd4, 0x86, 0xda, 0x64, 0x5f, 0x4e, 0xf0, 0xf1, 0xff, 0x01, 0x00, 0x00, 0xff, 0xff, 0x7a, 0x7e,
+	0x57, 0x32, 0x03, 0x07, 0x00, 0x00,
 }

nic/agent/protos/generated/restapi/netagent/ipam_api.go
@@ -7,8 +7,12 @@ Input file: ipam.proto
 package restapi
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"time"
 
+	protoTypes "github.com/gogo/protobuf/types"
 	"github.com/gorilla/mux"
 
 	"github.com/pensando/sw/api"
@@ -22,6 +26,14 @@ func (s *RestServer) AddIPAMPolicyAPIRoutes(r *mux.Router) {
 
 	r.Methods("GET").Subrouter().HandleFunc("/", httputils.MakeHTTPHandler(s.listIPAMPolicyHandler))
 
+	r.Methods("GET").Subrouter().HandleFunc("/{ObjectMeta.Tenant}/{ObjectMeta.Namespace}/{ObjectMeta.Name}", httputils.MakeHTTPHandler(s.getIPAMPolicyHandler))
+
+	r.Methods("POST").Subrouter().HandleFunc("/", httputils.MakeHTTPHandler(s.postIPAMPolicyHandler))
+
+	r.Methods("DELETE").Subrouter().HandleFunc("/{ObjectMeta.Tenant}/{ObjectMeta.Namespace}/{ObjectMeta.Name}", httputils.MakeHTTPHandler(s.deleteIPAMPolicyHandler))
+
+	r.Methods("PUT").Subrouter().HandleFunc("/{ObjectMeta.Tenant}/{ObjectMeta.Namespace}/{ObjectMeta.Name}", httputils.MakeHTTPHandler(s.putIPAMPolicyHandler))
+
 }
 
 func (s *RestServer) listIPAMPolicyHandler(r *http.Request) (interface{}, error) {
@@ -30,4 +42,104 @@ func (s *RestServer) listIPAMPolicyHandler(r *http.Request) (interface{}, error)
 	}
 
 	return s.pipelineAPI.HandleIPAMPolicy(types.List, o)
+}
+
+func (s *RestServer) getIPAMPolicyHandler(r *http.Request) (interface{}, error) {
+	tenant, _ := mux.Vars(r)["ObjectMeta.Tenant"]
+	namespace, _ := mux.Vars(r)["ObjectMeta.Namespace"]
+	name, _ := mux.Vars(r)["ObjectMeta.Name"]
+	o := netproto.IPAMPolicy{
+		TypeMeta: api.TypeMeta{Kind: "IPAMPolicy"},
+		ObjectMeta: api.ObjectMeta{
+			Tenant:    tenant,
+			Namespace: namespace,
+			Name:      name,
+		},
+	}
+
+	data, err := s.pipelineAPI.HandleIPAMPolicy(types.Get, o)
+	if err != nil {
+		return Response{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+	return data, nil
+
+}
+
+func (s *RestServer) postIPAMPolicyHandler(r *http.Request) (interface{}, error) {
+	var o netproto.IPAMPolicy
+	b, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(b, &o)
+	if err != nil {
+		return nil, err
+	}
+	c, _ := protoTypes.TimestampProto(time.Now())
+	o.CreationTime = api.Timestamp{
+		Timestamp: *c,
+	}
+	o.ModTime = api.Timestamp{
+		Timestamp: *c,
+	}
+
+	_, err = s.pipelineAPI.HandleIPAMPolicy(types.Create, o)
+
+	if err != nil {
+		return Response{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+	return Response{
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (s *RestServer) deleteIPAMPolicyHandler(r *http.Request) (interface{}, error) {
+	tenant, _ := mux.Vars(r)["ObjectMeta.Tenant"]
+	namespace, _ := mux.Vars(r)["ObjectMeta.Namespace"]
+	name, _ := mux.Vars(r)["ObjectMeta.Name"]
+	o := netproto.IPAMPolicy{
+		TypeMeta: api.TypeMeta{Kind: "IPAMPolicy"},
+		ObjectMeta: api.ObjectMeta{
+			Tenant:    tenant,
+			Namespace: namespace,
+			Name:      name,
+		},
+	}
+
+	_, err := s.pipelineAPI.HandleIPAMPolicy(types.Delete, o)
+	if err != nil {
+		return Response{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+	return Response{
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func (s *RestServer) putIPAMPolicyHandler(r *http.Request) (interface{}, error) {
+	var o netproto.IPAMPolicy
+	b, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(b, &o)
+	if err != nil {
+		return nil, err
+	}
+	c, _ := protoTypes.TimestampProto(time.Now())
+	o.CreationTime = api.Timestamp{
+		Timestamp: *c,
+	}
+	o.ModTime = api.Timestamp{
+		Timestamp: *c,
+	}
+
+	_, err = s.pipelineAPI.HandleIPAMPolicy(types.Update, o)
+	if err != nil {
+		return Response{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+	return Response{
+		StatusCode: http.StatusOK,
+	}, nil
 }

nic/agent/protos/generated/restclient/ipam_client.go
@@ -15,7 +15,35 @@ import (
 func (cl *AgentClient) IPAMPolicyList() ([]netproto.IPAMPolicy, error) {
 	var ipampolicyList []netproto.IPAMPolicy
 
-	err := netutils.HTTPGet("http://"+cl.agentURL+"/api/ipampolicys/", &ipampolicyList)
+	err := netutils.HTTPGet("http://"+cl.agentURL+"/api/ipam-policies/", &ipampolicyList)
 
 	return ipampolicyList, err
+}
+
+// IPAMPolicyPost creates IPAMPolicy object
+func (cl *AgentClient) IPAMPolicyCreate(postData netproto.IPAMPolicy) error {
+	var resp Response
+
+	err := netutils.HTTPPost("http://"+cl.agentURL+"/api/ipam-policies/", &postData, &resp)
+
+	return err
+
+}
+
+// IPAMPolicyDelete deletes IPAMPolicy object
+func (cl *AgentClient) IPAMPolicyDelete(deleteData netproto.IPAMPolicy) error {
+	var resp Response
+
+	err := netutils.HTTPDelete("http://"+cl.agentURL+"/api/ipam-policies/default/default/testDeleteIPAMPolicy", &deleteData, &resp)
+
+	return err
+}
+
+// IPAMPolicyPut updates IPAMPolicy object
+func (cl *AgentClient) IPAMPolicyUpdate(putData netproto.IPAMPolicy) error {
+	var resp Response
+
+	err := netutils.HTTPPut("http://"+cl.agentURL+"/api/ipam-policies/default/default/preCreatedIPAMPolicy", &putData, &resp)
+
+	return err
 }

nic/agent/protos/netproto/ipam.proto
@@ -15,7 +15,7 @@ service IPAMPolicyApiV1 {
     // options for auto generating rest endpoints
     option(venice.naplesRestService) = {
         Object: "IPAMPolicy",
-        Method: ["list"],
+        Method: ["list", "get", "post", "delete", "put"],
         Pattern: "/{ObjectMeta.Tenant}/{ObjectMeta.Namespace}/{ObjectMeta.Name}"
         Prefix: "api/ipam-policies"
         Version: "V1"

nic/agent/protos/templates/restclient/restclient.tmpl
@@ -59,6 +59,8 @@ import (
 														err := netutils.HTTPGet("http://"+cl.agentURL+"/api/telemetry/flowexports/", &{{lower $crudObject}}{{title .}} )
 												{{else if strEq $crudObject "Profile"}}
 														err := netutils.HTTPGet("http://"+cl.agentURL+"/api/profiles/", &{{lower $crudObject}}{{title .}} )
+												{{else if strEq $crudObject "IPAMPolicy"}}
+														err := netutils.HTTPGet("http://"+cl.agentURL+"/api/ipam-policies/", &{{lower $crudObject}}{{title .}} )
                         {{else}}
                             err := netutils.HTTPGet("http://"+cl.agentURL+"/api/{{lower $crudObject}}s/", &{{lower $crudObject}}{{title .}} )
                         {{end}}
@@ -120,6 +122,8 @@ import (
 														err := netutils.HTTPPost("http://"+cl.agentURL+"/api/telemetry/flowexports/", &{{lower .}}Data, &resp)
 												{{else if strEq $crudObject "Profile"}}
 														err := netutils.HTTPPost("http://"+cl.agentURL+"/api/profiles/", &{{lower .}}Data, &resp)
+												{{else if strEq $crudObject "IPAMPolicy"}}
+														err := netutils.HTTPPost("http://"+cl.agentURL+"/api/ipam-policies/", &{{lower .}}Data, &resp)
                         {{end}}
 
                         return err
@@ -181,6 +185,8 @@ import (
 																	err := netutils.HTTPPut("http://"+cl.agentURL+"/api/telemetry/flowexports/default/default/preCreated{{title $crudObject}}", &{{lower .}}Data, &resp)
 															{{else if strEq $crudObject "Profile"}}
 																	err := netutils.HTTPPut("http://"+cl.agentURL+"/api/profiles/default/default/preCreated{{title $crudObject}}", &{{lower .}}Data, &resp)
+															{{else if strEq $crudObject "IPAMPolicy"}}
+																	err := netutils.HTTPPut("http://"+cl.agentURL+"/api/ipam-policies/default/default/preCreated{{title $crudObject}}", &{{lower .}}Data, &resp)
                             {{end}}
 
                             return err
@@ -241,6 +247,8 @@ import (
 																	err := netutils.HTTPDelete("http://"+cl.agentURL+"/api/telemetry/flowexports/default/default/test{{title .}}{{title $crudObject}}", &{{lower .}}Data, &resp)
 															{{else if strEq $crudObject "Profile"}}
 																	err := netutils.HTTPDelete("http://"+cl.agentURL+"/api/profiles/default/default/test{{title .}}{{title $crudObject}}", &{{lower .}}Data, &resp)
+															{{else if strEq $crudObject "IPAMPolicy"}}
+																	err := netutils.HTTPDelete("http://"+cl.agentURL+"/api/ipam-policies/default/default/test{{title .}}{{title $crudObject}}", &{{lower .}}Data, &resp)
                         {{end}}
                         return err
                     }

commit 9effc63cf1433e1d0a6cf17ddbe99b08f9eb936c

Fix autogenerated files broken by PR #21176 (#21380)

api/generated/network/ipam.pb.go
@@ -7,8 +7,8 @@
 	It is generated from these files:
 		ipam.proto
 		lb.proto
-		networkinterface.proto
 		network.proto
+		networkinterface.proto
 		route.proto
 		service.proto
 		svc_network.proto
@@ -24,6 +24,10 @@
 		LbPolicy
 		LbPolicySpec
 		LbPolicyStatus
+		Network
+		NetworkSpec
+		NetworkStatus
+		OrchestratorInfo
 		NetworkInterface
 		NetworkInterfaceHostStatus
 		NetworkInterfaceSpec
@@ -31,10 +35,6 @@
 		NetworkInterfaceUplinkStatus
 		PauseSpec
 		TransceiverStatus
-		Network
-		NetworkSpec
-		NetworkStatus
-		OrchestratorInfo
 		BGPConfig
 		BGPNeighbor
 		EVPNConfig

api/generated/pkgmanifest
@@ -8,7 +8,7 @@ true diagnostics module.proto svc_diagnostics.proto
 true events events.proto svc_events.proto 
 true fwlog fwlog.proto 
 true monitoring alerts.proto archive.proto auditpolicy.proto eventpolicy.proto export.proto mirror.proto svc_monitoring.proto techsupport.proto telemetry.proto troubleshooting.proto 
-true network ipam.proto lb.proto networkinterface.proto network.proto route.proto service.proto svc_network.proto vrf.proto 
+true network ipam.proto lb.proto network.proto networkinterface.proto route.proto service.proto svc_network.proto vrf.proto 
 true objstore objstore.proto svc_objstore.proto 
 true orchestration orchestration.proto svc_orchestration.proto 
 true rollout rollout.proto svc_rollout.proto 