0805b32b4aa4d39f904e2c1a41bb07738259ad05
Parameshwaran B <param@pensando.io>
2020-03-27 19:33:56 +0530

    use collect_techsupport script in apulu IOTA (#21410)

--- iota/test/apulu/verif/utils/collect_techsupport.py
+++ iota/test/apulu/verif/utils/collect_techsupport.py
@@ -1,13 +1,14 @@
 #! /usr/bin/python3
 from datetime import datetime
+import re
 
 import iota.harness.api as api
 
-TS_DIR  = "/data/techsupport/"
-TS_BIN  = "/nic/bin/techsupport"
-TS_CONF = "/nic/conf/techsupport.json"
+TS_DIR = "/data/techsupport/"
+TS_SCRIPT = "/nic/tools/collect_techsupport.sh"
+TS_SCRIPT_OP_PATTERN = 'Techsupport collected at (\S+)\r\n'
 
-CHMOD_BASE_CMD = "chmod 644 "
+CHMOD_BASE_CMD = "chmod "
 
 class __TCData: pass
 
@@ -23,15 +24,11 @@ def delete_ts_dir(tc):
             result = False
     return result
 
-def generate_ts_filename(node):
-    tstamp = datetime.now().strftime("%Y%m%d-%Hh%Mm%Ss")
-    ts_file = f"techsupport-{node}-{tstamp}.tar.gz"
+def get_techsupport_file(cmd_output):
+    api.Logger.verbose(f"techsupport script output {cmd_output}")
+    matches = re.search(TS_SCRIPT_OP_PATTERN, cmd_output)
+    ts_file = matches.groups()[0] if matches else None
     return ts_file
-
-def get_ts_cmd(ts_file):
-    # run techsupport without skipcores option
-    ts_cmd = f"{TS_BIN} -c {TS_CONF} -d {TS_DIR} -o {ts_file}"
-    return ts_cmd
 
 def trigger_techsupport_requests(tc):
     result = delete_ts_dir(tc)
@@ -39,25 +36,21 @@ def trigger_techsupport_requests(tc):
         api.Logger.error(f"Failed to delete {TS_DIR} before request")
         return result
 
-    req = api.Trigger_CreateExecuteCommandsRequest()
-    cmd_cookies = []
+    req = api.Trigger_CreateAllParallelCommandsRequest()
     for node in tc.nodes:
         api.Logger.info(f"Collecting techsupport from {node}")
-        ts_filename = generate_ts_filename(node)
-        ts_file = TS_DIR + ts_filename
-        # initiate techsupport collect request
-        ts_cmd = get_ts_cmd(ts_filename)
-        api.Logger.verbose(f"Running {ts_cmd} on {node}")
-        api.Trigger_AddNaplesCommand(req, node, ts_cmd)
-        # set the proper permissions so that we can copy it out
-        chmod_cmd = CHMOD_BASE_CMD + ts_file
-        api.Trigger_AddNaplesCommand(req, node, chmod_cmd)
-        # store the ts info to retrieve later
-        cmd_cookies.append((node, ts_file))
-
+        api.Trigger_AddNaplesCommand(req, node, TS_SCRIPT)
     tc.resp = api.Trigger(req)
-    tc.cmd_cookies = cmd_cookies
-    return result
+    return True
+
+def fix_file_permission(node, file, perm='644'):
+    cmd = f"{CHMOD_BASE_CMD} {perm} {file}"
+    req = api.Trigger_CreateAllParallelCommandsRequest()
+    api.Trigger_AddNaplesCommand(req, node, cmd)
+    resp = api.Trigger(req)
+    if resp and resp.commands[0].exit_code == 0:
+        return True
+    return False
 
 def collect_techsupports(tc):
     result = True
@@ -65,21 +58,25 @@ def collect_techsupports(tc):
         api.Logger.error("Failed to trigger techsupport requests")
         return False
 
-    cookie_idx = 0
-    cmd_iter = iter(tc.resp.commands)
-    for ts_cmd, chmod_cmd in zip(cmd_iter, cmd_iter):
-        node, ts_file = tc.cmd_cookies[cookie_idx]
-        # check if any of the cmd failed
-        if any([ts_cmd.exit_code, chmod_cmd.exit_code]):
-            api.PrintCommandResults(ts_cmd)
-            api.PrintCommandResults(chmod_cmd)
+    for cmd in tc.resp.commands:
+        node = cmd.node_name
+        if cmd.exit_code != 0:
+            api.PrintCommandResults(cmd)
             api.Logger.error(f"Failed to collect techsupport for {node}")
             result = False
         else:
+            ts_file = get_techsupport_file(cmd.stdout)
+            if ts_file is None:
+                api.Logger.error(f"Failed to parse techsupport op for {node}")
+                result = False
+                continue
+            if not fix_file_permission(node, ts_file):
+                api.Logger.error(f"Failed to fix perm for techsupport on {node}")
+                result = False
+                continue
             # copy out the generated techsupport tarball from naples
             api.Logger.debug(f"Copying out {ts_file} from {node} to {tc.dir}")
             api.CopyFromNaples(node, [ts_file], tc.dir, True)
-        cookie_idx += 1
     return result
 
 def __setup(tc):
