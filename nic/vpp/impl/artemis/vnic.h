ee44a7dcd84bb57fc4e630eb5761f914b242a308
Akshaya Nadahalli <akshaya@pensando.io>
2020-03-27 21:40:35 +0530

    Fix VPP - apollo/artemis build failures (#21413)

---
+++ nic/vpp/impl/artemis/vnic.h
@@ -0,0 +1,10 @@
+//
+// {C} Copyright 2020 Pensando Systems Inc. All rights reserved
+//
+
+#ifndef __VPP_IMPL_ARTEMIS_VNIC_H__
+#define __VPP_IMPL_ARTEMIS_VNIC_H__
+
+#include <nic/vpp/impl/stub/vnic.h>
+
+#endif    // __VPP_IMPL_ARTEMIS_VNIC_H__
