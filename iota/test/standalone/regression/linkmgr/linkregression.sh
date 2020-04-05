1ea023005157557e7ec7eaf31e8942aca079667e
Dmitry <dbarsky@pensando.io>
2020-04-04 20:43:19 -0700

    [WIP] link regression: Fix paths to the FW.tar on MTP (#21740)
    Signed-off-by: Dmitry Barsky <dbarsky@pensando.io>

--- iota/test/standalone/regression/linkmgr/linkregression.sh
+++ iota/test/standalone/regression/linkmgr/linkregression.sh
@@ -3,7 +3,7 @@ set -x
 
 cleanup ()
 {
-	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "rm $LinkMgrTestPath/$FW"
+	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "rm $LinkMgrTestPath/$FW.tar"
 }
 
 FW=$1
@@ -11,14 +11,16 @@ LinkMgrTest=$2
 LinkMgrTestPath=/home/diag/linkMGR
 LinkMgrTestScript=$LinkMgrTestPath/link_stress_run.py 
 
+sshpass -p lab123 scp -o StrictHostKeyChecking=no /sw/nic/naples_fw_.tar diag@192.168.69.245:$LinkMgrTestPath/$FW.tar
+
 if   [ $LinkMgrTest = "100Gb" ]; then
-	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_6_100G_only.json -t 103 -v $LinkMgrTestScript/$FW.tar"
+	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_6_100G_only.json -t 103 -v $LinkMgrTestPath/$FW.tar"
 elif [ $LinkMgrTest = "40Gb" ]; then
-	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_3_40G_only.json -t 103 -v /home/diag/${TARGET_ID}.tar"
+	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_3_40G_only.json -t 103 -v $LinkMgrTestPath/$FW.tar"
 elif [ $LinkMgrTest = "25Gb" ]; then
-	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_4_10-25G_only.json -t 103 -v /home/diag/${TARGET_ID}.tar"
+	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_4_10-25G_only.json -t 103 -v $LinkMgrTestPath/$FW.tar"
 elif [ $LinkMgrTest = "100Gb_vomero" ]; then
-	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_4_100g_vomero.json -t 103 -v /home/diag/${TARGET_ID}.tar"
+	sshpass -p lab123 ssh -o StrictHostKeyChecking=no diag@192.168.69.245 "$LinkMgrTestScript -s $LinkMgrTestPath/setup_4_100g_vomero.json -t 103 -v $LinkMgrTestPath/$FW.tar"
 fi
 
 
