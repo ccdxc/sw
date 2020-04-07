7b5fda459039c844ef15a352f84a4982ebec0a46
bharatb226 <bharatb226@users.noreply.github.com>
2020-04-06 20:50:25 -0700

    Cleaning up nmd deps (#21863)

--- nic/tools/start-nmd.sh
+++ nic/tools/start-nmd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #timeout if bond0 doesn't get created in 60 seconds
-read -t 60 <> /tmp/bond0_fifo
-rm /tmp/bond0_fifo
-ifconfig bond0 down
-kill -9 $(pidof dhclient)
+#read -t 60 <> /tmp/bond0_fifo
+#rm /tmp/bond0_fifo
+#ifconfig bond0 down
+#kill -9 $(pidof dhclient)
 exec env TMPDIR=/data /nic/bin/nmd
