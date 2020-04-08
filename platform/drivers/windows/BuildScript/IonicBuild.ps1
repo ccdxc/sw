42c854fa2b57358da06cef29e9b9d7c970effba2
valeriuilie <62261562+valeriuilie@users.noreply.github.com>
2020-04-08 12:53:32 -0700

    Added IonicInstaller WiX project to Pensando solution. Integrated with build script. (#21922)
    Signed-off-by: valeriuilie <val@pensando.io>

--- platform/drivers/windows/BuildScript/IonicBuild.ps1
+++ platform/drivers/windows/BuildScript/IonicBuild.ps1
@@ -71,7 +71,7 @@ param(
     [int] $VerBuild = -1,
 
     [Parameter(Mandatory=$false)]
-    [string] $VerExt = ".",
+    [string] $VerExt = "Development",
 
     [Parameter(Mandatory=$false)]
     [ValidateRange(660, 1000)]
@@ -116,20 +116,20 @@ function PauseThenExit {
 
 $OperationStartTime = Get-Date
 
-# Check the path to solution file (SolutionNameAndPath argument)
+# Check the path to solution file (SolutionNameAndPath argument).
 if (!(Test-Path -Path $SolutionNameAndPath -PathType Leaf)) {
     throw "Invalid path to solution: " + $SolutionNameAndPath
 }
 $PathToSln = Split-Path -Path $SolutionNameAndPath
 
-# Look for the artifacts list file (ArtifactsList.txt)
+# Look for the artifacts list file (ArtifactsList.txt).
 $PathToArtifactsListFile = $PSScriptRoot+"\ArtifactsList.txt"
 if (!(Test-Path -Path $PathToArtifactsListFile -PathType Leaf)) {
     Write-Host -BackgroundColor Red -ForegroundColor White "Artifacts list is missing. Make sure the file 'ArtifactsList.txt' is in the same location as the build script:" $PathToArtifactsListFile c
     PauseThenExit -ExitCode 3
 }
 
-
+# Look for VsWhere utility and execute it in order to get the path to MsBuild.
 $RelPathVsWhere = '\Microsoft Visual Studio\Installer\vswhere.exe'
 $PathToVsWhere = ${Env:ProgramFiles(x86)} + $RelPathVsWhere
 
@@ -139,10 +139,39 @@ if(Test-Path -Path $PathToVsWhere -PathType Leaf) {
     throw "Invalid path to vswhere.exe: " + $PathToVsWhere
 }
 
+# Validate path to MsBuild.
 if(!(Test-Path -Path $PathToMsBuild -PathType Leaf)) {
     throw "Invalid path to MSBuild.exe: " + $PathToMsBuild
 }
 
+# Look for the Pensando build environment folder and the text file keeping the build number.
+#
+$BuildVersionFile = "IonicBuildVersion.txt"
+$PathToBuildEnv = $Env:AllUsersProfile + "\PensandoBuildEnv"
+$PathToBuildVersionFile = $PathToBuildEnv + "\" + $BuildVersionFile
+# Validate the path to build environment folder.
+if(!(Test-Path -Path $PathToBuildEnv -PathType Container)) {
+    New-Item -Path $Env:AllUsersProfile -ItemType Directory -Name "PensandoBuildEnv"
+}
+# If the build number file is not there, just create and set build number to "0".
+if(!(Test-Path -Path $PathToBuildVersionFile -PathType Leaf)) {
+    New-Item -Path $PathToBuildEnv -ItemType File -Name $BuildVersionFile
+    Set-Content -Path $PathToBuildVersionFile -Value '0'
+}
+if ($ResetVerBuild) {
+    $VerBuild = 0
+    Set-Content -Path $PathToBuildVersionFile -Value '0'
+}
+# If the build number was not explicitly set, then read it from the file and increment it.
+if ((-1 -eq $VerBuild) -or ($ResetVerBuild)) {
+    $VerBuild = Get-Content -Path $PathToBuildVersionFile
+    $VerBuild = $VerBuild -as [int]
+    $VerBuild += 1;
+}
+
+
+# If the string variant of the version is received as an argument, then parse it into
+# its numeric variants, otherwise build the string variant from the numeric one.
 if (!([string]::IsNullOrWhiteSpace($VerStr))) {
     $VerStrL = $VerStr.Split("-")[0]
     $VerStrR1 = $VerStr.Split("-")[1]
@@ -170,28 +199,12 @@ if (!([string]::IsNullOrWhiteSpace($VerStr))) {
     if (!([string]::IsNullOrWhiteSpace($VerStrExt))) {
         $VerExt += $VerStrExt
     }
+} 
+else {
+	$VerStr = "" + $VerMaj + "." + $VerMin + "." + $VerSP + "." + $VerBuild + "-" + $VerExt
 }
 
-$BuildVersionFile = "IonicBuildVersion.txt"
-$PathToBuildEnv = $Env:AllUsersProfile + "\PensandoBuildEnv"
-$PathToBuildVersionFile = $PathToBuildEnv + "\" + $BuildVersionFile
-if(!(Test-Path -Path $PathToBuildEnv -PathType Container)) {
-    New-Item -Path $Env:AllUsersProfile -ItemType Directory -Name "PensandoBuildEnv"
-}
-if(!(Test-Path -Path $PathToBuildVersionFile -PathType Leaf)) {
-    New-Item -Path $PathToBuildEnv -ItemType File -Name $BuildVersionFile
-    Set-Content -Path $PathToBuildVersionFile -Value '0'
-}
-if ($ResetVerBuild) {
-    $VerBuild = 0
-    Set-Content -Path $PathToBuildVersionFile -Value '0'
-}
-if ((-1 -eq $VerBuild) -or ($ResetVerBuild)) {
-    $VerBuild = Get-Content -Path $PathToBuildVersionFile
-    $VerBuild = $VerBuild -as [int]
-    $VerBuild += 1;
-}
-
+# Set version environment variables to be picked up by the visual studio solution and included projects.
 $Env:IONIC_NDIS_VERSION = $NdisVer
 
 $Env:IONIC_VERSION_MAJOR = $VerMaj
@@ -204,18 +217,23 @@ $Env:IONIC_VERSION_BUILD = $VerBuild
 
 $Env:IONIC_VERSION_EXT = $VerExt
 
+$Env:IONIC_VERSION_STR = $VerStr
+
+# Clean all previous build logs.
 $BuildLogPath = $PathToSln+"\BuildLogs\"
 if (Test-Path -Path $BuildLogPath -PathType Container) {
     $AllLogs = $BuildLogPath+"\*"
     Remove-Item -Path $AllLogs -Force -Recurse
 }
+
+# Execute msbuild "clean".
 $BuildLogFile = $BuildLogPath + "IonicClean.log"
 & $PathToMsBuild $SolutionNameAndPath -t:Clean -p:Configuration=$Configuration -p:Platform=$Platform -fl -flp:logfile=$BuildLogFile
-
+# Exit here if CleanOnly was specified.
 if ($CleanOnly) {
     PauseThenExit -ExitCode 0
 }
-
+# Check for msbuild error.
 if ($LASTEXITCODE -ne 0) {
     Write-Host -BackgroundColor Red "MSBuild operation failed.Exit code: "$LASTEXITCODE
     PauseThenExit -ExitCode $LASTEXITCODE
@@ -223,20 +241,24 @@ if ($LASTEXITCODE -ne 0) {
 
 Write-Host -BackgroundColor DarkCyan "Building Version: " $VerMaj "." $VerMin "." $VerSP "." $VerBuild
 
+# Execute msbuild "rebuild".
 $BuildLogFile = $BuildLogPath + "IonicRebuild.log"
 & $PathToMsBuild $SolutionNameAndPath -t:Rebuild -p:Configuration=$Configuration -p:Platform=$Platform -fl -flp:logfile=$BuildLogFile
-
+# Check for msbuild error.
 if ($LASTEXITCODE -ne 0) {
     Write-Host -BackgroundColor Red "MSBuild operation failed.Exit code: "$LASTEXITCODE
     PauseThenExit -ExitCode $LASTEXITCODE
 }
 
+# This was a successful build -> record build number in the build number file.
 Set-Content -Path $PathToBuildVersionFile -Value $VerBuild
 
 $OperationEndTime = Get-Date
 $OperationRunTime = $OperationEndTime - $OperationStartTime
 "Script execution Time - $($OperationRunTime.Hours):$('{0:00}' -f $OperationRunTime.Minutes):$('{0:00}' -f $OperationRunTime.Seconds).$('{0:000}' -f $OperationRunTime.Milliseconds)" | Write-Host
 
+# ==== Artifacts collection ====
+# Use path to solution as destination for collecting artifacts if an explicit path was not provided.
 $CollectArtifacts=$false
 if (($ArtifactsPath -eq "-NOT-SUPPLIED-") -or (!(Test-Path -Path $ArtifactsPath -PathType Container))) {
     $ArtifactsPath = $PathToSln
@@ -263,6 +285,8 @@ else {
 }
 
 if ($CollectArtifacts) {
+	# Remove any previous artifacts from the destination folder or 
+	# create the destination folder if it didn't exist.
     $NewArtifactsFolder = $ArtifactsPath + "\Artifacts"
     if (Test-Path -Path $NewArtifactsFolder -PathType Container) {
         $AllArtifacts = $NewArtifactsFolder+"\*"
@@ -271,7 +295,7 @@ if ($CollectArtifacts) {
     else {
         New-Item -Path $ArtifactsPath -ItemType Directory -Name "Artifacts"
     }
-
+	# Read artifacts list file and copy any item listed to the destination folder.
     Get-Content $PathToArtifactsListFile | Foreach-Object {
         $Line = $_
         $Line.Trim()
@@ -284,7 +308,8 @@ if ($CollectArtifacts) {
             }
         }
     }
-
+	# Remove any previous artifacts from the destination zip folder or 
+	# create the destination zip folder if it didn't exist.
     $ArtifactsZipFolder = $ArtifactsPath + "\ArtifactsZipped"
     if (Test-Path -Path $ArtifactsZipFolder -PathType Container) {
         $AllArtifacts = $ArtifactsZipFolder+"\*"
@@ -293,6 +318,7 @@ if ($CollectArtifacts) {
     else {
         New-Item -Path $ArtifactsPath -ItemType Directory -Name "ArtifactsZipped"
     }
+	# Zip artifacts
     $ZipFilePath = $ArtifactsZipFolder + "\Artifacts.zip"
     Add-Type -AssemblyName "system.io.compression.filesystem"
     [io.compression.zipfile]::CreateFromDirectory($NewArtifactsFolder, $ZipFilePath)
---
+++ platform/drivers/windows/IonicInstaller/BuildFiles/PensandoToolsCmd.bat
@@ -0,0 +1,5 @@
+@echo off
+cd %~dp0
+cmd /K "echo Tools available in this folder: & dir /b *.exe"
+
+
