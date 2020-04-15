58fc834f9082216b0f592012c1d1786fe4591a08
akoradha <32655671+akoradha@users.noreply.github.com>
2020-04-15 08:47:38 -0700

    Re-enable pcie as a process for Apulu, cpu affinity for pcie (#22255)
    * Revert "Backout pcie mgr apulu commit as its breakign athena pipeline (#22272)"
    
    This reverts commit 2651bd8da04584256c4f86563c4a4ad25405c835.
    
    * cpu affinity for pcie
    
    * add athena to pciemgrd module.mk
    
    * add pciemgrd to sysmgr athena/apollo json
    
    * update MODULE_PIPELINE
    
    * pciemgr as a process only for Apulu

--- platform/src/app/pciemgrd/src/module.mk
+++ platform/src/app/pciemgrd/src/module.mk
@@ -2,31 +2,45 @@
 
 include ${MKDEFS}/pre.mk
 MODULE_TARGET   := pciemgrd.bin
-MODULE_PIPELINE := iris
 MODULE_SOLIBS   := pciemgrd pciemgr_if pciemgr pciemgrutils pciehdevices \
-		   pcieport pcietlp intrutils cfgspace pal misc evutils \
-		   delphisdk utils upgrade_app upgradeproto \
-		   sdkpal logger sysmgr upgradeutils pciemgrproto \
-		   ${NULL}
+                   pcieport pcietlp intrutils cfgspace pal misc evutils
+MODULE_INCS     := ${MODULE_SRC_DIR}/../include
+MODULE_SRCS     := ${MODULE_SRC_DIR}/pciemgrd.cc      \
+                   ${MODULE_SRC_DIR}/logger.cc        \
+                   ${MODULE_SRC_DIR}/cli.cc
+
+ifneq (,$(filter $(PIPELINE),apulu))
+
+MODULE_PIPELINE := apulu
+MODULE_SRCS     += ${MODULE_SRC_DIR}/delphic_stubs.cc \
+                   ${NULL}
+MODULE_LDLIBS   := ev edit ncurses
 ifeq ($(ARCH),aarch64)
+MODULE_SOLIBS   += catalog sdkfru
+endif
+
+else
+
+MODULE_PIPELINE := iris
+MODULE_SOLIBS   += delphisdk utils upgrade_app upgradeproto       \
+                   sdkpal logger sysmgr upgradeutils pciemgrproto \
+                   ${NULL}
+MODULE_INCS     += ${BLD_PROTOGEN_DIR}
+MODULE_SRCS     += ${MODULE_SRC_DIR}/svchandler.cc \
+                   ${MODULE_SRC_DIR}/delphic.cc    \
+                   ${NULL}
+MODULE_LDLIBS   := rt dl pthread ev m edit ncurses \
+                   ${NIC_THIRDPARTY_GOOGLE_LDLIBS} \
+                   ${NULL}
+MODULE_ARLIBS    = delphishm
+ifeq ($(ARCH),aarch64)
+MODULE_LDLIBS   += ${NIC_COMMON_LDLIBS}           \
+                   ${SDK_THIRDPARTY_CAPRI_LDLIBS} \
+                   ${NIC_CAPSIM_LDLIBS}           \
+                   ${NULL}
 MODULE_SOLIBS   += catalog sdkfru sensor
 endif
-MODULE_ARLIBS    = delphishm
-MODULE_INCS     := ${MODULE_SRC_DIR}/../include \
-		   ${BLD_PROTOGEN_DIR}
-MODULE_LDLIBS   := rt dl pthread ev m edit ncurses \
-		   ${NIC_THIRDPARTY_GOOGLE_LDLIBS} \
-		   ${NULL}
-ifeq ($(ARCH),aarch64)
-MODULE_LDLIBS   += ${NIC_COMMON_LDLIBS} \
-		   ${SDK_THIRDPARTY_CAPRI_LDLIBS} \
-		   ${NIC_CAPSIM_LDLIBS} \
-		   ${NULL}
+
 endif
-MODULE_SRCS     := ${MODULE_SRC_DIR}/pciemgrd.cc \
-		   ${MODULE_SRC_DIR}/cli.cc \
-		   ${MODULE_SRC_DIR}/svchandler.cc \
-		   ${MODULE_SRC_DIR}/logger.cc \
-		   ${MODULE_SRC_DIR}/delphic.cc \
-		   ${NULL}
+
 include ${MKDEFS}/post.mk
