e7665861227062a1a4acb56067148b770b1a1e81
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2020-04-27 15:21:08 -0700

    mirror proto changes + fixing the vnic trace msg (#23000)

--- nic/apollo/api/vnic.cc
+++ nic/apollo/api/vnic.cc
@@ -200,13 +200,10 @@ sdk_ret_t
 vnic_entry::program_create(api_obj_ctxt_t *obj_ctxt) {
     pds_vnic_spec_t *spec = &obj_ctxt->api_params->vnic_spec;
 
-    PDS_TRACE_DEBUG("Programming vnic %s, subnet %s, v4 meter %s, "
-                    "v6 meter %s, mac %s\nvnic encap %s, fabric encap %s, "
-                    "rxmirror bitmap %x, tx mirror bitmap %x, "
-                    "binding checks %s, host if %s",
-                    key_.str(), spec->subnet.str(),
-                    spec->v4_meter.str(), spec->v6_meter.str(),
-                    macaddr2str(spec->mac_addr),
+    PDS_TRACE_DEBUG("Programming vnic %s, subnet %s, mac %s\n"
+                    "vnic encap %s, fabric encap %s, rxmirror bitmap %x, "
+                    "tx mirror bitmap %x, binding checks %s, host if %s",
+                    key_.str(), spec->subnet.str(), macaddr2str(spec->mac_addr),
                     pds_encap2str(&spec->vnic_encap),
                     pds_encap2str(&spec->fabric_encap),
                     spec->rx_mirror_session_bmap,
