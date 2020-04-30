24b55aaedecfccaf450ce8b1fd7bd3b171da7d8f
valeriuilie <62261562+valeriuilie@users.noreply.github.com>
2020-04-30 12:00:31 -0700

    Added setdsc.ps1 to Windows installer (double commit #23194) (#23195)
    * Installer executes script for changing management interfaces IPs
    
    Signed-off-by: valeriuilie <val@pensando.io>
    
    * Renamed script.
    
    * typo fix

---
+++ platform/drivers/windows/IonicInstaller/BuildFiles/setdsc.ps1
@@ -0,0 +1,35 @@
+﻿$MgmtIfPnPDevices = Get-PnpDevice -InstanceId "PCI\VEN_1DD8&DEV_1004*"
+$PnPEntities = Get-WmiObject Win32_PnPEntity
+$MgMtIfNo = 0
+foreach ($MgmtIfPnPDevice in $MgmtIfPnPDevices) {
+    $InstanceId = $MgmtIfPnPDevice.InstanceId
+    #Write-Host "Instance ID:" $InstanceId
+    $PCIBusfound = $false
+    foreach ($PnPEntity in $PnPEntities ) {
+        if ($PnPEntity.DeviceID -eq $InstanceId) {
+            $Location = $PnPEntity.GetDeviceProperties('DEVPKEY_Device_LocationInfo').deviceProperties.data
+            $PCIBus = $Location.Split(' ,')[2]
+            if (![string]::IsNullOrWhiteSpace($PCIBus)) {
+                $PCIBusfound = $true
+                #Write-Host "   DeviceID:" $PnPEntity.DeviceID "PCI Bus:"$PCIBus
+            }
+            break 
+        }
+    }
+    if ($PCIBusfound) {
+        $FriendlyName = $MgmtIfPnPDevice.FriendlyName
+        $Adapters = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "Description = '$FriendlyName'"
+        foreach ($Adapter in $Adapters) {
+            if ($Adapter.Description -eq $FriendlyName) {
+                $ipaddr="169.254.$PCIBus.2"
+                $netmask="255.255.255.0"
+                $Adapter.EnableStatic($ipaddr, $netmask) > $null
+                Write-Host "Name:" $Adapter.Description "IP: " $ipaddr "Mask: " $netmask
+                $DSCUrlEnv = "DSC_URL_"+$MgMtIfNo
+                Write-Host $DSCUrlEnv "=http://169.254.$PCIBus.1"
+                [System.Environment]::SetEnvironmentVariable($DSCUrlEnv, "http://169.254.$PCIBus.1",[System.EnvironmentVariableTarget]::Machine)
+                $MgMtIfNo += 1
+            }
+        }
+    }
+}
