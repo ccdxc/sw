45dc34226d9e003f7cfae9afd4ed6a42ee2a9446
Vinod Chamunny <46167917+vchamunny@users.noreply.github.com>
2020-05-04 11:09:54 -0700

    fail rollout if rollout image download fails (#23355)
    * Fail the rollout when image download fails
    
    * revert change to master
    
    * fail rollout if rollout image download fails

--- venice/ctrler/rollout/statemgr/fsm.go
+++ venice/ctrler/rollout/statemgr/fsm.go
@@ -292,6 +292,7 @@ func fsmAcCreated(ros *RolloutState) {
 		if allVeniceHalthy == false {
 			log.Errorf("Precheck failed: %v", msg)
 			ros.setVenicePhase(nodeName, "", msg, rollout.RolloutPhase_FAIL)
+			ros.veniceRolloutFailed = true
 			ros.eventChan <- fsmEvFail
 			return
 		}
@@ -300,6 +301,7 @@ func fsmAcCreated(ros *RolloutState) {
 		numPendingPrecheck, n, err := ros.preCheckNextVeniceNode()
 		if err != nil {
 			log.Errorf("Precheck failed: %v", err)
+			ros.veniceRolloutFailed = true
 			ros.setVenicePhase(n, "", err.Error(), rollout.RolloutPhase_FAIL)
 			ros.eventChan <- fsmEvFail
 			return
