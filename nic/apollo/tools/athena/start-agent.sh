8001b8fa1ccd7da4a3fba24a6d0f001dc8ce25f3
Sameer Kittur <29764151+skittur@users.noreply.github.com>
2020-05-12 20:26:10 -0700

    - Prevent mounting of hugetlbf for every instance of the athena_app (#23953)
    - Skip if it is already mounted
            - Enables the next instance to reuse the huge pages and prevent
    a buffer leak

--- nic/apollo/tools/athena/start-agent.sh
+++ nic/apollo/tools/athena/start-agent.sh
@@ -12,8 +12,10 @@ ulimit -c unlimited
 export PERSISTENT_LOG_DIR=/obfl/
 
 #Huge-pages for DPDK
-echo 64 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-mkdir -p /dev/hugepages
-mount -t hugetlbfs nodev /dev/hugepages
+if  ! mount | grep hugetlbfs ; then
+    echo 64 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+    mkdir -p /dev/hugepages
+    mount -t hugetlbfs nodev /dev/hugepages
+fi
 
 exec taskset 1 $PDSPKG_TOPDIR/bin/athena_app -c hal_hw.json -m cpp $* >$NON_PERSISTENT_LOGDIR/athena_app_console.log &
--- nic/apollo/tools/athena/start-sec-agent.sh
+++ nic/apollo/tools/athena/start-sec-agent.sh
@@ -12,8 +12,10 @@ ulimit -c unlimited
 export PERSISTENT_LOG_DIR=/obfl_sec/
 
 #Huge-pages for DPDK
-echo 64 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-mkdir -p /dev/hugepages
-mount -t hugetlbfs nodev /dev/hugepages
+if  ! mount | grep hugetlbfs ; then
+    echo 64 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+    mkdir -p /dev/hugepages
+    mount -t hugetlbfs nodev /dev/hugepages
+fi
 
 exec taskset 1 $PDSPKG_TOPDIR/bin/athena_app -c hal_hw.json -m soft-init $* >$NON_PERSISTENT_LOGDIR/athena_sec_app_console.log &
