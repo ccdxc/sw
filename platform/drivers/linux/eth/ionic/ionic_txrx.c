06550bbdc46dcf05c1110307818491525e6695b0
Neel Patel <nspatel1986@yahoo.com>
2020-05-18 21:38:31 -0700

    ionic: fix for duplicate napi_schedule in busy_poll_stop (#24206)
    It seems busy_poll_stop assumes napi is not scheduled when it is
    called. With the recent tx budget changes it's possible that napi
    will get scheduled when tx_work_done > budget.
    
    Co-authored-by: Neel Patel <neel@pensando.io>

--- platform/drivers/linux/eth/ionic/ionic_txrx.c
+++ platform/drivers/linux/eth/ionic/ionic_txrx.c
@@ -609,11 +609,9 @@ int ionic_txrx_napi(struct napi_struct *napi, int budget)
 	struct ionic_lif *lif;
 	struct ionic_qcq *txqcq;
 	struct ionic_cq *txcq;
-	u32 rx_work_done = 0;
 	u32 tx_work_done = 0;
-	u32 work_done = 0;
+	u32 rx_work_done = 0;
 	u32 flags = 0;
-	bool unmask;
 
 	lif = rxcq->bound_q->lif;
 	idev = &lif->ionic->idev;
@@ -628,16 +626,12 @@ int ionic_txrx_napi(struct napi_struct *napi, int budget)
 	if (rx_work_done)
 		ionic_rx_fill_cb(rxcq->bound_q);
 
-	unmask = (rx_work_done < budget) && (tx_work_done < tx_budget);
-
-	if (unmask && napi_complete_done(napi, rx_work_done)) {
+	if (rx_work_done < budget && napi_complete_done(napi, rx_work_done)) {
 		flags |= IONIC_INTR_CRED_UNMASK;
 		DEBUG_STATS_INTR_REARM(rxcq->bound_intr);
-		work_done = rx_work_done;
-	} else
-		work_done = budget;
+	}
 
-	if (work_done || flags) {
+	if (rx_work_done || flags) {
 		flags |= IONIC_INTR_CRED_RESET_COALESCE;
 		if (!lif->ionic->neth_eqs) {
 			ionic_intr_credits(idev->intr_ctrl,
@@ -668,7 +662,7 @@ int ionic_txrx_napi(struct napi_struct *napi, int budget)
 	DEBUG_STATS_NAPI_POLL(rxqcq, rx_work_done);
 	DEBUG_STATS_NAPI_POLL(txqcq, tx_work_done);
 
-	return work_done;
+	return rx_work_done;
 }
 
 static dma_addr_t ionic_tx_map_single(struct ionic_queue *q,
