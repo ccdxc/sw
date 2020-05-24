0282dff40456fb9e1c34acbbdb1f85608928512e
Sarat Kamisetty <sarat-k@users.noreply.github.com>
2020-05-24 16:06:43 -0700

    Revert "Revert "Miscellaneous VPP fixes (#24357)" (#24482)" (#24493)
    This reverts commit 8aaba4308d8be043c17588ed9501afcbdc9f69de.

--- nic/vpp/flow/ageing.c
+++ nic/vpp/flow/ageing.c
@@ -571,19 +571,19 @@ always_inline uint8_t
 pds_flow_get_ctr_idx (uint8_t proto, bool isv4)
 {
     switch(proto) {
-    case IP_PROTOCOL_TCP:
+    case PDS_FLOW_PROTO_TCP:
         if (isv4) {
             return FLOW_TYPE_COUNTER_TCPV4;
         } else {
             return FLOW_TYPE_COUNTER_TCPV6;
         }
-    case IP_PROTOCOL_UDP:
+    case PDS_FLOW_PROTO_UDP:
         if (isv4) {
             return FLOW_TYPE_COUNTER_UDPV4;
         } else {
             return FLOW_TYPE_COUNTER_UDPV6;
         }
-    case IP_PROTOCOL_ICMP:
+    case PDS_FLOW_PROTO_ICMP:
         if (isv4) {
             return FLOW_TYPE_COUNTER_ICMPV4;
         } else {
